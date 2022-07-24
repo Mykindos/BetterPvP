62806badab3127fc4ff35457f9f313abf57ba84e
Mykindos <5389944+Mykindos@users.noreply.github.com>
2022-07-25 06:03:52 +1000

    A new beginning

---
+++ clans/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    java
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+}
+
+repositories {
+    mavenCentral()
+    maven { url = uri("https://repo.papermc.io/repository/maven-public/") }
+}
+
+dependencies {
+    implementation(project(":core"))
+
+    compileOnly("io.papermc.paper:paper-api:1.19-R0.1-SNAPSHOT")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
---
+++ core/src/main/java/me/mykindos/betterpvp/core/Core.java
@@ -0,0 +1,12 @@
+package me.mykindos.betterpvp.core;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Core extends JavaPlugin {
+
+    @Override
+    public void onEnable(){
+
+    }
+
+}
