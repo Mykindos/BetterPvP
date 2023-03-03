cd1b189963f378c484e670293175ec69a07f79fa
Mykindos <5389944+Mykindos@users.noreply.github.com>
2023-03-04 03:25:21 +1100

    add clan properties

---
+++ clans/src/main/java/me/mykindos/betterpvp/clans/clans/listeners/ClanPropertyListener.java
@@ -0,0 +1,41 @@
+package me.mykindos.betterpvp.clans.clans.listeners;
+
+import me.mykindos.betterpvp.clans.clans.ClanManager;
+import me.mykindos.betterpvp.clans.clans.events.ClanPropertyUpdateEvent;
+import me.mykindos.betterpvp.core.framework.events.scoreboard.ScoreboardUpdateEvent;
+import me.mykindos.betterpvp.core.framework.updater.UpdateEvent;
+import me.mykindos.betterpvp.core.gamer.GamerManager;
+import me.mykindos.betterpvp.core.gamer.properties.GamerPropertyUpdateEvent;
+import me.mykindos.betterpvp.core.listener.BPvPListener;
+import me.mykindos.betterpvp.core.utilities.UtilServer;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+
+import javax.inject.Inject;
+import java.util.UUID;
+
+@BPvPListener
+public class ClanPropertyListener extends ClanListener{
+
+    @Inject
+    public ClanPropertyListener(ClanManager clanManager, GamerManager gamerManager) {
+        super(clanManager, gamerManager);
+    }
+
+    @EventHandler
+    public void onSettingsUpdated(ClanPropertyUpdateEvent event) {
+        clanManager.getRepository().saveProperty(event.getClan(), event.getProperty(), event.getValue());
+
+        if(event.isUpdateScoreboard()) {
+            event.getClan().getMembersAsPlayers().forEach(player -> {
+                UtilServer.callEvent(new ScoreboardUpdateEvent(player));
+            });
+        }
+    }
+
+    @UpdateEvent(delay = 120_000)
+    public void processStatUpdates(){
+        clanManager.getRepository().processPropertyUpdates(true);
+    }
+}
