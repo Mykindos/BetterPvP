a11e32e6fb966ba381b49059b61426891677022b
Braulio <bchamerry@gmail.com>
2023-09-19 14:15:57 -0700

    Make champions sql migrations use table prefix for core tables as per the rest of the project

--- champions/src/main/resources/champions-migrations/V5__Add_imbuements.sql
+++ champions/src/main/resources/champions-migrations/V5__Add_imbuements.sql
@@ -26,22 +26,22 @@ INSERT IGNORE INTO ${tablePrefix}imbuement_data (ImbuementName, NamespaceKey, Af
     ("Fortune", "champions:imbuement-fortune", "<green>10% <gray>chance to get double resources", "DUNE_ARMOR_TRIM_SMITHING_TEMPLATE", 10, 0, 0, 1);
 
 -- Add runes to items & item lore
-INSERT IGNORE INTO items (Material, Module, Name, Glow) VALUES ('SHAPER_ARMOR_TRIM_SMITHING_TEMPLATE', 'Champions', '<yellow>Rune of Skill Haste', 0);
-SELECT id INTO @itemId FROM items WHERE Material = 'SHAPER_ARMOR_TRIM_SMITHING_TEMPLATE';
-INSERT IGNORE INTO itemlore VALUES (@itemId, 0, '<gray>Can be applied to any equippable item'), (@itemId, 1, ''), (@itemId, 2, '<blue>Affix'), (@itemId, 3, '<green>15% <gray>cooldown reduction');
+INSERT IGNORE INTO ${coreTablePrefix}items (Material, Module, Name, Glow) VALUES ('SHAPER_ARMOR_TRIM_SMITHING_TEMPLATE', 'Champions', '<yellow>Rune of Skill Haste', 0);
+SELECT id INTO @itemId FROM ${coreTablePrefix}items WHERE Material = 'SHAPER_ARMOR_TRIM_SMITHING_TEMPLATE';
+INSERT IGNORE INTO ${coreTablePrefix}itemlore VALUES (@itemId, 0, '<gray>Can be applied to any equippable item'), (@itemId, 1, ''), (@itemId, 2, '<blue>Affix'), (@itemId, 3, '<green>15% <gray>cooldown reduction');
 
-INSERT IGNORE INTO items (Material, Module, Name, Glow) VALUES ('RAISER_ARMOR_TRIM_SMITHING_TEMPLATE', 'Champions', '<yellow>Rune of Vigor', 0);
-SELECT id INTO @itemId FROM items WHERE Material = 'RAISER_ARMOR_TRIM_SMITHING_TEMPLATE';
-INSERT IGNORE INTO itemlore VALUES (@itemId, 0, '<gray>Can only be applied to weapons'), (@itemId, 1, ''), (@itemId, 2, '<blue>Affix'), (@itemId, 3, '<green>20% <gray>increased damage');
+INSERT IGNORE INTO ${coreTablePrefix}items (Material, Module, Name, Glow) VALUES ('RAISER_ARMOR_TRIM_SMITHING_TEMPLATE', 'Champions', '<yellow>Rune of Vigor', 0);
+SELECT id INTO @itemId FROM ${coreTablePrefix}items WHERE Material = 'RAISER_ARMOR_TRIM_SMITHING_TEMPLATE';
+INSERT IGNORE INTO ${coreTablePrefix}itemlore VALUES (@itemId, 0, '<gray>Can only be applied to weapons'), (@itemId, 1, ''), (@itemId, 2, '<blue>Affix'), (@itemId, 3, '<green>20% <gray>increased damage');
 
-INSERT IGNORE INTO items (Material, Module, Name, Glow) VALUES ( 'WARD_ARMOR_TRIM_SMITHING_TEMPLATE', 'Champions', '<yellow>Rune of Fortification', 0);
-SELECT id INTO @itemId FROM items WHERE Material = 'WARD_ARMOR_TRIM_SMITHING_TEMPLATE';
-INSERT IGNORE INTO itemlore VALUES (@itemId, 0, '<gray>Can only be applied to armour'), (@itemId, 1, ''), (@itemId, 2, '<blue>Affix'), (@itemId, 3, '<green>10% <gray>damage reduction');
+INSERT IGNORE INTO ${coreTablePrefix}items (Material, Module, Name, Glow) VALUES ( 'WARD_ARMOR_TRIM_SMITHING_TEMPLATE', 'Champions', '<yellow>Rune of Fortification', 0);
+SELECT id INTO @itemId FROM ${coreTablePrefix}items WHERE Material = 'WARD_ARMOR_TRIM_SMITHING_TEMPLATE';
+INSERT IGNORE INTO ${coreTablePrefix}itemlore VALUES (@itemId, 0, '<gray>Can only be applied to armour'), (@itemId, 1, ''), (@itemId, 2, '<blue>Affix'), (@itemId, 3, '<green>10% <gray>damage reduction');
 
-INSERT IGNORE INTO items (Material, Module, Name, Glow) VALUES ('WAYFINDER_ARMOR_TRIM_SMITHING_TEMPLATE', 'Champions', '<yellow>Rune of Insight', 0);
-SELECT id INTO @itemId FROM items WHERE Material = 'WAYFINDER_ARMOR_TRIM_SMITHING_TEMPLATE';
-INSERT IGNORE INTO itemlore VALUES (@itemId, 0, '<gray>Can be applied to any equippable item'), (@itemId, 1, ''), (@itemId, 2, '<blue>Affix'), (@itemId, 3, '<green>10% <gray>energy cost reduction');
+INSERT IGNORE INTO ${coreTablePrefix}items (Material, Module, Name, Glow) VALUES ('WAYFINDER_ARMOR_TRIM_SMITHING_TEMPLATE', 'Champions', '<yellow>Rune of Insight', 0);
+SELECT id INTO @itemId FROM ${coreTablePrefix}items WHERE Material = 'WAYFINDER_ARMOR_TRIM_SMITHING_TEMPLATE';
+INSERT IGNORE INTO ${coreTablePrefix}itemlore VALUES (@itemId, 0, '<gray>Can be applied to any equippable item'), (@itemId, 1, ''), (@itemId, 2, '<blue>Affix'), (@itemId, 3, '<green>10% <gray>energy cost reduction');
 
-INSERT IGNORE INTO items (Material, Module, Name, Glow) VALUES ('DUNE_ARMOR_TRIM_SMITHING_TEMPLATE', 'Champions', '<yellow>Rune of Fortune', 0);
-SELECT id INTO @itemId FROM items WHERE Material = 'DUNE_ARMOR_TRIM_SMITHING_TEMPLATE';
-INSERT IGNORE INTO itemlore VALUES (@itemId, 0, '<gray>Can only be applied to tools'),(@itemId, 1, ''), (@itemId, 2, '<blue>Affix'), (@itemId, 3, '<green>10% <gray>chance to get double resources');
+INSERT IGNORE INTO ${coreTablePrefix}items (Material, Module, Name, Glow) VALUES ('DUNE_ARMOR_TRIM_SMITHING_TEMPLATE', 'Champions', '<yellow>Rune of Fortune', 0);
+SELECT id INTO @itemId FROM ${coreTablePrefix}items WHERE Material = 'DUNE_ARMOR_TRIM_SMITHING_TEMPLATE';
+INSERT IGNORE INTO ${coreTablePrefix}itemlore VALUES (@itemId, 0, '<gray>Can only be applied to tools'),(@itemId, 1, ''), (@itemId, 2, '<blue>Affix'), (@itemId, 3, '<green>10% <gray>chance to get double resources');
