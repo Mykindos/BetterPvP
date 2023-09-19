21b5130839c2c117dd7cb74f24405fd8f49b93b8
ezrn <145294235+ezrn@users.noreply.github.com>
2023-09-18 23:50:14 -0700

    new archery skill

---
+++ champions/src/main/java/me/mykindos/betterpvp/champions/champions/skills/skills/assassin/passives/Archery.java
@@ -0,0 +1,72 @@
+package me.mykindos.betterpvp.champions.champions.skills.skills.assassin.passives;
+
+
+import me.mykindos.betterpvp.champions.Champions;
+import me.mykindos.betterpvp.champions.champions.ChampionsManager;
+import me.mykindos.betterpvp.champions.champions.skills.Skill;
+import me.mykindos.betterpvp.champions.champions.skills.data.SkillWeapons;
+import me.mykindos.betterpvp.champions.champions.skills.types.PassiveSkill;
+import me.mykindos.betterpvp.core.combat.events.CustomDamageEvent;
+import me.mykindos.betterpvp.core.components.champions.Role;
+import me.mykindos.betterpvp.core.components.champions.SkillType;
+import me.mykindos.betterpvp.core.listener.BPvPListener;
+import me.mykindos.betterpvp.core.utilities.UtilMath;
+import me.mykindos.betterpvp.core.utilities.UtilPlayer;
+import org.bukkit.ChatColor;
+import org.bukkit.Effect;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import java.util.Optional;
+
+
+@Singleton
+@BPvPListener
+public class Archery extends Skill implements PassiveSkill, Listener {
+
+
+    @Inject
+    public Archery(Champions champions, ChampionsManager championsManager) {
+        super(champions, championsManager);
+    }
+
+    @Override
+    public String getName() {
+        return "Archery";
+    }
+
+    @Override
+    public Role getClassType() {
+        return Role.ASSASSIN;
+    }
+
+    @Override
+    public SkillType getType() {
+        return SkillType.PASSIVE_A;
+    }
+
+    @Override
+    public String[] getDescription(int level) {
+
+        return new String[]{
+                "Your arrows will deal an extra",
+                ChatColor.GREEN + (1 + level) + " damage per shot"};
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onSlow(CustomDamageEvent event) {
+        if(!(event.getProjectile() instanceof Arrow)) return;
+        if(!(event.getDamager() instanceof Player damager)) return;
+
+        int level = getLevel(damager);
+        if(level > 0) {
+            event.setDamage(event.getDamage() + (1 + level));
+        }
+
+    }
+
