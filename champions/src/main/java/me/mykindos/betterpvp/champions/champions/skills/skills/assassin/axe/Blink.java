2d3be31b963edcabe8d015e3f33c5689e8232301
ezrn <145294235+ezrn@users.noreply.github.com>
2023-09-18 17:25:41 -0700

    scaled blink 3 extra blocks per level

--- champions/src/main/java/me/mykindos/betterpvp/champions/champions/skills/skills/assassin/axe/Blink.java
+++ champions/src/main/java/me/mykindos/betterpvp/champions/champions/skills/skills/assassin/axe/Blink.java
@@ -56,7 +56,7 @@ public class Blink extends Skill implements InteractSkill, CooldownSkill, Listen
         return new String[]{
                 "Right click with a axe to activate.",
                 "",
-                "Instantly teleport forwards 15 Blocks.",
+                "Instantly teleport forwards " + ChatColor.GREEN + 15 + (3 * (level-1)) + ChatColor.GRAY + " Blocks.",
                 "Cannot be used while Slowed.",
                 "",
                 "Using again within 5 seconds De-Blinks,",
@@ -204,10 +204,10 @@ public class Blink extends Skill implements InteractSkill, CooldownSkill, Listen
         UtilServer.runTaskLater(champions, () -> {
             Vector direction = player.getLocation().getDirection();
             Location targetLocation = player.getLocation().add(0, 1, 0);
-
-            double maxDistance = maxTravelDistance;
-
-            for (double currentDistance = 0; currentDistance < maxDistance; currentDistance += 1) {
+            
+            double scaledMaxDistance = maxTravelDistance + (3 * (level - 1));
+            
+            for (double currentDistance = 0; currentDistance < scaledMaxDistance; currentDistance += 1) {
                 Location testLocation = targetLocation.clone().add(direction.clone());
                 Block testBlock = testLocation.getBlock();
                 if (!UtilBlock.isWall(testBlock)) {
