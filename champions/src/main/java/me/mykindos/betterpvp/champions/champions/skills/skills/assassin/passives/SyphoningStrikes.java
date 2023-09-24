825774fbe6ccc44f68a19a22033bf30e18a86c98
ezrn <zanderaguirre@icloud.com>
2023-09-24 01:33:43 -0700

    added syphoning strikes

---
+++ champions/src/main/java/me/mykindos/betterpvp/champions/champions/skills/skills/assassin/passives/SyphoningStrikes.java
@@ -0,0 +1,112 @@
+package me.mykindos.betterpvp.champions.champions.skills.skills.assassin.passives;
+
+
+import java.util.HashSet;
+import java.util.WeakHashMap;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import me.mykindos.betterpvp.champions.Champions;
+import me.mykindos.betterpvp.champions.champions.ChampionsManager;
+import me.mykindos.betterpvp.champions.champions.skills.Skill;
+import me.mykindos.betterpvp.champions.champions.skills.types.PassiveSkill;
+import me.mykindos.betterpvp.core.combat.events.CustomDamageEvent;
+import me.mykindos.betterpvp.core.components.champions.Role;
+import me.mykindos.betterpvp.core.components.champions.SkillType;
+import me.mykindos.betterpvp.core.framework.updater.UpdateEvent;
+import me.mykindos.betterpvp.core.listener.BPvPListener;
+import me.mykindos.betterpvp.core.utilities.UtilTime;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
+
+@Singleton
+@BPvPListener
+public class SyphoningStrikes extends Skill implements PassiveSkill, Listener {
+
+    private final WeakHashMap<Player, Double> repeat = new WeakHashMap<>();
+    private final WeakHashMap<Player, Long> last = new WeakHashMap<>();
+
+    @Inject
+    public SyphoningStrikes(Champions champions, ChampionsManager championsManager) {
+        super(champions, championsManager);
+    }
+
+
+    @Override
+    public String getName() {
+        return "Syphoning Strikes";
+    }
+
+    private double healthIncrement;
+    private double duration;
+
+    @Override
+    public String[] getDescription(int level) {
+
+        return new String[]{
+                "Each time you attack, you siphon health",
+                "from the enemy, starting at <stat>" + (healthIncrement * 2) + "%</stat>",
+                "and increasing by <val>" + healthIncrement + "%</val> for each hit",
+                "",
+                "You can gain up to <val>" + (level * healthIncrement) + "</val> health per hit",
+                "",
+                "Not attacking for <stat>" + duration + "</stat> seconds clears",
+                "your bonus damage."};
+    }
+
+    @Override
+    public Role getClassType() {
+        return Role.ASSASSIN;
+    }
+
+    @Override
+    public SkillType getType() {
+        return SkillType.PASSIVE_A;
+    }
+
+
+    @EventHandler(priority = EventPriority.HIGH)
+    public void onDamage(CustomDamageEvent event) {
+        if (event.getCause() != DamageCause.ENTITY_ATTACK) return;
+        if (!(event.getDamager() instanceof Player damager)) return;
+        if (!championsManager.getRoles().hasRole(damager, Role.ASSASSIN)) return;
+
+        int level = getLevel(damager);
+        if (level > 0) {
+            double currentIncrement = repeat.getOrDefault(damager, 2 * healthIncrement);
+
+            currentIncrement = Math.min(currentIncrement + healthIncrement, level * healthIncrement);
+
+            double healthToAdd = damager.getMaxHealth() * (currentIncrement / 100);
+            damager.setHealth(Math.min(damager.getHealth() + healthToAdd, damager.getMaxHealth()));
+
+            repeat.put(damager, currentIncrement);
+            last.put(damager, System.currentTimeMillis());
+        }
+    }
+
+
+    @UpdateEvent(delay = 500)
+    public void onUpdate() {
+
+        HashSet<Player> remove = new HashSet<>();
+
+        for (Player player : repeat.keySet()) {
+            if (UtilTime.elapsed(last.get(player), (long) duration * 1000)) {
+                remove.add(player);
+            }
+        }
+
+        for (Player player : remove) {
+            repeat.remove(player);
+            last.remove(player);
+        }
+    }
+    @Override
+    public void loadSkillConfig(){
+        healthIncrement = getConfig("healthIncrement", 0.1, Double.class);
+        duration = getConfig("duration", 2.0, Double.class);
+    }
+}
