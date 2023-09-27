8f1876a9144ffaad04d52a21e1b832d0522f3332
StudentAlleg <studentalleg-pilot@yahoo.com>
2023-09-26 23:27:30 -0700

    Added usage for commands

--- clans/src/main/java/me/mykindos/betterpvp/clans/clans/commands/subcommands/NeutralSubCommand.java
+++ clans/src/main/java/me/mykindos/betterpvp/clans/clans/commands/subcommands/NeutralSubCommand.java
@@ -37,6 +37,11 @@ public class NeutralSubCommand extends ClanSubCommand {
     }
 
     @Override
+    public String getUsage() {
+        return super.getUsage() + " <clan>";
+    }
+
+    @Override
     public void execute(Player player, Client client, String... args) {
         if(args.length == 0) {
             UtilMessage.message(player, "Clans", "You must specify a clan to request neutral status with.");
