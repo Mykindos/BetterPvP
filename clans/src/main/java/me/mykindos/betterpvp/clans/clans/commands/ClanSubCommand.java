398401694968825f7650d8d54ed31bdf3bba1d41
StudentAlleg <studentalleg-pilot@yahoo.com>
2023-09-26 20:19:31 -0700

    reset changes to ClanSubCommand.java (were all unused)

--- clans/src/main/java/me/mykindos/betterpvp/clans/clans/commands/ClanSubCommand.java
+++ clans/src/main/java/me/mykindos/betterpvp/clans/clans/commands/ClanSubCommand.java
@@ -5,7 +5,6 @@ import me.mykindos.betterpvp.clans.clans.ClanManager;
 import me.mykindos.betterpvp.core.client.Client;
 import me.mykindos.betterpvp.core.client.Rank;
 import me.mykindos.betterpvp.core.command.Command;
-import me.mykindos.betterpvp.core.command.CommandManager;
 import me.mykindos.betterpvp.core.components.clans.data.ClanMember;
 import me.mykindos.betterpvp.core.gamer.Gamer;
 import me.mykindos.betterpvp.core.gamer.GamerManager;
@@ -22,6 +21,7 @@ public abstract class ClanSubCommand extends Command {
 
     protected final ClanManager clanManager;
     protected final GamerManager gamerManager;
+
     public ClanSubCommand(ClanManager clanManager, GamerManager gamerManager) {
         this.clanManager = clanManager;
         this.gamerManager = gamerManager;
