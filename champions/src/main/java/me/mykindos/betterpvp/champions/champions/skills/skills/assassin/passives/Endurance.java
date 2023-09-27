32ae42eb9f91eeeb7733235d8d9ecf129e1290df
ezrn <zanderaguirre@icloud.com>
2023-09-27 11:35:55 -0700

    completed endurance

--- champions/src/main/java/me/mykindos/betterpvp/champions/champions/skills/skills/assassin/passives/Endurance.java
+++ champions/src/main/java/me/mykindos/betterpvp/champions/champions/skills/skills/assassin/passives/Endurance.java
@@ -11,16 +11,21 @@ import me.mykindos.betterpvp.champions.champions.skills.types.ToggleSkill;
 import me.mykindos.betterpvp.core.components.champions.Role;
 import me.mykindos.betterpvp.core.components.champions.SkillType;
 import me.mykindos.betterpvp.core.listener.BPvPListener;
+import org.bukkit.Bukkit;
+import org.bukkit.Effect;
+import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
-import org.bukkit.event.Listener;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeInstance;
 
 @Singleton
 @BPvPListener
-public class Endurance extends Skill implements ToggleSkill, CooldownSkill, Listener {
+public class Endurance extends Skill implements ToggleSkill, CooldownSkill{
 
-    private int effectDuration=5;
+    private double effectDuration;
     @Inject
     public Endurance(Champions champions, ChampionsManager championsManager) {
         super(champions, championsManager);
@@ -38,7 +43,7 @@ public class Endurance extends Skill implements ToggleSkill, CooldownSkill, List
         return new String[]{
                 "Drop your Sword / Axe to activate",
                 "",
-                "Instantly gain <stat>3</stat> extra hearts and <effect>Speed III</effect> for <val>" + (5 + (level - 1)),
+                "Instantly gain <stat>3</stat> extra hearts and <effect>Speed III</effect> for <val>" + (5 + (level - 1)) + "</val> seconds",
                 "",
                 "Cooldown: <val>" + getCooldown(level)
         };
@@ -46,9 +51,22 @@ public class Endurance extends Skill implements ToggleSkill, CooldownSkill, List
 
     @Override
     public void toggle(Player player, int level) {
-        player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED,(5+(level-1)) , 2,true,true));
-        player.addPotionEffect(new PotionEffect(PotionEffectType.HEALTH_BOOST,(5+(level-1)) , 1,true,true));
 
+        player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, ((5 + (level - 1)) * 20), 2, true, true));
+        player.getWorld().playSound(player.getLocation(), Sound.ENTITY_WARDEN_DEATH, 1.0F, 1.0F);
+        player.getWorld().playEffect(player.getLocation(), Effect.STEP_SOUND, Material.REDSTONE_BLOCK);
+
+        AttributeInstance maxHealth = player.getAttribute(Attribute.GENERIC_MAX_HEALTH);
+        if (maxHealth != null) {
+            maxHealth.setBaseValue(26.0);
+            if (player.getHealth() < 26.0) player.setHealth(player.getHealth()+6);
+        }
+
+        Bukkit.getScheduler().runTaskLater(champions, () -> {
+            if (maxHealth != null) {
+                maxHealth.setBaseValue(20.0);
+            }
+        }, (5 + (level - 1)) * 20L);
     }
 
     @Override
@@ -66,8 +84,12 @@ public class Endurance extends Skill implements ToggleSkill, CooldownSkill, List
     @Override
     public double getCooldown(int level) {
 
-        return cooldown - ((level - 1) * 2);
-        //25s
+        return cooldown - ((level - 1) * 2.5);
+    }
+
+    @Override
+    public void loadSkillConfig(){
+        effectDuration = getConfig("effectDuration", 5.0, Double.class);
     }
 }
 
