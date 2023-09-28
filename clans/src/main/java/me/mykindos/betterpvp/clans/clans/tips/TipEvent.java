0eec40dace76fb0c125eaf5ab5d50d364edf5a84
StudentAlleg <studentalleg-pilot@yahoo.com>
2023-09-28 14:13:39 -0700

    Removed unused imports

--- clans/src/main/java/me/mykindos/betterpvp/clans/clans/tips/TipEvent.java
+++ clans/src/main/java/me/mykindos/betterpvp/clans/clans/tips/TipEvent.java
@@ -2,10 +2,7 @@ package me.mykindos.betterpvp.clans.clans.tips;
 
 import lombok.Data;
 import lombok.Getter;
-import lombok.Setter;
 import me.mykindos.betterpvp.core.framework.events.CustomCancellableEvent;
-import me.mykindos.betterpvp.core.framework.events.CustomEvent;
-import me.mykindos.betterpvp.core.gamer.Gamer;
 import org.bukkit.entity.Player;
 
 @Getter
