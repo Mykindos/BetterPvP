c8161584c8e2328aae450293a3ff688b53901e5e
Tom Hoogstra <tomhoogstra@live.com.au>
2023-10-13 21:36:16 +1100

    make arrow damage get reduced depending on bow charge

--- champions/src/main/java/me/mykindos/betterpvp/champions/listeners/ArrowListener.java
+++ champions/src/main/java/me/mykindos/betterpvp/champions/listeners/ArrowListener.java
@@ -1,5 +1,6 @@
 package me.mykindos.betterpvp.champions.listeners;
 
+import me.mykindos.betterpvp.core.combat.events.CustomDamageEvent;
 import me.mykindos.betterpvp.core.config.Config;
 import me.mykindos.betterpvp.core.framework.updater.UpdateEvent;
 import me.mykindos.betterpvp.core.listener.BPvPListener;
@@ -7,13 +8,16 @@ import org.bukkit.Location;
 import org.bukkit.Particle;
 import org.bukkit.entity.Arrow;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityShootBowEvent;
 import org.bukkit.util.Vector;
 
 import javax.inject.Inject;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Map;
 import java.util.Set;
 
 @BPvPListener
@@ -23,22 +27,24 @@ public class ArrowListener implements Listener {
     @Config(path = "combat.crit-arrows", defaultValue = "true")
     private boolean critArrowsEnabled;
 
-    private final Set<Arrow> arrows = new HashSet<>();
+    private final HashMap<Arrow, Float> arrows = new HashMap<>();
 
 
     @UpdateEvent
     public void displayCritTrail() {
-        Iterator<Arrow> it = arrows.iterator();
+        Iterator<Map.Entry<Arrow, Float>> it = arrows.entrySet().iterator();
         while (it.hasNext()) {
-            Arrow next = it.next();
-            if (next == null) {
+            Map.Entry<Arrow, Float> next = it.next();
+            Arrow arrow = next.getKey();
+            if (arrow == null) {
                 it.remove();
-            } else if (next.isDead()) {
+            } else if (arrow.isDead()) {
                 it.remove();
             } else {
-                Location loc = next.getLocation().add(new Vector(0, 0.25, 0));
-                Particle.CRIT.builder().location(loc).count(3).extra(0).receivers(60, true).spawn();
-
+                if (critArrowsEnabled && next.getValue() == 1.0) {
+                    Location loc = arrow.getLocation().add(new Vector(0, 0.25, 0));
+                    Particle.CRIT.builder().location(loc).count(3).extra(0).receivers(60, true).spawn();
+                }
             }
         }
     }
@@ -53,13 +59,22 @@ public class ArrowListener implements Listener {
         if (event.getProjectile() instanceof Arrow arrow) {
             arrow.setCritical(false);
 
-            if (critArrowsEnabled) {
-                // Only show particle for fully charged bows
-                if (event.getForce() == 1.0) {
-                    arrows.add(arrow);
-                }
+            arrows.put(arrow, event.getForce());
+
+
+        }
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onArrowDamage(CustomDamageEvent event) {
+        if (event.getProjectile() instanceof Arrow arrow) {
+            if (arrows.containsKey(arrow)) {
+                float dmgMultiplier = arrows.get(arrow);
+                event.setDamage(event.getDamage() * dmgMultiplier);
+                arrows.remove(arrow);
             }
         }
+
     }
 
 }
