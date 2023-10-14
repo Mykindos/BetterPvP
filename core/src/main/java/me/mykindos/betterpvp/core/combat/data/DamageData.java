f319473b0e366c96be6e3d40bd4f5323fcd02e09
Mykindos <5389944+Mykindos@users.noreply.github.com>
2023-10-14 15:58:40 +1100

    damage stacking

--- core/src/main/java/me/mykindos/betterpvp/core/combat/data/DamageData.java
+++ core/src/main/java/me/mykindos/betterpvp/core/combat/data/DamageData.java
@@ -8,6 +8,7 @@ public class DamageData {
 
     private final String uuid;
     private final EntityDamageEvent.DamageCause cause;
+    private final String damager;
     private final long damageDelay;
     private final long timeOfDamage = System.currentTimeMillis();
 
