84a0170af918f767842e4f7a9b560ee26d86b36f
Tom Hoogstra <tomhoogstra@live.com.au>
2023-10-16 03:20:47 +1100

    Increase sword damage / bring down netherite a bit

--- champions/src/main/resources/champions-migrations/V2__Armour_weapon_values.sql
+++ champions/src/main/resources/champions-migrations/V2__Armour_weapon_values.sql
@@ -6,17 +6,17 @@ create table if not exists ${tablePrefix}damagevalues
         primary key (Material)
 );
 
-INSERT IGNORE INTO ${tableprefix}damagevalues VALUES ("DIAMOND_SWORD", 6);
-INSERT IGNORE INTO ${tableprefix}damagevalues VALUES ("DIAMOND_AXE", 6);
-INSERT IGNORE INTO ${tableprefix}damagevalues VALUES ("GOLDEN_SWORD", 5);
-INSERT IGNORE INTO ${tableprefix}damagevalues VALUES ("GOLDEN_AXE", 5);
-INSERT IGNORE INTO ${tableprefix}damagevalues VALUES ("NETHERITE_SWORD", 6);
-INSERT IGNORE INTO ${tableprefix}damagevalues VALUES ("NETHERITE_AXE", 6);
-INSERT IGNORE INTO ${tableprefix}damagevalues VALUES ("IRON_SWORD", 5);
-INSERT IGNORE INTO ${tableprefix}damagevalues VALUES ("IRON_AXE", 5);
-INSERT IGNORE INTO ${tableprefix}damagevalues VALUES ("STONE_SWORD", 3);
-INSERT IGNORE INTO ${tableprefix}damagevalues VALUES ("WOODEN_SWORD", 2);
-INSERT IGNORE INTO ${tableprefix}damagevalues VALUES ("STONE_AXE", 2);
+INSERT IGNORE INTO ${tableprefix}damagevalues VALUES ("DIAMOND_SWORD", 7);
+INSERT IGNORE INTO ${tableprefix}damagevalues VALUES ("DIAMOND_AXE", 7);
+INSERT IGNORE INTO ${tableprefix}damagevalues VALUES ("GOLDEN_SWORD", 6);
+INSERT IGNORE INTO ${tableprefix}damagevalues VALUES ("GOLDEN_AXE", 6);
+INSERT IGNORE INTO ${tableprefix}damagevalues VALUES ("NETHERITE_SWORD", 7);
+INSERT IGNORE INTO ${tableprefix}damagevalues VALUES ("NETHERITE_AXE", 7);
+INSERT IGNORE INTO ${tableprefix}damagevalues VALUES ("IRON_SWORD", 6);
+INSERT IGNORE INTO ${tableprefix}damagevalues VALUES ("IRON_AXE", 6);
+INSERT IGNORE INTO ${tableprefix}damagevalues VALUES ("STONE_SWORD", 5);
+INSERT IGNORE INTO ${tableprefix}damagevalues VALUES ("WOODEN_SWORD", 3);
+INSERT IGNORE INTO ${tableprefix}damagevalues VALUES ("STONE_AXE", 5);
 INSERT IGNORE INTO ${tableprefix}damagevalues VALUES ("WOODEN_AXE", 1);
 INSERT IGNORE INTO ${tableprefix}damagevalues VALUES ("TRIDENT", 2);
 INSERT IGNORE INTO ${tableprefix}damagevalues VALUES ("DIAMOND_SHOVEL", 2);
