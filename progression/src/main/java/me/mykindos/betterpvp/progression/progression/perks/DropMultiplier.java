c4cf42a00f42d4d9f0862d5f51e1dba95cdba5cc
StudentAlleg <studentalleg-pilot@yahoo.com>
2023-10-16 08:42:27 -0700

    multiple fishing drops

--- progression/src/main/java/me/mykindos/betterpvp/progression/progression/perks/DropMultiplier.java
+++ progression/src/main/java/me/mykindos/betterpvp/progression/progression/perks/DropMultiplier.java
@@ -9,14 +9,24 @@ import java.util.Random;
 public interface DropMultiplier {
 
 
+    int minLevel = 0;
+
+    int maxLevel = 1000;
+
+    double increasePerLevel = 0.25;
+
     Random RANDOM = new Random();
 
     default int getMultiplier(double chance) {
-        int multiplier = (int) Math.floor(chance/100);
+        int multiplier = (int) Math.nextDown(chance/100);
         double newChance = chance % 100;
         if (RANDOM.nextInt(100) < newChance) {
             multiplier++;
         }
         return multiplier;
+    }
+
+    default int getMultiplier(int level) {
+        return getMultiplier(level * increasePerLevel);
     }
 }
