2660a2c405926f38d862605acbf7591214f69904
Tom Hoogstra <tomhoogstra@live.com.au>
2023-10-18 21:26:38 +1100

    Fix health indicator flicker

--- clans/src/main/java/me/mykindos/betterpvp/clans/scoreboards/ClansHealthScoreboardListener.java
+++ clans/src/main/java/me/mykindos/betterpvp/clans/scoreboards/ClansHealthScoreboardListener.java
@@ -1,19 +1,25 @@
 package me.mykindos.betterpvp.clans.scoreboards;
 
+import lombok.extern.slf4j.Slf4j;
 import me.mykindos.betterpvp.core.framework.events.scoreboard.ScoreboardUpdateEvent;
 import me.mykindos.betterpvp.core.framework.updater.UpdateEvent;
 import me.mykindos.betterpvp.core.listener.BPvPListener;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.scoreboard.Criteria;
 import org.bukkit.scoreboard.DisplaySlot;
 import org.bukkit.scoreboard.Objective;
 import org.bukkit.scoreboard.Scoreboard;
 
+import java.util.ConcurrentModificationException;
+
 @BPvPListener
+@Slf4j
 public class ClansHealthScoreboardListener implements Listener {
 
     @EventHandler
@@ -23,20 +29,24 @@ public class ClansHealthScoreboardListener implements Listener {
 
         Objective healthObjective = scoreboard.getObjective("healthDisplay");
         if (healthObjective == null) {
-            healthObjective = scoreboard.registerNewObjective("healthDisplay", "dummy", Component.text(ChatColor.RED + "\u2764"));
+            healthObjective = scoreboard.registerNewObjective("healthDisplay", Criteria.DUMMY, Component.text("\u2764", NamedTextColor.RED));
             healthObjective.setDisplaySlot(DisplaySlot.BELOW_NAME);
         }
     }
 
-    @UpdateEvent(delay=200, isAsync = true)
-    public void updateHealth(){
-        for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
-            Objective health = onlinePlayer.getScoreboard().getObjective("healthDisplay");
-            if (health != null) {
-                for (Player player : Bukkit.getOnlinePlayers()) {
-                    health.getScore(player.getName()).setScore((int) player.getHealth());
+    @UpdateEvent(delay = 200, isAsync = true)
+    public void updateHealth() {
+        try {
+            for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
+                Objective health = onlinePlayer.getScoreboard().getObjective("healthDisplay");
+                if (health != null) {
+                    for (Player player : Bukkit.getOnlinePlayers()) {
+                        health.getScore(player.getName()).setScore((int) player.getHealth());
+                    }
                 }
             }
+        } catch (ConcurrentModificationException ex) {
+            log.warn("ConcurrentModificationException caught while updating health scoreboard. Ignoring...");
         }
     }
 
