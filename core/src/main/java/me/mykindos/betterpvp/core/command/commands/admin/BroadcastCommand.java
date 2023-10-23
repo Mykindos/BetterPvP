8b12c6b86f2777c92e32e51e8daeb882a44b0400
StudentAlleg <studentalleg-pilot@yahoo.com>
2023-10-23 07:52:30 -0700

    update to use UtilSound and remove getRequiredRank

--- core/src/main/java/me/mykindos/betterpvp/core/command/commands/admin/BroadcastCommand.java
+++ core/src/main/java/me/mykindos/betterpvp/core/command/commands/admin/BroadcastCommand.java
@@ -1,24 +1,18 @@
 package me.mykindos.betterpvp.core.command.commands.admin;
 
-import com.comphenix.protocol.wrappers.MinecraftKey;
 import com.google.inject.Inject;
 import me.mykindos.betterpvp.core.client.Client;
 import me.mykindos.betterpvp.core.client.Rank;
 import me.mykindos.betterpvp.core.command.Command;
-import me.mykindos.betterpvp.core.gamer.Gamer;
 import me.mykindos.betterpvp.core.gamer.GamerManager;
 import me.mykindos.betterpvp.core.utilities.UtilMessage;
-import me.mykindos.betterpvp.core.world.WorldHandler;
+import me.mykindos.betterpvp.core.utilities.UtilSound;
 import net.kyori.adventure.key.Key;
-import net.kyori.adventure.sound.Sound;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.Bukkit;
-import org.bukkit.World;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
-import org.bukkit.packs.DataPack;
-
-import java.util.Optional;
 
 public class BroadcastCommand extends Command {
     private final GamerManager gamerManager;
@@ -42,25 +36,15 @@ public class BroadcastCommand extends Command {
 
     @Override
     public void execute(Player player, Client client, String... args) {
-        Optional<Gamer> gamerOptional = gamerManager.getObject(player.getUniqueId().toString());
         if(args.length == 0) {
             UtilMessage.message(player, "Core", "You must specify a message");
             return;
         }
-
         Component message = Component.empty().append(client.getRank().getPlayerNameMouseOver(player.getName()).decorate(TextDecoration.BOLD))
                         .append(UtilMessage.deserialize(" <red>%s", String.join(" ", args)));
-
-        Sound notification = Sound.sound(Key.key(Key.MINECRAFT_NAMESPACE, "block.note_block.bell"), Sound.Source.NEUTRAL, 1.0f, 1.0f);
         for (Player playerToSend : Bukkit.getOnlinePlayers()) {
             UtilMessage.message(playerToSend, message);
-            playerToSend.playSound(notification, Sound.Emitter.self());
+            UtilSound.playSound(playerToSend, Sound.BLOCK_NOTE_BLOCK_BELL, 1.0f, 1.0f, true);
         }
     }
-
-    @Override
-    public Rank getRequiredRank() {
-        return Rank.HELPER;
-    }
-
 }
