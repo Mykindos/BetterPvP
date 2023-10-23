0792901357bf98f7166441e59ce3b13b0e93aa84
StudentAlleg <36683605+StudentAlleg@users.noreply.github.com>
2023-10-23 16:43:38 -0700

    add a broadcast command for staff (#155)
    * add a broadcast command for staff
    
    * update to use UtilSound and remove getRequiredRank

---
+++ core/src/main/java/me/mykindos/betterpvp/core/command/commands/admin/BroadcastCommand.java
@@ -0,0 +1,50 @@
+package me.mykindos.betterpvp.core.command.commands.admin;
+
+import com.google.inject.Inject;
+import me.mykindos.betterpvp.core.client.Client;
+import me.mykindos.betterpvp.core.client.Rank;
+import me.mykindos.betterpvp.core.command.Command;
+import me.mykindos.betterpvp.core.gamer.GamerManager;
+import me.mykindos.betterpvp.core.utilities.UtilMessage;
+import me.mykindos.betterpvp.core.utilities.UtilSound;
+import net.kyori.adventure.key.Key;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.TextDecoration;
+import org.bukkit.Bukkit;
+import org.bukkit.Sound;
+import org.bukkit.entity.Player;
+
+public class BroadcastCommand extends Command {
+    private final GamerManager gamerManager;
+
+    @Inject
+    public BroadcastCommand(GamerManager gamerManager){
+        this.gamerManager = gamerManager;
+
+        aliases.add("bc");
+    }
+
+    @Override
+    public String getName() {
+        return "broadcast";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Send an emphasized message to the server";
+    }
+
+    @Override
+    public void execute(Player player, Client client, String... args) {
+        if(args.length == 0) {
+            UtilMessage.message(player, "Core", "You must specify a message");
+            return;
+        }
+        Component message = Component.empty().append(client.getRank().getPlayerNameMouseOver(player.getName()).decorate(TextDecoration.BOLD))
+                        .append(UtilMessage.deserialize(" <red>%s", String.join(" ", args)));
+        for (Player playerToSend : Bukkit.getOnlinePlayers()) {
+            UtilMessage.message(playerToSend, message);
+            UtilSound.playSound(playerToSend, Sound.BLOCK_NOTE_BLOCK_BELL, 1.0f, 1.0f, true);
+        }
+    }
+}
