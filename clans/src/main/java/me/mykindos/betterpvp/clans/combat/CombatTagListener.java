724ebfa3524fc6acd58c9216f3190e90cbd5b1f8
ezrn <zanderaguirre@icloud.com>
2023-10-23 12:27:37 -0700

    added suggestions to combatTagListener

--- clans/src/main/java/me/mykindos/betterpvp/clans/combat/CombatTagListener.java
+++ clans/src/main/java/me/mykindos/betterpvp/clans/combat/CombatTagListener.java
@@ -11,12 +11,14 @@ import me.mykindos.betterpvp.core.listener.BPvPListener;
 import me.mykindos.betterpvp.core.utilities.UtilMessage;
 import me.mykindos.betterpvp.core.utilities.UtilTime;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.title.Title;
 import net.kyori.adventure.title.TitlePart;
 import org.bukkit.Bukkit;
 import org.bukkit.Particle;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 
+import java.time.Duration;
 import java.util.*;
 
 @BPvPListener
@@ -65,30 +67,29 @@ public class CombatTagListener implements Listener {
             Optional<Gamer> gamerOptional = gamerManager.getObject(player.getUniqueId());
             gamerOptional.ifPresent(gamer -> {
                 UUID playerId = player.getUniqueId();
-                if (clanManager.isInSafeZone(player)) {
-                    if (!UtilTime.elapsed(gamer.getLastDamaged(), 15000)) {
 
+                if (!UtilTime.elapsed(gamer.getLastDamaged(), 15000)) {
+                    if (clanManager.isInSafeZone(player)) {
                         playersShownSafeMessage.remove(playerId);
 
                         long remainingMillis = 15000 - (System.currentTimeMillis() - gamer.getLastDamaged());
                         double remainingSeconds = remainingMillis / 1000.0;
 
                         Component subtitleText = UtilMessage.deserialize("<gray>Unsafe for: <red>" + String.format("%.1f", remainingSeconds) + "s");
-                        player.sendTitle("", "", 0, 5, 0);
+                        player.sendTitlePart(TitlePart.TIMES, Title.Times.times(Duration.ofMillis(0), Duration.ofMillis(250), Duration.ofMillis(0)));
                         player.sendTitlePart(TitlePart.SUBTITLE, subtitleText);
-                    } else if (!playersShownSafeMessage.contains(playerId)) {
-
-                        playersShownSafeMessage.add(playerId);
-
-                        Component safeText = UtilMessage.deserialize("<green>Safe!");
-                        player.sendTitle("", "", 0, 20, 20);
-                        player.sendTitlePart(TitlePart.SUBTITLE, safeText);
                     }
+                } else if (!playersShownSafeMessage.contains(playerId)) {
+                    playersShownSafeMessage.add(playerId);
+
+                    Component safeText = UtilMessage.deserialize("<green>Safe!");
+                    player.sendTitlePart(TitlePart.TIMES, Title.Times.times(Duration.ofMillis(0), Duration.ofMillis(1000), Duration.ofMillis(500)));
+                    player.sendTitlePart(TitlePart.SUBTITLE, safeText);
                 } else {
                     if (playersShownSafeMessage.contains(playerId)) {
-                        player.sendTitle("", "", 0, 20, 0);
+                        player.sendTitlePart(TitlePart.TIMES, Title.Times.times(Duration.ofMillis(0), Duration.ofMillis(1000), Duration.ofMillis(500)));
+                        player.sendTitlePart(TitlePart.SUBTITLE, Component.empty());
                     }
-
                     playersShownSafeMessage.remove(playerId);
                 }
             });
