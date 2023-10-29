dab497966fb7eb5b71a2bbd1995d484a47d3a188
Tom Hoogstra <tomhoogstra@live.com.au>
2023-10-29 16:40:31 +1100

    StudentAlleg - Additional fishing perks

--- progression/src/main/java/me/mykindos/betterpvp/progression/progression/perks/DropMultiplier.java
+++ progression/src/main/java/me/mykindos/betterpvp/progression/progression/perks/DropMultiplier.java
@@ -1,11 +1,6 @@
 package me.mykindos.betterpvp.progression.progression.perks;
 
-import me.mykindos.betterpvp.core.config.Config;
 import me.mykindos.betterpvp.core.utilities.UtilMath;
-import net.kyori.adventure.text.Component;
-import org.bukkit.Bukkit;
-
-import java.util.Random;
 
 public interface DropMultiplier {
 
