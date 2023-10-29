5987c974a9cc67bcf5d3c102846ec3dbe38f4aec
ezrn <zanderaguirre@icloud.com>
2023-10-29 13:44:12 -0700

    make hilt smash missable

--- champions/src/main/java/me/mykindos/betterpvp/champions/champions/skills/skills/knight/sword/HiltSmash.java
+++ champions/src/main/java/me/mykindos/betterpvp/champions/champions/skills/skills/knight/sword/HiltSmash.java
@@ -12,6 +12,7 @@ import me.mykindos.betterpvp.core.combat.events.CustomDamageEvent;
 import me.mykindos.betterpvp.core.components.champions.Role;
 import me.mykindos.betterpvp.core.components.champions.SkillType;
 import me.mykindos.betterpvp.core.components.champions.events.PlayerUseSkillEvent;
+import me.mykindos.betterpvp.core.cooldowns.CooldownManager;
 import me.mykindos.betterpvp.core.effects.EffectType;
 import me.mykindos.betterpvp.core.listener.BPvPListener;
 import me.mykindos.betterpvp.core.utilities.UtilDamage;
@@ -26,11 +27,17 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.EquipmentSlot;
 
 @Singleton
 @BPvPListener
 public class HiltSmash extends Skill implements CooldownSkill, Listener {
+
+    private boolean playerClickedEntity = false;
+
+    @Inject
+    private CooldownManager cooldownManager;
 
     @Inject
     public HiltSmash(Champions champions, ChampionsManager championsManager) {
@@ -67,17 +74,32 @@ public class HiltSmash extends Skill implements CooldownSkill, Listener {
     }
 
     @EventHandler
-    public void onInteract(PlayerInteractEntityEvent event) {
+    public void onEntityInteract(PlayerInteractEntityEvent event) {
         if (event.getHand() == EquipmentSlot.OFF_HAND) return;
-        Player player = event.getPlayer();
-        if (!UtilPlayer.isHoldingItem(player, SkillWeapons.SWORDS)) return;
+        playerClickedEntity = true;
+        if (event.getRightClicked() instanceof LivingEntity) {
+            onInteract(event.getPlayer(), (LivingEntity) event.getRightClicked());
+        } else {
+            onInteract(event.getPlayer(), null);
+        }
+        event.setCancelled(true);
+    }
 
+    @EventHandler
+    public void onInteract(PlayerInteractEvent event) {
+        if (event.getHand() == EquipmentSlot.OFF_HAND) return;
+        if (!playerClickedEntity) { // This means onInteract wasn't called through onEntityInteract
+            onInteract(event.getPlayer(), null);
+        }
+        playerClickedEntity = false; // Reset the flag for next interactions
+    }
+
+    private void onInteract(Player player, LivingEntity ent) {
+        if (!UtilPlayer.isHoldingItem(player, SkillWeapons.SWORDS)) return;
 
         int level = getLevel(player);
         if (level > 0) {
-
-            if (event.getRightClicked() instanceof LivingEntity ent) {
-
+            if (ent != null) {
                 PlayerUseSkillEvent playerUseSkillEvent = UtilServer.callEvent(new PlayerUseSkillEvent(player, this, level));
                 if (playerUseSkillEvent.isCancelled()) return;
                 if (UtilMath.offset(player, ent) <= 3.0) {
@@ -94,15 +116,18 @@ public class HiltSmash extends Skill implements CooldownSkill, Listener {
 
                     UtilDamage.doCustomDamage(new CustomDamageEvent(ent, player, null, DamageCause.ENTITY_ATTACK, 3 + level, false, getName()));
                     ent.getWorld().playSound(ent.getLocation(), Sound.ENTITY_ZOMBIE_BREAK_WOODEN_DOOR, 1.0F, 1.2F);
-
-
                 } else {
                     UtilMessage.simpleMessage(player, getClassType().getName(), "You failed <green>%s", getName());
                     player.getWorld().playSound(player.getLocation(), Sound.BLOCK_ANVIL_LAND, 1F, 0.1F);
+                    cooldownManager.use(player, getName(), getCooldown(level), true);
+
                 }
+            } else {
+                UtilMessage.simpleMessage(player, getClassType().getName(), "You failed <green>%s", getName());
+                player.getWorld().playSound(player.getLocation(), Sound.BLOCK_ANVIL_LAND, 1F, 0.1F);
+                cooldownManager.use(player, getName(), getCooldown(level), true);
             }
         }
-
     }
 
     @Override
