0d032ac6872c2a55a05a7e434b7e95cd352f780b
ezrn <zanderaguirre@icloud.com>
2023-10-29 15:10:42 -0700

    thanks rey

--- champions/src/main/java/me/mykindos/betterpvp/champions/champions/skills/skills/knight/sword/HiltSmash.java
+++ champions/src/main/java/me/mykindos/betterpvp/champions/champions/skills/skills/knight/sword/HiltSmash.java
@@ -12,14 +12,9 @@ import me.mykindos.betterpvp.core.combat.events.CustomDamageEvent;
 import me.mykindos.betterpvp.core.components.champions.Role;
 import me.mykindos.betterpvp.core.components.champions.SkillType;
 import me.mykindos.betterpvp.core.components.champions.events.PlayerUseSkillEvent;
-import me.mykindos.betterpvp.core.cooldowns.CooldownManager;
 import me.mykindos.betterpvp.core.effects.EffectType;
 import me.mykindos.betterpvp.core.listener.BPvPListener;
-import me.mykindos.betterpvp.core.utilities.UtilDamage;
-import me.mykindos.betterpvp.core.utilities.UtilMath;
-import me.mykindos.betterpvp.core.utilities.UtilMessage;
-import me.mykindos.betterpvp.core.utilities.UtilPlayer;
-import me.mykindos.betterpvp.core.utilities.UtilServer;
+import me.mykindos.betterpvp.core.utilities.*;
 import org.bukkit.Sound;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
@@ -30,14 +25,13 @@ import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.EquipmentSlot;
 
+import java.util.WeakHashMap;
+
 @Singleton
 @BPvPListener
 public class HiltSmash extends Skill implements CooldownSkill, Listener {
 
-    private boolean playerClickedEntity = false;
-
-    @Inject
-    private CooldownManager cooldownManager;
+    private WeakHashMap<Player, Boolean> rightClicked = new WeakHashMap<>();
 
     @Inject
     public HiltSmash(Champions champions, ChampionsManager championsManager) {
@@ -76,9 +70,9 @@ public class HiltSmash extends Skill implements CooldownSkill, Listener {
     @EventHandler
     public void onEntityInteract(PlayerInteractEntityEvent event) {
         if (event.getHand() == EquipmentSlot.OFF_HAND) return;
-        playerClickedEntity = true;
-        if (event.getRightClicked() instanceof LivingEntity) {
-            onInteract(event.getPlayer(), (LivingEntity) event.getRightClicked());
+        rightClicked.put(event.getPlayer(), true);
+        if (event.getRightClicked() instanceof LivingEntity entity) {
+            onInteract(event.getPlayer(), entity);
         } else {
             onInteract(event.getPlayer(), null);
         }
@@ -87,47 +81,54 @@ public class HiltSmash extends Skill implements CooldownSkill, Listener {
 
     @EventHandler
     public void onInteract(PlayerInteractEvent event) {
-        if (event.getHand() == EquipmentSlot.OFF_HAND) return;
-        if (!playerClickedEntity) { // This means onInteract wasn't called through onEntityInteract
+        if (event.getHand() == EquipmentSlot.OFF_HAND || !event.getAction().isRightClick()) return;
+        if (!rightClicked.getOrDefault(event.getPlayer(), false)) { // This means onInteract wasn't called through onEntityInteract
             onInteract(event.getPlayer(), null);
         }
-        playerClickedEntity = false; // Reset the flag for next interactions
+        rightClicked.remove(event.getPlayer()); // Reset the flag for next interactions
+    }
+
+    @Override
+    public void invalidatePlayer(Player player) {
+        rightClicked.remove(player);
     }
 
     private void onInteract(Player player, LivingEntity ent) {
         if (!UtilPlayer.isHoldingItem(player, SkillWeapons.SWORDS)) return;
 
         int level = getLevel(player);
-        if (level > 0) {
-            if (ent != null) {
-                PlayerUseSkillEvent playerUseSkillEvent = UtilServer.callEvent(new PlayerUseSkillEvent(player, this, level));
-                if (playerUseSkillEvent.isCancelled()) return;
-                if (UtilMath.offset(player, ent) <= 3.0) {
-                    if (ent instanceof Player damagee) {
-                        UtilMessage.simpleMessage(damagee, getClassType().getName(), "<yellow>%s<gray> hit you with <green>%s<gray>.",
-                                player.getName(), getName() + " " + level);
+        if (level <= 0) {
+            return; // Skill not active
+        }
 
-                        championsManager.getEffects().addEffect(damagee, EffectType.SHOCK, (level * 1000L) / 2);
-                        championsManager.getEffects().addEffect(damagee, EffectType.SILENCE, (((level * 1000L) / 2)));
-                    }
+        // Cooldown's applied in the event monitor
+        final PlayerUseSkillEvent event = UtilServer.callEvent(new PlayerUseSkillEvent(player, this, level));
+        if (event.isCancelled()) {
+            return; // Skill was cancelled
+        }
 
-                    UtilMessage.simpleMessage(player, getClassType().getName(), "You hit <yellow>%s<gray> with <green>%s<gray>.",
-                            ent.getName(), getName() + " " + level);
-
-                    UtilDamage.doCustomDamage(new CustomDamageEvent(ent, player, null, DamageCause.ENTITY_ATTACK, 3 + level, false, getName()));
-                    ent.getWorld().playSound(ent.getLocation(), Sound.ENTITY_ZOMBIE_BREAK_WOODEN_DOOR, 1.0F, 1.2F);
-                } else {
-                    UtilMessage.simpleMessage(player, getClassType().getName(), "You failed <green>%s", getName());
-                    player.getWorld().playSound(player.getLocation(), Sound.BLOCK_ANVIL_LAND, 1F, 0.1F);
-                    cooldownManager.use(player, getName(), getCooldown(level), true);
-
+        if (ent != null) {
+            if (UtilMath.offset(player, ent) <= 3.0) {
+                if (ent instanceof Player damagee) {
+                    UtilMessage.simpleMessage(damagee, getClassType().getName(), "<yellow>%s<gray> hit you with <green>%s<gray>.", player.getName(), getName() + " " + level);
+                    championsManager.getEffects().addEffect(damagee, EffectType.SHOCK, (level * 1000L) / 2);
+                    championsManager.getEffects().addEffect(damagee, EffectType.SILENCE, (((level * 1000L) / 2)));
                 }
+
+                UtilMessage.simpleMessage(player, getClassType().getName(), "You hit <yellow>%s<gray> with <green>%s<gray>.",
+                        ent.getName(), getName() + " " + level);
+
+                UtilDamage.doCustomDamage(new CustomDamageEvent(ent, player, null, DamageCause.ENTITY_ATTACK, 3 + level, false, getName()));
+                ent.getWorld().playSound(ent.getLocation(), Sound.ENTITY_ZOMBIE_BREAK_WOODEN_DOOR, 1.0F, 1.2F);
             } else {
                 UtilMessage.simpleMessage(player, getClassType().getName(), "You failed <green>%s", getName());
                 player.getWorld().playSound(player.getLocation(), Sound.BLOCK_ANVIL_LAND, 1F, 0.1F);
-                cooldownManager.use(player, getName(), getCooldown(level), true);
             }
+        } else {
+            UtilMessage.simpleMessage(player, getClassType().getName(), "You failed <green>%s", getName());
+            player.getWorld().playSound(player.getLocation(), Sound.BLOCK_ANVIL_LAND, 1F, 0.1F);
         }
+        player.swingMainHand();
     }
 
     @Override
