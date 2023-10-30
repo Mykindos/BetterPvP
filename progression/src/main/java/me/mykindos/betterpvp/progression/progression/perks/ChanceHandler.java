08252825b84ec98938f5ce89d47cbc14debb80d7
StudentAlleg <studentalleg-pilot@yahoo.com>
2023-10-30 14:15:00 -0700

    #137 change increase to change to a chance to fish up a fish that is 20% heavier

--- progression/src/main/java/me/mykindos/betterpvp/progression/progression/perks/ChanceHandler.java
+++ progression/src/main/java/me/mykindos/betterpvp/progression/progression/perks/ChanceHandler.java
@@ -2,9 +2,9 @@ package me.mykindos.betterpvp.progression.progression.perks;
 
 import me.mykindos.betterpvp.core.utilities.UtilMath;
 
-public interface DropMultiplier {
+public interface ChanceHandler {
 
-    default int getExtraDrops(double chance) {
+    default int getChance(double chance) {
         int multiplier = (int) Math.floor(chance/100);
         double newChance = chance % 100;
         if (UtilMath.randomInt(0, 100) < newChance) {
