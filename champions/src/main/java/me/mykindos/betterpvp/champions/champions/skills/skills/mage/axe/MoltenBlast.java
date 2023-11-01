567b89a1cef0a4e73c583aaa9e12aa0b3063f8cf
ezrn <zanderaguirre@icloud.com>
2023-11-01 11:18:16 -0700

    added KB to molten blast

--- champions/src/main/java/me/mykindos/betterpvp/champions/champions/skills/skills/mage/axe/MoltenBlast.java
+++ champions/src/main/java/me/mykindos/betterpvp/champions/champions/skills/skills/mage/axe/MoltenBlast.java
@@ -12,9 +12,15 @@ import me.mykindos.betterpvp.champions.champions.skills.types.InteractSkill;
 import me.mykindos.betterpvp.core.combat.events.CustomDamageEvent;
 import me.mykindos.betterpvp.core.components.champions.Role;
 import me.mykindos.betterpvp.core.components.champions.SkillType;
+import me.mykindos.betterpvp.core.framework.customtypes.KeyValue;
 import me.mykindos.betterpvp.core.framework.updater.UpdateEvent;
 import me.mykindos.betterpvp.core.listener.BPvPListener;
+import me.mykindos.betterpvp.core.utilities.UtilMessage;
+import me.mykindos.betterpvp.core.utilities.UtilPlayer;
 import me.mykindos.betterpvp.core.utilities.UtilServer;
+import me.mykindos.betterpvp.core.utilities.UtilVelocity;
+import me.mykindos.betterpvp.core.utilities.events.EntityProperty;
+import org.bukkit.Location;
 import org.bukkit.Particle;
 import org.bukkit.Sound;
 import org.bukkit.entity.LargeFireball;
@@ -26,10 +32,9 @@ import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityExplodeEvent;
 import org.bukkit.event.entity.ProjectileHitEvent;
+import org.bukkit.util.Vector;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 @Singleton
 @BPvPListener
@@ -107,8 +112,26 @@ public class MoltenBlast extends Skill implements InteractSkill, CooldownSkill, 
         if (event.getEntity() instanceof LargeFireball largeFireball) {
             fireballs.remove(largeFireball);
             largeFireball.getWorld().playSound(largeFireball.getLocation(), Sound.ENTITY_DRAGON_FIREBALL_EXPLODE, 2.0f, 1.0f);
+
+            Location impactLocation = largeFireball.getLocation();
+
+            if (largeFireball.getShooter() instanceof Player player) {
+                UtilMessage.message(player, getClassType().getName(), "fireball is being detected and has a player instance");
+                UtilMessage.message(player, getClassType().getName(), "Fireball location: "+largeFireball.getLocation());
+
+                List<KeyValue<Player, EntityProperty>> nearbyPlayers = UtilPlayer.getNearbyPlayers(player, impactLocation, 4.0, EntityProperty.ALL);
+
+                for (KeyValue<Player, EntityProperty> nearbyPlayer : nearbyPlayers) {
+                    UtilMessage.message(player, getClassType().getName(), "There is a player in the list");
+                    Location playerLocation = nearbyPlayer.getKey().getLocation();
+
+                    Vector direction = playerLocation.toVector().subtract(impactLocation.toVector()).normalize();
+                    UtilVelocity.velocity(nearbyPlayer.getKey(), direction, 3, false, 0.0, 1.0, 3.0, true);
+                }
+            }
         }
     }
+
 
 
     @EventHandler
@@ -120,7 +143,9 @@ public class MoltenBlast extends Skill implements InteractSkill, CooldownSkill, 
 
                 event.setKnockback(true);
                 event.setDamage(damage);
-                event.addReason(getName());
+                Set<String> reasons = new HashSet<>();
+                reasons.add(getName());
+                event.setReason(reasons);
                 UtilServer.runTaskLater(champions, () -> event.getDamagee().setFireTicks((int) (20 * (0 + (getLevel(player) * 0.5)))), 2);
 
             }
