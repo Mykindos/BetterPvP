1284aceb793f8b67e3ccd02511915b8aa4c1aded
StudentAlleg <36683605+StudentAlleg@users.noreply.github.com>
2023-11-04 12:47:13 -0700

    Add more fishing perks (#137)
    * StudentAlleg - Additional fishing perks
    
    * StudentAlleg - fixes
    
    * #137 PR fixes
    
    * #137 move fishing perks to own package
    
    * #137 change increase to change to a chance to fish up a fish that is 20% heavier
    
    * 137 remove incorrect semi-colon
    
    * #137 WIP fish weight
    
    * #137 logging, re-add onCatch lost in fixing
    
    * #137 remove unused event parameter from PlayerStopFishingEvent lost in fixes
    
    * #137 remove logging
    
    * #137 re-enabled IncreaseTreasureChance
    
    * Fix perks
    
    * StudentAlleg - Additional fishing perks
    
    * StudentAlleg - fixes
    
    * #137 PR fixes
    
    * #137 move fishing perks to own package
    
    * #137 change increase to change to a chance to fish up a fish that is 20% heavier
    
    * 137 remove incorrect semi-colon
    
    * #137 WIP fish weight
    
    * #137 logging, re-add onCatch lost in fixing
    
    * #137 remove unused event parameter from PlayerStopFishingEvent lost in fixes
    
    * #137 remove logging
    
    * #137 re-enabled IncreaseTreasureChance
    
    * Fix perks
    
    ---------
    
    Co-authored-by: Tom Hoogstra <tomhoogstra@live.com.au>
    Co-authored-by: Braulio <bchamerry@gmail.com>

---
+++ progression/src/main/java/me/mykindos/betterpvp/progression/tree/fishing/event/PlayerCaughtFishEvent.java
@@ -0,0 +1,23 @@
+package me.mykindos.betterpvp.progression.tree.fishing.event;
+
+import lombok.Getter;
+import lombok.Setter;
+import me.mykindos.betterpvp.progression.tree.fishing.model.FishingLoot;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.FishHook;
+import org.bukkit.entity.Player;
+
+@Getter
+public class PlayerCaughtFishEvent extends ProgressionFishingEvent {
+    @Setter
+    private FishingLoot loot;
+    final FishHook hook;
+    final Entity caught;
+
+    public PlayerCaughtFishEvent(Player player, FishingLoot loot, FishHook hook, Entity caught) {
+        super(player);
+        this.loot = loot;
+        this.hook = hook;
+        this.caught = caught;
+    }
+}
