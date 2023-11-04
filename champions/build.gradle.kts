3d37a39126e0810546cf4d8cf2fa52592d8c4ed8
Mykindos <5389944+Mykindos@users.noreply.github.com>
2023-11-04 18:32:58 +1100

    qodana analysis

--- champions/build.gradle.kts
+++ champions/build.gradle.kts
@@ -34,7 +34,7 @@ dependencies {
 
     implementation("com.google.inject:guice:5.1.0")
     implementation("org.reflections:reflections:0.10.2")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.13.3")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.13.4.1")
 
     compileOnly(project(":core"))
     compileOnly("io.papermc.paper:paper-api:1.20.2-R0.1-SNAPSHOT")
