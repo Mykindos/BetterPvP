d75a8604dee595344a22f878fafd6fb98020d4a6
StudentAlleg <studentalleg-pilot@yahoo.com>
2023-11-09 18:35:54 -0800

    #208 Brute passives + brute config - sword

--- champions/src/main/java/me/mykindos/betterpvp/champions/champions/skills/skills/brute/passives/Stampede.java
+++ champions/src/main/java/me/mykindos/betterpvp/champions/champions/skills/skills/brute/passives/Stampede.java
@@ -11,6 +11,7 @@ import me.mykindos.betterpvp.core.components.champions.Role;
 import me.mykindos.betterpvp.core.components.champions.SkillType;
 import me.mykindos.betterpvp.core.framework.updater.UpdateEvent;
 import me.mykindos.betterpvp.core.listener.BPvPListener;
+import me.mykindos.betterpvp.core.utilities.UtilFormat;
 import me.mykindos.betterpvp.core.utilities.UtilTime;
 import me.mykindos.betterpvp.core.utilities.UtilVelocity;
 import org.bukkit.Bukkit;
@@ -34,6 +35,8 @@ public class Stampede extends Skill implements PassiveSkill {
     private double durationPerStack;
     private double damage;
 
+    private int maxSpeedStrength;
+
     @Inject
     public Stampede(Champions champions, ChampionsManager championsManager) {
         super(champions, championsManager);
@@ -51,7 +54,7 @@ public class Stampede extends Skill implements PassiveSkill {
                 "You slowly build up speed as you",
                 "sprint, gaining one level of <effect>Speed</effect>",
                 "for every <val>" + (durationPerStack - level) + "</val> seconds, up to a max",
-                "of <effect>Speed II</effect>",
+                "of <effect>Speed " + UtilFormat.getRomanNumeral(maxSpeedStrength + 1) + "</effect>",
                 "",
                 "Attacking during stampede deals",
                 "<stat>" + damage + "</stat> bonus damage per speed level"};
@@ -76,7 +79,7 @@ public class Stampede extends Skill implements PassiveSkill {
             if (level > 0) {
                 if (!sprintTime.containsKey(player)) {
                     sprintTime.put(player, System.currentTimeMillis());
-                    sprintStr.put(player, 0);
+                    sprintStr.put(player, -1);
                 }
 
                 if (!player.isSprinting() || player.isInWater()) {
@@ -86,19 +89,19 @@ public class Stampede extends Skill implements PassiveSkill {
                 } else {
                     long time = sprintTime.get(player);
                     int str = sprintStr.get(player);
-                    if (str > 0) {
+                    if (str >= 0) {
                         if (player.hasPotionEffect(PotionEffectType.SPEED)) {
                             player.removePotionEffect(PotionEffectType.SPEED);
                         }
 
-                        player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 40, str - 1));
+                        player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 40, str));
                     }
                     if (UtilTime.elapsed(time, (long) ((durationPerStack - level) * 1000L))) {
                         sprintTime.put(player, System.currentTimeMillis());
-                        if (str < 2) {
+                        if (str < maxSpeedStrength) {
                             sprintStr.put(player, str + 1);
 
-                            player.getWorld().playSound(player.getLocation(), Sound.ENTITY_ZOMBIE_AMBIENT, 2.0F, 0.2F * str + 1.0F);
+                            player.getWorld().playSound(player.getLocation(), Sound.ENTITY_ZOMBIE_AMBIENT, 2.0F, 0.2F * str + 2.0F);
                         }
                     }
                 }
@@ -111,8 +114,8 @@ public class Stampede extends Skill implements PassiveSkill {
     public void onDamage(CustomDamageEvent event) {
         if (event.getCause() != DamageCause.ENTITY_ATTACK) return;
         if (!(event.getDamager() instanceof Player damager)) return;
-        int str = sprintStr.getOrDefault(damager, 0);
-        if (str <= 0) return;
+        int str = sprintStr.getOrDefault(damager, -1);
+        if (str < 0) return;
 
         sprintTime.remove(damager);
         sprintStr.remove(damager);
@@ -120,12 +123,13 @@ public class Stampede extends Skill implements PassiveSkill {
 
         event.setKnockback(false);
         UtilVelocity.velocity(event.getDamagee(), UtilVelocity.getTrajectory2d(damager, event.getDamagee()), 2.0D, true, 0.0D, 0.4D, 1.0D, true, true);
-        event.setDamage(event.getDamage() + (str * damage));
+        event.setDamage(event.getDamage() + ((str + 1) * damage));
     }
 
     @Override
     public void loadSkillConfig() {
         durationPerStack = getConfig("durationPerStack", 8.0, Double.class);
         damage = getConfig("damageMultiplier", 2.0, Double.class);
+        maxSpeedStrength = getConfig("maxSpeedStrength", 1, Integer.class);
     }
 }
