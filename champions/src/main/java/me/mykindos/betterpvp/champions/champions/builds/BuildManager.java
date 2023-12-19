a9576dd45304570ad52508e11ca8b31a2c7c01f2
StudentAlleg <36683605+StudentAlleg@users.noreply.github.com>
2023-12-18 17:56:54 -0800

    Update build loading. (#254)
    * #253 change how default builds are loaded, to be in line with regular builds being loaded. #210 on reload, builds are now reloaded and correctly do not take points. Add notification that this happens.
    
    * #254 - only give feedback for active builds that have disabled skills

--- champions/src/main/java/me/mykindos/betterpvp/champions/champions/builds/BuildManager.java
+++ champions/src/main/java/me/mykindos/betterpvp/champions/champions/builds/BuildManager.java
@@ -3,8 +3,13 @@ package me.mykindos.betterpvp.champions.champions.builds;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import lombok.Getter;
+import me.mykindos.betterpvp.champions.Champions;
+import me.mykindos.betterpvp.champions.champions.builds.event.LoadBuildsEvent;
 import me.mykindos.betterpvp.champions.champions.builds.repository.BuildRepository;
 import me.mykindos.betterpvp.core.framework.manager.Manager;
+import me.mykindos.betterpvp.core.utilities.UtilServer;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 
 @Singleton
 @Getter
@@ -12,8 +17,30 @@ public class BuildManager extends Manager<GamerBuilds> {
 
     private final BuildRepository buildRepository;
 
+    private final Champions champions;
+
     @Inject
-    public BuildManager(BuildRepository buildRepository) {
+    public BuildManager(BuildRepository buildRepository, Champions champions) {
         this.buildRepository = buildRepository;
+        this.champions = champions;
     }
+
+    public void loadBuilds(Player player) {
+        GamerBuilds builds = new GamerBuilds(player.getUniqueId().toString());
+        getBuildRepository().loadBuilds(builds);
+        getBuildRepository().loadDefaultBuilds(builds);
+        addObject(player.getUniqueId().toString(), builds);
+        UtilServer.runTask(champions, () -> {
+            UtilServer.callEvent(new LoadBuildsEvent(player, builds));
+        });
+    }
+
+    public void reloadBuilds() {
+        getObjects().clear();
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            loadBuilds(player);
+        }
+    }
+
+
 }
