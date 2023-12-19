d0e739543505d2fd4fc0ca57f78f56f5f872dee8
StudentAlleg <studentalleg-pilot@yahoo.com>
2023-12-18 17:35:07 -0800

    #252 - Moved ~ handling to UtilLocation.java

--- core/src/main/java/me/mykindos/betterpvp/core/command/commands/qol/TeleportPositionCommand.java
+++ core/src/main/java/me/mykindos/betterpvp/core/command/commands/qol/TeleportPositionCommand.java
@@ -4,8 +4,8 @@ import com.google.inject.Singleton;
 import me.mykindos.betterpvp.core.client.Client;
 import me.mykindos.betterpvp.core.command.Command;
 import me.mykindos.betterpvp.core.framework.annotations.WithReflection;
+import me.mykindos.betterpvp.core.utilities.UtilLocation;
 import me.mykindos.betterpvp.core.utilities.UtilMessage;
-import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
 @Singleton
@@ -30,36 +30,8 @@ public class TeleportPositionCommand extends Command {
     public void execute(Player player, Client client, String... args) {
         double x = 0, y = 0, z = 0;
         if (args.length == 3) {
-            if (args[0].startsWith("~")) {
-                args[0] = args[0].substring(1);
-                x = player.getX();
-            }
-            try {
-                x += Double.parseDouble(args[0]);
-            } catch (NumberFormatException e) {
-                x += 0;
-            }
-            if (args[1].startsWith("~")) {
-                args[1] = args[1].substring(1);
-                y = player.getY();
-            }
-            try {
-                y += Double.parseDouble(args[1]);
-            } catch (NumberFormatException e) {
-                y +=0;
-            }
-
-            if (args[2].startsWith("~")) {
-                args[2] = args[2].substring(1);
-                z = player.getZ();
-            }
-            try {
-                z += Double.parseDouble(args[2]);
-            } catch (NumberFormatException e) {
-                z += 0;
-            }
-
-            player.teleport(new Location(player.getWorld(), x, y, z));
+            player.teleport(UtilLocation.getTeleportLocation(player.getLocation(), args));
+            UtilMessage.message(player, UtilMessage.deserialize("You teleported to (<green>%s</green>, <green>%s</green>, <green>%s</green>)", x, y, z));
         } else {
             UtilMessage.message(player, "Teleport", "Correct usage: /tppos x y z");
         }
