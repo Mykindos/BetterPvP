c24109c45262fbb4ce2ca00048089da22a9e38b8
Braulio <bchamerry@gmail.com>
2023-12-29 15:19:05 -0800

    Remove hard redis requirement to host a BetterPvP server

--- core/src/main/java/me/mykindos/betterpvp/core/redis/Redis.java
+++ core/src/main/java/me/mykindos/betterpvp/core/redis/Redis.java
@@ -1,19 +1,54 @@
 package me.mykindos.betterpvp.core.redis;
 
+import com.google.common.base.Preconditions;
 import com.google.inject.Singleton;
+import lombok.extern.slf4j.Slf4j;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.jetbrains.annotations.NotNull;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
 
 import java.time.Duration;
 
 @Singleton
+@Slf4j
 public class Redis {
 
     private JedisPool pool;
 
     public void credentials(final RedisCredentials redisCredentials) {
+        Preconditions.checkNotNull(redisCredentials, "Redis credentials cannot be null!");
+        Preconditions.checkState(pool == null, "Redis credentials have already been set!");
+
+        final JedisPoolConfig poolConfig = getJedisPoolConfig();
+        if (redisCredentials.getPassword() == null || redisCredentials.getPassword().isEmpty()) {
+            // Passwordless redis
+            this.pool = new JedisPool(
+                    poolConfig,
+                    redisCredentials.getHost(),
+                    redisCredentials.getPort(),
+                    0,
+                    null,
+                    redisCredentials.getDatabase()
+            );
+        } else {
+            // Passworded redis
+            this.pool = new JedisPool(
+                    poolConfig,
+                    redisCredentials.getHost(),
+                    redisCredentials.getPort(),
+                    0,
+                    redisCredentials.getPassword(),
+                    redisCredentials.getDatabase()
+            );
+        }
+
+        log.info("Redis has been enabled.");
+    }
+
+    @NotNull
+    private static JedisPoolConfig getJedisPoolConfig() {
         final JedisPoolConfig poolConfig = new JedisPoolConfig();
         poolConfig.setMaxTotal(128);
         poolConfig.setMaxIdle(128);
@@ -25,21 +60,18 @@ public class Redis {
         poolConfig.setTimeBetweenEvictionRuns(Duration.ofSeconds(30));
         poolConfig.setNumTestsPerEvictionRun(3);
         poolConfig.setBlockWhenExhausted(true);
-
-        this.pool = new JedisPool(
-                poolConfig,
-                redisCredentials.getHost(),
-                redisCredentials.getPort(),
-                0,
-                redisCredentials.getPassword(),
-                redisCredentials.getDatabase()
-        );
+        return poolConfig;
     }
 
     public void credentials(final FileConfiguration fileConfiguration) {
         final ConfigurationSection section = fileConfiguration.getConfigurationSection("core.redis");
         if (section == null) {
             throw new IllegalArgumentException("The configuration file provided does not contain database details!");
+        }
+
+        if (!section.getBoolean("enabled")) {
+            log.warn("Redis has been disabled in the configuration file. Cross-server functionality will not work.");
+            return;
         }
 
         final RedisCredentials redisInfo = new RedisCredentials(
@@ -54,6 +86,10 @@ public class Redis {
 
     public RedisAgent createAgent() {
         return new RedisAgent(pool);
+    }
+
+    public boolean isEnabled() {
+        return pool != null;
     }
 
 }
