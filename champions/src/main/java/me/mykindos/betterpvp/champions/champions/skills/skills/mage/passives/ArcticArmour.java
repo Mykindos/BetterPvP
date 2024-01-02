b539ce50785ff379c6dd78505f2d07a602bf0799
Braulio <bchamerry@gmail.com>
2024-01-02 12:44:03 -0800

    Improve restore blocks, finish arctic armor

--- champions/src/main/java/me/mykindos/betterpvp/champions/champions/skills/skills/mage/passives/ArcticArmour.java
+++ champions/src/main/java/me/mykindos/betterpvp/champions/champions/skills/skills/mage/passives/ArcticArmour.java
@@ -9,26 +9,31 @@ import me.mykindos.betterpvp.champions.champions.skills.types.EnergySkill;
 import me.mykindos.betterpvp.core.components.champions.Role;
 import me.mykindos.betterpvp.core.components.champions.SkillType;
 import me.mykindos.betterpvp.core.effects.EffectType;
+import me.mykindos.betterpvp.core.framework.customtypes.KeyValue;
 import me.mykindos.betterpvp.core.framework.updater.UpdateEvent;
 import me.mykindos.betterpvp.core.listener.BPvPListener;
 import me.mykindos.betterpvp.core.utilities.UtilBlock;
 import me.mykindos.betterpvp.core.utilities.UtilFormat;
+import me.mykindos.betterpvp.core.utilities.UtilLocation;
 import me.mykindos.betterpvp.core.utilities.UtilMessage;
 import me.mykindos.betterpvp.core.utilities.UtilPlayer;
 import me.mykindos.betterpvp.core.utilities.events.EntityProperty;
 import me.mykindos.betterpvp.core.world.blocks.WorldBlockHandler;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.Particle;
 import org.bukkit.Sound;
 import org.bukkit.block.Block;
-import org.bukkit.block.BlockFace;
-import org.bukkit.block.data.Levelled;
 import org.bukkit.entity.Player;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Singleton
@@ -41,9 +46,7 @@ public class ArcticArmour extends ActiveToggleSkill implements EnergySkill {
     private int radiusIncreasePerLevel;
     private double baseDuration;
     private double durationIncreasePerLevel;
-
     private int resistanceStrength;
-
     private int slownessStrength;
 
     @Inject
@@ -97,79 +100,94 @@ public class ArcticArmour extends ActiveToggleSkill implements EnergySkill {
 
     @UpdateEvent(delay = 125)
     public void snowAura() {
-
         Iterator<UUID> iterator = active.iterator();
         while (iterator.hasNext()) {
             UUID uuid = iterator.next();
             Player player = Bukkit.getPlayer(uuid);
-            if (player != null) {
-
-                int level = getLevel(player);
-
-                if (level <= 0) {
-                    iterator.remove();
-                } else if (!championsManager.getEnergy().use(player, getName(), getEnergy(level) / 2, true)) {
-                    iterator.remove();
-                } else if (championsManager.getEffects().hasEffect(player, EffectType.SILENCE)) {
-                    iterator.remove();
-                } else {
-
-                    int distance = getRadius(level);
-                    HashMap<Block, Double> blocks = UtilBlock.getInRadius(player.getLocation(), distance);
-
-                    player.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 20, resistanceStrength));
-                    championsManager.getEffects().addEffect(player, EffectType.RESISTANCE, resistanceStrength + 1, 1000);
-                    // Apply ice effect
-                    convertWaterToIce(player, getDuration(level), distance);
-
-                    player.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 20, 0));
-                    championsManager.getEffects().addEffect(player, EffectType.RESISTANCE, 1, 1000);
-
-                    for (var data : UtilPlayer.getNearbyPlayers(player, distance)) {
-                        Player target = data.getKey();
-                        boolean friendly = data.getValue() == EntityProperty.FRIENDLY;
-                        if (friendly) {
-                            target.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 20, resistanceStrength));
-                            championsManager.getEffects().addEffect(target, EffectType.RESISTANCE, resistanceStrength + 1, 1000);
-                        } else {
-                            target.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 20, slownessStrength));
-                        }
-                    }
-
-                    if (UtilBlock.isGrounded(player)) {
-                        for (Block block : blocks.keySet()) {
-                            if (block.getLocation().getY() <= player.getLocation().getY()) {
-                                Block relDown = block.getRelative(BlockFace.DOWN);
-                                if (relDown.getType() != Material.SNOW && relDown.getType() != Material.AIR
-                                        && UtilBlock.shouldPlaceSnowOn(relDown)) {
-                                    if (block.getType() == Material.AIR || block.getType() == Material.SNOW) {
-                                        blockHandler.addRestoreBlock(block, Material.SNOW, (long) getDuration(level) * 1000);
-                                    }
-
-                                }
-                            }
-                        }
-                    }
-                }
-            } else {
+            if (player == null) {
                 iterator.remove();
+                continue;
             }
-        }
 
+            int level = getLevel(player);
+            final int distance = getRadius(level);
+            if (level <= 0) {
+                iterator.remove();
+                continue;
+            }
+
+            if (!championsManager.getEnergy().use(player, getName(), getEnergy(level) / 2, true)) {
+                iterator.remove();
+                continue;
+            }
+
+            // Apply resistance and slow effects
+            final List<KeyValue<Player, EntityProperty>> nearby = UtilPlayer.getNearbyPlayers(player, distance);
+            nearby.add(new KeyValue<>(player, EntityProperty.FRIENDLY));
+            for (KeyValue<Player, EntityProperty> nearbyEnt : nearby) {
+                final Player target = nearbyEnt.getKey();
+                final boolean friendly = nearbyEnt.getValue() == EntityProperty.FRIENDLY;
+
+                if (friendly) {
+                    target.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 20, resistanceStrength));
+                    championsManager.getEffects().addEffect(target, EffectType.RESISTANCE, resistanceStrength + 1, 1000);
+                } else {
+                    target.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 20, slownessStrength));
+                }
+            }
+
+            // Apply cue effects
+            // Spin particles around the player in the radius
+            final int angle = (int) ((System.currentTimeMillis() / 10) % 360);
+            playEffects(player, distance, -angle);
+            playEffects(player, distance, angle);
+            playEffects(player, distance, -angle + 180);
+            playEffects(player, distance, angle + 180);
+
+            convertWaterToIce(player, getDuration(level), distance);
+        }
+    }
+
+    private void playEffects(final Player player, float radius, float angle) {
+        final Location reference = player.getLocation();
+        reference.setPitch(0);
+
+        final Location relative = UtilLocation.fromAngleDistance(reference, radius, angle);
+        final Optional<Location> closestSurface = UtilLocation.getClosestSurfaceBlock(relative, 3, true);
+        closestSurface.ifPresent(loc -> loc.add(0, 2.2, 0));
+        final Location result = closestSurface.orElse(relative.add(0, 1.2, 0));
+        Particle.CLOUD.builder().extra(0).location(result).receivers(60).spawn();
     }
 
     private void convertWaterToIce(Player player, double duration, int radius) {
-        HashMap<Block, Double> blocks = UtilBlock.getInRadius(player.getLocation(), radius);
-        for (Block block : blocks.keySet()) {
-            if (block.getLocation().getY() <= player.getLocation().getY()) {
-                if (block.getType() == Material.WATER) {
-                    // Check if it's a water source block
-                    if (block.getBlockData() instanceof Levelled water) {
-                        if (water.getLevel() == 0) {
-                            blockHandler.addRestoreBlock(block, Material.ICE, (long) duration * 1000);
-                        }
-                    }
-                }
+        // Sort by height descending
+        final HashMap<Block, Double> inRadius = UtilBlock.getInRadius(player.getLocation(), radius);
+        Collection<Block> blocks = inRadius.keySet().stream()
+                .sorted((b1, b2) -> b2.getLocation().getBlockY() - b1.getLocation().getBlockY())
+                .toList();
+
+        for (Block block : blocks) {
+            if (block.getLocation().getY() > player.getLocation().getY()) {
+                continue;
+            }
+
+            final boolean water = UtilBlock.isWater(block);
+            if (!water && block.getType() != Material.ICE) {
+                continue;
+            }
+
+            final Block top = block.getRelative(0, 1, 0);
+            if (UtilBlock.isWater(top)) {
+                continue;
+            }
+
+            final long expiryOffset = (long) (100 * (inRadius.get(block) * radius));
+            final long delay = (long) Math.pow((1 - inRadius.get(block)) * radius, 2);
+            blockHandler.scheduleRestoreBlock(block, Material.ICE, delay, ((long) duration * 1000) + expiryOffset, false);
+
+            final double chance = Math.random();
+            if (chance < 0.025) {
+                Particle.SNOWFLAKE.builder().extra(0).location(block.getLocation()).receivers(60).spawn();
             }
         }
     }
@@ -178,7 +196,6 @@ public class ArcticArmour extends ActiveToggleSkill implements EnergySkill {
     public SkillType getType() {
         return SkillType.PASSIVE_B;
     }
-
 
     @Override
     public float getEnergy(int level) {
--- core/src/main/java/me/mykindos/betterpvp/core/world/blocks/RestoreBlockListener.java
+++ core/src/main/java/me/mykindos/betterpvp/core/world/blocks/RestoreBlockListener.java
@@ -1,19 +1,27 @@
 package me.mykindos.betterpvp.core.world.blocks;
 
+import com.google.common.graph.GraphBuilder;
+import com.google.common.graph.MutableGraph;
 import com.google.inject.Inject;
 import me.mykindos.betterpvp.core.framework.updater.UpdateEvent;
 import me.mykindos.betterpvp.core.listener.BPvPListener;
+import me.mykindos.betterpvp.core.utilities.UtilBlock;
 import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockFadeEvent;
+import org.bukkit.event.block.BlockFromToEvent;
 
-import java.util.ListIterator;
+import java.util.Iterator;
+import java.util.Map;
 
+@SuppressWarnings("UnstableApiUsage")
 @BPvPListener
 public class RestoreBlockListener implements Listener {
 
+    private final MutableGraph<Block> collidingBlocks = GraphBuilder.undirected().build();
     private final WorldBlockHandler blockHandler;
 
     @Inject
@@ -28,20 +36,53 @@ public class RestoreBlockListener implements Listener {
         if (blockHandler.isRestoreBlock(block)) {
             event.setCancelled(true);
         }
+    }
 
+    @EventHandler
+    public void onPhysics(BlockFromToEvent event) {
+        if (!UtilBlock.isWater(event.getBlock()) && !UtilBlock.isWater(event.getToBlock())) {
+            return; // Not water
+        }
+
+        final Block block = event.getBlock();
+        final boolean colliding = addCollidingRestoreBlock(event.getBlock(), block.getRelative(BlockFace.UP))
+                || addCollidingRestoreBlock(event.getBlock(), block.getRelative(BlockFace.DOWN))
+                || addCollidingRestoreBlock(event.getBlock(), block.getRelative(BlockFace.NORTH))
+                || addCollidingRestoreBlock(event.getBlock(), block.getRelative(BlockFace.SOUTH))
+                || addCollidingRestoreBlock(event.getBlock(), block.getRelative(BlockFace.EAST))
+                || addCollidingRestoreBlock(event.getBlock(), block.getRelative(BlockFace.WEST));
+
+        if (colliding) {
+            event.setCancelled(true);
+        }
     }
 
     @UpdateEvent
     public void processBlocks() {
-        ListIterator<RestoreBlock> restoreBlockListIterator = blockHandler.getRestoreBlocks().listIterator();
+        Iterator<RestoreBlock> restoreBlockListIterator = blockHandler.getRestoreBlocks().values().iterator();
         while (restoreBlockListIterator.hasNext()) {
             RestoreBlock next = restoreBlockListIterator.next();
             if (System.currentTimeMillis() >= next.getExpire()) {
-                next.restore();
                 restoreBlockListIterator.remove();
+                next.restore();
+
+                // If the block is colliding with another block, and it's not a restore block, attempt to update it
+                if (this.collidingBlocks.nodes().contains(next.getBlock())) {
+                    this.collidingBlocks.adjacentNodes(next.getBlock()).forEach(this::attemptUpdate);
+                }
             }
         }
 
+        // We do this to avoid scheduling tons of future tasks at once
+        // Instead, we batch them
+        final Iterator<Map.Entry<Runnable, Long>> scheduledIterator = blockHandler.getScheduledBlocks().entrySet().iterator();
+        while (scheduledIterator.hasNext()) {
+            final Map.Entry<Runnable, Long> next = scheduledIterator.next();
+            if (System.currentTimeMillis() >= next.getValue()) {
+                next.getKey().run();
+                scheduledIterator.remove();
+            }
+        }
     }
 
     @EventHandler
@@ -50,5 +91,29 @@ public class RestoreBlockListener implements Listener {
         if (blockHandler.isRestoreBlock(block)) {
             event.setCancelled(true);
         }
+    }
+
+    // Attempt to update a block if it's not colliding with any restore blocks
+    private void attemptUpdate(Block block) {
+        if (this.blockHandler.isRestoreBlock(block)) {
+            return; // Return if the block is a restore block
+        }
+
+        if (this.collidingBlocks.nodes().contains(block) && !this.collidingBlocks.adjacentNodes(block).isEmpty()) {
+            return; // Return if the block is colliding with anything
+        }
+
+        block.getState().update(false, true);
+        this.collidingBlocks.removeNode(block);
+    }
+
+    // Attempt to add a colliding restore block to the graph
+    // Only if the colliding block is a restore block
+    private boolean addCollidingRestoreBlock(Block block, Block colliding) {
+        if (this.blockHandler.isRestoreBlock(colliding)) {
+            this.collidingBlocks.putEdge(block, colliding);
+            return true;
+        }
+        return false;
     }
 }
