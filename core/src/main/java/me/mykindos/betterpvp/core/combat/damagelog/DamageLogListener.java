dbf7a0c6c60b5661b128caa93b30aa58f6f3bbf9
Braulio <bchamerry@gmail.com>
2024-01-13 21:31:23 -0800

    #348 - Death summary

--- core/src/main/java/me/mykindos/betterpvp/core/combat/damagelog/DamageLogListener.java
+++ core/src/main/java/me/mykindos/betterpvp/core/combat/damagelog/DamageLogListener.java
@@ -4,18 +4,23 @@ import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import me.mykindos.betterpvp.core.framework.updater.UpdateEvent;
 import me.mykindos.betterpvp.core.listener.BPvPListener;
+import me.mykindos.betterpvp.core.utilities.UtilMessage;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.event.ClickCallback;
+import net.kyori.adventure.text.event.ClickEvent;
+import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
 
 @Singleton
 @BPvPListener
 public class DamageLogListener implements Listener {
 
-    private final DamageLogManager damageLogManager;
-
     @Inject
-    public DamageLogListener(DamageLogManager damageLogManager) {
-        this.damageLogManager = damageLogManager;
-    }
+    private DamageLogManager damageLogManager;
 
     @UpdateEvent(delay = 100, isAsync = true)
     public void processDamageLogs() {
@@ -24,5 +29,25 @@ public class DamageLogListener implements Listener {
         });
 
         damageLogManager.getObjects().entrySet().removeIf(entry -> entry.getValue().isEmpty());
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void onDeath(PlayerDeathEvent event) {
+        final long deathTime = System.currentTimeMillis();
+        final ClickEvent clickEvent = ClickEvent.callback(
+                audience -> damageLogManager.showDeathSummary(deathTime, (Player) audience),
+                ClickCallback.Options.builder().uses(1).build()
+        );
+
+        final Component component = Component.text("Click")
+                .appendSpace()
+                .append(Component.text("here").color(NamedTextColor.WHITE))
+                .appendSpace()
+                .append(Component.text("to view your death summary."))
+                .clickEvent(clickEvent);
+
+        final Component hover = Component.text("What killed you?");
+
+        UtilMessage.simpleMessage(event.getPlayer(), "Death", component, hover);
     }
 }
