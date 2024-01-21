5eeb692a166d6c7107d6b09ac3520b9d5e1db147
StudentAlleg <studentalleg-pilot@yahoo.com>
2024-01-21 01:36:44 -0800

    #110 - WIP change HasteFieldsPerk to only function while holding a pickaxe

--- clans/src/main/java/me/mykindos/betterpvp/clans/progression/perks/HasteFieldsPerk.java
+++ clans/src/main/java/me/mykindos/betterpvp/clans/progression/perks/HasteFieldsPerk.java
@@ -9,18 +9,24 @@ import me.mykindos.betterpvp.clans.clans.ClanManager;
 import me.mykindos.betterpvp.clans.clans.events.ClanChangeTerritoryEvent;
 import me.mykindos.betterpvp.clans.progression.ProgressionAdapter;
 import me.mykindos.betterpvp.core.config.ExtendedYamlConfiguration;
+import me.mykindos.betterpvp.core.utilities.UtilItem;
+import me.mykindos.betterpvp.core.utilities.UtilMessage;
 import me.mykindos.betterpvp.core.utilities.UtilServer;
 import me.mykindos.betterpvp.core.utilities.model.ConfigAccessor;
 import me.mykindos.betterpvp.progression.model.ProgressionPerk;
 import me.mykindos.betterpvp.progression.model.ProgressionTree;
 import me.mykindos.betterpvp.progression.model.stats.ProgressionData;
 import me.mykindos.betterpvp.progression.tree.mining.Mining;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerItemHeldEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 
@@ -63,42 +69,69 @@ public class HasteFieldsPerk implements Listener, ConfigAccessor, ProgressionPer
     }
 
     @EventHandler
+    public void onHoldPickaxe(PlayerItemHeldEvent event) {
+        if (!enabled) return;
+        if(!mining.isEnabled()) return;
+        Player player = event.getPlayer();
+        ItemStack itemStack = player.getInventory().getItem(event.getNewSlot());
+        Material material = itemStack == null ? null : itemStack.getType();
+        processHaste(player, material, manager.getClanByLocation(player.getLocation()).orElse(null));
+    }
+
+    @EventHandler
     public void onEnterTerritory(ClanChangeTerritoryEvent event) {
         if (!enabled) return;
         if(!mining.isEnabled()) return;
         Player player = event.getPlayer();
-        //no change in territory we care about
-        if (isLakeOrField(event.getFromClan()) == isLakeOrField(event.getToClan())) {
-            return;
-        }
-        if (isLakeOrField(event.getFromClan()) && !isLakeOrField(event.getToClan())) {
-            player.removePotionEffect(PotionEffectType.FAST_DIGGING);
-            return;
-        }
+        processHaste(player, player.getInventory().getItemInMainHand().getType(), event.getToClan());
+    }
 
-        mining.hasPerk(player, getClass()).whenComplete((hasPerk, throwable) -> {
-        if (hasPerk) {
-            mining.getLevel(player).whenComplete((level, throwable1) -> {
-                for (int i = hasteLevels.size() - 1; i >= 0; i--) {
-                    if (level >= hasteLevels.get(i)) {
-                        int I = i;
-                        UtilServer.runTask(clans, () -> player.addPotionEffect(new PotionEffect(PotionEffectType.FAST_DIGGING, -1, I)));
-                        break;
-                    }
-                }
-            }).exceptionally(throwable1 -> {
-                log.error("Failed to check if player " + player.getName() + " has a level ", throwable);
-                return null;
-            });
+    private void processHaste(Player player, @Nullable Material itemMaterial, @Nullable Clan clan) {
+        if (clan == null) {
+            removeHaste(player);
+            return;
         }
+        if (itemMaterial == null) {
+            removeHaste(player);
+            return;
+        }
+        mining.hasPerk(player, getClass()).whenComplete((hasPerk, throwable) -> {
+            if (hasPerk) {
+                if (!UtilItem.isPickaxe(itemMaterial)) {
+                    removeHaste(player);
+                    return;
+                }
+                if (manager.isFields(clan) || manager.isLake(clan)) {
+                    removeHaste(player);
+                    return;
+                }
+                mining.getLevel(player).whenComplete((level, throwable1) -> {
+                    for (int i = hasteLevels.size() - 1; i >= 0; i--) {
+                        if (level >= hasteLevels.get(i)) {
+                            addHaste(player, i);
+                            break;
+                        }
+                    }
+                }).exceptionally(throwable1 -> {
+                    log.error("Failed to check if player " + player.getName() + " has a level ", throwable);
+                    return null;
+                });
+            }
         }).exceptionally(throwable -> {
             log.error("Failed to check if player " + player.getName() + " has perk " + getName(), throwable);
             return null;
         });
     }
 
-    private boolean isLakeOrField(Clan clan) {
-        return manager.isFields(clan) || manager.isLake(clan);
+    private void removeHaste(Player player) {
+        if (!player.hasPotionEffect(PotionEffectType.FAST_DIGGING)) return;
+        UtilServer.runTask(clans, () -> player.removePotionEffect(PotionEffectType.FAST_DIGGING));
+        UtilMessage.message(player, "Mining", UtilMessage.deserialize("Your mining vigor has diminished"));
+    }
+
+    private void addHaste(Player player, int level) {
+        UtilServer.runTask(clans, () -> player.addPotionEffect(new PotionEffect(PotionEffectType.FAST_DIGGING, -1, level)));
+        UtilMessage.message(player, "Mining", UtilMessage.deserialize("Your mining vigor has increased"));
     }
 
     @Override
