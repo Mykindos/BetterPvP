65f2e8d5a0aae8295702655ec3e3e523008ec165
Braulio <bchamerry@gmail.com>
2024-01-23 11:39:29 -0800

    #388 - Add block-like cannon placement

--- clans/src/main/java/me/mykindos/betterpvp/clans/weapons/impl/cannon/CannonWeapon.java
+++ clans/src/main/java/me/mykindos/betterpvp/clans/weapons/impl/cannon/CannonWeapon.java
@@ -11,17 +11,24 @@ import me.mykindos.betterpvp.core.combat.weapon.types.CooldownWeapon;
 import me.mykindos.betterpvp.core.combat.weapon.types.InteractWeapon;
 import me.mykindos.betterpvp.core.framework.adapter.Compatibility;
 import me.mykindos.betterpvp.core.framework.adapter.PluginAdapter;
-import me.mykindos.betterpvp.core.utilities.UtilBlock;
 import me.mykindos.betterpvp.core.utilities.UtilMessage;
+import me.mykindos.betterpvp.core.utilities.model.SoundEffect;
+import org.bukkit.FluidCollisionMode;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.util.RayTraceResult;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 @PluginAdapter("ModelEngine")
 @Singleton
 public class CannonWeapon extends Weapon implements InteractWeapon, CooldownWeapon {
 
     private final CannonManager cannonManager;
+    private final Map<UUID, Location> cannonLocations = new HashMap<>();
 
     @Inject
     public CannonWeapon(CannonManager cannonManager) {
@@ -31,8 +38,7 @@ public class CannonWeapon extends Weapon implements InteractWeapon, CooldownWeap
 
     @Override
     public void activate(Player player) {
-        final Location cannonLocation = player.getLocation();
-        cannonLocation.setPitch(0f);
+        final Location cannonLocation = cannonLocations.remove(player.getUniqueId());
         final Cannon cannon = this.cannonManager.spawn(player.getUniqueId(), cannonLocation);
         final CannonPlaceEvent event = new CannonPlaceEvent(cannon, cannonLocation, player);
         event.callEvent();
@@ -56,9 +62,17 @@ public class CannonWeapon extends Weapon implements InteractWeapon, CooldownWeap
         if (!Compatibility.MODEL_ENGINE) {
             event.cancel("Cannons are not supported on this server. There are missing dependencies.");
             UtilMessage.message(player, "Combat", "Cannons are not supported on this server. <red>Please contact an administrator.");
-        } else if (!UtilBlock.isGrounded(player)) {
-            event.cancel("Not Grounded");
-            UtilMessage.message(player, "Combat", "You must be standing on the ground to place a cannon.");
+            SoundEffect.LOW_PITCH_PLING.play(player);
+        } else {
+            final RayTraceResult rayTraceResult = player.rayTraceBlocks(4.5, FluidCollisionMode.ALWAYS);
+            if (rayTraceResult == null || rayTraceResult.getHitBlock() == null) {
+                event.cancel("No block in sight.");
+            } else {
+                final Location location = rayTraceResult.getHitPosition().toLocation(player.getWorld());
+                location.setYaw(player.getLocation().getYaw());
+                location.setPitch(0f);
+                cannonLocations.put(player.getUniqueId(), location);
+            }
         }
         event.callEvent();
         return !event.isCancelled();
