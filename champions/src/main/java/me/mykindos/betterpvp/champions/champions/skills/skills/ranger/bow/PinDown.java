bbdb2048070c258ea253fb82d4880e0f0c0ee7b5
ezrn <145294235+ezrn@users.noreply.github.com>
2024-01-26 21:48:28 -0800

    arrow particles and misc fixes (#423)
    * fissure SUCKSSSSS
    
    * fissure SUCKSSSSS
    
    * finish arrows
    
    * finish arrows 2
    
    * added volley damage to config
    
    * fix healing shot cd and make it cleanse
    
    * removed print statement

--- champions/src/main/java/me/mykindos/betterpvp/champions/champions/skills/skills/ranger/bow/PinDown.java
+++ champions/src/main/java/me/mykindos/betterpvp/champions/champions/skills/skills/ranger/bow/PinDown.java
@@ -1,5 +1,6 @@
 package me.mykindos.betterpvp.champions.champions.skills.skills.ranger.bow;
 
+import com.destroystokyo.paper.ParticleBuilder;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import me.mykindos.betterpvp.champions.Champions;
@@ -17,6 +18,7 @@ import me.mykindos.betterpvp.core.utilities.UtilFormat;
 import me.mykindos.betterpvp.core.utilities.UtilInventory;
 import me.mykindos.betterpvp.core.utilities.UtilMessage;
 import org.bukkit.Effect;
+import org.bukkit.GameMode;
 import org.bukkit.Material;
 import org.bukkit.Particle;
 import org.bukkit.entity.Arrow;
@@ -91,7 +93,9 @@ public class PinDown extends Skill implements InteractSkill, CooldownSkill, List
 
     @Override
     public void activate(Player player, int level) {
-        UtilInventory.remove(player, Material.ARROW, 1);
+        if (player.getGameMode() != GameMode.CREATIVE) {
+            UtilInventory.remove(player, Material.ARROW, 1);
+        }
 
         Arrow proj = player.launchProjectile(Arrow.class);
         proj.setShooter(player);
@@ -117,13 +121,14 @@ public class PinDown extends Skill implements InteractSkill, CooldownSkill, List
                 return true;
             }
 
-            Particle.REDSTONE.builder()
+            new ParticleBuilder(Particle.CRIT)
                     .location(arrow.getLocation())
-                    .color(128, 0, 128)
-                    .count(3)
+                    .count(1)
+                    .offset(0, 0, 0)
                     .extra(0)
                     .receivers(60)
                     .spawn();
+
             return false;
         });
     }
