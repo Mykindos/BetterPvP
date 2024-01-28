83d97cbf8104c4221027c3b7080e0c116920445b
StudentAlleg <studentalleg-pilot@yahoo.com>
2024-01-28 02:17:28 -0800

    #447 - Give feedback for ActiveToggleSkills being cancelled due to an effect.

--- champions/src/main/java/me/mykindos/betterpvp/champions/champions/skills/types/ActiveToggleSkill.java
+++ champions/src/main/java/me/mykindos/betterpvp/champions/champions/skills/types/ActiveToggleSkill.java
@@ -5,9 +5,8 @@ import me.mykindos.betterpvp.champions.champions.ChampionsManager;
 import me.mykindos.betterpvp.champions.champions.skills.Skill;
 import me.mykindos.betterpvp.core.effects.EffectType;
 import me.mykindos.betterpvp.core.effects.events.EffectReceiveEvent;
-import me.mykindos.betterpvp.core.framework.updater.UpdateEvent;
 import me.mykindos.betterpvp.core.utilities.UtilBlock;
-import me.mykindos.betterpvp.core.utilities.UtilEntity;
+import me.mykindos.betterpvp.core.utilities.UtilMessage;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -44,21 +43,30 @@ public abstract class ActiveToggleSkill extends Skill implements ToggleSkill, Li
     @EventHandler
     public void onCustomEffect(EffectReceiveEvent event) {
         if ((event.getTarget() instanceof Player player)) {
-            if (!canUseWhileSilenced() && (event.getEffect().getEffectType() == EffectType.SILENCE)) {
-                cancel(player);
-            }
-            if (!canUseWhileLevitating() && (event.getEffect().getEffectType() == EffectType.LEVITATION)) {
-                cancel(player);
-            }
-            if (!canUseWhileStunned() && (event.getEffect().getEffectType() == EffectType.STUN)) {
-                cancel(player);
+            if (hasSkill(player) && active.contains(player.getUniqueId())) {
+                int level = getLevel(player);
+                if (!canUseWhileSilenced() && (event.getEffect().getEffectType() == EffectType.SILENCE)) {
+                    UtilMessage.message(player, "Champions", UtilMessage.deserialize("<green>%s %s</green> was cancelled because you were <white>silenced</white>.", getName(), level));
+                    cancel(player);
+                }
+                if (!canUseWhileLevitating() && (event.getEffect().getEffectType() == EffectType.LEVITATION)) {
+                    UtilMessage.message(player, "Champions", UtilMessage.deserialize("<green>%s %s</green> was cancelled because you are now <white>levitating</white>.", getName(), level));
+                    cancel(player);
+                }
+                if (!canUseWhileStunned() && (event.getEffect().getEffectType() == EffectType.STUN)) {
+                    UtilMessage.message(player, "Champions", UtilMessage.deserialize("<green>%s %s</green> was cancelled because you were <white>stunned</white>.", getName(), level));
+                    cancel(player);
+                }
             }
         }
     }
 
     @EventHandler
     public void onEnterWater(PlayerMoveEvent event) {
-        if (UtilBlock.isInWater(event.getPlayer()) && !canUseInLiquid()) {
+        Player player = event.getPlayer();
+        if (hasSkill(player) && active.contains(player.getUniqueId()) && UtilBlock.isInWater(player) && !canUseInLiquid()) {
+            int level = getLevel(player);
+            UtilMessage.message(player, "Champions", UtilMessage.deserialize("<green>%s %s</green> was cancelled because you entered water.", getName(), level));
             cancel(event.getPlayer());
         }
     }
