f268158fd199957f42b9fbb1370887e87b5d1915
Mykindos <5389944+Mykindos@users.noreply.github.com>
2024-02-01 21:34:21 +1100

    Display % damage reduction on individual armour pieces

---
+++ core/src/main/java/me/mykindos/betterpvp/core/combat/armour/ArmourListener.java
@@ -0,0 +1,31 @@
+package me.mykindos.betterpvp.core.combat.armour;
+
+import me.mykindos.betterpvp.core.framework.events.items.ItemUpdateLoreEvent;
+import me.mykindos.betterpvp.core.listener.BPvPListener;
+import me.mykindos.betterpvp.core.utilities.UtilItem;
+import me.mykindos.betterpvp.core.utilities.UtilMessage;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+@Singleton
+@BPvPListener
+public class ArmourListener implements Listener {
+
+    private final ArmourManager armourManager;
+
+    @Inject
+    public ArmourListener(ArmourManager armourManager) {
+        this.armourManager = armourManager;
+    }
+
+    @EventHandler
+    public void onLoreUpdate(ItemUpdateLoreEvent event) {
+        if(!UtilItem.isArmour(event.getItemStack().getType())) return;
+        event.getItemLore().clear();
+        event.getItemLore().add(UtilMessage.deserialize("Damage reduction: <yellow>" + armourManager.getReductionForItem(event.getItemStack()) + "%"));
+    }
+
+}
