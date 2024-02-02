58db4259fbc0645e980fd3bb46a841032d354a00
StudentAlleg <studentalleg-pilot@yahoo.com>
2024-02-01 18:02:57 -0800

    #486 - show class damage reduction and effective health in build menu

--- champions/src/main/java/me/mykindos/betterpvp/champions/champions/builds/menus/buttons/ClassSelectionButton.java
+++ champions/src/main/java/me/mykindos/betterpvp/champions/champions/builds/menus/buttons/ClassSelectionButton.java
@@ -4,8 +4,11 @@ import me.mykindos.betterpvp.champions.champions.builds.BuildManager;
 import me.mykindos.betterpvp.champions.champions.builds.GamerBuilds;
 import me.mykindos.betterpvp.champions.champions.builds.menus.BuildMenu;
 import me.mykindos.betterpvp.champions.champions.skills.SkillManager;
+import me.mykindos.betterpvp.core.combat.armour.ArmourManager;
 import me.mykindos.betterpvp.core.components.champions.Role;
 import me.mykindos.betterpvp.core.menu.Windowed;
+import me.mykindos.betterpvp.core.utilities.UtilFormat;
+import me.mykindos.betterpvp.core.utilities.UtilMessage;
 import me.mykindos.betterpvp.core.utilities.model.SoundEffect;
 import me.mykindos.betterpvp.core.utilities.model.item.ItemView;
 import net.kyori.adventure.text.Component;
@@ -17,6 +20,8 @@ import org.bukkit.inventory.ItemFlag;
 import org.jetbrains.annotations.NotNull;
 import xyz.xenondevs.invui.item.impl.SimpleItem;
 
+import java.util.List;
+
 public class ClassSelectionButton extends SimpleItem {
 
     private final BuildManager buildManager;
@@ -24,9 +29,11 @@ public class ClassSelectionButton extends SimpleItem {
     private final SkillManager skillManager;
     private final Windowed parent;
 
-    public ClassSelectionButton(BuildManager buildManager, SkillManager skillManager, Role role, Windowed parent) {
+    public ClassSelectionButton(BuildManager buildManager, SkillManager skillManager, Role role, ArmourManager armorManager, Windowed parent) {
         super(ItemView.builder().material(role.getChestplate())
                 .displayName(Component.text(role.getName(), role.getColor(), TextDecoration.BOLD))
+                .lore(List.of(UtilMessage.deserialize("Class Damage Reduction: <yellow>" + armorManager.getReductionForArmourSet(role.getChestplate().name().replace("_CHESTPLATE", "")) + "%"),
+                              UtilMessage.deserialize("Effective Health: <red>" + UtilFormat.formatNumber(20 * ( 1 + (armorManager.getReductionForArmourSet(role.getChestplate().name().replace("_CHESTPLATE", ""))/100)), 1))))
                 .flag(ItemFlag.HIDE_ATTRIBUTES)
                 .build());
         this.buildManager = buildManager;
