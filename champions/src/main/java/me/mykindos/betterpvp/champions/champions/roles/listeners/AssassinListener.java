6b44957ad535f4ae0a5363488174112118f7906f
zande <zanderaguirre@icloud.com>
2024-02-06 12:14:29 -0800

    made assassin take fall damage and changed armor values

--- champions/src/main/java/me/mykindos/betterpvp/champions/champions/roles/listeners/AssassinListener.java
+++ champions/src/main/java/me/mykindos/betterpvp/champions/champions/roles/listeners/AssassinListener.java
@@ -35,17 +35,6 @@ public class AssassinListener implements Listener {
         this.roleManager = roleManager;
     }
 
-    @EventHandler
-    public void onFallDamage(CustomDamageEvent event) {
-        if (event.getCause() != EntityDamageEvent.DamageCause.FALL) return;
-        if (!(event.getDamagee() instanceof Player damagee)) return;
-
-        if (roleManager.hasRole(damagee, Role.ASSASSIN)) {
-            event.cancel("Feather falling");
-        }
-
-    }
-
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onAssassinKnockback(CustomDamageEvent event) {
         if (event.isCancelled()) return;
@@ -66,7 +55,6 @@ public class AssassinListener implements Listener {
                 }
             }
         }
-
     }
 
     @UpdateEvent(delay = 500)
