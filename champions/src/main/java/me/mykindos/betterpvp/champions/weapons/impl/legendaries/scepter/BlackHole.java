09a930b4845ea275cd22e498250cc7003ca85e0d
Braulio <40250304+braulio-dev@users.noreply.github.com>
2024-02-19 15:48:52 -0800

    Feat/meridian beam (#605)
    * Fix velocity fall damage
    
    * Add Meridian Beam, fix couple of right-click bugs

--- champions/src/main/java/me/mykindos/betterpvp/champions/weapons/impl/legendaries/scepter/BlackHole.java
+++ champions/src/main/java/me/mykindos/betterpvp/champions/weapons/impl/legendaries/scepter/BlackHole.java
@@ -1,16 +1,12 @@
 package me.mykindos.betterpvp.champions.weapons.impl.legendaries.scepter;
 
-import com.google.common.base.Preconditions;
 import lombok.Getter;
-import lombok.Setter;
 import me.mykindos.betterpvp.champions.champions.skills.data.ChargeData;
-import me.mykindos.betterpvp.core.utilities.UtilBlock;
 import me.mykindos.betterpvp.core.utilities.UtilLocation;
 import me.mykindos.betterpvp.core.utilities.UtilTime;
 import me.mykindos.betterpvp.core.utilities.math.VectorLine;
 import me.mykindos.betterpvp.core.utilities.model.SoundEffect;
 import org.bukkit.Color;
-import org.bukkit.FluidCollisionMode;
 import org.bukkit.Location;
 import org.bukkit.Particle;
 import org.bukkit.Sound;
@@ -21,57 +17,44 @@ import org.bukkit.util.RayTraceResult;
 import org.bukkit.util.Vector;
 
 import java.util.List;
-import java.util.Optional;
 
 @Getter
-public class BlackHole {
+public class BlackHole extends ScepterProjectile {
 
-    private final Player caster;
-    private final double hitboxSize;
-    private final double size;
     private final double pullStrength;
     private final double pullRadius;
     private final double aliveSeconds;
     private final double expandSeconds;
-    private double speed = 1;
-    private Location location;
-    private boolean impacted;
-    private final long creationTime = System.currentTimeMillis();
     private ChargeData chargeData;
     private List<Location> sphere;
-    private long impactTime;
-    private Vector direction;
-    @Setter
-    private boolean markForRemoval;
 
-    public BlackHole(Player caster, final Location location, double hitboxSize, double size, double pullStrength, double pullRadius, double aliveSeconds, double expandSeconds) {
-        this.caster = caster;
-        this.location = location;
-        this.hitboxSize = hitboxSize;
-        this.size = size;
+    public BlackHole(Player caster, final Location location, double hitboxSize, double size, double pullStrength, double pullRadius, double aliveSeconds, double expandSeconds, long expireTime) {
+        super(caster, hitboxSize, size, location, expireTime);
         this.pullStrength = pullStrength;
         this.pullRadius = pullRadius;
         this.aliveSeconds = aliveSeconds;
         this.expandSeconds = expandSeconds;
     }
 
-    public void tick() {
+    @Override
+    protected void onTick() {
         if (!impacted) {
-            final Optional<Location> result = tryImpact();
-            result.ifPresent(this::impact);
-            if (result.isEmpty()) {
-                move();
-            }
-
             // Play travel particles
-            Particle.REDSTONE.builder()
-                    .location(location)
-                    .count(1)
-                    .extra(0.5)
-                    .offset(0.1, 0.1, 0.1)
-                    .data(new Particle.DustOptions(Color.fromRGB(64, 138, 199), 2))
-                    .receivers(60)
-                    .spawn();
+            Location[] line = lastLocation == null  || lastLocation.equals(location)
+                    ? new Location[] { location }
+                    : VectorLine.withStepSize(lastLocation, location, 0.5).toLocations();
+
+            for (Location point : line) {
+                // Play travel particles
+                Particle.REDSTONE.builder()
+                        .location(point)
+                        .count(1)
+                        .extra(0.5)
+                        .offset(0.1, 0.1, 0.1)
+                        .data(new Particle.DustOptions(Color.fromRGB(64, 138, 199), 2))
+                        .receivers(60)
+                        .spawn();
+            }
             return;
         }
 
@@ -130,50 +113,15 @@ public class BlackHole {
         }
     }
 
-    private void move() {
-        if (direction != null) {
-            location = location.add(direction);
-        }
-    }
-
-    private Optional<Location> tryImpact() {
-        if (UtilBlock.solid(location.getBlock()) || UtilTime.elapsed(creationTime, 2_000)) {
-            return Optional.ofNullable(location);
-        }
-
-        final RayTraceResult rayTrace = location.getWorld().rayTrace(location,
-                direction,
-                speed,
-                FluidCollisionMode.NEVER,
-                true,
-                hitboxSize,
-                entity -> entity != caster && entity instanceof LivingEntity);
-        if (rayTrace != null) {
-            return Optional.of(rayTrace.getHitPosition().toLocation(location.getWorld()));
-        }
-
-        return Optional.empty();
-    }
-
-    public void impact(Location location) {
-        Preconditions.checkState(!impacted, "Black hole already impacted");
-        this.location = location;
-        impacted = true;
-        impactTime = System.currentTimeMillis();
-        sphere = UtilLocation.getSphere(location, size, 6);
-        chargeData = new ChargeData((float) (1 / expandSeconds));
-    }
-
+    @Override
     public void redirect(Vector vector) {
-        this.direction = vector.normalize().multiply(speed);
+        super.redirect(vector);
         new SoundEffect(Sound.ENTITY_WARDEN_ATTACK_IMPACT, 0f, 1f).play(location);
     }
 
-    public void setSpeed(double speed) {
-        this.speed = speed;
-        if (direction != null) {
-            direction = direction.normalize().multiply(speed);
-        }
+    @Override
+    protected void onImpact(Location location, RayTraceResult result) {
+        sphere = UtilLocation.getSphere(location, size, 6);
+        chargeData = new ChargeData((float) (1 / expandSeconds));
     }
-
 }
