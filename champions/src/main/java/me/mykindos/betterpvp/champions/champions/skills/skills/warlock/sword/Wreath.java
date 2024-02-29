0989d41a1824a730cab710187bfd17b67f1bee04
ezrn <145294235+ezrn@users.noreply.github.com>
2024-02-29 02:14:31 -0800

    Wreath Rework (#634)
    * wreath
    
    * wreath fixes

--- champions/src/main/java/me/mykindos/betterpvp/champions/champions/skills/skills/warlock/sword/Wreath.java
+++ champions/src/main/java/me/mykindos/betterpvp/champions/champions/skills/skills/warlock/sword/Wreath.java
@@ -6,18 +6,25 @@ import me.mykindos.betterpvp.champions.Champions;
 import me.mykindos.betterpvp.champions.champions.ChampionsManager;
 import me.mykindos.betterpvp.champions.champions.skills.Skill;
 import me.mykindos.betterpvp.champions.champions.skills.data.SkillActions;
-import me.mykindos.betterpvp.champions.champions.skills.types.CooldownSkill;
+import me.mykindos.betterpvp.champions.champions.skills.skills.warlock.data.WreathData;
 import me.mykindos.betterpvp.champions.champions.skills.types.InteractSkill;
+import me.mykindos.betterpvp.core.client.gamer.Gamer;
 import me.mykindos.betterpvp.core.combat.events.CustomDamageEvent;
 import me.mykindos.betterpvp.core.components.champions.Role;
 import me.mykindos.betterpvp.core.components.champions.SkillType;
+import me.mykindos.betterpvp.core.framework.updater.UpdateEvent;
 import me.mykindos.betterpvp.core.listener.BPvPListener;
 import me.mykindos.betterpvp.core.utilities.UtilBlock;
 import me.mykindos.betterpvp.core.utilities.UtilDamage;
 import me.mykindos.betterpvp.core.utilities.UtilEntity;
 import me.mykindos.betterpvp.core.utilities.UtilFormat;
+import me.mykindos.betterpvp.core.utilities.UtilMessage;
 import me.mykindos.betterpvp.core.utilities.UtilPlayer;
 import me.mykindos.betterpvp.core.utilities.UtilServer;
+import me.mykindos.betterpvp.core.utilities.model.display.PermanentComponent;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.Sound;
@@ -25,6 +32,7 @@ import org.bukkit.entity.EntityType;
 import org.bukkit.entity.EvokerFangs;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.potion.PotionEffect;
@@ -33,10 +41,36 @@ import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scheduler.BukkitTask;
 import org.bukkit.util.Vector;
 
+import java.util.Iterator;
+import java.util.Map;
+import java.util.WeakHashMap;
+
 @Singleton
 @BPvPListener
-public class Wreath extends Skill implements InteractSkill, CooldownSkill {
+public class Wreath extends Skill implements InteractSkill, Listener {
 
+    private final WeakHashMap<Player, WreathData> charges = new WeakHashMap<>();
+
+    private final PermanentComponent actionBarComponent = new PermanentComponent(gamer -> {
+        final Player player = gamer.getPlayer();
+
+        // Only display charges in hotbar if holding the weapon
+        if (player == null || !charges.containsKey(player) || !isHolding(player)) {
+            return null; // Skip if not online or not charging
+        }
+
+        final int maxCharges = getMaxCharges(getLevel(player));
+        final int newCharges = charges.get(player).getCharges();
+
+        return Component.text(getName() + " ").color(NamedTextColor.WHITE).decorate(TextDecoration.BOLD)
+                .append(Component.text("\u25A0".repeat(newCharges)).color(NamedTextColor.GREEN))
+                .append(Component.text("\u25A0".repeat(Math.max(0, maxCharges - newCharges))).color(NamedTextColor.RED));
+    });
+
+    private int maxCharges;
+    private int maxChargesIncreasePerLevel;
+    private double rechargeSeconds;
+    private double rechargeSecondsDecreasePerLevel;
     private double baseSlowDuration;
     private double slowDurationIncreasePerLevel;
     private double baseDamage;
@@ -57,7 +91,7 @@ public class Wreath extends Skill implements InteractSkill, CooldownSkill {
     @Override
     public String[] getDescription(int level) {
         return new String[]{
-                "Right click with a Sword to cast",
+                "Right click with a Sword to activate",
                 "",
                 "Release a barrage of teeth that",
                 "deal <val>" + String.format("%.2f", getDamage(level)) + "</val> damage and apply <effect>Slowness " + UtilFormat.getRomanNumeral(slowStrength + 1) + "</effect>",
@@ -65,7 +99,9 @@ public class Wreath extends Skill implements InteractSkill, CooldownSkill {
                 "",
                 "For each enemy hit, restore <val>" + healthPerEnemyHit + "</val> health.",
                 "",
-                "Cooldown: <val>" + getCooldown(level)
+                "Store up to <stat>" + getMaxCharges(level) + "</stat> charges",
+                "",
+                "Gain a charge every: <val>" + getRechargeSeconds(level) + "</val> seconds"
         };
     }
 
@@ -76,6 +112,41 @@ public class Wreath extends Skill implements InteractSkill, CooldownSkill {
 
     public double getSlowDuration(int level) {
         return baseSlowDuration + ((level - 1) * slowDurationIncreasePerLevel);
+    }
+
+    @Override
+    public boolean canUse(Player player) {
+        WreathData wreathData = charges.get(player);
+        if (wreathData != null && wreathData.getCharges() > 0) {
+            return true;
+        }
+
+        UtilMessage.simpleMessage(player, getClassType().getName(), "You don't have any <alt>" + getName() + "</alt> charges.");
+        return false;
+    }
+
+    @Override
+    public void invalidatePlayer(Player player, Gamer gamer) {
+        charges.remove(player);
+        gamer.getActionBar().remove(actionBarComponent);
+    }
+
+    @Override
+    public void trackPlayer(Player player, Gamer gamer) {
+        charges.computeIfAbsent(player, k -> new WreathData());
+        gamer.getActionBar().add(900, actionBarComponent);
+    }
+
+    private void notifyCharges(Player player, int charges) {
+        UtilMessage.simpleMessage(player, getClassType().getName(), "Wreath Charges: <alt2>" + charges);
+    }
+
+    public int getMaxCharges(int level){
+        return maxCharges + ((level - 1) * maxChargesIncreasePerLevel);
+    }
+
+    public double getRechargeSeconds(int level){
+        return rechargeSeconds - ((level - 1) * rechargeSecondsDecreasePerLevel);
     }
 
     @Override
@@ -144,16 +215,53 @@ public class Wreath extends Skill implements InteractSkill, CooldownSkill {
 
     }
 
+    @UpdateEvent(delay = 100)
+    public void recharge() {
+        final Iterator<Map.Entry<Player, WreathData>> iterator = charges.entrySet().iterator();
+        while (iterator.hasNext()) {
+            final Map.Entry<Player, WreathData> entry = iterator.next();
+            final Player player = entry.getKey();
+            final int level = getLevel(player);
+            if (level <= 0) {
+                iterator.remove();
+                continue;
+            }
 
-    @Override
-    public double getCooldown(int level) {
-        return cooldown - ((level - 1) * cooldownDecreasePerLevel);
+            final WreathData data = entry.getValue();
+            final int maxCharges = getMaxCharges(level);
+
+            if (data.getCharges() >= maxCharges) {
+                continue; // skip if already at max charges
+            }
+
+            if (!championsManager.getCooldowns().use(player, getName(), getRechargeSeconds(level), false, true, true)) {
+                continue; // skip if not enough time has passed
+            }
+
+            // add a charge
+            data.addCharge();
+            notifyCharges(player, data.getCharges());
+        }
     }
+
 
     @Override
     public void activate(Player player, int level) {
+        WreathData wreathData = charges.get(player);
+        if (wreathData == null) {
+            return;
+        }
+
+        final int curCharges = wreathData.getCharges();
         processPlayerAction(player, level);
         player.getWorld().playSound(player.getLocation(), Sound.ENTITY_RAVAGER_ATTACK, 2.0f, 1.8f);
+
+        if (curCharges >= getMaxCharges(level)) {
+            championsManager.getCooldowns().use(player, getName(), getRechargeSeconds(level), false, true, true);
+        }
+
+        final int newCharges = curCharges - 1;
+        wreathData.setCharges(newCharges);
     }
 
     @Override
@@ -161,17 +269,17 @@ public class Wreath extends Skill implements InteractSkill, CooldownSkill {
         return SkillActions.RIGHT_CLICK;
     }
 
-
     @Override
     public void loadSkillConfig() {
         baseSlowDuration = getConfig("baseSlowDuration", 2.0, Double.class);
         slowDurationIncreasePerLevel = getConfig("slowDurationIncreasePerLevel", 0.0, Double.class);
-
         baseDamage = getConfig("baseDamage", 4.0, Double.class);
         damageIncreasePerLevel = getConfig("damageIncreasePerLevel", 0.66, Double.class);
-
         healthPerEnemyHit = getConfig("healthPerEnemyHit", 1.0, Double.class);
-
         slowStrength = getConfig("slowStrength", 1, Integer.class);
+        maxCharges = getConfig("maxCharges", 3, Integer.class);
+        maxChargesIncreasePerLevel = getConfig("maxChargesIncreasePerLevel", 0, Integer.class);
+        rechargeSeconds = getConfig("rechargeSeconds", 10.0, Double.class);
+        rechargeSecondsDecreasePerLevel = getConfig("rechargeSecondsDecreasePerLevel", 1.0, Double.class);
     }
 }
