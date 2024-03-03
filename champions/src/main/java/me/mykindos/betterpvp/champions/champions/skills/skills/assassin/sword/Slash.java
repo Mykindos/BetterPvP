7c613969d796c88646b36dc0d4a632802383dfab
Braulio <bchamerry@gmail.com>
2024-03-03 15:30:37 -0800

    Flash and Slash abstraction + fix slash

--- champions/src/main/java/me/mykindos/betterpvp/champions/champions/skills/skills/assassin/sword/Slash.java
+++ champions/src/main/java/me/mykindos/betterpvp/champions/champions/skills/skills/assassin/sword/Slash.java
@@ -10,23 +10,16 @@ import me.mykindos.betterpvp.champions.champions.skills.types.InteractSkill;
 import me.mykindos.betterpvp.core.combat.events.CustomDamageEvent;
 import me.mykindos.betterpvp.core.components.champions.Role;
 import me.mykindos.betterpvp.core.components.champions.SkillType;
-import me.mykindos.betterpvp.core.cooldowns.CooldownManager;
-import me.mykindos.betterpvp.core.effects.EffectType;
-import me.mykindos.betterpvp.core.framework.customtypes.KeyValue;
 import me.mykindos.betterpvp.core.listener.BPvPListener;
-import me.mykindos.betterpvp.core.utilities.UtilBlock;
 import me.mykindos.betterpvp.core.utilities.UtilDamage;
 import me.mykindos.betterpvp.core.utilities.UtilEntity;
+import me.mykindos.betterpvp.core.utilities.UtilFormat;
 import me.mykindos.betterpvp.core.utilities.UtilLocation;
-import me.mykindos.betterpvp.core.utilities.UtilPlayer;
-import me.mykindos.betterpvp.core.utilities.events.EntityProperty;
 import me.mykindos.betterpvp.core.utilities.math.VectorLine;
+import me.mykindos.betterpvp.core.utilities.model.MultiRayTraceResult;
 import org.bukkit.Location;
-import org.bukkit.Material;
 import org.bukkit.Particle;
 import org.bukkit.Sound;
-import org.bukkit.block.Block;
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -34,25 +27,21 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.EntityDamageEvent;
-import org.bukkit.util.BoundingBox;
-import org.bukkit.util.Vector;
+import org.bukkit.util.RayTraceResult;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
-import java.util.Arrays;
-import java.util.List;
 
 @Singleton
 @BPvPListener
 public class Slash extends Skill implements InteractSkill, CooldownSkill, Listener {
 
     private double distance;
-    private double cooldownReductionPerHit;
-    private double perHitReductionPerLevelIncrease;
-    public double damage;
-    public double damageIncreasePerLevel;
-    @Inject
-    private CooldownManager cooldownManager;
+    private double distanceIncreasePerLevel;
+    private double cooldownReduction;
+    private double cooldownReductionPerLevel;
+    private double damage;
+    private double damageIncreasePerLevel;
 
     @Inject
     public Slash(Champions champions, ChampionsManager championsManager) {
@@ -66,15 +55,24 @@ public class Slash extends Skill implements InteractSkill, CooldownSkill, Listen
 
     @Override
     public String[] getDescription(int level) {
-        return new String[]{
+        return new String[] {
                 "Right click with a Sword to activate",
                 "",
-                "Dash forwards <stat>" + distance + "</stat> blocks, dealing <val>" + getDamage(level) + "</val>",
+                "Dash forwards <stat>" + UtilFormat.formatNumber(getDistance(level), 1) + "</stat> blocks, dealing <val>" + UtilFormat.formatNumber(getDamage(level), 1) + "</val>",
                 "damage to anything you pass through",
                 "",
-                "Every hit will reduce the cooldown by <stat>" + cooldownReductionPerHit + "</stat> seconds",
+                "Every hit will reduce the cooldown by <stat>" + UtilFormat.formatNumber(getCooldownDecrease(level), 1) + "</stat> seconds",
                 "",
-                "Cooldown: <val>" + getCooldown(level)};
+                "Cooldown: <val>" + getCooldown(level)
+        };
+    }
+
+    public double getCooldownDecrease(int level) {
+        return cooldownReduction + (cooldownReductionPerLevel * (level - 1));
+    }
+
+    public double getDistance(int level) {
+        return distance + (distanceIncreasePerLevel * (level - 1));
     }
 
     public double getDamage(int level){
@@ -83,97 +81,58 @@ public class Slash extends Skill implements InteractSkill, CooldownSkill, Listen
 
     @Override
     public void activate(Player player, int level) {
-        Location originalLocation = player.getLocation();
-        final Vector direction = player.getEyeLocation().getDirection();
-        Location teleportLocation = originalLocation.clone();
+        final Location originalLocation = player.getLocation();
+        UtilLocation.teleportForward(player, getDistance(level), false, success -> {
+            Particle.SWEEP_ATTACK.builder().location(player.getLocation()).count(1).receivers(30).extra(0).spawn();
+            player.getWorld().playSound(player.getLocation(), Sound.ENTITY_PLAYER_ATTACK_SWEEP, 1.0F, 1.6F);
 
-        final int iterations = (int) Math.ceil(distance / 0.2);
-        for (int i = 1; i <= iterations; i++) {
-            final Vector increment = direction.clone().multiply(0.2 * i);
-            final Location checkLocation = originalLocation.clone().add(increment);
-
-
-            List<KeyValue<LivingEntity, EntityProperty>> nearbyEntities = UtilEntity.getNearbyEntities(player, checkLocation, 1, EntityProperty.ENEMY);
-            for (KeyValue<LivingEntity, EntityProperty> keyValue : nearbyEntities) {
-                LivingEntity entity = keyValue.getKey();
-
-                CustomDamageEvent cde = new CustomDamageEvent(entity, player, null, EntityDamageEvent.DamageCause.CUSTOM, getDamage(level), false, "Slash");
-                UtilDamage.doCustomDamage(cde);
-                cde.setDamageDelay(0);
-                entity.getWorld().playSound(entity.getLocation().add(0, 1, 0), Sound.ENTITY_PLAYER_HURT, 0.2f, 2f);
-                entity.getWorld().playSound(entity.getLocation().add(0, 1, 0), Sound.ITEM_TRIDENT_HIT, 0.2f, 1.5f);
-
-                break;
+            if (!success) {
+                return;
             }
 
-            BoundingBox relativeBoundingBox = UtilLocation.copyAABBToLocation(player.getBoundingBox(), checkLocation);
-
-            final Location blockOnTop = checkLocation.clone().add(0, 1.0, 0);
-            if (wouldCollide(blockOnTop.getBlock(), relativeBoundingBox)) {
-                break;
+            final Location teleportLocation = player.getLocation();
+            final Location lineStart = player.getLocation().add(0.0, player.getHeight() / 2, 0.0);
+            final Location lineEnd = teleportLocation.clone().add(0.0, player.getHeight() / 2, 0.0);
+            final VectorLine line = VectorLine.withStepSize(lineStart, lineEnd, 0.25f);
+            for (Location point : line.toLocations()) {
+                Particle.CRIT.builder().location(point).count(2).receivers(30).extra(0).spawn();
             }
 
-            Location newTeleportLocation = checkLocation;
-            if (wouldCollide(checkLocation.getBlock(), relativeBoundingBox)) {
-                if (!blockOnTop.clone().add(0.0, 1.0, 0.0).getBlock().isPassable()) {
-                    break;
-                }
-
-                final Vector horizontalIncrement = increment.clone().setY(0);
-                final Location frontLocation = originalLocation.clone().add(horizontalIncrement);
-                relativeBoundingBox = UtilLocation.copyAABBToLocation(player.getBoundingBox(), frontLocation);
-                if (wouldCollide(frontLocation.getBlock(), relativeBoundingBox)) {
-                    continue;
-                }
-
-                newTeleportLocation = frontLocation;
-            }
-
-            final Location headBlock = checkLocation.clone().add(0.0, relativeBoundingBox.getHeight(), 0.0);
-            if (wouldCollide(headBlock.getBlock(), relativeBoundingBox)) {
-                break;
-            }
-
-            if (!player.hasLineOfSight(checkLocation) && !player.hasLineOfSight(headBlock)) {
-                break;
-            }
-
-            teleportLocation = newTeleportLocation;
-        }
-
-        player.leaveVehicle();
-        teleportLocation = UtilLocation.shiftOutOfBlocks(teleportLocation, player.getBoundingBox());
-
-        Particle.SWEEP_ATTACK.builder().location(teleportLocation).count(1).receivers(30).extra(0).spawn();
-
-        final Location lineStart = player.getLocation().add(0.0, player.getHeight() / 2, 0.0);
-        final Location lineEnd = teleportLocation.clone().add(0.0, player.getHeight() / 2, 0.0);
-        final VectorLine line = VectorLine.withStepSize(lineStart, lineEnd, 0.25f);
-        for (Location point : line.toLocations()) {
-            Particle.CRIT.builder().location(point).count(2).receivers(30).extra(0).spawn();
-        }
-
-        player.teleportAsync(teleportLocation);
-
-        player.getWorld().playSound(player.getLocation(), Sound.ENTITY_PLAYER_ATTACK_SWEEP, 1.0F, 1.6F);
+            // Collision
+            UtilEntity.interpolateMultiCollision(originalLocation,
+                            teleportLocation,
+                            0.5f,
+                            ent -> UtilEntity.IS_ENEMY.test(player, ent))
+                    .map(MultiRayTraceResult::stream)
+                    .ifPresent(stream -> stream.map(RayTraceResult::getHitEntity)
+                            .map(LivingEntity.class::cast)
+                            .forEach(hit -> hit(player, level, hit)));
+        });
     }
 
-    @EventHandler(priority = EventPriority.HIGH)
+    private void hit(Player caster, int level, LivingEntity hit) {
+        CustomDamageEvent cde = new CustomDamageEvent(hit, caster, null, EntityDamageEvent.DamageCause.CUSTOM, getDamage(level), false, "Slash");
+        cde.setDamageDelay(0);
+        UtilDamage.doCustomDamage(cde);
+
+        if (!cde.isCancelled()) {
+            hit.getWorld().playSound(hit.getLocation().add(0, 1, 0), Sound.ENTITY_PLAYER_HURT, 0.2f, 2f);
+            hit.getWorld().playSound(hit.getLocation().add(0, 1, 0), Sound.ITEM_TRIDENT_HIT, 0.2f, 1.5f);
+        }
+    }
+
+    @EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
     public void onHit(CustomDamageEvent event) {
-        if (!(event.getDamager() instanceof Player player)) return;
-        if (event.isCancelled()) return;
+        if (!(event.getDamager() instanceof Player player)) {
+            return;
+        }
 
-        boolean isEntityAttack = event.getCause() == EntityDamageEvent.DamageCause.ENTITY_ATTACK;
-        boolean isDashReason = Arrays.stream(event.getReason()).anyMatch(reason -> reason.equals("Slash"));
+        if (event.getCause() != EntityDamageEvent.DamageCause.ENTITY_ATTACK && !event.hasReason("Slash")) {
+            return;
+        }
 
-        if (!isEntityAttack && !isDashReason) return;
-
-        cooldownManager.reduceCooldown(player, getName(), cooldownReductionPerHit);
-    }
-
-
-    private boolean wouldCollide(Block block, BoundingBox boundingBox) {
-        return !block.isPassable() && UtilBlock.doesBoundingBoxCollide(boundingBox, block);
+        int level = getLevel(player);
+        this.championsManager.getCooldowns().reduceCooldown(player, getName(), getCooldownDecrease(level));
     }
 
     @Override
@@ -201,6 +160,8 @@ public class Slash extends Skill implements InteractSkill, CooldownSkill, Listen
         damage = getConfig("damage", 2.0, Double.class);
         damageIncreasePerLevel = getConfig("damageIncreasePerLevel", 2.0, Double.class);
         distance = getConfig("distance", 5.0, Double.class);
-        cooldownReductionPerHit = getConfig("cooldownReductionPerHit", 4.0, Double.class);
+        distanceIncreasePerLevel = getConfig("distanceIncreasePerLevel", 0.0, Double.class);
+        cooldownReduction = getConfig("cooldownReduction", 4.0, Double.class);
+        cooldownReductionPerLevel = getConfig("cooldownReductionPerLevel", 0.0, Double.class);
     }
 }
