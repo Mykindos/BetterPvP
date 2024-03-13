b31e4c97f3741c44a33db85ff0f7f2c5d594a95f
Tom Hoogstra <tomhoogstra@live.com.au>
2024-03-14 02:13:11 +1100

    Fix some stuff and start dungeon buff

--- core/src/main/java/me/mykindos/betterpvp/core/effects/listeners/EffectListener.java
+++ core/src/main/java/me/mykindos/betterpvp/core/effects/listeners/EffectListener.java
@@ -41,8 +41,7 @@ public class EffectListener implements Listener {
             value.removeIf(effect -> {
                 Entity entity = Bukkit.getEntity(UUID.fromString(effect.getUuid()));
                 if (entity instanceof LivingEntity livingEntity) {
-                    if (effect.hasExpired() || (effect.getRemovalPredicate() != null && effect.getRemovalPredicate().test(livingEntity))) {
-
+                    if ((effect.hasExpired() && !effect.isPermanent()) || (effect.getRemovalPredicate() != null && effect.getRemovalPredicate().test(livingEntity))) {
                         UtilServer.callEvent(new EffectExpireEvent(livingEntity, effect));
                         return true;
 
