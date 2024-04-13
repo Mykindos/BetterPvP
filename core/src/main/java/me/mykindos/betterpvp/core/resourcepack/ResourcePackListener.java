965ba215e0085a00089b1c851468b3a69e0280dc
Mykindos <5389944+Mykindos@users.noreply.github.com>
2024-04-13 22:57:48 +1000

    Fix dura, packet stuff, resource pack stuff

--- core/src/main/java/me/mykindos/betterpvp/core/resourcepack/ResourcePackListener.java
+++ core/src/main/java/me/mykindos/betterpvp/core/resourcepack/ResourcePackListener.java
@@ -14,6 +14,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerResourcePackStatusEvent;
 
 @Singleton
@@ -58,6 +59,13 @@ public class ResourcePackListener implements Listener {
             }
         }
 
+    }
+
+    @EventHandler
+    public void onMoveWhileLoading(PlayerMoveEvent event) {
+        if (event.getPlayer().getResourcePackStatus() != PlayerResourcePackStatusEvent.Status.SUCCESSFULLY_LOADED) {
+            event.setCancelled(true);
+        }
     }
 
 
