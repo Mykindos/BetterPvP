9be592026a2968e30b60cb1035f8f8946b6e0066
Mykindos <5389944+Mykindos@users.noreply.github.com>
2024-04-14 02:48:56 +1000

    Revert "Revert "Fix dura, packet stuff, resource pack stuff""
    This reverts commit af45e52761a0e6ff595142fd2962f03e12591ccf.

--- core/src/main/java/me/mykindos/betterpvp/core/combat/listeners/CombatListener.java
+++ core/src/main/java/me/mykindos/betterpvp/core/combat/listeners/CombatListener.java
@@ -10,6 +10,7 @@ import me.mykindos.betterpvp.core.combat.damagelog.DamageLog;
 import me.mykindos.betterpvp.core.combat.damagelog.DamageLogManager;
 import me.mykindos.betterpvp.core.combat.data.DamageData;
 import me.mykindos.betterpvp.core.combat.data.SoundProvider;
+import me.mykindos.betterpvp.core.combat.events.CustomDamageDurabilityEvent;
 import me.mykindos.betterpvp.core.combat.events.CustomDamageEvent;
 import me.mykindos.betterpvp.core.combat.events.CustomDamageReductionEvent;
 import me.mykindos.betterpvp.core.combat.events.CustomKnockbackEvent;
@@ -174,6 +175,8 @@ public class CombatListener implements Listener {
 
     private void finalizeDamage(CustomDamageEvent event, CustomDamageReductionEvent reductionEvent) {
 
+        updateDurability(event);
+
         if (event.getProjectile() instanceof Arrow) {
             if (event.getDamager() instanceof Player player) {
                 player.playSound(player.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1.5f, 0.7f);
@@ -214,6 +217,18 @@ public class CombatListener implements Listener {
 
         }
     }
+
+    private void updateDurability(CustomDamageEvent event) {
+
+        CustomDamageDurabilityEvent cdde = new CustomDamageDurabilityEvent(event);
+        if (!event.isDoDurability()) {
+            cdde.setDamagerTakeDurability(false);
+        }
+
+        UtilServer.callEvent(cdde);
+
+    }
+
 
     private void processDamageData(CustomDamageEvent event) {
         if (event.getDamagee() instanceof Player damagee) {
