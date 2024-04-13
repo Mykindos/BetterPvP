f09dc5dbe83b11ab08b3872cf028eded0b2bc7b5
ImMuscleMan <95445240+ImMuscleMan@users.noreply.github.com>
2024-04-13 17:05:36 +0100

    Revert "multi resource pack support"
    This reverts commit 3cac0b972431aeb563cea481651d32d235f4ff3d.

--- core/src/main/java/me/mykindos/betterpvp/core/resourcepack/ResourcePackListener.java
+++ core/src/main/java/me/mykindos/betterpvp/core/resourcepack/ResourcePackListener.java
@@ -1,74 +1,65 @@
 package me.mykindos.betterpvp.core.resourcepack;
 
 import com.google.inject.Inject;
-import com.google.inject.Singleton;
+import me.mykindos.betterpvp.core.Core;
 import me.mykindos.betterpvp.core.client.events.ClientJoinEvent;
-import me.mykindos.betterpvp.core.framework.updater.UpdateEvent;
 import me.mykindos.betterpvp.core.listener.BPvPListener;
+import me.mykindos.betterpvp.core.utilities.UtilServer;
 import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.format.NamedTextColor;
-import net.kyori.adventure.text.format.TextDecoration;
-import net.kyori.adventure.title.Title;
-import net.kyori.adventure.util.Ticks;
-import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerResourcePackStatusEvent;
 
-@Singleton
 @BPvPListener
 public class ResourcePackListener implements Listener {
 
+
     private final ResourcePackHandler resourcePackHandler;
 
-    private static final Title.Times TIME = Title.Times.times(Ticks.duration(0), Ticks.duration(10), Ticks.duration(0));
-    private static final Title TITLE = Title.title(Component.text("Applying resource pack", NamedTextColor.GREEN, TextDecoration.BOLD),
-            Component.text("Please wait...", NamedTextColor.GRAY), TIME);
-
+    private final Core core;
 
     @Inject
-    public ResourcePackListener(ResourcePackHandler resourcePackHandler) {
+    public ResourcePackListener(ResourcePackHandler resourcePackHandler, Core core) {
         this.resourcePackHandler = resourcePackHandler;
+        this.core = core;
     }
 
     @EventHandler
     public void onClientLogin(ClientJoinEvent event) {
 
-        Player player = event.getPlayer();
-        ResourcePack mainPack = resourcePackHandler.getResourcePack("main");
-        if (mainPack == null) return;
+        if(resourcePackHandler.isResourcePackEnabled()) {
+            UtilServer.runTaskLater(core, () -> {
+                event.getPlayer().setResourcePack(resourcePackHandler.getResourcePackUrl(), resourcePackHandler.getResourcePackSha());
+            }, 2L);
+        }
 
-        Component message = Component.text("You must accept the resource pack to play on this server", NamedTextColor.RED);
-        player.setResourcePack(mainPack.getUuid(), mainPack.getUrl(), mainPack.getHashBytes(), message, true);
 
     }
 
     @EventHandler
-    public void onTexturepackStatus(PlayerResourcePackStatusEvent event) {
+    public void onTexturepackStatus(PlayerResourcePackStatusEvent e) {
+        if(!resourcePackHandler.isResourcePackEnabled()) return;
 
-        ResourcePack mainPack = resourcePackHandler.getResourcePack("main");
-        if (mainPack == null) return;
-
-        if (event.getID().equals(mainPack.getUuid())) {
-            if (event.getStatus() == PlayerResourcePackStatusEvent.Status.DECLINED) {
-                event.getPlayer().kick(Component.text("You must allow the resource pack"));
-            } else if (event.getStatus() == PlayerResourcePackStatusEvent.Status.FAILED_DOWNLOAD) {
-                event.getPlayer().kick(Component.text("Resource pack failed to load"));
+        if (resourcePackHandler.isForceResourcePack()) {
+            if (e.getStatus() == PlayerResourcePackStatusEvent.Status.DECLINED) {
+                e.getPlayer().kick(Component.text("You must allow the resource pack"));
+            } else if (e.getStatus() == PlayerResourcePackStatusEvent.Status.FAILED_DOWNLOAD) {
+                e.getPlayer().kick(Component.text("Resource pack failed to load"));
             }
         }
 
-    }
+        if (e.getStatus() == PlayerResourcePackStatusEvent.Status.ACCEPTED) {
+            //System.out.println("Added Texture Pack immunity to " + e.getPlayer().getName());
+            //EffectManager.addEffect(e.getPlayer(), EffectType.TEXTURELOADING, 15000);
+        } else if (e.getStatus() == PlayerResourcePackStatusEvent.Status.SUCCESSFULLY_LOADED) {
+            //new BukkitRunnable() {
+            //    @Override
+            //    public void run() {
+            //        System.out.println("Removed Texture Pack immunity from " + e.getPlayer().getName());
+            //        EffectManager.removeEffect(e.getPlayer(), EffectType.TEXTURELOADING);
+            //    }
+            //}.runTaskLater(getInstance(), 10);
 
-
-
-    @UpdateEvent(delay = 300)
-    public void sendResourcePackTitle() {
-        for (Player player : Bukkit.getOnlinePlayers()) {
-            if (player.getResourcePackStatus() != PlayerResourcePackStatusEvent.Status.SUCCESSFULLY_LOADED) {
-
-                player.showTitle(TITLE);
-            }
         }
     }
 }
--- core/src/main/java/me/mykindos/betterpvp/core/resourcepack/exceptions/ResourcePackShaException.java
+++
@@ -1,9 +0,0 @@
-package me.mykindos.betterpvp.core.resourcepack.exceptions;
-
-public class ResourcePackShaException extends RuntimeException {
-
-    public ResourcePackShaException(String message) {
-        super(message);
-    }
-
-}
