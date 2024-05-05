b723bd89507a71bbab11bd3980657c5f0e1693ef
StudentAlleg <36683605+StudentAlleg@users.noreply.github.com>
2024-05-04 20:06:22 -0700

    #719 - /kit no longer resets inventory, it now replaces armor, and adds bow/sword/axe/book if not present in the inventory (#727)

--- champions/src/main/java/me/mykindos/betterpvp/champions/champions/commands/menu/KitButton.java
+++ champions/src/main/java/me/mykindos/betterpvp/champions/champions/commands/menu/KitButton.java
@@ -21,20 +21,29 @@ public class KitButton extends SimpleItem {
 
     @Override
     public void handleClick(@NotNull ClickType clickType, @NotNull Player player, @NotNull InventoryClickEvent event) {
-        player.getInventory().clear();
         player.getInventory().setHelmet(new ItemStack(role.getHelmet()));
         player.getInventory().setChestplate(new ItemStack(role.getChestplate()));
         player.getInventory().setLeggings(new ItemStack(role.getLeggings()));
         player.getInventory().setBoots(new ItemStack(role.getBoots()));
 
-        player.getInventory().addItem(new ItemStack(Material.IRON_SWORD));
-        player.getInventory().addItem(new ItemStack(Material.IRON_AXE));
+        if (!player.getInventory().contains(Material.IRON_SWORD)) {
+            player.getInventory().addItem(new ItemStack(Material.IRON_SWORD));
+        }
+
+        if (!player.getInventory().contains(Material.IRON_AXE)) {
+            player.getInventory().addItem(new ItemStack(Material.IRON_AXE));
+        }
+
         if((role == Role.ASSASSIN) || (role == Role.RANGER)) {
-            player.getInventory().addItem(new ItemStack(Material.BOW));
+            if (!player.getInventory().contains(Material.BOW)) {
+                player.getInventory().addItem(new ItemStack(Material.BOW));
+            }
             int numArrows = (role == Role.RANGER ? 64 : 32);
             player.getInventory().addItem(new ItemStack(Material.ARROW, numArrows));
         }
-        player.getInventory().addItem(new ItemStack(Material.BOOK));
+        if (!player.getInventory().contains(Material.BOOK)) {
+            player.getInventory().addItem(new ItemStack(Material.BOOK));
+        }
 
         player.closeInventory();
     }
