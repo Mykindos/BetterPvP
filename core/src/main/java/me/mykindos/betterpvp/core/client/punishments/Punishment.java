2c4d11bc23c42e810f288c3e377fce17a0afdecd
StudentAlleg <36683605+StudentAlleg@users.noreply.github.com>
2024-05-04 22:47:40 -0700

    Update Punish Feedback (#726)
    * #724 - Add notification when a punishment expires, start work on notifying player of punish duration/reason
    
    * #724 - Change how informing a player of the specific punishment is handled, fix pvp lock expire message
    
    * #726 - Remove unused prettyTime assignment
    
    * #726 - convert checkdelay to static
    
    * #726 - Remove onReceive override for build lock, change to loop through online players instead of online clients

--- core/src/main/java/me/mykindos/betterpvp/core/client/punishments/Punishment.java
+++ core/src/main/java/me/mykindos/betterpvp/core/client/punishments/Punishment.java
@@ -4,7 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import me.mykindos.betterpvp.core.client.punishments.types.IPunishmentType;
+import me.mykindos.betterpvp.core.utilities.UtilMessage;
+import net.kyori.adventure.text.Component;
+import org.ocpsoft.prettytime.PrettyTime;
 
+import java.util.Date;
 import java.util.UUID;
 
 @Data
@@ -26,6 +30,18 @@ public class Punishment {
 
     public boolean isActive() {
         return !revoked && !hasExpired();
+    }
+
+    /**
+     *
+     * @return The formatted information about the punishment
+     */
+    public Component getInformation() {
+        if (!this.isActive()) {
+            return UtilMessage.deserialize("You are no longer <red>%s</red>", this.type.getChatLabel());
+        }
+        String formattedTime = new PrettyTime().format(new Date(this.getExpiryTime())).replace(" from now", "");
+        return UtilMessage.deserialize("You are <red>%s</red> for <green>%s</green> for <yellow>%s</yellow>", this.type.getChatLabel(), formattedTime, this.getReason());
     }
 
 }
