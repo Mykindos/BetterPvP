cf7d7da069e87c37269c9c9c8b0121133a694a8d
Manu <73852745+manu10000@users.noreply.github.com>
2024-05-28 21:24:09 +0100

    Made the following quality of life changes to the class selector:
    -Added confirmation when deleting builds.
    -Added preview dialog when hovering over different builds.
    -Made it so when you edit a build you also select the build at the same time.

--- champions/src/main/java/me/mykindos/betterpvp/champions/champions/builds/menus/buttons/EditBuildButton.java
+++ champions/src/main/java/me/mykindos/betterpvp/champions/champions/builds/menus/buttons/EditBuildButton.java
@@ -2,10 +2,13 @@ package me.mykindos.betterpvp.champions.champions.builds.menus.buttons;
 
 import me.mykindos.betterpvp.champions.champions.builds.BuildManager;
 import me.mykindos.betterpvp.champions.champions.builds.GamerBuilds;
+import me.mykindos.betterpvp.champions.champions.builds.RoleBuild;
 import me.mykindos.betterpvp.champions.champions.builds.menus.SkillMenu;
-import me.mykindos.betterpvp.champions.champions.skills.ChampionsSkillManager;
+import me.mykindos.betterpvp.champions.champions.builds.menus.events.ApplyBuildEvent;
+import me.mykindos.betterpvp.champions.champions.skills.SkillManager;
 import me.mykindos.betterpvp.core.components.champions.Role;
 import me.mykindos.betterpvp.core.menu.Windowed;
+import me.mykindos.betterpvp.core.utilities.UtilServer;
 import me.mykindos.betterpvp.core.utilities.model.SoundEffect;
 import me.mykindos.betterpvp.core.utilities.model.item.ItemView;
 import net.kyori.adventure.text.Component;
@@ -17,16 +20,18 @@ import org.bukkit.event.inventory.InventoryClickEvent;
 import org.jetbrains.annotations.NotNull;
 import xyz.xenondevs.invui.item.impl.SimpleItem;
 
+import java.util.Optional;
+
 public class EditBuildButton extends SimpleItem {
 
     private final Role role;
     private final int build;
     private final GamerBuilds builds;
-    private final ChampionsSkillManager skillManager;
+    private final SkillManager skillManager;
     private final Windowed previous;
     private final BuildManager buildManager;
 
-    public EditBuildButton(GamerBuilds builds, Role role, int build, BuildManager buildManager, ChampionsSkillManager skillManager, Windowed previous) {
+    public EditBuildButton(GamerBuilds builds, Role role, int build, BuildManager buildManager, SkillManager skillManager, Windowed previous) {
         super(ItemView.builder().material(Material.ANVIL)
                 .displayName(Component.text("Edit Build " + build, NamedTextColor.GRAY))
                 .build());
@@ -40,6 +45,18 @@ public class EditBuildButton extends SimpleItem {
 
     @Override
     public void handleClick(@NotNull ClickType clickType, @NotNull Player player, @NotNull InventoryClickEvent event) {
+        Optional<RoleBuild> roleBuildOptional = builds.getBuild(role, build);
+        roleBuildOptional.ifPresent(selected -> {
+            RoleBuild activeBuild = builds.getActiveBuilds().get(role.getName());
+            activeBuild.setActive(false);
+
+            selected.setActive(true);
+            builds.getActiveBuilds().put(role.getName(), selected);
+
+            UtilServer.callEvent(new ApplyBuildEvent(player, builds, activeBuild, selected));
+            notifyWindows();
+        });
+
         new SkillMenu(builds, role, build, buildManager, skillManager, previous).show(player);
         SoundEffect.HIGH_PITCH_PLING.play(player);
     }
