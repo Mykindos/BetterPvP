d5200ebf69e15907c01721a83a3c2787b1c95ba9
Mykindos <tomhoogstra@live.com.au>
2024-05-29 07:09:45 +1000

    manu - Made the following quality of life changes to the class selector:        -Added confirmation when deleting builds.        -Added preview dialog when hovering over different builds.        -Made it so when you edit a build you also select the build at the same time.

--- champions/src/main/java/me/mykindos/betterpvp/champions/champions/builds/menus/buttons/DeleteBuildButton.java
+++ champions/src/main/java/me/mykindos/betterpvp/champions/champions/builds/menus/buttons/DeleteBuildButton.java
@@ -5,8 +5,7 @@ import me.mykindos.betterpvp.champions.champions.builds.GamerBuilds;
 import me.mykindos.betterpvp.champions.champions.builds.RoleBuild;
 import me.mykindos.betterpvp.champions.champions.builds.menus.BuildMenu;
 import me.mykindos.betterpvp.champions.champions.builds.menus.events.DeleteBuildEvent;
-import me.mykindos.betterpvp.champions.champions.skills.SkillManager;
-import me.mykindos.betterpvp.core.combat.armour.ArmourManager;
+import me.mykindos.betterpvp.champions.champions.skills.ChampionsSkillManager;
 import me.mykindos.betterpvp.core.components.champions.Role;
 import me.mykindos.betterpvp.core.menu.Windowed;
 import me.mykindos.betterpvp.core.menu.impl.ConfirmationMenu;
@@ -14,7 +13,6 @@ import me.mykindos.betterpvp.core.utilities.UtilServer;
 import me.mykindos.betterpvp.core.utilities.model.item.ItemView;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
-import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
@@ -31,10 +29,10 @@ public class DeleteBuildButton extends SimpleItem {
     private final Role role;
     private final int buildNumber;
     private final BuildManager buildManager;
-    private final SkillManager skillManager;
+    private final ChampionsSkillManager skillManager;
     private final Windowed parent;
 
-    public DeleteBuildButton(GamerBuilds builds, Role role, int build, BuildManager buildManager, SkillManager skillManager, Windowed parent) {
+    public DeleteBuildButton(GamerBuilds builds, Role role, int build, BuildManager buildManager, ChampionsSkillManager skillManager, Windowed parent) {
         super(ItemView.builder().material(Material.RED_CONCRETE)
                 .displayName(Component.text("Delete Build " + build , NamedTextColor.RED))
                 .build());
