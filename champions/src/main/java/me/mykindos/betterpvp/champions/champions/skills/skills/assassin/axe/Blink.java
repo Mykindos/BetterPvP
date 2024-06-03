b4d94ea9d45d0e8446be34d2d28b01554f594491
ezrn <145294235+ezrn@users.noreply.github.com>
2024-06-03 01:15:08 -0700

    deblink nerf (#822)
    * blink
    
    * deblink

--- champions/src/main/java/me/mykindos/betterpvp/champions/champions/skills/skills/assassin/axe/Blink.java
+++ champions/src/main/java/me/mykindos/betterpvp/champions/champions/skills/skills/assassin/axe/Blink.java
@@ -234,7 +234,7 @@ public class Blink extends Skill implements InteractSkill, CooldownSkill, Listen
     public void loadSkillConfig(){
         maxTravelDistance = getConfig("maxTravelDistance", 9, Integer.class);
         distanceIncreasePerLevel = getConfig("distanceIncreasePerLevel", 3, Integer.class);
-        deblinkTime = getConfig("deblinkTime", 6, Integer.class);
+        deblinkTime = getConfig("deblinkTime", 4, Integer.class);
         deblinkTimeIncreasePerLevel = getConfig("deblinkTimeIncreasePerLevel", 0, Integer.class);
     }
 
