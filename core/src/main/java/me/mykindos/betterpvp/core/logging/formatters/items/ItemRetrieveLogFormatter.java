bfa27d329a631237c7e10ec99947b57a67c243a9
StudentAlleg <studentalleg-pilot@yahoo.com>
2024-06-07 20:30:22 -0700

    Fix NPE from a log that sometimes did not have the required context (ItemRetrieveLog)

--- core/src/main/java/me/mykindos/betterpvp/core/logging/formatters/items/ItemRetrieveLogFormatter.java
+++ core/src/main/java/me/mykindos/betterpvp/core/logging/formatters/items/ItemRetrieveLogFormatter.java
@@ -1,5 +1,6 @@
 package me.mykindos.betterpvp.core.logging.formatters.items;
 
+import lombok.extern.slf4j.Slf4j;
 import me.mykindos.betterpvp.core.framework.annotations.WithReflection;
 import me.mykindos.betterpvp.core.logging.LogContext;
 import me.mykindos.betterpvp.core.logging.formatters.ILogFormatter;
@@ -10,6 +11,7 @@ import net.kyori.adventure.text.format.NamedTextColor;
 import java.util.HashMap;
 
 @WithReflection
+@Slf4j
 public class ItemRetrieveLogFormatter implements ILogFormatter {
 
     @Override
@@ -17,15 +19,17 @@ public class ItemRetrieveLogFormatter implements ILogFormatter {
         return "ITEM_RETRIEVE";
     }
 
-
     @Override
     public Component formatLog(HashMap<String, String> context) {
+
+        log.info(context.toString());
+        log.info(context.get(LogContext.LOCATION));
         return Component.text(context.get(LogContext.CLIENT_NAME), NamedTextColor.YELLOW)
                 .append(Component.text(" retrieved ", NamedTextColor.GRAY))
                 .append(Component.text(context.get(LogContext.ITEM_NAME), NamedTextColor.GREEN)
                         .hoverEvent(HoverEvent.showText(Component.text(context.get(LogContext.ITEM)))))
                 .append(Component.text(" from ", NamedTextColor.GRAY))
-                .append(Component.text(context.get(LogContext.BLOCK), NamedTextColor.GREEN))
+                .append(Component.text(context.get(LogContext.BLOCK) == null ? "NULL" : context.get(LogContext.BLOCK), NamedTextColor.GREEN))
                 .append(Component.text(" at ", NamedTextColor.GRAY))
                 .append(Component.text(context.get(LogContext.LOCATION), NamedTextColor.YELLOW));
 
