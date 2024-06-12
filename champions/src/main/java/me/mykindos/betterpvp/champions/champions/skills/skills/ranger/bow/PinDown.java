ddea2d3f7ccbf6029a457e391bbdd70fa0200afe
Mykindos <tomhoogstra@live.com.au>
2024-06-12 22:55:24 +1000

    Fix #890

--- champions/src/main/java/me/mykindos/betterpvp/champions/champions/skills/skills/ranger/bow/PinDown.java
+++ champions/src/main/java/me/mykindos/betterpvp/champions/champions/skills/skills/ranger/bow/PinDown.java
@@ -9,6 +9,7 @@ import me.mykindos.betterpvp.champions.champions.skills.Skill;
 import me.mykindos.betterpvp.champions.champions.skills.data.SkillActions;
 import me.mykindos.betterpvp.champions.champions.skills.types.CooldownSkill;
 import me.mykindos.betterpvp.champions.champions.skills.types.InteractSkill;
+import me.mykindos.betterpvp.core.combat.events.EntityCanHurtEntityEvent;
 import me.mykindos.betterpvp.core.components.champions.Role;
 import me.mykindos.betterpvp.core.components.champions.SkillType;
 import me.mykindos.betterpvp.core.effects.EffectTypes;
@@ -17,6 +18,7 @@ import me.mykindos.betterpvp.core.listener.BPvPListener;
 import me.mykindos.betterpvp.core.utilities.UtilFormat;
 import me.mykindos.betterpvp.core.utilities.UtilInventory;
 import me.mykindos.betterpvp.core.utilities.UtilMessage;
+import me.mykindos.betterpvp.core.utilities.UtilServer;
 import org.bukkit.Effect;
 import org.bukkit.GameMode;
 import org.bukkit.Material;
@@ -26,6 +28,7 @@ import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Projectile;
+import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
@@ -146,6 +149,11 @@ public class PinDown extends Skill implements InteractSkill, CooldownSkill, List
         final Entity entity = event.getHitEntity();
         if (!(entity instanceof LivingEntity target)) {
             UtilMessage.message(shooter, getName(), "You missed <alt>%s</alt>.", getName());
+            return;
+        }
+
+        var canHurtEvent = UtilServer.callEvent(new EntityCanHurtEntityEvent(shooter, target));
+        if(canHurtEvent.getResult() == Event.Result.DENY){
             return;
         }
 
