b43d9a968421eccb876dd57efed64c23f83bc32f
StudentAlleg <studentalleg-pilot@yahoo.com>
2024-06-13 01:19:50 -0700

    #896 - Fixed the problem by making everything flash. It is now in the right order though.

---
+++ core/src/main/java/me/mykindos/betterpvp/core/utilities/model/data/PriorityDataBlockingQueue.java
@@ -0,0 +1,53 @@
+package me.mykindos.betterpvp.core.utilities.model.data;
+
+import org.apache.commons.lang3.tuple.Pair;
+
+import java.util.concurrent.PriorityBlockingQueue;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class PriorityDataBlockingQueue<T> extends PriorityBlockingQueue<Pair<PriorityData, T>> {
+
+    public PriorityDataBlockingQueue(int intialCapacity) {
+        super(intialCapacity, new PriorityPairComparator());
+    }
+
+    /**
+     * Put a new element with a priority, assigning it the correct instance
+     * Time complexity: O(n)
+     * @param priority the priority
+     * @param element the element
+     */
+    public void put(int priority, T element) {
+        AtomicLong instance = new AtomicLong();
+        forEach(priorityDataTPair -> {
+            PriorityData priorityData = priorityDataTPair.getKey();
+            if (priorityData.getPriority() == priority) {
+                if (priorityData.getInstance() >= instance.get()) {
+                    instance.set(priorityData.getInstance() + 1);
+                }
+            }
+        });
+        PriorityData newPriorityData = new PriorityData(priority, instance.get());
+        super.put(Pair.of(newPriorityData, element));
+    }
+
+    /**
+     * Put a new element with a priority, assigning it the correct instance
+     * Time complexity: O(n)
+     * @param priority the priority
+     * @param element the element
+     */
+    public void add(int priority, T element) {
+        AtomicLong instance = new AtomicLong();
+        forEach(priorityDataTPair -> {
+            PriorityData priorityData = priorityDataTPair.getKey();
+            if (priorityData.getPriority() == priority) {
+                if (priorityData.getInstance() >= instance.get()) {
+                    instance.set(priorityData.getInstance() + 1);
+                }
+            }
+        });
+        PriorityData newPriorityData = new PriorityData(priority, instance.get());
+        super.add(Pair.of(newPriorityData, element));
+    }
+}
