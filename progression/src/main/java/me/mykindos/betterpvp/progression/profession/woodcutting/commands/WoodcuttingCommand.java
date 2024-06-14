4cba55a4f9116d4acac82d2cfc9cbc35ca1aec2c
8SIXSect <eightysixthsector@outlook.com>
2024-06-13 22:11:39 -0500

    Created '/woodcutting' command to be like all the others; not implemented yet

---
+++ progression/src/main/java/me/mykindos/betterpvp/progression/profession/woodcutting/commands/WoodcuttingCommand.java
@@ -0,0 +1,38 @@
+package me.mykindos.betterpvp.progression.profession.woodcutting.commands;
+
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import me.mykindos.betterpvp.core.client.Client;
+import me.mykindos.betterpvp.core.command.Command;
+import me.mykindos.betterpvp.progression.profession.skill.ProgressionSkillManager;
+import me.mykindos.betterpvp.progression.profile.ProfessionProfileManager;
+import org.bukkit.entity.Player;
+
+@Singleton
+public class WoodcuttingCommand extends Command {
+    private final ProfessionProfileManager professionProfileManager;
+    private final ProgressionSkillManager progressionSkillManager;
+
+    @Inject
+    public WoodcuttingCommand(ProfessionProfileManager professionProfileManager, ProgressionSkillManager progressionSkillManager) {
+        this.professionProfileManager = professionProfileManager;
+        this.progressionSkillManager = progressionSkillManager;
+    }
+
+    @Override
+    public String getName() {
+        return "woodcutting";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Woodcutting! command";
+    }
+
+    @Override
+    public void execute(Player player, Client client, String... args) {
+        if (args.length != 0) return;
+
+        player.sendMessage("wow woodcutting!!!");
+    }
+}
