cc9394a8bc1c3d146dbf894acc1014200186c901
8SIXSect <eightysixthsector@outlook.com>
2024-06-14 18:53:24 -0500

    Implemented the leaderboard as well as its necessary fetching methods inside of the Woodcutting Repository

--- progression/src/main/java/me/mykindos/betterpvp/progression/profession/woodcutting/leaderboards/TotalLogsChoppedLeaderboard.java
+++ progression/src/main/java/me/mykindos/betterpvp/progression/profession/woodcutting/leaderboards/TotalLogsChoppedLeaderboard.java
@@ -1,2 +1,74 @@
-package me.mykindos.betterpvp.progression.profession.woodcutting.leaderboards;public class TotalLogsChoppedLeaderboard {
+package me.mykindos.betterpvp.progression.profession.woodcutting.leaderboards;
+
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import lombok.CustomLog;
+import lombok.SneakyThrows;
+import me.mykindos.betterpvp.core.database.Database;
+import me.mykindos.betterpvp.core.stats.PlayerLeaderboard;
+import me.mykindos.betterpvp.core.stats.SearchOptions;
+import me.mykindos.betterpvp.core.stats.sort.SortType;
+import me.mykindos.betterpvp.core.stats.sort.Sorted;
+import me.mykindos.betterpvp.core.stats.sort.TemporalSort;
+import me.mykindos.betterpvp.progression.Progression;
+import me.mykindos.betterpvp.progression.profession.woodcutting.WoodcuttingHandler;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Comparator;
+import java.util.Map;
+import java.util.Objects;
+import java.util.UUID;
+
+@CustomLog
+@Singleton
+public class TotalLogsChoppedLeaderboard extends PlayerLeaderboard<Long> implements Sorted {
+    private final WoodcuttingHandler woodcuttingHandler;
+
+    @Inject
+    public TotalLogsChoppedLeaderboard(Progression progression, WoodcuttingHandler woodcuttingHandler) {
+        super(progression);
+        this.woodcuttingHandler = woodcuttingHandler;
+        init();
+    }
+
+    @Override
+    public String getName() {
+        return "Total Logs Chopped";
+    }
+
+    @Override
+    public Comparator<Long> getSorter(SearchOptions searchOptions) {
+        return Comparator.comparing(Long::intValue).reversed();
+    }
+
+    /**
+     * There's only 1 kind of way you can sort so this is just boilerplate
+     */
+    @Override
+    public SortType[] acceptedSortTypes() {
+        return TemporalSort.values();
+    }
+
+    /**
+     * Pretty sure this method is like some weird functional way to implement
+     * a reduce function but since this Leaderboard only sorts actual numbers,
+     * it can be this basic
+     */
+    @Override
+    protected Long join(Long value, Long add) {
+        return value + add;
+    }
+
+    @Override
+    @SneakyThrows
+    protected Long fetch(@NotNull SearchOptions options, @NotNull Database database, @NotNull UUID entry) {
+        return woodcuttingHandler.getWoodcuttingRepository().getTotalChoppedLogsForPlayer(entry);
+    }
+
+    @Override
+    @SneakyThrows
+    protected Map<UUID, Long> fetchAll(@NotNull SearchOptions options, @NotNull Database database) {
+        final TemporalSort type = (TemporalSort) Objects.requireNonNull(options.getSort());
+        return woodcuttingHandler.getWoodcuttingRepository().getTopLogsChoppedByCount(type.getDays()).join();
+    }
 }
--- progression/src/main/java/me/mykindos/betterpvp/progression/profession/woodcutting/repository/WoodcuttingRepository.java
+++ progression/src/main/java/me/mykindos/betterpvp/progression/profession/woodcutting/repository/WoodcuttingRepository.java
@@ -5,26 +5,36 @@ import com.google.inject.Singleton;
 import lombok.CustomLog;
 import me.mykindos.betterpvp.core.database.Database;
 import me.mykindos.betterpvp.core.database.query.Statement;
+import me.mykindos.betterpvp.core.database.query.values.DoubleStatementValue;
+import me.mykindos.betterpvp.core.database.query.values.IntegerStatementValue;
 import me.mykindos.betterpvp.core.database.query.values.StringStatementValue;
 import me.mykindos.betterpvp.core.database.query.values.UuidStatementValue;
 import me.mykindos.betterpvp.core.utilities.UtilServer;
 import me.mykindos.betterpvp.core.utilities.UtilWorld;
 import me.mykindos.betterpvp.progression.Progression;
+import me.mykindos.betterpvp.progression.profile.ProfessionProfile;
+import me.mykindos.betterpvp.progression.profile.ProfessionProfileManager;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Optional;
 import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
 
 @CustomLog
 @Singleton
 public class WoodcuttingRepository {
     private final Database database;
+    private final ProfessionProfileManager profileManager;
 
     @Inject
-    public WoodcuttingRepository(Database database) {
+    public WoodcuttingRepository(Database database, ProfessionProfileManager profileManager) {
         this.database = database;
+        this.profileManager = profileManager;
     }
 
     public void saveChoppedLog(UUID playerUUID, Material material, Location location) {
@@ -37,4 +47,44 @@ public class WoodcuttingRepository {
 
         UtilServer.runTaskAsync(JavaPlugin.getPlugin(Progression.class), () -> database.executeUpdate(statement));
     }
+
+    /**
+     * Gets the total chopped logs for a player given a unique ID
+     */
+    public Long getTotalChoppedLogsForPlayer(UUID playerUUID) {
+        Optional<ProfessionProfile> professionProfileOptional = profileManager.getObject(playerUUID.toString());
+        if (professionProfileOptional.isPresent()) {
+            ProfessionProfile professionProfile = professionProfileOptional.get();
+            return (long) professionProfile.getProfessionDataMap().get("Woodcutting").getProperty("TOTAL_LOGS_CHOPPED").orElse(0L);
+        }
+
+        return 0L;
+    }
+
+    /**
+     * docs tbd
+     */
+    public CompletableFuture<HashMap<UUID, Long>> getTopLogsChoppedByCount(double days) {
+        return CompletableFuture.supplyAsync(() -> {
+            HashMap<UUID, Long> leaderboard = new HashMap<>();
+            Statement statement = new Statement("CALL GetTopLogsChoppedByCount(?, ?)",
+                    new DoubleStatementValue(days),
+                    new IntegerStatementValue(10));
+            database.executeProcedure(statement, -1, result -> {
+                try {
+                    while (result.next()) {
+                        final String gamer = result.getString(1);
+                        final long count = result.getLong(2);
+                        leaderboard.put(UUID.fromString(gamer), count);
+                    }
+                } catch (SQLException e) {
+                    log.error("Error fetching woodcutting leaderboard data", e).submit();
+                }
+            });
+
+            return leaderboard;
+
+        });
+    }
+
 }
