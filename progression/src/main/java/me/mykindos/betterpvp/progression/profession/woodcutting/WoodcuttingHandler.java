9477b11e7b8c3be48b419d0d4f581868538255c4
8SIXSect <135760919+8SIXSect@users.noreply.github.com>
2024-06-16 11:11:43 -0500

    Implemented Foundation for Woodcutting Progression (#899)
    * Added a comment explaining a part of ambiguous code
    
    * Added a migration for Woodcutting to automatically create its table
    
    * Created the 'WoodcuttingHandler' class to contain the logic to handle all the interactions with profession.
    
    * Populated the config yaml file with necessary options for woodcutting profession
    
    * Created new class to listen for whenever a block is broken and notify the WoodcuttingHandler appropriately
    
    * Completely Implemented 'attemptToMineWood' method
    
    * Added a line to betterpvp_global db initial migration to allow 'TOTAL_TREES_CHOPPED' to work with 'progression_properties' table
    
    * Created '/woodcutting' command to be like all the others; not implemented yet
    
    * Integrated Woodcutting Progression Menu into the command
    
    * Removed repository important & usage
    
    * Updated documentation for certain Woodcutting classes
    
    * Fixed an issue with the way that the numbers cast
    
    * Appendend 'L' to numbers in order to fix casting issues
    
    * Instances where the project said 'TOTAL_TREES_CHOPPED' now use 'TOTAL_LOGS_CHOPPED'
    
    * WoodcuttingHandler is now able to save chopped log data to 'progression_woodcutting' table
    
    * Added the 'id' field just like fishing has. You'll sometimes encounter an error where you chop wood at the same location of the same type and it won't get put in db so this fixes that
    
    * Used 'UtilWorld.locationToString' instead of 'Location.toString'

---
+++ progression/src/main/java/me/mykindos/betterpvp/progression/profession/woodcutting/WoodcuttingHandler.java
@@ -0,0 +1,125 @@
+package me.mykindos.betterpvp.progression.profession.woodcutting;
+
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import lombok.CustomLog;
+import lombok.Getter;
+import me.mykindos.betterpvp.core.framework.CoreNamespaceKeys;
+import me.mykindos.betterpvp.core.utilities.UtilBlock;
+import me.mykindos.betterpvp.progression.Progression;
+import me.mykindos.betterpvp.progression.profession.ProfessionHandler;
+import me.mykindos.betterpvp.progression.profession.woodcutting.repository.WoodcuttingRepository;
+import me.mykindos.betterpvp.progression.profile.ProfessionData;
+import me.mykindos.betterpvp.progression.profile.ProfessionProfileManager;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.Player;
+import org.bukkit.persistence.PersistentDataContainer;
+
+import java.util.EnumMap;
+import java.util.Map;
+import java.util.function.LongUnaryOperator;
+
+
+/**
+ * This class's purpose is to listen for whenever a block is broken
+ * and notify the WoodcuttingHandler appropriately.
+ */
+@Singleton
+@CustomLog
+@Getter
+public class WoodcuttingHandler extends ProfessionHandler {
+    private final WoodcuttingRepository woodcuttingRepository;
+    private Map<Material, Long> experiencePerWood = new EnumMap<>(Material.class);
+
+    @Inject
+    public WoodcuttingHandler(Progression progression, ProfessionProfileManager professionProfileManager, WoodcuttingRepository woodcuttingRepository) {
+        super(progression, professionProfileManager, "Woodcutting");
+        this.woodcuttingRepository = woodcuttingRepository;
+    }
+
+    /**
+     * @param material The (type of) wood material that was mined by the player
+     * @return The experience gained from mining said wood material
+     */
+    public long getExperienceFor(Material material) {
+        return experiencePerWood.getOrDefault(material, 0L);
+    }
+
+    /**
+     * Whenever a player mines a block but there is no experience modifier (or whatever
+     * is calling this method doesn't know in what manor it needs to modify the xp),
+     * it will get passed here
+     */
+    public void attemptToChopLog(Player player, Block block) {
+        attemptToChopLog(player, block, LongUnaryOperator.identity());
+    }
+
+    /**
+     * Just like the other declaration but this one will take in LongUnaryOperator
+     * (which is just a higher order function that returns a long primitive type).
+     * @param experienceModifier represents a higher order function that modifies
+     *                           the experience gained by the player here.
+     */
+    public void attemptToChopLog(Player player, Block block, LongUnaryOperator experienceModifier) {
+        ProfessionData professionData = getProfessionData(player.getUniqueId());
+        if (professionData == null) {
+            return;
+        }
+
+        long experience = getExperienceFor(block.getType());
+        if (experience <= 0) {
+            return;
+        }
+
+        // if this is identity, it will just return 'experience'
+        final long finalExperience = experienceModifier.applyAsLong(experience);
+
+        // no xp if a player placed
+        final PersistentDataContainer persistentDataContainer = UtilBlock.getPersistentDataContainer(block);
+        boolean playerPlaced = persistentDataContainer.has(CoreNamespaceKeys.PLAYER_PLACED_KEY);
+        if (playerPlaced) {
+            professionData.grantExperience(0, player);
+            return;
+        }
+
+        professionData.grantExperience(finalExperience, player);
+        woodcuttingRepository.saveChoppedLog(player.getUniqueId(), block.getType(), player.getLocation());
+
+        log.info("{} chopped {} for {} experience", player.getName(), block.getType(), finalExperience)
+                .addClientContext(player).addBlockContext(block).addLocationContext(block.getLocation())
+                .addContext("Experience", finalExperience + "").submit();
+
+        long logsChopped = (long) professionData.getProperties().getOrDefault("TOTAL_LOGS_CHOPPED", 0L);
+        professionData.getProperties().put("TOTAL_LOGS_CHOPPED", logsChopped + 1L);
+    }
+
+    @Override
+    public String getName() {
+        return "Woodcutting";
+    }
+
+    public void loadConfig() {
+        super.loadConfig();
+
+        // not entirely sure if this line is necessary
+        experiencePerWood = new EnumMap<>(Material.class);
+        var config = progression.getConfig();
+
+        ConfigurationSection experienceSection = config.getConfigurationSection("woodcutting.experiencePerWood");
+        if (experienceSection == null) {
+            experienceSection = config.createSection("woodcutting.experiencePerWood");
+        }
+
+        for (String key : experienceSection.getKeys(false)) {
+
+            Material woodLogMaterial = Material.getMaterial(key.toUpperCase());
+            if (woodLogMaterial == null) continue;
+
+            long experienceGiven = config.getLong("woodcutting.experiencePerWood." + key);
+            experiencePerWood.put(woodLogMaterial, experienceGiven);
+        }
+        log.info("Loaded " + experiencePerWood.size() + " woodcutting blocks").submit();
+    }
+}
