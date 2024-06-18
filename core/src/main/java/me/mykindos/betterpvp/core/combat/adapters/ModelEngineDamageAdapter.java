5ef3e6cb1538e1c763c8b968928423b74f320ece
Mykindos <tomhoogstra@live.com.au>
2024-06-18 22:42:30 +1000

    Fix throwables hitting mythic mobs

--- core/src/main/java/me/mykindos/betterpvp/core/combat/adapters/ModelEngineDamageAdapter.java
+++ core/src/main/java/me/mykindos/betterpvp/core/combat/adapters/ModelEngineDamageAdapter.java
@@ -1,19 +1,28 @@
 package me.mykindos.betterpvp.core.combat.adapters;
 
 import com.ticxo.modelengine.api.ModelEngineAPI;
+import com.ticxo.modelengine.api.nms.entity.HitboxEntity;
+import io.lumine.mythic.bukkit.MythicBukkit;
+import io.lumine.mythic.core.mobs.ActiveMob;
+import lombok.CustomLog;
 import me.mykindos.betterpvp.core.combat.events.PreCustomDamageEvent;
+import me.mykindos.betterpvp.core.combat.throwables.events.ThrowableHitEntityEvent;
 import me.mykindos.betterpvp.core.framework.adapter.PluginAdapter;
 import me.mykindos.betterpvp.core.listener.BPvPListener;
 import me.mykindos.betterpvp.core.utilities.events.FetchNearbyEntityEvent;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDeathEvent;
 
 import javax.inject.Singleton;
 
 @PluginAdapter("ModelEngine")
+@PluginAdapter("MythicMobs")
 @Singleton
 @BPvPListener
+@CustomLog
 public class ModelEngineDamageAdapter implements Listener {
 
     /**
@@ -30,6 +39,26 @@ public class ModelEngineDamageAdapter implements Listener {
     @EventHandler
     public void onFetchEntity(FetchNearbyEntityEvent<?> event) {
         event.getEntities().removeIf(entity -> ModelEngineAPI.getNMSHandler().getEntityHandler().castHitbox(entity.getKey()) != null);
+    }
+
+    @EventHandler
+    public void onThrowableHit(ThrowableHitEntityEvent event) {
+        HitboxEntity hitboxEntity = ModelEngineAPI.getNMSHandler().getEntityHandler().castHitbox(event.getCollision());
+        if(hitboxEntity != null) {
+            ActiveMob mythicMobInstance = MythicBukkit.inst().getAPIHelper().getMythicMobInstance(event.getCollision());
+            if(mythicMobInstance.getEntity().getBukkitEntity() instanceof LivingEntity newTarget) {
+                event.setCollision(newTarget);
+            }
+
+        }
+    }
+
+    @EventHandler
+    public void entityDeath(EntityDeathEvent event) {
+        HitboxEntity hitboxEntity = ModelEngineAPI.getNMSHandler().getEntityHandler().castHitbox(event.getEntity());
+        if(hitboxEntity != null) {
+            log.error("Hitbox entity died? {} - {} - {}", event.getEntity().getType(), event.getEntity().getUniqueId(), event.getEntity().getName()).submit();
+        }
     }
 
 }
--- core/src/main/java/me/mykindos/betterpvp/core/combat/throwables/events/ThrowableHitEntityEvent.java
+++ core/src/main/java/me/mykindos/betterpvp/core/combat/throwables/events/ThrowableHitEntityEvent.java
@@ -2,14 +2,16 @@ package me.mykindos.betterpvp.core.combat.throwables.events;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.Setter;
 import me.mykindos.betterpvp.core.combat.throwables.ThrowableItem;
 import org.bukkit.entity.LivingEntity;
 
 @EqualsAndHashCode(callSuper = true)
+@Getter
+@Setter
 public class ThrowableHitEntityEvent extends ThrowableHitEvent {
 
-    @Getter
-    private final LivingEntity collision;
+    private LivingEntity collision;
 
     public ThrowableHitEntityEvent(ThrowableItem throwable, LivingEntity livingEntity) {
         super(throwable);
