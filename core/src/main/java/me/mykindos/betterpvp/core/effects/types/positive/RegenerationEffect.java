478d485d1c53a427dbb1f1b949fb7592e6699035
StudentAlleg <studentalleg-pilot@yahoo.com>
2024-06-22 19:36:18 -0700

    #889 - Fix regeneration second calculation

--- core/src/main/java/me/mykindos/betterpvp/core/effects/types/positive/RegenerationEffect.java
+++ core/src/main/java/me/mykindos/betterpvp/core/effects/types/positive/RegenerationEffect.java
@@ -24,7 +24,8 @@ public class RegenerationEffect extends VanillaEffectType {
     @Override
     public String getDescription(int level) {
         //https://minecraft.wiki/w/Regeneration
-        double seconds = Math.max(Math.floor(50d/((level - 1)^2) / 20d), 1);
+        int ticks = (int) Math.max(Math.floor((50d/((level - 1)^2))), 1);
+        double seconds = ticks / 20d;
         return "<white>" + getName() + " " + UtilFormat.getRomanNumeral(level) + "</white> increases health by an additional <stat>5</stat>% every <val>" + UtilFormat.formatNumber(seconds, 3) + "</val> seconds";
     }
 }
