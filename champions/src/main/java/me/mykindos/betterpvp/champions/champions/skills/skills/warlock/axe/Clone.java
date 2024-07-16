d673130eadf3b10add206244a45ab1fab9ff457b
Manu <73852745+manu10000@users.noreply.github.com>
2024-07-16 15:06:50 +0100

    changed AI to Vindicator, made it focus one player if theres player within 16 blocks, gave clone speed 2, made the clone leap forward (like ssm) instead of teleport

--- champions/src/main/java/me/mykindos/betterpvp/champions/champions/skills/skills/warlock/axe/Clone.java
+++ champions/src/main/java/me/mykindos/betterpvp/champions/champions/skills/skills/warlock/axe/Clone.java
@@ -1,6 +1,9 @@
 package me.mykindos.betterpvp.champions.champions.skills.skills.warlock.axe;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 import me.libraryaddict.disguise.DisguiseAPI;
 import me.libraryaddict.disguise.disguisetypes.Disguise;
 import me.libraryaddict.disguise.disguisetypes.PlayerDisguise;
@@ -10,6 +13,7 @@ import me.mykindos.betterpvp.champions.champions.skills.Skill;
 import me.mykindos.betterpvp.champions.champions.skills.data.SkillActions;
 import me.mykindos.betterpvp.champions.champions.skills.types.*;
 import me.mykindos.betterpvp.core.combat.events.CustomDamageEvent;
+import me.mykindos.betterpvp.core.combat.events.VelocityType;
 import me.mykindos.betterpvp.core.components.champions.Role;
 import me.mykindos.betterpvp.core.components.champions.SkillType;
 import me.mykindos.betterpvp.core.effects.EffectTypes;
@@ -17,16 +21,17 @@ import me.mykindos.betterpvp.core.framework.updater.UpdateEvent;
 import me.mykindos.betterpvp.core.listener.BPvPListener;
 import me.mykindos.betterpvp.core.utilities.*;
 import me.mykindos.betterpvp.core.utilities.events.EntityProperty;
+import me.mykindos.betterpvp.core.utilities.math.VelocityData;
 import org.bukkit.Bukkit;
 import org.bukkit.Particle;
 import org.bukkit.Sound;
 import org.bukkit.entity.EntityType;
-import org.bukkit.entity.PiglinBrute;
+import org.bukkit.entity.Vindicator;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
-import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.entity.EntityTargetEvent;
@@ -43,13 +48,17 @@ import java.util.*;
 @BPvPListener
 public class Clone extends Skill implements InteractSkill, CooldownSkill, Listener, OffensiveSkill, DebuffSkill {
 
-    WeakHashMap<PiglinBrute, Long> clones = new WeakHashMap<>();
+    WeakHashMap<Vindicator, CloneData> clones = new WeakHashMap<>();
 
     private double duration;
     private double durationIncreasePerLevel;
     private double baseHealth;
     private double healthIncreasePerLevel;
-    private double teleportDistance;
+    private double leapStrength;
+    private double effectDuration;
+    private int blindnessLevel;
+    private int slownessLevel;
+
 
     @Inject
     public Clone(Champions champions, ChampionsManager championsManager) {
@@ -70,8 +79,8 @@ public class Clone extends Skill implements InteractSkill, CooldownSkill, Listen
                 "that has " + getValueString(this::getCloneHealth, level) + " health",
                 "",
                 "Every hit your clone gets on an enemy player, they inflict:",
-                "<effect>Blindness I</effect> and <effect>Slowness I</effect>, and deals knockback.",
-                "These effects last for 1 second.",
+                "<effect>Blindness " + UtilFormat.getRomanNumeral(blindnessLevel) +"</effect> and <effect>Slowness "+ UtilFormat.getRomanNumeral(slownessLevel) +"</effect>, and deals knockback.",
+                "These effects last for <val>" + effectDuration + "</val> seconds",
                 "",
                 "Cooldown: " + getValueString(this::getCooldown, level) + " seconds."
         };
@@ -88,7 +97,7 @@ public class Clone extends Skill implements InteractSkill, CooldownSkill, Listen
 
     @Override
     public void activate(Player player, int level) {
-        PiglinBrute clone = (PiglinBrute) player.getWorld().spawnEntity(player.getLocation(), EntityType.PIGLIN_BRUTE);
+        Vindicator clone = (Vindicator) player.getWorld().spawnEntity(player.getLocation(), EntityType.VINDICATOR);
 
         Disguise disguise = new PlayerDisguise(player).setNameVisible(false);
         DisguiseAPI.disguiseToAll(clone, disguise);
@@ -97,17 +106,25 @@ public class Clone extends Skill implements InteractSkill, CooldownSkill, Listen
         setCloneProperties(clone, player);
         clone.setMetadata("spawner", new FixedMetadataValue(champions, player.getUniqueId()));
 
-        clones.put(clone, System.currentTimeMillis());
+        //Target nearest enemy
+        List<Player> nearbyEnemies = UtilPlayer.getNearbyEnemies(player, player.getLocation(), 16);
+        Player target = null;
+        if(!nearbyEnemies.isEmpty()){
+            target = nearbyEnemies.get(0);
+        }
 
-        //teleport the clone forward slightly
-        UtilLocation.teleportForward(clone, teleportDistance, false, success -> {});
+        clones.put(clone, new CloneData(target, System.currentTimeMillis()));
+
+        //leap the clone forward
+        VelocityData velocityData = new VelocityData(clone.getLocation().getDirection(), leapStrength, false, 0.0D, 0.2D, 1.0D, true);
+        UtilVelocity.velocity(clone, null, velocityData, VelocityType.CUSTOM);
     }
 
     @UpdateEvent(delay = 100)
     public void onUpdate() {
-        Iterator<Map.Entry<PiglinBrute, Long>> it = clones.entrySet().iterator();
+        Iterator<Map.Entry<Vindicator, CloneData>> it = clones.entrySet().iterator();
         while (it.hasNext()) {
-            PiglinBrute clone = it.next().getKey();
+            Vindicator clone = it.next().getKey();
 
             if (clone == null) {
                 it.remove();
@@ -130,29 +147,38 @@ public class Clone extends Skill implements InteractSkill, CooldownSkill, Listen
                 continue;
             }
 
-            if (UtilTime.elapsed(clones.get(clone), (long) getDuration(level) * 1000)) {
+            if (UtilTime.elapsed(clones.get(clone).getSpawnTime(), (long) getDuration(level) * 1000)) {
                 it.remove();
                 removeClone(clone);
             }
         }
     }
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.LOW)
     public void onEntityTarget(EntityTargetEvent event) {
-        if (event.getEntity() instanceof PiglinBrute clone && event.getTarget() instanceof Player target && clones.containsKey(clone)) {
+        if (event.getEntity() instanceof Vindicator clone && event.getTarget() instanceof Player target && clones.containsKey(clone)) {
+
+            //keep target if it is set
+            if(!(clones.get(clone).getTarget() == null)){
+                clone.setTarget(clones.get(clone).getTarget());
+            }
+
             final Player player = getCloneSpawner(clone);
-            if (UtilEntity.getRelation(player, target) == EntityProperty.FRIENDLY) event.setCancelled(true);
+            if (UtilEntity.getRelation(player, target) == EntityProperty.FRIENDLY) {
+                event.setCancelled(true);
+            }
         }
     }
 
     @EventHandler
-    public void onDamageEvent(EntityDamageByEntityEvent event) {
-        if (event.getEntity() instanceof Player player && event.getDamager() instanceof PiglinBrute clone && clones.containsKey(clone)) {
+    public void onDamageEvent(CustomDamageEvent event) {
+        if (event.getDamagee() instanceof Player player && event.getDamager() instanceof Vindicator clone && clones.containsKey(clone)) {
             event.setCancelled(true);
             handleCloneDamage(player, clone);
             return;
         }
-        if (event.getEntity() instanceof PiglinBrute clone && event.getDamager() instanceof Player player && clones.containsKey(clone)) {
+        if (event.getDamagee() instanceof Vindicator clone && event.getDamager() instanceof Player player && clones.containsKey(clone)) {
+            event.setKnockback(false);
             if (UtilEntity.getRelation(getCloneSpawner(clone), player) == EntityProperty.FRIENDLY) {
                 event.setCancelled(true);
             }
@@ -161,12 +187,12 @@ public class Clone extends Skill implements InteractSkill, CooldownSkill, Listen
 
     @EventHandler
     public void onCloneDeath(EntityDeathEvent event){
-        if(event.getEntity() instanceof PiglinBrute clone && clones.containsKey(clone)){
+        if(event.getEntity() instanceof Vindicator clone && clones.containsKey(clone)){
             removeClone(clone);
         }
     }
 
-    private void handleCloneDamage(Player player, PiglinBrute clone) {
+    private void handleCloneDamage(Player player, Vindicator clone) {
         final Player spawner = getCloneSpawner(clone);
 
         int level = getLevel(spawner);
@@ -177,8 +203,9 @@ public class Clone extends Skill implements InteractSkill, CooldownSkill, Listen
 
         UtilDamage.doCustomDamage(new CustomDamageEvent(player, spawner, null, EntityDamageEvent.DamageCause.CUSTOM, 0, true, getName()));
 
-        championsManager.getEffects().addEffect(player, EffectTypes.BLINDNESS, 1, 1000L);
-        championsManager.getEffects().addEffect(player, EffectTypes.SLOWNESS, 1, 1000L);
+        long eDuration = (long) (this.effectDuration * 1000);
+        championsManager.getEffects().addEffect(player, EffectTypes.BLINDNESS, blindnessLevel, eDuration);
+        championsManager.getEffects().addEffect(player, EffectTypes.SLOWNESS, slownessLevel, eDuration);
 
         player.playSound(player.getLocation(), Sound.BLOCK_CONDUIT_AMBIENT, 2.0f, 1.f);
         player.getWorld().playSound(player.getLocation(), Sound.BLOCK_BUBBLE_COLUMN_BUBBLE_POP, 2.0F, 1.0F);
@@ -186,12 +213,12 @@ public class Clone extends Skill implements InteractSkill, CooldownSkill, Listen
         player.getWorld().playSound(player.getLocation(), Sound.ENTITY_FIREWORK_ROCKET_SHOOT, 2.0F, 1.0F);
     }
 
-    private void setCloneProperties(PiglinBrute clone, Player player) {
+    private void setCloneProperties(Vindicator clone, Player player) {
         PlayerInventory playerInventory = player.getInventory();
         clone.setAI(true);
         clone.setHealth(getCloneHealth(getLevel(player)));
 
-        clone.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, -1, 0));
+        clone.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, -1, 1));
 
         // Copy player equipment
         clone.getEquipment().clear();
@@ -200,15 +227,9 @@ public class Clone extends Skill implements InteractSkill, CooldownSkill, Listen
         clone.getEquipment().setLeggings(playerInventory.getLeggings());
         clone.getEquipment().setBoots(playerInventory.getBoots());
         clone.getEquipment().setItemInMainHand(playerInventory.getItemInMainHand());
-
-        //Target nearest enemy
-        List<Player> nearbyEnemies = UtilPlayer.getNearbyEnemies(player, player.getLocation(), 16);
-        if(!nearbyEnemies.isEmpty()){
-            clone.setTarget(nearbyEnemies.get(0));
-        }
     }
 
-    private void removeClone(PiglinBrute clone) {
+    private void removeClone(Vindicator clone) {
         clone.getWorld().spawnParticle(Particle.SQUID_INK, clone.getLocation(), 50, 0.5, 0.5, 0.5, 0.01);
         clone.getWorld().playSound(clone.getLocation(), Sound.ENTITY_GENERIC_EXTINGUISH_FIRE, 2.0F, 1.0F);
 
@@ -218,7 +239,7 @@ public class Clone extends Skill implements InteractSkill, CooldownSkill, Listen
         clones.remove(clone);
     }
    
-    private Player getCloneSpawner(PiglinBrute clone) {
+    private Player getCloneSpawner(Vindicator clone) {
         return Bukkit.getPlayer((UUID) Objects.requireNonNull(clone.getMetadata("spawner").get(0).value()));
     }
 
@@ -248,6 +269,17 @@ public class Clone extends Skill implements InteractSkill, CooldownSkill, Listen
         durationIncreasePerLevel = getConfig("durationIncreasePerLevel", 1.5, Double.class);
         baseHealth = getConfig("baseHealth", 5.0, Double.class);
         healthIncreasePerLevel = getConfig("healthIncreasePerLevel", 2.5, Double.class);
-        teleportDistance = getConfig("healthIncreasePerLevel", 2.0, Double.class);
+        leapStrength = getConfig("leapStrength", 2.0, Double.class);
+        blindnessLevel = getConfig("blindnessLevel", 2, Integer.class);
+        slownessLevel = getConfig("slownessLevel", 1, Integer.class);
+        effectDuration = getConfig("effectDuration", 1.0, Double.class);
+    }
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    private static class CloneData {
+        private final Player target;
+        private final long spawnTime;
     }
 }
