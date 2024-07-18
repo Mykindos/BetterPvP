5aa1165bf51ed334dee8437a9a9fcd495ff2b60e
Polisvh <145322774+Polisvh@users.noreply.github.com>
2024-07-18 23:51:26 +0200

    Update SmokeBomb.java

--- champions/src/main/java/me/mykindos/betterpvp/champions/champions/skills/skills/assassin/passives/SmokeBomb.java
+++ champions/src/main/java/me/mykindos/betterpvp/champions/champions/skills/skills/assassin/passives/SmokeBomb.java
@@ -70,7 +70,7 @@ public class SmokeBomb extends Skill implements CooldownToggleSkill, Listener, D
                 "or taking damage",
                 "will cause you to reappear",
                 "",
-                "Cooldown: " + getValueString(this::getCooldown, level)
+                "Cooldown: " + getValueString(this::getCooldown, level),
                 "",
                 EffectTypes.VANISH.getDescription(0)
         };
