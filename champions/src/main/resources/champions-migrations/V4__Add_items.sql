3e0b0a82482f6dd0dda0c772c2a4b3c8f48c33bb
Mykindos <5389944+Mykindos@users.noreply.github.com>
2024-07-27 05:38:42 +1000

    Remove wings of zanzul

--- champions/src/main/resources/champions-migrations/V4__Add_items.sql
+++ champions/src/main/resources/champions-migrations/V4__Add_items.sql
@@ -325,6 +325,3 @@ INSERT IGNORE INTO items (Material, Namespace, Keyname, Name, ModelData, Glow, H
 
 INSERT IGNORE INTO items (Material, Namespace, Keyname, Name, ModelData, Glow, HasUUID) VALUES
     ('MUSIC_DISC_CAT', 'champions', 'giants_broadsword', '<orange>Giant''s Broadsword', 1, 0, 1);
-
-INSERT IGNORE INTO items (Material, Namespace, Keyname, Name, ModelData, Glow, HasUUID) VALUES
-    ('ELYTRA', 'champions', 'wings_of_zanzul', '<orange>Wings of Zanzul', 1, 0, 1);
