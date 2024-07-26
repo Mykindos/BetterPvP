cde6d44ed4ff03797a4707e884d56b35f8ac3509
Braulio <40250304+braulio-dev@users.noreply.github.com>
2024-07-25 21:53:36 -0700

    Allow unbound cannons that can be used by anyone (#969)
    * Allow unbound cannons that can be used by anyone
    
    * Remove debug

--- clans/src/main/java/me/mykindos/betterpvp/clans/weapons/impl/cannon/event/CannonPlaceEvent.java
+++ clans/src/main/java/me/mykindos/betterpvp/clans/weapons/impl/cannon/event/CannonPlaceEvent.java
@@ -6,6 +6,7 @@ import me.mykindos.betterpvp.core.framework.events.CustomEvent;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * Called when a {@link Player} places a cannon at a {@link Location}.
@@ -15,9 +16,9 @@ public class CannonPlaceEvent extends CustomEvent {
 
     private final @NotNull Cannon cannon;
     private final @NotNull Location cannonLocation;
-    private final @NotNull Player player;
+    private final @Nullable Player player;
 
-    public CannonPlaceEvent(@NotNull Cannon cannon, @NotNull Location cannonLocation, @NotNull Player player) {
+    public CannonPlaceEvent(@NotNull Cannon cannon, @NotNull Location cannonLocation, @Nullable Player player) {
         this.cannon = cannon;
         this.cannonLocation = cannonLocation;
         this.player = player;
