bfdbffef3aaa468d887d19ec36d9c93fa1824777
Polisvh <145322774+Polisvh@users.noreply.github.com>
2024-07-27 15:49:52 +0200

    Update Bloodlust Description
    Used immolate logic for strenght description so it can appear yellow as static dmg

--- champions/src/main/java/me/mykindos/betterpvp/champions/champions/skills/skills/brute/passives/Bloodlust.java
+++ champions/src/main/java/me/mykindos/betterpvp/champions/champions/skills/skills/brute/passives/Bloodlust.java
@@ -64,7 +64,7 @@ public class Bloodlust extends Skill implements PassiveSkill, BuffSkill, HealthS
                 "Bloodlust can stack up to " + getValueString(this::getMaxStacks, level) + " times",
                 "boosting the level of <effect>Speed</effect> and <effect>Strength</effect> by 1",
                 "",
-                EffectTypes.STRENGTH.getGenericDescription()
+                EffectTypes.STRENGTH.getDescription(strengthLevel)
         };
     }
 
