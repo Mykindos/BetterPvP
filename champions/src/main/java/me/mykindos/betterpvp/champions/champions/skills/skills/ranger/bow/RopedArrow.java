59aff8e6d6a7c161abd5d0964f96a6a8f53c4c86
Mykindos <5389944+Mykindos@users.noreply.github.com>
2024-08-02 22:51:36 +1000

    Fix no fall

--- champions/src/main/java/me/mykindos/betterpvp/champions/champions/skills/skills/ranger/bow/RopedArrow.java
+++ champions/src/main/java/me/mykindos/betterpvp/champions/champions/skills/skills/ranger/bow/RopedArrow.java
@@ -97,7 +97,7 @@ public class RopedArrow extends PrepareArrowSkill implements MovementSkill {
             Player target = Bukkit.getPlayer(uuid);
             if(target != null) {
                 championsManager.getEffects().addEffect(player, player, EffectTypes.NO_FALL,getName(), (int) fallDamageLimit,
-                        50L, true, true, UtilBlock::isGrounded);
+                        250L, true, true, UtilBlock::isGrounded);
             }
         }, 1000));
 
