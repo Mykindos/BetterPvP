f4a6957cc2bfb2cb8e884fe04c97977313c388c3
zande <zanderaguirre@icloud.com>
2024-08-14 18:16:16 -0700

    test

--- champions/src/main/java/me/mykindos/betterpvp/champions/champions/skills/skills/brute/axe/UnstoppableForce.java
+++ champions/src/main/java/me/mykindos/betterpvp/champions/champions/skills/skills/brute/axe/UnstoppableForce.java
@@ -12,8 +12,6 @@ import me.mykindos.betterpvp.champions.champions.skills.types.InteractSkill;
 import me.mykindos.betterpvp.champions.champions.skills.types.MovementSkill;
 import me.mykindos.betterpvp.champions.champions.skills.types.OffensiveSkill;
 import me.mykindos.betterpvp.core.client.gamer.Gamer;
-import me.mykindos.betterpvp.core.combat.events.CustomDamageEvent;
-import me.mykindos.betterpvp.core.combat.events.VelocityType;
 import me.mykindos.betterpvp.core.components.champions.Role;
 import me.mykindos.betterpvp.core.components.champions.SkillType;
 import me.mykindos.betterpvp.core.effects.EffectTypes;
@@ -21,7 +19,6 @@ import me.mykindos.betterpvp.core.effects.events.EffectReceiveEvent;
 import me.mykindos.betterpvp.core.framework.updater.UpdateEvent;
 import me.mykindos.betterpvp.core.listener.BPvPListener;
 import me.mykindos.betterpvp.core.utilities.UtilBlock;
-import me.mykindos.betterpvp.core.utilities.UtilDamage;
 import me.mykindos.betterpvp.core.utilities.UtilEntity;
 import me.mykindos.betterpvp.core.utilities.UtilMessage;
 import me.mykindos.betterpvp.core.utilities.UtilPlayer;
@@ -30,16 +27,18 @@ import me.mykindos.betterpvp.core.utilities.UtilVelocity;
 import me.mykindos.betterpvp.core.utilities.math.VelocityData;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.Particle;
 import org.bukkit.Sound;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.block.Action;
-import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityPotionEffectEvent;
 import org.bukkit.potion.PotionEffectType;
 import org.bukkit.util.RayTraceResult;
+import org.bukkit.util.Vector;
 
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Optional;
 import java.util.UUID;
@@ -51,6 +50,8 @@ public class UnstoppableForce extends ChannelSkill implements InteractSkill, Ene
     private double baseDamage;
     private double damageIncreasePerLevel;
     private double hitboxExpansion;
+    private double chargeSensitivity;
+    private HashMap<UUID, Vector> initialDirections = new HashMap<>();
 
     @Inject
     public UnstoppableForce(Champions champions, ChampionsManager championsManager) {
@@ -65,16 +66,15 @@ public class UnstoppableForce extends ChannelSkill implements InteractSkill, Ene
     @Override
     public String[] getDescription(int level) {
         return new String[]{
-                "Hold right click with an axe to channel",
+                "Hold right click with a Sword to channel",
                 "",
                 "Raise your shield and begin to charge at high speed.",
-                "Deals " + getValueString(this::getDamage, level) + " damage and knocks back any enemy hit.",
+                "Deals <val>" + getDamage(level) + "</val> damage and knocks back any enemy hit.",
                 "",
                 "While charging, you are immune to any crowd control effects",
                 "",
-                "Energy: " + getValueString(this::getEnergy, level) + " per second",
-                "Cooldown: " + getValueString(this::getCooldown, level) + " seconds starting when the charge ends"
-
+                "Energy: <val>" + getEnergy(level) + "</val> per second",
+                "Cooldown: <val>" + getCooldown(level) + "</val> seconds starting when the charge ends"
         };
     }
 
@@ -90,10 +90,11 @@ public class UnstoppableForce extends ChannelSkill implements InteractSkill, Ene
             return;
         }
         active.add(player.getUniqueId());
+        initialDirections.put(player.getUniqueId(), player.getLocation().getDirection());
     }
 
     @UpdateEvent
-    public void doUnstoppableForce() {
+    public void doCharge() {
         final Iterator<UUID> iterator = active.iterator();
         while (iterator.hasNext()) {
             Player player = Bukkit.getPlayer(iterator.next());
@@ -102,25 +103,23 @@ public class UnstoppableForce extends ChannelSkill implements InteractSkill, Ene
                 continue;
             }
 
-
             Gamer gamer = championsManager.getClientManager().search().online(player).getGamer();
             if (!gamer.isHoldingRightClick()) {
-                finishUnstoppableForce(player);
+                finishCharge(player);
                 iterator.remove();
                 continue;
             }
 
             int level = getLevel(player);
             if (level <= 0) {
-                finishUnstoppableForce(player);
+                finishCharge(player);
                 iterator.remove();
             } else if (!championsManager.getEnergy().use(player, getName(), getEnergy(level) / 20, true)) {
-                finishUnstoppableForce(player);
+                finishCharge(player);
                 iterator.remove();
             } else if (!isHolding(player)) {
-                finishUnstoppableForce(player);
+                finishCharge(player);
                 iterator.remove();
-
             }
 
             if (UtilBlock.isGrounded(player)) {
@@ -128,8 +127,22 @@ public class UnstoppableForce extends ChannelSkill implements InteractSkill, Ene
 
                 final Location newLocation = UtilPlayer.getMidpoint(player).clone();
 
-                VelocityData velocityData = new VelocityData(player.getLocation().getDirection(), 0.5, true, 0, 0, 0.0, false);
+                Vector initialDirection = initialDirections.get(player.getUniqueId());
+                Vector currentDirection = player.getLocation().getDirection();
+                Vector newDirection = initialDirection.clone().multiply(1 - chargeSensitivity).add(currentDirection.clone().multiply(chargeSensitivity)).normalize();
+                initialDirections.put(player.getUniqueId(), newDirection);
+
+                double slownessMultiplier = 1.0;
+                if (player.hasPotionEffect(PotionEffectType.SLOWNESS)) {
+                    int slownessLevel = player.getPotionEffect(PotionEffectType.SLOWNESS).getAmplifier() + 1;
+                    slownessMultiplier = Math.max(0, 1 - (slownessLevel * 0.15));
+                }
+
+                VelocityData velocityData = new VelocityData(newDirection, 0.6 * slownessMultiplier, true, 0, 0, 0.0, false);
                 UtilVelocity.velocity(player, null, velocityData);
+
+                Location chestLocation = player.getLocation().clone().add(0, 1.2, 0);
+                player.getWorld().spawnParticle(Particle.GUST, chestLocation, 1, 0.2, 0.2, 0.2, 0);
 
                 final Optional<LivingEntity> hit = UtilEntity.interpolateCollision(newLocation,
                                 newLocation.clone().add(velocityData.getVector()),
@@ -139,16 +152,16 @@ public class UnstoppableForce extends ChannelSkill implements InteractSkill, Ene
 
                 if (hit.isPresent()) {
                     final LivingEntity target = hit.get();
-                    var cde = UtilDamage.doCustomDamage(new CustomDamageEvent(target, player, player, EntityDamageEvent.DamageCause.CUSTOM, getDamage(level), false, getName()));
-                    if (cde != null && !cde.isCancelled()) {
-                        VelocityData targetVelocityData = new VelocityData(player.getLocation().getDirection(), 2, true, 0.4, 0.4, 0.4, true);
-                        UtilVelocity.velocity(target, player, targetVelocityData, VelocityType.KNOCKBACK_CUSTOM);
-                        player.getWorld().playSound(target.getLocation(), Sound.ENTITY_ZOMBIE_ATTACK_IRON_DOOR, 0.5f, 0.9f);
+
+                    if (player.getPassengers().isEmpty()) {
+                        player.addPassenger(target);
+                    } else {
+                        LivingEntity currentPassenger = (LivingEntity) player.getPassengers().get(0);
+                        currentPassenger.addPassenger(target);
                     }
                 }
             }
         }
-
     }
 
     @UpdateEvent(delay = 100)
@@ -161,11 +174,16 @@ public class UnstoppableForce extends ChannelSkill implements InteractSkill, Ene
         }
     }
 
-    private void finishUnstoppableForce(Player player) {
+    private void finishCharge(Player player) {
         championsManager.getCooldowns().use(player, getName(), getCooldown(getLevel(player)), true,
-                true, false, isHolding(player) && (getType() == SkillType.AXE));
+                true, false, isHolding(player) && (getType() == SkillType.SWORD));
         championsManager.getEffects().removeEffect(player, EffectTypes.NO_JUMP, getName());
+        initialDirections.remove(player.getUniqueId());
+
+        // Dismount all entities
+        player.getPassengers().forEach(entity -> entity.leaveVehicle());
     }
+
 
     @EventHandler
     public void onPotionEffect(EntityPotionEffectEvent event) {
@@ -175,7 +193,7 @@ public class UnstoppableForce extends ChannelSkill implements InteractSkill, Ene
         var effect = event.getNewEffect();
         if (effect == null) return;
 
-        if (effect.getType() == PotionEffectType.SLOWNESS || effect.getType() == PotionEffectType.LEVITATION) {
+        if (effect.getType() == PotionEffectType.LEVITATION) {
             event.setCancelled(true);
         }
     }
@@ -183,10 +201,8 @@ public class UnstoppableForce extends ChannelSkill implements InteractSkill, Ene
     @EventHandler
     public void onNegativeEffect(EffectReceiveEvent event) {
         if (!active.contains(event.getTarget().getUniqueId())) return;
-        if (event.getEffect().getEffectType() == EffectTypes.STUN
-                || event.getEffect().getEffectType() == EffectTypes.SILENCE
-                || event.getEffect().getEffectType() == EffectTypes.SLOWNESS) {
-            event.setCancelled(true);
+        if (event.getEffect().getEffectType() == EffectTypes.SILENCE) {
+            finishCharge((Player)event.getTarget());
         }
     }
 
@@ -194,6 +210,7 @@ public class UnstoppableForce extends ChannelSkill implements InteractSkill, Ene
     public Action[] getActions() {
         return SkillActions.RIGHT_CLICK;
     }
+
 
     @Override
     public Role getClassType() {
@@ -232,10 +249,11 @@ public class UnstoppableForce extends ChannelSkill implements InteractSkill, Ene
     public void loadSkillConfig() {
         baseDamage = getConfig("baseDamage", 4.0, Double.class);
         damageIncreasePerLevel = getConfig("damageIncreasePerLevel", 1.0, Double.class);
-        cooldown = getConfig("cooldown", 15.0, Double.class);
-        cooldownDecreasePerLevel = getConfig("cooldownDecreasePerLevel", 1.0, Double.class);
-        energy = getConfig("energy", 55, Integer.class);
-        energyDecreasePerLevel = getConfig("energyDecreasePerLevel", 1.0, Double.class);
-        hitboxExpansion = getConfig("hitboxExpansion", 0.3, Double.class);
+        cooldown = getConfig("cooldown", 12.0, Double.class);
+        cooldownDecreasePerLevel = getConfig("cooldownDecreasePerLevel", 1.5, Double.class);
+        energy = getConfig("energy", 80, Integer.class);
+        energyDecreasePerLevel = getConfig("energyDecreasePerLevel", 10.0, Double.class);
+        hitboxExpansion = getConfig("hitboxExpansion", 1.2, Double.class);
+        chargeSensitivity = getConfig("chargeSensitivity", 0.03, Double.class);
     }
 }
