b9f10991ac827fd0e13040f141ddcefa08057c76
zande <zanderaguirre@icloud.com>
2024-08-16 12:40:49 -0700

    sin passive changes

--- champions/src/main/java/me/mykindos/betterpvp/champions/champions/skills/skills/assassin/passives/ComboAttack.java
+++ champions/src/main/java/me/mykindos/betterpvp/champions/champions/skills/skills/assassin/passives/ComboAttack.java
@@ -1,8 +1,5 @@
 package me.mykindos.betterpvp.champions.champions.skills.skills.assassin.passives;
 
-
-import com.google.inject.Inject;
-import com.google.inject.Singleton;
 import me.mykindos.betterpvp.champions.Champions;
 import me.mykindos.betterpvp.champions.champions.ChampionsManager;
 import me.mykindos.betterpvp.champions.champions.skills.Skill;
@@ -22,21 +19,24 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.WeakHashMap;
 
 @Singleton
 @BPvPListener
 public class ComboAttack extends Skill implements PassiveSkill, Listener, DamageSkill, OffensiveSkill {
 
-    private final WeakHashMap<Player, Double> repeat = new WeakHashMap<>();
+    private final WeakHashMap<Player, Map<Player, Double>> repeat = new WeakHashMap<>();
     private final WeakHashMap<Player, Long> last = new WeakHashMap<>();
 
     @Inject
     public ComboAttack(Champions champions, ChampionsManager championsManager) {
         super(champions, championsManager);
     }
-
 
     @Override
     public String getName() {
@@ -82,37 +82,35 @@ public class ComboAttack extends Skill implements PassiveSkill, Listener, Damage
         return SkillType.PASSIVE_B;
     }
 
-
     @EventHandler(priority = EventPriority.HIGH)
     public void onDamage(CustomDamageEvent event) {
         if (event.getCause() != DamageCause.ENTITY_ATTACK) return;
         if (!(event.getDamager() instanceof Player damager)) return;
+        if (!(event.getDamagee() instanceof Player target)) return;
         if (!championsManager.getRoles().hasRole(damager, Role.ASSASSIN)) return;
 
         int level = getLevel(damager);
         if (level > 0) {
+            repeat.putIfAbsent(damager, new HashMap<>());
+            Map<Player, Double> targetDamage = repeat.get(damager);
+            targetDamage.putIfAbsent(target, 0.0);
 
-            if (!repeat.containsKey(damager)) {
-                repeat.put(damager, 0.0);
-            }
-            double cur = repeat.get(damager);
+            double cur = targetDamage.get(target);
             event.setDamage(event.getDamage() + cur);
-            repeat.put(damager, Math.min(getMaxDamageIncrement(level), cur + damageIncrement));
+            targetDamage.put(target, Math.min(getMaxDamageIncrement(level), cur + damageIncrement));
             last.put(damager, System.currentTimeMillis());
             event.addReason(getName());
 
-            damager.getWorld().playSound(damager.getLocation(), Sound.BLOCK_NOTE_BLOCK_HAT, 1f, (float) (0.7f + (0.3f * repeat.get(damager))));
-
+            damager.getWorld().playSound(damager.getLocation(), Sound.BLOCK_NOTE_BLOCK_HAT, 1f, (float) (0.7f + (0.3f * targetDamage.get(target))));
         }
     }
-
 
     @UpdateEvent(delay = 500)
     public void onUpdate() {
 
         HashSet<Player> remove = new HashSet<>();
 
-        for (Player player : repeat.keySet()) {
+        for (Player player : last.keySet()) {
             if (UtilTime.elapsed(last.get(player), (long) duration * 1000)) {
                 remove.add(player);
             }
