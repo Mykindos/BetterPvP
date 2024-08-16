41c61934f8bc2b43e15779dfaee46f5d0bc5e351
StudentAlleg <studentalleg-pilot@yahoo.com>
2024-08-15 20:03:10 -0700

    Update Invsee

--- core/src/main/java/me/mykindos/betterpvp/core/command/commands/admin/InvSeeCommand.java
+++ core/src/main/java/me/mykindos/betterpvp/core/command/commands/admin/InvSeeCommand.java
@@ -1,13 +1,18 @@
 package me.mykindos.betterpvp.core.command.commands.admin;
 
 import com.google.inject.Singleton;
+import lombok.extern.slf4j.Slf4j;
 import me.mykindos.betterpvp.core.client.Client;
 import me.mykindos.betterpvp.core.command.Command;
+import me.mykindos.betterpvp.core.command.menus.PlayerInventoryMenu;
 import me.mykindos.betterpvp.core.utilities.UtilMessage;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 
 @Singleton
+@Slf4j
 public class InvSeeCommand extends Command {
 
     public InvSeeCommand() {
@@ -33,7 +38,7 @@ public class InvSeeCommand extends Command {
 
         Player target = Bukkit.getPlayer(args[0]);
         if (target != null) {
-            player.openInventory(target.getInventory());
+            new PlayerInventoryMenu(target).show(player);
             UtilMessage.simpleMessage(player, "You opened <yellow>" + target.getName() + "'s <gray>inventory.");
         } else {
             UtilMessage.simpleMessage(player, "Could not find player <yellow>" + args[0]);
