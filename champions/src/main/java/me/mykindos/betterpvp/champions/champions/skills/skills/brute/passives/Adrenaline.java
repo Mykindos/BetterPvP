6cf18c6cf72d6a6b28aafeae33b41d81ec9d3ee1
zande <zanderaguirre@icloud.com>
2024-08-25 21:46:07 -0700

    adrenaline

---
+++ champions/src/main/java/me/mykindos/betterpvp/champions/champions/skills/skills/brute/passives/Adrenaline.java
@@ -0,0 +1,122 @@
+package me.mykindos.betterpvp.champions.champions.skills.skills.brute.passives;
+
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import me.mykindos.betterpvp.champions.Champions;
+import me.mykindos.betterpvp.champions.champions.ChampionsManager;
+import me.mykindos.betterpvp.champions.champions.skills.Skill;
+import me.mykindos.betterpvp.champions.champions.skills.skills.assassin.data.FlashData;
+import me.mykindos.betterpvp.champions.champions.skills.types.DamageSkill;
+import me.mykindos.betterpvp.champions.champions.skills.types.OffensiveSkill;
+import me.mykindos.betterpvp.champions.champions.skills.types.PassiveSkill;
+import me.mykindos.betterpvp.core.client.gamer.Gamer;
+import me.mykindos.betterpvp.core.combat.events.CustomDamageEvent;
+import me.mykindos.betterpvp.core.components.champions.Role;
+import me.mykindos.betterpvp.core.components.champions.SkillType;
+import me.mykindos.betterpvp.core.effects.EffectTypes;
+import me.mykindos.betterpvp.core.framework.updater.UpdateEvent;
+import me.mykindos.betterpvp.core.listener.BPvPListener;
+import org.bukkit.Bukkit;
+import org.bukkit.Sound;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.scheduler.BukkitTask;
+
+import java.util.WeakHashMap;
+
+@Singleton
+@BPvPListener
+public class Adrenaline extends Skill implements PassiveSkill, Listener, DamageSkill, OffensiveSkill {
+
+    private double speedOneHealth;
+    private double speedOneHealthIncreasePerLevel;
+    private double speedTwoHealth;
+    private double speedTwoHealthIncreasePerLevel;
+
+    private final WeakHashMap<Player, Integer> playerSpeedLevels = new WeakHashMap<>();
+
+    @Inject
+    public Adrenaline(Champions champions, ChampionsManager championsManager) {
+        super(champions, championsManager);
+    }
+
+    @Override
+    public String getName() {
+        return "Adrenaline";
+    }
+
+    @Override
+    public String[] getDescription(int level) {
+        return new String[]{
+                "Below " + getValueString(this::getSpeedOneHealth, level, 100, "%", 1) + " health you gain <effect>Speed I</effect>,",
+                "and below " + getValueString(this::getSpeedTwoHealth, level, 100, "%", 1) + " health you gain <effect>Speed II</effect> ",
+        };
+    }
+
+    public double getSpeedOneHealth(int level) {
+        return speedOneHealth + ((level - 1) * speedOneHealthIncreasePerLevel);
+    }
+
+    public double getSpeedTwoHealth(int level) {
+        return speedTwoHealth + ((level - 1) * speedTwoHealthIncreasePerLevel);
+    }
+
+    @Override
+    public Role getClassType() {
+        return Role.BRUTE;
+    }
+
+    @Override
+    public SkillType getType() {
+        return SkillType.PASSIVE_B;
+    }
+
+    @UpdateEvent
+    public void giveSpeed() {
+        for (Player player : playerSpeedLevels.keySet()) {
+            int level = getLevel(player);
+            if (level > 0) {
+                double healthThresholdSpeedOne = getSpeedOneHealth(level) * player.getMaxHealth();
+                double healthThresholdSpeedTwo = getSpeedTwoHealth(level) * player.getMaxHealth();
+
+                if (player.getHealth() <= healthThresholdSpeedTwo) {
+                    // Player should have Speed II
+                    championsManager.getEffects().addEffect(player, player, EffectTypes.SPEED, 2, 500);
+                    playerSpeedLevels.put(player, 2);
+                } else if (player.getHealth() <= healthThresholdSpeedOne) {
+                    // Player should have Speed I
+                    championsManager.getEffects().addEffect(player, player, EffectTypes.SPEED, 1, 500);
+                    playerSpeedLevels.put(player, 1);
+                } else {
+                    Integer currentSpeedLevel = playerSpeedLevels.get(player);
+                    if (currentSpeedLevel != null && currentSpeedLevel > 0) {
+                        championsManager.getEffects().removeEffect(player, EffectTypes.SPEED);
+                        playerSpeedLevels.put(player, 0);
+                    }
+                }
+            }
+        }
+    }
+
+    @Override
+    public void invalidatePlayer(Player player, Gamer gamer) {
+        playerSpeedLevels.remove(player);
+    }
+
+    @Override
+    public void trackPlayer(Player player, Gamer gamer) {
+        playerSpeedLevels.put(player, 0);
+    }
+
+    @Override
+    public void loadSkillConfig() {
+        speedOneHealth = getConfig("speedOneHealth", 0.35, Double.class);
+        speedOneHealthIncreasePerLevel = getConfig("speedOneHealthIncreasePerLevel", 0.15, Double.class);
+        speedTwoHealth = getConfig("speedTwoHealth", 0.15, Double.class);
+        speedTwoHealthIncreasePerLevel = getConfig("speedTwoHealthIncreasePerLevel", 0.075, Double.class);
+    }
+}
