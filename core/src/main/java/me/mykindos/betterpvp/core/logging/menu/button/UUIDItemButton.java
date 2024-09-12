dd1ff2a66b9c1e0baa42dc272cf790645dd70755
StudentAlleg <studentalleg-pilot@yahoo.com>
2024-09-12 00:11:05 -0700

    Add Filters to logs

--- core/src/main/java/me/mykindos/betterpvp/core/logging/menu/button/UUIDItemButton.java
+++ core/src/main/java/me/mykindos/betterpvp/core/logging/menu/button/UUIDItemButton.java
@@ -1,24 +1,15 @@
 
 package me.mykindos.betterpvp.core.logging.menu.button;
 
-import me.mykindos.betterpvp.core.Core;
 import me.mykindos.betterpvp.core.framework.BPvPPlugin;
 import me.mykindos.betterpvp.core.inventory.item.ItemProvider;
-import me.mykindos.betterpvp.core.inventory.item.impl.AbstractItem;
-import me.mykindos.betterpvp.core.logging.CachedLog;
 import me.mykindos.betterpvp.core.logging.LogContext;
 import me.mykindos.betterpvp.core.logging.menu.CachedLogMenu;
 import me.mykindos.betterpvp.core.logging.repository.LogRepository;
 import me.mykindos.betterpvp.core.menu.Windowed;
-import me.mykindos.betterpvp.core.utilities.UtilServer;
 import me.mykindos.betterpvp.core.utilities.model.item.ItemView;
 import net.kyori.adventure.text.Component;
 import org.bukkit.Material;
-import org.bukkit.entity.Player;
-import org.bukkit.event.inventory.ClickType;
-import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.plugin.java.JavaPlugin;
-import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
@@ -26,7 +17,7 @@ public class UUIDItemButton extends LogRepositoryButton {
     private final String uuid;
 
     public UUIDItemButton(String name, String uuid, BPvPPlugin plugin, LogRepository logRepository, Windowed previous) {
-        super(name, LogContext.ITEM, uuid, "ITEM_", plugin, logRepository, previous);
+        super(name, LogContext.ITEM, uuid, "ITEM_", CachedLogMenu.ITEM, plugin, logRepository, previous);
         this.uuid = uuid;
     }
 
