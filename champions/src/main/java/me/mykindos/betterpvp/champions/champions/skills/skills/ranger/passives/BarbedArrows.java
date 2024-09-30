317ae1e9d39099d85ed0004c17e271dc2680a226
zande <zanderaguirre@icloud.com>
2024-09-30 12:22:30 -0700

    passive A

--- champions/src/main/java/me/mykindos/betterpvp/champions/champions/skills/skills/ranger/passives/BarbedArrows.java
+++ champions/src/main/java/me/mykindos/betterpvp/champions/champions/skills/skills/ranger/passives/BarbedArrows.java
@@ -5,29 +5,48 @@ import com.google.inject.Singleton;
 import me.mykindos.betterpvp.champions.Champions;
 import me.mykindos.betterpvp.champions.champions.ChampionsManager;
 import me.mykindos.betterpvp.champions.champions.skills.Skill;
-import me.mykindos.betterpvp.champions.champions.skills.types.DebuffSkill;
-import me.mykindos.betterpvp.champions.champions.skills.types.OffensiveSkill;
+import me.mykindos.betterpvp.champions.champions.skills.types.DamageSkill;
 import me.mykindos.betterpvp.champions.champions.skills.types.PassiveSkill;
 import me.mykindos.betterpvp.core.combat.events.CustomDamageEvent;
 import me.mykindos.betterpvp.core.components.champions.Role;
 import me.mykindos.betterpvp.core.components.champions.SkillType;
-import me.mykindos.betterpvp.core.effects.EffectTypes;
+import me.mykindos.betterpvp.core.framework.updater.UpdateEvent;
 import me.mykindos.betterpvp.core.listener.BPvPListener;
-import me.mykindos.betterpvp.core.utilities.UtilFormat;
+import me.mykindos.betterpvp.core.utilities.UtilMessage;
+import me.mykindos.betterpvp.core.utilities.UtilServer;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Particle;
+import org.bukkit.Sound;
+import org.bukkit.World;
 import org.bukkit.entity.Arrow;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
+import org.bukkit.entity.Trident;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityShootBowEvent;
+import org.bukkit.event.entity.ProjectileHitEvent;
+import org.bukkit.util.Vector;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.UUID;
+import java.util.WeakHashMap;
 
 @Singleton
 @BPvPListener
-public class BarbedArrows extends Skill implements PassiveSkill, DebuffSkill, OffensiveSkill {
+public class BarbedArrows extends Skill implements PassiveSkill, DamageSkill {
+    private final WeakHashMap<LivingEntity, Double> data = new WeakHashMap<>();
+    private final Map<UUID, Long> arrowHitTime = new HashMap<>();
+    private final WeakHashMap<Projectile, Location> barbedProjectiles = new WeakHashMap<>();
+    private final Map<UUID, LivingEntity> playerToEntityMap = new HashMap<>();
 
-    private double baseDuration;
-
-    private double durationIncreasePerLevel;
-
-    private int slownessStrength;
+    private double baseDamage;
+    private double damageIncreasePerLevel;
+    private double damageResetTime;
 
     @Inject
     public BarbedArrows(Champions champions, ChampionsManager championsManager) {
@@ -41,14 +60,25 @@ public class BarbedArrows extends Skill implements PassiveSkill, DebuffSkill, Of
 
     @Override
     public String[] getDescription(int level) {
-        return new String[] {
-                "Your arrows apply <effect>Slowness " + UtilFormat.getRomanNumeral(slownessStrength) + "</effect> to any",
-                "damageable target for " + getValueString(this::getDuration, level) + " seconds"
+        return new String[]{
+                "Hitting an arrow will stick a barb into the target",
+                "melee hits on that target will rip the barb out,",
+                "dealing " + getValueString(this::getDamage, level) + " extra damage",
+                "",
+                "The barb will fall out after " + getValueString(this::getDamageResetTime, level) + " seconds"
         };
     }
 
-    public double getDuration(int level) {
-        return baseDuration + (level - 1) * durationIncreasePerLevel;
+    public double getDamage(int level) {
+        return baseDamage + (damageIncreasePerLevel * (level - 1));
+    }
+
+    public double getDamageResetTime(int level) {
+        return damageResetTime;
+    }
+
+    private boolean isValidProjectile(Projectile projectile) {
+        return projectile instanceof Arrow || projectile instanceof Trident;
     }
 
     @Override
@@ -56,29 +86,158 @@ public class BarbedArrows extends Skill implements PassiveSkill, DebuffSkill, Of
         return Role.RANGER;
     }
 
+    @EventHandler
+    public void onProjectileHit(CustomDamageEvent event) {
+        if (!(event.getProjectile() instanceof Projectile projectile)) return;
+        if (!isValidProjectile(projectile)) return;
+        if (!(event.getDamager() instanceof Player player)) return;
+
+        int level = getLevel(player);
+        if (level > 0) {
+            LivingEntity damagee = event.getDamagee();
+            data.put(damagee, getDamage(level));
+            arrowHitTime.put(player.getUniqueId(), System.currentTimeMillis());
+            playerToEntityMap.put(player.getUniqueId(), damagee);
+            barbedProjectiles.remove(projectile);
+            damagee.getWorld().playSound(damagee.getLocation(), Sound.ENTITY_ARROW_HIT, 1.0f, 1.0f);
+        }
+    }
+
+
+
+    @EventHandler
+    public void onHit(CustomDamageEvent event){
+        if (!(event.getDamager() instanceof Player player)) return;
+        if (event.getCause() != EntityDamageEvent.DamageCause.ENTITY_ATTACK) return;
+        int level = getLevel(player);
+
+        if (level > 0) {
+            if (!data.containsKey(event.getDamagee())) {
+                return;
+            }
+
+            double extraDamage = data.get(event.getDamagee());
+            event.addReason(getName());
+            event.setDamage(event.getDamage() + extraDamage);
+
+            UtilMessage.simpleMessage(player, getClassType().getName(), "<alt>%s</alt> dealt <alt2>%s</alt2> extra damage", getName(), extraDamage);
+            player.playSound(player.getLocation(), Sound.ENTITY_BREEZE_JUMP, 1.0f, 1.0f);
+            data.remove(event.getDamagee());
+            arrowHitTime.remove(player.getUniqueId());
+        }
+    }
+
+    @EventHandler
+    public void onProjectileHit(ProjectileHitEvent event) {
+        if (event.getEntity() instanceof Projectile projectile) {
+            if (event.getHitBlock() != null || event.getHitEntity() == null) {
+                barbedProjectiles.entrySet().removeIf(entry -> entry.getKey().equals(projectile));
+            }
+
+            UtilServer.runTaskLater(champions, () -> {
+                barbedProjectiles.entrySet().removeIf(entry -> entry.getKey().equals(projectile));
+            }, 2L);
+        }
+    }
+
+    @UpdateEvent
+    public void updateBarbedData() {
+        long currentTime = System.currentTimeMillis();
+
+        Iterator<Map.Entry<UUID, Long>> iterator = arrowHitTime.entrySet().iterator();
+        while (iterator.hasNext()) {
+            Map.Entry<UUID, Long> entry = iterator.next();
+            UUID playerUuid = entry.getKey();
+            Long lastTimeHit = entry.getValue();
+
+            Player player = Bukkit.getPlayer(playerUuid);
+            int level = getLevel(player);
+
+            if (player == null || level <= 0) {
+                iterator.remove();
+                playerToEntityMap.remove(playerUuid);
+                continue;
+            }
+
+            if (currentTime - lastTimeHit > getDamageResetTime(level) * 1000) {
+                UtilMessage.simpleMessage(player, getClassType().getName(), "Your <alt>%s</alt> have fallen out.", getName());
+                iterator.remove();
+
+                LivingEntity hitEntity = playerToEntityMap.get(playerUuid);
+                if (hitEntity != null) {
+                    data.remove(hitEntity);
+                    playerToEntityMap.remove(playerUuid);
+                }
+            }
+        }
+
+        data.entrySet().removeIf(entry -> !playerToEntityMap.containsValue(entry.getKey()));
+    }
+
+
+
+
+    @UpdateEvent
+    public void updateArrowTrail() {
+        Iterator<Projectile> it = barbedProjectiles.keySet().iterator();
+        while (it.hasNext()) {
+            Projectile next = it.next();
+            if (next == null) {
+                it.remove();
+            } else if (next.isDead()) {
+                it.remove();
+            } else {
+                Location location = next.getLocation();
+                Particle.ENCHANTED_HIT.builder()
+                        .count(1)
+                        .extra(0)
+                        .offset(0.0, 0.0, 0.0)
+                        .location(location)
+                        .receivers(30)
+                        .spawn();
+            }
+        }
+    }
+
+    @EventHandler
+    public void onShoot(EntityShootBowEvent event){
+        if (!(event.getProjectile() instanceof Arrow arrow)) return;
+        if (!(arrow.getShooter() instanceof Player player)) return;
+
+        int level = getLevel(player);
+        if (level > 0) {
+            barbedProjectiles.put(arrow, arrow.getLocation());
+        }
+    }
+
+    @UpdateEvent
+    public void initializeTridents() {
+        for (World world : Bukkit.getServer().getWorlds()) {
+            for (Trident trident : world.getEntitiesByClass(Trident.class)) {
+                if (barbedProjectiles.containsKey(trident)) {
+                    continue;
+                }
+                if (!(trident.getShooter() instanceof Player player)) {
+                    continue;
+                }
+
+                int level = getLevel(player);
+                if (level > 0) {
+                    barbedProjectiles.put(trident, trident.getLocation());
+                }
+            }
+        }
+    }
+
     @Override
     public SkillType getType() {
         return SkillType.PASSIVE_A;
     }
 
-    @EventHandler(priority = EventPriority.HIGHEST)
-    public void onSlow(CustomDamageEvent event) {
-        if (event.isCancelled()) return;
-        if (!(event.getProjectile() instanceof Arrow)) return;
-        if (!(event.getDamager() instanceof Player damager)) return;
-
-        int level = getLevel(damager);
-        if (level > 0) {
-            event.addReason(getName());
-            championsManager.getEffects().addEffect(event.getDamagee(), damager, EffectTypes.SLOWNESS, slownessStrength, (long) (getDuration(level) * 1000));
-        }
-    }
-
     @Override
-    public void loadSkillConfig(){
-        baseDuration = getConfig("baseDuration", 2.5, Double.class);
-        durationIncreasePerLevel = getConfig("durationIncreasePerLevel", 0.5, Double.class);
-        slownessStrength = getConfig("slownessStrength", 2, Integer.class);
+    public void loadSkillConfig() {
+        baseDamage = getConfig("baseDamage", 1.5, Double.class);
+        damageIncreasePerLevel = getConfig("damageIncreasePerLevel", 0.5, Double.class);
+        damageResetTime = getConfig("damageResetTime", 2.0, Double.class);
     }
-
 }
--- champions/src/main/java/me/mykindos/betterpvp/champions/champions/skills/skills/ranger/passives/VitalitySpores.java
+++ champions/src/main/java/me/mykindos/betterpvp/champions/champions/skills/skills/ranger/passives/VitalitySpores.java
@@ -1,5 +1,6 @@
 package me.mykindos.betterpvp.champions.champions.skills.skills.ranger.passives;
 
+import com.destroystokyo.paper.ParticleBuilder;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import me.mykindos.betterpvp.champions.Champions;
@@ -11,33 +12,51 @@ import me.mykindos.betterpvp.champions.champions.skills.types.PassiveSkill;
 import me.mykindos.betterpvp.core.combat.events.CustomDamageEvent;
 import me.mykindos.betterpvp.core.components.champions.Role;
 import me.mykindos.betterpvp.core.components.champions.SkillType;
-import me.mykindos.betterpvp.core.effects.EffectTypes;
 import me.mykindos.betterpvp.core.framework.updater.UpdateEvent;
 import me.mykindos.betterpvp.core.listener.BPvPListener;
-import me.mykindos.betterpvp.core.utilities.UtilFormat;
-import me.mykindos.betterpvp.core.utilities.UtilTime;
-import org.bukkit.Bukkit;
+import me.mykindos.betterpvp.core.utilities.UtilPlayer;
+import org.bukkit.Location;
+import org.bukkit.Particle;
+import org.bukkit.Sound;
+import org.bukkit.entity.Arrow;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Trident;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.EntityDamageEvent;
 
-import java.util.WeakHashMap;
+import java.util.*;
 
 @Singleton
 @BPvPListener
 public class VitalitySpores extends Skill implements PassiveSkill, DefensiveSkill, HealthSkill {
 
-    private final WeakHashMap<Player, Long> lastDamagedMap;
+    private final WeakHashMap<LivingEntity, List<SporeCharge>> sporeCharges = new WeakHashMap<>();
+    private double sporeRemovalTime;
+    private int maxSporeCharges;
+    private int maxSporeChargesIncreasePerLevel;
+    private double healing;
+    private double healingIncreasePerLevel;
 
-    private double baseDuration;
+    private static class SporeCharge {
+        double xOffset;
+        double yOffset;
+        double zOffset;
+        long appliedTime;
+        Player applier;
 
-    private double durationDecreasePerLevel;
-
-    private int regenerationStrength;
+        SporeCharge(double xOffset, double yOffset, double zOffset, long appliedTime, Player applier) {
+            this.xOffset = xOffset;
+            this.yOffset = yOffset;
+            this.zOffset = zOffset;
+            this.appliedTime = appliedTime;
+            this.applier = applier;
+        }
+    }
 
     @Inject
     public VitalitySpores(Champions champions, ChampionsManager championsManager) {
         super(champions, championsManager);
-        this.lastDamagedMap = new WeakHashMap<>();
     }
 
     @Override
@@ -47,18 +66,27 @@ public class VitalitySpores extends Skill implements PassiveSkill, DefensiveSkil
 
     @Override
     public String[] getDescription(int level) {
-
         return new String[]{
-                "After " + getValueString(this::getDuration, level) + " seconds of not taking damage,",
-                "forest spores surround you, giving",
-                "you <effect>Regeneration " + UtilFormat.getRomanNumeral(regenerationStrength) + "</effect>",
+                "Players hit with your arrows will receive",
+                "a spore charge for " + getValueString(this::getSporeRemovalTime, level) + " seconds",
                 "",
-                "You will keep the buff until you take damage"
+                "Each time you hit someone with a spore",
+                "charge, you will heal " + getValueString(this::getHealing, level) + " health.",
+                "",
+                "Maximum spore charges: " + getValueString(this::getMaxSporeCharges, level),
         };
     }
 
-    public double getDuration(int level) {
-        return baseDuration - ((level - 1) * durationDecreasePerLevel);
+    public double getSporeRemovalTime(int level) {
+        return sporeRemovalTime;
+    }
+
+    public double getHealing(int level) {
+        return healing + ((level - 1) * healingIncreasePerLevel);
+    }
+
+    public int getMaxSporeCharges(int level) {
+        return maxSporeCharges + ((level - 1) * maxSporeChargesIncreasePerLevel);
     }
 
     @Override
@@ -66,31 +94,68 @@ public class VitalitySpores extends Skill implements PassiveSkill, DefensiveSkil
         return Role.RANGER;
     }
 
-    @UpdateEvent(delay = 500)
-    public void onUpdate() {
-        for (Player player : Bukkit.getOnlinePlayers()) {
-            int level = getLevel(player);
-            if (level > 0) {
-                long lastDamaged = lastDamagedMap.getOrDefault(player, 0L);
-                if (UtilTime.elapsed(lastDamaged, (long) (getDuration(level) * 1000L))) {
-                    championsManager.getEffects().addEffect(player, EffectTypes.REGENERATION, getName(), regenerationStrength, 1000);
-                }
+    @EventHandler
+    public void onProjectileHit(CustomDamageEvent event) {
+        if (!(event.getDamager() instanceof Player player)) return;
+        if (!(event.getProjectile() instanceof Arrow || event.getProjectile() instanceof Trident)) return;
+        LivingEntity target = event.getDamagee();
+
+        int level = getLevel(player);
+        if (level > 0) {
+            sporeCharges.putIfAbsent(target, new ArrayList<>());
+            List<SporeCharge> currentCharges = sporeCharges.get(target);
+            if (currentCharges.size() < getMaxSporeCharges(level)) {
+                double xOffset = (Math.random() - 0.5) * 2;
+                double yOffset = Math.random() * 2;
+                double zOffset = (Math.random() - 0.5) * 2;
+                currentCharges.add(new SporeCharge(xOffset, yOffset, zOffset, System.currentTimeMillis(), player));
             }
         }
-
     }
 
     @EventHandler
-    public void onDamageReceived(CustomDamageEvent event) {
-        if (!(event.getDamagee() instanceof Player player)) return;
+    public void onMeleeHit(CustomDamageEvent event) {
+        if (!(event.getDamager() instanceof Player player)) return;
+        if (event.getCause() != EntityDamageEvent.DamageCause.ENTITY_ATTACK) return;
+        LivingEntity target = event.getDamagee();
 
-        if (hasSkill(player)) {
-            championsManager.getEffects().removeEffect(player, EffectTypes.REGENERATION, getName());
-
-            lastDamagedMap.put(player, System.currentTimeMillis());
+        if (sporeCharges.containsKey(target) && !sporeCharges.get(target).isEmpty()) {
+            int level = getLevel(player);
+            List<SporeCharge> currentCharges = sporeCharges.get(target);
+            if (level > 0 && !currentCharges.isEmpty()) {
+                UtilPlayer.health(player, getHealing(level));
+                currentCharges.removeFirst();
+                if (currentCharges.isEmpty()) {
+                    sporeCharges.remove(target);
+                }
+                player.playSound(player.getLocation(), Sound.ENTITY_PLAYER_LEVELUP, 0.4F, 1.5F);
+                player.getWorld().spawnParticle(Particle.HEART, player.getLocation().add(0, 1.5, 0), 5, 0.5, 0.5, 0.5, 0);
+            }
         }
     }
 
+    @UpdateEvent
+    public void updateVitalityData() {
+        long currentTime = System.currentTimeMillis();
+
+        sporeCharges.forEach((entity, charges) -> {
+            charges.removeIf(charge -> {
+                if (currentTime - charge.appliedTime > getSporeRemovalTime(getLevel(charge.applier)) * 1000) {
+                    return true;
+                } else {
+                    Location particleLocation = entity.getLocation().add(charge.xOffset, charge.yOffset, charge.zOffset);
+                    new ParticleBuilder(Particle.DUST)
+                            .location(particleLocation)
+                            .count(1)
+                            .offset(0.0, 0.0, 0.0)
+                            .data(new Particle.DustOptions(org.bukkit.Color.fromRGB(0, 255, 0), 0.5f))
+                            .receivers(charge.applier)
+                            .spawn();
+                    return false;
+                }
+            });
+        });
+    }
 
     @Override
     public SkillType getType() {
@@ -99,10 +164,10 @@ public class VitalitySpores extends Skill implements PassiveSkill, DefensiveSkil
 
     @Override
     public void loadSkillConfig() {
-        baseDuration = getConfig("baseDuration", 7.0, Double.class);
-        durationDecreasePerLevel = getConfig("durationDecreasePerLevel", 1.0, Double.class);
-
-        regenerationStrength = getConfig("regenerationStrength", 1, Integer.class);
+        maxSporeCharges = getConfig("maxSporeCharges", 2, Integer.class);
+        maxSporeChargesIncreasePerLevel = getConfig("maxSporeChargesIncreasePerLevel", 1, Integer.class);
+        sporeRemovalTime = getConfig("sporeRemovalTime", 5.0, Double.class);
+        healingIncreasePerLevel = getConfig("healingIncreasePerLevel", 0.0, Double.class);
+        healing = getConfig("healing", 1.5, Double.class);
     }
-
 }
