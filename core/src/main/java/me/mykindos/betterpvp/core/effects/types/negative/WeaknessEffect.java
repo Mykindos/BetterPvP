8fef555a9945e8f685d6bedf288cf1a105488cbf
zande <zanderaguirre@icloud.com>
2024-10-05 00:21:03 -0700

    add weakness

--- core/src/main/java/me/mykindos/betterpvp/core/effects/types/negative/WeaknessEffect.java
+++ core/src/main/java/me/mykindos/betterpvp/core/effects/types/negative/WeaknessEffect.java
@@ -1,6 +1,7 @@
 package me.mykindos.betterpvp.core.effects.types.negative;
 
 import me.mykindos.betterpvp.core.effects.VanillaEffectType;
+import me.mykindos.betterpvp.core.utilities.UtilFormat;
 import org.bukkit.potion.PotionEffectType;
 
 public class WeaknessEffect extends VanillaEffectType {
@@ -17,7 +18,16 @@ public class WeaknessEffect extends VanillaEffectType {
 
     @Override
     public PotionEffectType getVanillaPotionType() {
-        return PotionEffectType.WEAKNESS;
+        return PotionEffectType.OOZING;
     }
 
+    @Override
+    public String getDescription(int level) {
+        return "<white>Weakness " + UtilFormat.getRomanNumeral(level) + " <reset>decreases melee damage dealt by <stat>" + (level * 1.0) + "</stat>";
+    }
+
+    @Override
+    public String getGenericDescription() {
+        return  "<white>" + getName() + "</white> decreases melee damage by <green>1.0</green> per level";
+    }
 }
