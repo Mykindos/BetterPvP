cb9917304111c70c58a6c79c4f35fc5f98acf87a
StudentAlleg <36683605+StudentAlleg@users.noreply.github.com>
2024-10-22 10:32:44 -0700

    Refactor Item Durability to use new Minecraft Functionality (#1101)
    * #1092 - Update custom durability to use setMaxDamage in display. For damageable items, use vanilla damage as the display.
    
    * #1092 - Refactor custom durability to use the vanilla system (which allows wanted behavior since 1.21). Remove CustomDamageDurabilityEvent and instead use PlayerItemDamageEvent to track damage.
    
    * #1101 - remove unused code
    
    ---------
    
    Co-authored-by: Braulio <40250304+braulio-dev@users.noreply.github.com>

--- core/src/main/java/me/mykindos/betterpvp/core/items/listener/ItemListener.java
+++ core/src/main/java/me/mykindos/betterpvp/core/items/listener/ItemListener.java
@@ -2,17 +2,12 @@ package me.mykindos.betterpvp.core.items.listener;
 
 import com.google.inject.Inject;
 import lombok.CustomLog;
-import me.mykindos.betterpvp.core.combat.events.CustomDamageDurabilityEvent;
-import me.mykindos.betterpvp.core.items.BPvPItem;
 import me.mykindos.betterpvp.core.items.ItemHandler;
 import me.mykindos.betterpvp.core.listener.BPvPListener;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.PlayerItemDamageEvent;
-import org.bukkit.inventory.ItemStack;
 
 @CustomLog
 @BPvPListener
@@ -27,45 +22,7 @@ public class ItemListener implements Listener {
 
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onDamageItem(PlayerItemDamageEvent event) {
-        if (event.isCancelled()) return;
-        event.setCancelled(damageCustomItem(event.getPlayer(), event.getItem(), 1));
+        itemHandler.updateNames(event.getItem());
     }
-
-    /**
-     * @param player    the player that the ItemStack is on
-     * @param itemStack the itemStack to try and damage
-     * @param damage    the amount of damage to apply
-     * @return true if the damage was processed (it was a custom item with durability), false if not
-     */
-    public boolean damageCustomItem(Player player, ItemStack itemStack, int damage) {
-        BPvPItem item = itemHandler.getItem(itemStack);
-        if (item != null && item.getMaxDurability() >= 0) {
-            item.damageItem(player, itemStack, damage);
-            return true;
-        }
-        return false;
-    }
-
-    @EventHandler(priority = EventPriority.MONITOR)
-    public void onCustomDamageDurability(CustomDamageDurabilityEvent event) {
-        if (event.isDamagerTakeDurability() && event.getCustomDamageEvent().getDamager() instanceof Player damager) {
-            if (event.getCustomDamageEvent().getCause() == EntityDamageEvent.DamageCause.ENTITY_ATTACK) {
-                if (damageCustomItem(damager, damager.getInventory().getItemInMainHand(), 1)) {
-                    //durability was handled, cancel it
-                    event.setDamagerTakeDurability(false);
-                }
-            }
-        }
-        if (event.isDamageeTakeDurability() && event.getCustomDamageEvent().getDamagee() instanceof Player damagee) {
-            for (ItemStack armour : damagee.getEquipment().getArmorContents()) {
-                if (armour == null) continue;
-                if (damageCustomItem(damagee, armour, 1)) {
-                    //durability was handled at least once, so cancel it (not perfect)
-                    event.setDamageeTakeDurability(false);
-                }
-            }
-        }
-    }
-
 
 }
