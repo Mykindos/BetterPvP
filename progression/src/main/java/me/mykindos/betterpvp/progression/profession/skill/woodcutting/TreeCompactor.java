59d8fe805deea10d9393452fdc3cc6afd33d3ab4
8SIXSect <135760919+8SIXSect@users.noreply.github.com>
2024-10-22 08:06:12 -0500

    Woodcutting: Tree Compactor (#1058)
    * Created Tree Compactor cmd
    
    * Created Tree Compactor item
    
    * Updated tree compactor description
    
    * Added basic compaction functionality
    
    * Fixed tab completions
    
    * Allowed for tree compactor to only compact specific log types
    
    * Implemented 5 minute cooldown for Tree Compactor
    
    * Now compacted logs can be sold to shop keeper
    
    * Added dependency for Tree Compactor
    
    * Temporarily removed perk from menu
    
    * Allowed the player to be able to see usage w/ the cooldown triggering
    
    * Removed second line
    
    * Changed code so that if no logs were compacted, cooldown will instantly reset (so just not happen)
    
    * Refactored code for tab completions
    
    * Admins can now place compacted logs
    
    * Created new SQL migration for the compacted log item

---
+++ progression/src/main/java/me/mykindos/betterpvp/progression/profession/skill/woodcutting/TreeCompactor.java
@@ -0,0 +1,107 @@
+package me.mykindos.betterpvp.progression.profession.skill.woodcutting;
+
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import lombok.Getter;
+import me.mykindos.betterpvp.core.client.Client;
+import me.mykindos.betterpvp.core.client.repository.ClientManager;
+import me.mykindos.betterpvp.core.listener.BPvPListener;
+import me.mykindos.betterpvp.core.utilities.UtilMessage;
+import me.mykindos.betterpvp.progression.Progression;
+import me.mykindos.betterpvp.progression.profession.skill.ProgressionSkillDependency;
+import me.mykindos.betterpvp.progression.profile.ProfessionProfile;
+import me.mykindos.betterpvp.progression.profile.ProfessionProfileManager;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Optional;
+
+@Singleton
+@BPvPListener
+public class TreeCompactor extends WoodcuttingProgressionSkill implements Listener {
+    private final ProfessionProfileManager professionProfileManager;
+    private final ClientManager clientManager;
+
+    @Getter
+    private double cooldown;
+
+    @Inject
+    public TreeCompactor(Progression progression, ProfessionProfileManager professionProfileManager,
+                         ClientManager clientManager) {
+        super(progression);
+        this.professionProfileManager = professionProfileManager;
+        this.clientManager = clientManager;
+    }
+
+    @Override
+    public String getName() {
+        return "Tree Compactor";
+    }
+
+    @Override
+    public String[] getDescription(int level) {
+        return new String[] {
+                "You gain access to the <green>/treecompactor</green> command",
+                "",
+                "This command lets you turn a stack of logs",
+                "into a singular log"
+        };
+    }
+
+    @Override
+    public Material getIcon() {
+        return Material.OAK_WOOD;
+    }
+
+    @Override
+    public boolean isGlowing() {
+        return true;
+    }
+
+    @Override
+    public void loadConfig() {
+        super.loadConfig();
+        cooldown = getConfig("cooldown", 20.0, Double.class);
+    }
+
+    @Override
+    public ProgressionSkillDependency getDependencies() {
+        final String[] dependencies = new String[]{"Bark Bounty"};
+        return new ProgressionSkillDependency(dependencies, 100);
+    }
+
+    // no dependencies yet
+
+    /**
+     * @return the player's skill level
+     */
+    public int getPlayerSkillLevel(Player player) {
+        Optional<ProfessionProfile> profile = professionProfileManager.getObject(player.getUniqueId().toString());
+
+        return profile.map(this::getPlayerSkillLevel).orElse(0);
+    }
+
+    /**
+     * This function's purpose is to return a boolean that tells you if the player has the skill
+     * <b>No More Leaves</b>
+     */
+    public boolean doesPlayerHaveSkill(Player player) {
+        return getPlayerSkillLevel(player) > 0;
+    }
+
+    @EventHandler
+    public void onPlaceCompactedLog(BlockPlaceEvent event) {
+        if (!event.getBlock().getType().equals(Material.OAK_WOOD)) return;
+
+        Player player = event.getPlayer();
+        Client client = clientManager.search().online(player);
+        if (!client.isAdministrating()) {
+            event.setCancelled(true);
+            UtilMessage.simpleMessage(player, "Progression", "You cannot place this block");
+        }
+    }
+}
