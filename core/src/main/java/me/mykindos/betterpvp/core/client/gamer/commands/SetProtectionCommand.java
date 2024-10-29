ab67a4381d43f79e2729a709595ada04e05237a2
Mykindos <5389944+Mykindos@users.noreply.github.com>
2024-10-29 16:39:13 +1100

    Fix setprotection

--- core/src/main/java/me/mykindos/betterpvp/core/client/gamer/commands/SetProtectionCommand.java
+++ core/src/main/java/me/mykindos/betterpvp/core/client/gamer/commands/SetProtectionCommand.java
@@ -11,6 +11,7 @@ import me.mykindos.betterpvp.core.effects.EffectManager;
 import me.mykindos.betterpvp.core.effects.EffectTypes;
 import me.mykindos.betterpvp.core.utilities.UtilMessage;
 import me.mykindos.betterpvp.core.utilities.UtilTime;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 @Singleton
@@ -63,14 +64,17 @@ public class SetProtectionCommand extends Command {
             }
 
             long newDuration = (long) (duration * 60 * 1000L);
+            String timeString = UtilTime.getTime(newDuration, 1);
             Client target = clientOptional.get();
+
             target.getGamer().saveProperty(GamerProperty.REMAINING_PVP_PROTECTION, newDuration);
             if (target.isOnline()) {
-                effectManager.removeEffect(player, EffectTypes.PROTECTION);
-                effectManager.addEffect(player, EffectTypes.PROTECTION, newDuration);
+                Player targetPlayer = target.getGamer().getPlayer();
+                effectManager.removeEffect(targetPlayer, EffectTypes.PROTECTION);
+                effectManager.addEffect(targetPlayer, EffectTypes.PROTECTION, newDuration);
+                UtilMessage.message(targetPlayer, "Protection", "Your protection timer was set for <green>%s</green>", timeString);
             }
-            String timeString = UtilTime.getTime(newDuration, 1);
-            UtilMessage.message(player, "Protection", "Your protection timer was set for <green>%s</green>", timeString);
+
             clientManager.sendMessageToRank("Protection",
                     UtilMessage.deserialize("<yellow>%s</yellow> set <yellow>%s</yellow>'s protection timer for <green>%s</green>",
                             player.getName(), target.getName(), timeString),
