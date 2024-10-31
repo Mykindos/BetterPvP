0cddb2688884d4801285a4ee3db5c10fe70b9aa4
Mykindos <tomhoogstra@live.com.au>
2024-10-31 20:36:26 +1100

    Fix DB issues

--- shops/src/main/resources/shops-migrations/V7_1__Fix_shop_items.sql
+++ shops/src/main/resources/shops-migrations/V7_1__Fix_shop_items.sql
@@ -1,5 +1,9 @@
+DELETE FROM shopitems WHERE ItemName = 'Compacted Log';
+DELETE FROM shopitems WHERE ItemName = 'Anvil';
+DELETE FROM shopitems WHERE ItemName = 'Speedy Bait';
+
 INSERT IGNORE INTO shopitems (Shopkeeper, Material, ItemName, ModelData, MenuSlot, MenuPage, BuyPrice, SellPrice) VALUES ('Lumberjack', 'OAK_WOOD', 'Compacted Log', 1, 27, 1, 2000, 960);
 
-INSERT IGNORE INTO shopitems (Shopkeeper, Material, ItemName, ModelData, MenuSlot, MenuPage, BuyPrice, SellPrice) VALUES ('Resources', 'ANVIL', 'Anvil', 0, 33, 1, 1, 50000);
+INSERT IGNORE INTO shopitems (Shopkeeper, Material, ItemName, ModelData, MenuSlot, MenuPage, BuyPrice, SellPrice) VALUES ('Resources', 'ANVIL', 'Anvil', 0, 33, 1, 50000, 0);
 
 INSERT IGNORE INTO shopitems (Shopkeeper, Material, ItemName, ModelData, MenuSlot, MenuPage, BuyPrice, SellPrice) VALUES ('Fisherman', 'ORANGE_GLAZED_TERRACOTTA', 'Speedy Bait', 1, 0, 1, 5000, 0);
