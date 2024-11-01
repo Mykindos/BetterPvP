fafa9ca1577000f62edaf1189f09fefd4d85bc90
Mykindos <5389944+Mykindos@users.noreply.github.com>
2024-11-02 07:14:29 +1100

    Support mythic mobs threat tables

--- core/src/main/java/me/mykindos/betterpvp/core/combat/adapters/MythicMobsDamageAdapter.java
+++ core/src/main/java/me/mykindos/betterpvp/core/combat/adapters/MythicMobsDamageAdapter.java
@@ -2,8 +2,11 @@ package me.mykindos.betterpvp.core.combat.adapters;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import io.lumine.mythic.api.adapters.AbstractPlayer;
+import io.lumine.mythic.bukkit.BukkitAdapter;
 import io.lumine.mythic.bukkit.MythicBukkit;
 import io.lumine.mythic.core.mobs.ActiveMob;
+import io.lumine.mythic.core.players.PlayerData;
 import me.mykindos.betterpvp.core.Core;
 import me.mykindos.betterpvp.core.combat.events.CustomDamageEvent;
 import me.mykindos.betterpvp.core.framework.adapter.PluginAdapter;
@@ -12,11 +15,13 @@ import me.mykindos.betterpvp.core.listener.BPvPListener;
 import me.mykindos.betterpvp.core.utilities.UtilFormat;
 import me.mykindos.betterpvp.core.utilities.UtilServer;
 import me.mykindos.betterpvp.core.utilities.events.FetchNearbyEntityEvent;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 
 import java.util.Iterator;
+import java.util.Optional;
 
 @PluginAdapter("MythicMobs")
 @Singleton
@@ -49,12 +54,19 @@ public class MythicMobsDamageAdapter implements Listener {
     }
 
     @EventHandler(priority = EventPriority.HIGHEST)
-    public void processDamageModidiers(CustomDamageEvent event) {
+    public void processDamageModifiers(CustomDamageEvent event) {
         var mobManager = MythicBukkit.inst().getMobManager();
         ActiveMob damagee = mobManager.getActiveMob(event.getDamagee().getUniqueId()).orElse(null);
         if (damagee != null) {
             if (damagee.getType().getDamageModifiers().containsKey(event.getCause().name())) {
                 event.setDamage(event.getDamage() * damagee.getType().getDamageModifiers().get(event.getCause().name()));
+
+                if (event.getDamager() instanceof Player player) {
+                    if (damagee.getType().usesThreatTable()) {
+                        AbstractPlayer mmPlayer = BukkitAdapter.adapt(player);
+                        damagee.getThreatTable().threatGain(mmPlayer, event.getDamage());
+                    }
+                }
             }
         }
     }
