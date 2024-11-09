04404e44acf514ef3e69d4432c872f5fbdf9e908
manu <73852745+expires@users.noreply.github.com>
2024-11-09 15:09:30 +0000

    Silencing Strikes and Shocking Strikes changes (#1214)
    * fix dogshit assassin passives
    
    * debuff silencing again
    
    * debuff silencing again

--- champions/src/main/java/me/mykindos/betterpvp/champions/champions/skills/skills/assassin/passives/ShockingStrikes.java
+++ champions/src/main/java/me/mykindos/betterpvp/champions/champions/skills/skills/assassin/passives/ShockingStrikes.java
@@ -5,6 +5,7 @@ import com.google.inject.Singleton;
 import me.mykindos.betterpvp.champions.Champions;
 import me.mykindos.betterpvp.champions.champions.ChampionsManager;
 import me.mykindos.betterpvp.champions.champions.skills.Skill;
+import me.mykindos.betterpvp.champions.champions.skills.skills.assassin.data.ShockingStrikesData;
 import me.mykindos.betterpvp.champions.champions.skills.types.DebuffSkill;
 import me.mykindos.betterpvp.champions.champions.skills.types.OffensiveSkill;
 import me.mykindos.betterpvp.champions.champions.skills.types.PassiveSkill;
@@ -12,24 +13,32 @@ import me.mykindos.betterpvp.core.combat.events.CustomDamageEvent;
 import me.mykindos.betterpvp.core.components.champions.Role;
 import me.mykindos.betterpvp.core.components.champions.SkillType;
 import me.mykindos.betterpvp.core.effects.EffectTypes;
+import me.mykindos.betterpvp.core.framework.updater.UpdateEvent;
 import me.mykindos.betterpvp.core.listener.BPvPListener;
-import me.mykindos.betterpvp.core.utilities.UtilFormat;
+import me.mykindos.betterpvp.core.utilities.UtilTime;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Singleton
 @BPvPListener
 public class ShockingStrikes extends Skill implements PassiveSkill, Listener, DebuffSkill, OffensiveSkill {
+
+    public List<ShockingStrikesData> data = new ArrayList<>();
 
     private double baseDuration;
 
     private double durationIncreasePerLevel;
 
-    private double slownessDuration;
-
     private int slownessStrength;
+
+    private int hitsNeeded;
+
+    private double timeSpan;
 
     @Inject
     public ShockingStrikes(Champions champions, ChampionsManager championsManager) {
@@ -45,9 +54,10 @@ public class ShockingStrikes extends Skill implements PassiveSkill, Listener, De
     public String[] getDescription(int level) {
 
         return new String[]{
-                "Your attacks <effect>Shock</effect> targets for",
-                getValueString(this::getDuration, level) + " seconds, giving them <effect>Slowness " + UtilFormat.getRomanNumeral(slownessStrength) + "</effect>",
-                "and <effect>Screen-Shake</effect>",
+                "Hit a player " + getValueString(this::getHitsNeeded, level, 0) + " consecutive times without letting",
+                getValueString(this::getTimeSpan, level) + " seconds pass to <effect>Slow</effect> them for " + getValueString(this::getDuration, level) + " seconds",
+                "",
+                "Every hit <effect>Shock</effect>'s the target",
                 "",
                 EffectTypes.SHOCK.getDescription(0)
         };
@@ -55,6 +65,14 @@ public class ShockingStrikes extends Skill implements PassiveSkill, Listener, De
 
     public double getDuration(int level) {
         return baseDuration + (durationIncreasePerLevel * (level - 1));
+    }
+
+    public int getHitsNeeded(int level) {
+        return hitsNeeded;
+    }
+
+    public double getTimeSpan(int level) {
+        return timeSpan;
     }
 
     @Override
@@ -72,20 +90,51 @@ public class ShockingStrikes extends Skill implements PassiveSkill, Listener, De
         if (event.isCancelled()) return;
         if (event.getCause() != DamageCause.ENTITY_ATTACK) return;
         if (!(event.getDamager() instanceof Player damager)) return;
+        if (!(event.getDamagee() instanceof Player damagee)) return;
+
         int level = getLevel(damager);
-        if (level <= 0) return;
+        if (level > 0) {
+            ShockingStrikesData shockingData = getShockingStrikesData(damager, damagee);
+            if (shockingData == null) {
+                shockingData = new ShockingStrikesData(damager.getUniqueId(), damagee.getUniqueId());
+                data.add(shockingData);
+            }
 
-        championsManager.getEffects().addEffect(event.getDamagee(), damager, EffectTypes.SHOCK, (long) (getDuration(level) * 1000L));
-        championsManager.getEffects().addEffect(event.getDamagee(), damager, EffectTypes.SLOWNESS, slownessStrength, (long) (slownessDuration * 1000));
-        event.addReason(getName());
+            shockingData.addCount();
+            shockingData.setLastHit(System.currentTimeMillis());
+            event.addReason(getName());
+            championsManager.getEffects().addEffect(event.getDamagee(), damager, EffectTypes.SHOCK, (long) (getDuration(level) * 1000L));
+            if (shockingData.getCount() == getHitsNeeded(level)) {
+                championsManager.getEffects().addEffect(event.getDamagee(), damager, EffectTypes.SLOWNESS, slownessStrength, (long) (getDuration(level) * 1000));
+                data.remove(shockingData);
+            }
+        }
+    }
 
+
+    @UpdateEvent
+    public void onUpdate() {
+        data.removeIf(shockingData -> UtilTime.elapsed(shockingData.getLastHit(), (long) (getTimeSpan(0) * 1000L)));
+    }
+
+
+    public ShockingStrikesData getShockingStrikesData(Player damager, Player damagee) {
+        for (ShockingStrikesData shockingData : data) {
+            if (shockingData.getPlayer().equals(damager.getUniqueId())) {
+                if (shockingData.getTarget().equals(damagee.getUniqueId())) {
+                    return shockingData;
+                }
+            }
+        }
+        return null;
     }
 
     @Override
     public void loadSkillConfig() {
         baseDuration = getConfig("baseDuration", 1.0, Double.class);
-        durationIncreasePerLevel = getConfig("durationIncreasePerLevel", 1.0, Double.class);
-        slownessDuration = getConfig("slownessDuration", 1.0, Double.class);
+        hitsNeeded = getConfig("hitsNeeded", 3, Integer.class);
+        timeSpan = getConfig("timeSpan", 1.0, Double.class);
+        durationIncreasePerLevel = getConfig("durationIncreasePerLevel", 0.5, Double.class);
         slownessStrength = getConfig("slownessStrength", 1, Integer.class);
     }
 }
