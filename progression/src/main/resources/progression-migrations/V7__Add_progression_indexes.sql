c3604bf39099d21c18d3a014c74afa5c817f72c1
Mykindos <5389944+Mykindos@users.noreply.github.com>
2024-11-27 03:55:01 +1100

    Add woodcutting and fishing indexes

---
+++ progression/src/main/resources/progression-migrations/V7__Add_progression_indexes.sql
@@ -0,0 +1,5 @@
+CREATE INDEX idx_progression_fishing_timestamp ON progression_fishing (timestamp);
+CREATE INDEX idx_progression_fishing_gamer ON progression_fishing (Gamer);
+
+CREATE INDEX idx_progression_woodcutting_timestamp ON progression_woodcutting (timestamp);
+CREATE INDEX idx_progression_woodcutting_gamer ON progression_woodcutting (Gamer);
