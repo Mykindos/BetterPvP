a44d73f63b63798d8482f2175e16841e18196518
Polisvh <145322774+Polisvh@users.noreply.github.com>
2024-11-28 23:06:53 +0100

    Update ShockingStrikesData.java

--- champions/src/main/java/me/mykindos/betterpvp/champions/champions/skills/skills/assassin/data/ShockingStrikesData.java
+++ champions/src/main/java/me/mykindos/betterpvp/champions/champions/skills/skills/assassin/data/ShockingStrikesData.java
@@ -10,10 +10,20 @@ public class ShockingStrikesData {
     private final UUID player;
     private final UUID target;
     private int count;
+    private int blindCount;
     private long lastHit = System.currentTimeMillis();
 
     public void addCount() {
         count++;
+    }
+    public void resetCount(){
+        count = 0;
+    }
+    public void addBlindCount() {
+        blindCount++;
+    }
+    public void resetBlindCount(){
+        blindCount = 0;
     }
 
 }
