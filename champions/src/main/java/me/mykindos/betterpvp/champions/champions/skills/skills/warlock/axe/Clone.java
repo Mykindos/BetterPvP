e666b88dddcff70bab56163f3958e5fcc15f8903
Mykindos <5389944+Mykindos@users.noreply.github.com>
2024-12-01 00:02:43 +1100

    Remove targets player cannot see from clone

--- champions/src/main/java/me/mykindos/betterpvp/champions/champions/skills/skills/warlock/axe/Clone.java
+++ champions/src/main/java/me/mykindos/betterpvp/champions/champions/skills/skills/warlock/axe/Clone.java
@@ -140,7 +140,7 @@ public class Clone extends Skill implements InteractSkill, CooldownSkill, Listen
             return;
         }
         //Check if player already has a clone - mainly to prevent op'd players from spamming clones
-        if(clones.containsKey(player)) return;
+        if (clones.containsKey(player)) return;
 
 
         double healthReduction = getHealthReduction(level);
@@ -159,6 +159,7 @@ public class Clone extends Skill implements InteractSkill, CooldownSkill, Listen
 
         //Find nearby enemies relative to the clones location after teleporting
         List<LivingEntity> nearbyEnemies = UtilEntity.getNearbyEnemies(player, clone.getLocation(), 24);
+        nearbyEnemies.removeIf(entity -> !player.canSee(entity));
         LivingEntity initTarget = null;
         if (!nearbyEnemies.isEmpty()) {
             //Pick a random nearby enemy
@@ -217,12 +218,12 @@ public class Clone extends Skill implements InteractSkill, CooldownSkill, Listen
     public void onCustomDamageEvent(CustomDamageEvent event) {
 
         //Lock/Switch clone onto player being damaged by its owner.
-        if (event.getDamager() instanceof Player damager && clones.containsKey(damager)){
+        if (event.getDamager() instanceof Player damager && clones.containsKey(damager)) {
             clones.get(damager).getPathFinder().setTarget(event.getDamagee());
             return;
         }
 
-        if(event.getDamager() instanceof Vindicator clone) {
+        if (event.getDamager() instanceof Vindicator clone) {
             Player cloneOwner = getCloneOwner(clone);
             if (clones.containsKey(cloneOwner)) {
 
@@ -244,12 +245,12 @@ public class Clone extends Skill implements InteractSkill, CooldownSkill, Listen
     }
 
     @EventHandler
-    public void onDeathEvent(EntityDeathEvent event){
-        if(event.getEntity() instanceof Vindicator clone && clones.containsKey(getCloneOwner(clone))){
+    public void onDeathEvent(EntityDeathEvent event) {
+        if (event.getEntity() instanceof Vindicator clone && clones.containsKey(getCloneOwner(clone))) {
             removeClone(clone, getCloneOwner(clone));
             return;
         }
-        if(event.getEntity() instanceof Player player && clones.containsKey(player)){
+        if (event.getEntity() instanceof Player player && clones.containsKey(player)) {
             removeClone(clones.get(player).getClone(), player);
         }
     }
@@ -259,7 +260,7 @@ public class Clone extends Skill implements InteractSkill, CooldownSkill, Listen
         championsManager.getEffects().addEffect(target, EffectTypes.BLINDNESS, blindnessLevel, eDuration);
         championsManager.getEffects().addEffect(target, EffectTypes.SLOWNESS, slownessLevel, eDuration);
 
-        if(target instanceof Player player) {
+        if (target instanceof Player player) {
             player.playSound(player.getLocation(), Sound.BLOCK_CONDUIT_AMBIENT, 2.0f, 1.f);
         }
 
@@ -296,11 +297,11 @@ public class Clone extends Skill implements InteractSkill, CooldownSkill, Listen
     }
 
     private Player getCloneOwner(Vindicator clone) {
-        if(clone == null) {
+        if (clone == null) {
             return null;
         }
 
-        if(!clone.hasMetadata("owner")) {
+        if (!clone.hasMetadata("owner")) {
             return null;
         }
 
