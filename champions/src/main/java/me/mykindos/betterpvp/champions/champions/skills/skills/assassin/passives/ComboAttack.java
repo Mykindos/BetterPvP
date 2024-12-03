11967f1836e70c4229f690b9410e471411515c49
manu <73852745+expires@users.noreply.github.com>
2024-12-03 13:49:34 +0000

    Combo Attack Fixes (#1344)
    * make combo semi decent again
    
    * Remove useless imports
    
    * Remove useless import
    
    * fix getLevel call
    
    * remove redundant import statement

--- champions/src/main/java/me/mykindos/betterpvp/champions/champions/skills/skills/assassin/passives/ComboAttack.java
+++ champions/src/main/java/me/mykindos/betterpvp/champions/champions/skills/skills/assassin/passives/ComboAttack.java
@@ -6,6 +6,7 @@ import com.google.inject.Singleton;
 import me.mykindos.betterpvp.champions.Champions;
 import me.mykindos.betterpvp.champions.champions.ChampionsManager;
 import me.mykindos.betterpvp.champions.champions.skills.Skill;
+import me.mykindos.betterpvp.champions.champions.skills.skills.assassin.data.ComboAttackData;
 import me.mykindos.betterpvp.champions.champions.skills.types.DamageSkill;
 import me.mykindos.betterpvp.champions.champions.skills.types.OffensiveSkill;
 import me.mykindos.betterpvp.champions.champions.skills.types.PassiveSkill;
@@ -14,6 +15,7 @@ import me.mykindos.betterpvp.core.components.champions.Role;
 import me.mykindos.betterpvp.core.components.champions.SkillType;
 import me.mykindos.betterpvp.core.framework.updater.UpdateEvent;
 import me.mykindos.betterpvp.core.listener.BPvPListener;
+import me.mykindos.betterpvp.core.utilities.UtilMessage;
 import me.mykindos.betterpvp.core.utilities.UtilTime;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
@@ -22,15 +24,19 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 
-import java.util.HashSet;
 import java.util.WeakHashMap;
 
 @Singleton
 @BPvPListener
 public class ComboAttack extends Skill implements PassiveSkill, Listener, DamageSkill, OffensiveSkill {
 
-    private final WeakHashMap<Player, Double> repeat = new WeakHashMap<>();
-    private final WeakHashMap<Player, Long> last = new WeakHashMap<>();
+    private final WeakHashMap<Player, ComboAttackData> repeat = new WeakHashMap<>();
+
+    private double baseDamageIncrement;
+    private double damageIncrement;
+    private double duration;
+    private double durationIncreasePerLevel;
+
 
     @Inject
     public ComboAttack(Champions champions, ChampionsManager championsManager) {
@@ -43,9 +49,6 @@ public class ComboAttack extends Skill implements PassiveSkill, Listener, Damage
         return "Combo Attack";
     }
 
-    private double baseDamageIncrement;
-    private double damageIncrement;
-    private double duration;
 
     @Override
     public String[] getDescription(int level) {
@@ -69,7 +72,7 @@ public class ComboAttack extends Skill implements PassiveSkill, Listener, Damage
     }
 
     private double getDuration(int level) {
-        return duration;
+        return duration + (level - 1) * durationIncreasePerLevel;
     }
 
     @Override
@@ -93,42 +96,45 @@ public class ComboAttack extends Skill implements PassiveSkill, Listener, Damage
         int level = getLevel(damager);
         if (level > 0) {
 
-            if (!repeat.containsKey(damager)) {
-                repeat.put(damager, 0.0);
+            if (!(repeat.containsKey(damager))) {
+                repeat.put(damager, new ComboAttackData(event.getDamagee().getUniqueId(), baseDamageIncrement, System.currentTimeMillis()));
+            } else if(repeat.get(damager).getLastTarget() != event.getDamagee().getUniqueId()){
+                repeat.remove(damager);
+                return;
             }
-            double cur = repeat.get(damager);
+
+            double cur = repeat.get(damager).getDamageIncrement();
             event.setDamage(event.getDamage() + cur);
-            repeat.put(damager, Math.min(getMaxDamageIncrement(level), cur + damageIncrement));
-            last.put(damager, System.currentTimeMillis());
+
+            repeat.get(damager).setDamageIncrement(Math.min(cur + damageIncrement, getMaxDamageIncrement(level)));
+            repeat.get(damager).setLastTarget(event.getDamagee().getUniqueId());
+            repeat.get(damager).setLast(System.currentTimeMillis());
+
             event.addReason(getName());
 
-            damager.getWorld().playSound(damager.getLocation(), Sound.BLOCK_NOTE_BLOCK_HAT, 1f, (float) (0.7f + (0.3f * repeat.get(damager))));
+            damager.getWorld().playSound(damager.getLocation(), Sound.BLOCK_NOTE_BLOCK_HAT, 1f, (float) (0.7f + (0.3f * repeat.get(damager).getDamageIncrement())));
 
         }
     }
-
 
     @UpdateEvent(delay = 500)
     public void onUpdate() {
-
-        HashSet<Player> remove = new HashSet<>();
-
         for (Player player : repeat.keySet()) {
-            if (UtilTime.elapsed(last.get(player), (long) duration * 1000)) {
-                remove.add(player);
+            int level = getLevel(player);
+            if (UtilTime.elapsed(repeat.get(player).getLast(), (long) getDuration(level) * 1000)) {
+                UtilMessage.message(player, getClassType().getName(), UtilMessage.deserialize("<green>%s %d</green> has ended.", getName(), level));
+                player.playSound(player.getLocation(), Sound.BLOCK_NOTE_BLOCK_HAT, 1f, 5.0f);
+                repeat.remove(player);
             }
         }
-
-        for (Player player : remove) {
-            repeat.remove(player);
-            last.remove(player);
-        }
     }
+
 
     @Override
     public void loadSkillConfig() {
         baseDamageIncrement = getConfig("baseDamageIncrement", 1.0, Double.class);
         damageIncrement = getConfig("damageIncrement", 1.0, Double.class);
         duration = getConfig("duration", 0.8, Double.class);
+        durationIncreasePerLevel = getConfig("durationIncreasePerLevel", 0.4, Double.class);
     }
 }
