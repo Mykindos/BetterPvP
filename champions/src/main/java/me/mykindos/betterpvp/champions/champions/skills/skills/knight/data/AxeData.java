454abb8ee906c67c1d1523ebc3f05058fcf51447
ezrn <145294235+ezrn@users.noreply.github.com>
2024-12-04 21:37:47 -0800

    Magnetic Axe (#1000)
    * test
    
    * roped axe progress
    
    * test
    
    * axe done
    
    * teleport fix
    
    * update description
    
    * fix error
    
    * fix magnetic axe

---
+++ champions/src/main/java/me/mykindos/betterpvp/champions/champions/skills/skills/knight/data/AxeData.java
@@ -0,0 +1,43 @@
+package me.mykindos.betterpvp.champions.champions.skills.skills.knight.data;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.bukkit.Location;
+import org.bukkit.entity.ItemDisplay;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.util.Vector;
+
+@Getter
+public class AxeData {
+    private final ItemDisplay axeDisplay;
+    private final Location originalPosition;
+    private final Vector initialVelocity;
+    private final Vector gravity;
+    private final long startTime;
+    private final float initialYaw;
+
+    @Setter
+    private Location initialPosition;
+
+    @Setter
+    private Vector currentVelocity;
+
+    @Setter
+    private ItemStack originalItem;
+
+    @Getter
+    @Setter
+    private boolean returning;
+
+    public AxeData(ItemDisplay axeDisplay, Location originalPosition, Vector initialVelocity, Vector gravity, long startTime, float initialYaw) {
+        this.axeDisplay = axeDisplay;
+        this.originalPosition = originalPosition;
+        this.initialPosition = originalPosition;
+        this.initialVelocity = initialVelocity;
+        this.gravity = gravity;
+        this.startTime = startTime;
+        this.initialYaw = initialYaw;
+        this.currentVelocity = initialVelocity;
+        this.returning = false;
+    }
+}
