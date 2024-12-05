17930cc81bd46031ba54a7336bd1cfade481d38b
Mykindos <5389944+Mykindos@users.noreply.github.com>
2024-12-05 18:41:48 +1100

    Update seismic slam

---
+++ champions/src/main/java/me/mykindos/betterpvp/champions/champions/skills/skills/brute/data/SeismicSlamData.java
@@ -0,0 +1,12 @@
+package me.mykindos.betterpvp.champions.champions.skills.skills.brute.data;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class SeismicSlamData {
+
+    private int maxY;
+
+}
