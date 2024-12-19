8cd0c2582ce7e181a3a2268093b101cd7097e4b6
StudentAlleg <36683605+StudentAlleg@users.noreply.github.com>
2024-12-18 19:34:02 -0800

    Improve Skill Feedback For Specific Skills (#1077)
    * #1062 - Add particles to stampede
    
    * #1068 - Fix DefensiveStance energy usage
    
    * #1062 - Change visual feedback for defensive stance to be less obtrusive
    
    * #1062 - Change fortify particles to RAID_OMEN
    
    * #1062 - Add particles to evade
    
    * #1062 - Add particles for melee damage
    
    * #1062 - WIP add feedback for colossus
    
    * #1062 - Add most recent damage as level indicator
    
    * #1053 - Fix particles and level to use the reduction damage intial raw damage, as this is the number expected
    
    * #1062 - Add sound + particles for Colossus
    
    * #1076 - Change Fortify particles to be static, back to blue redstone dust
    
    * #1076 - Fix being able to rename items, reduce stampede particles
    
    * #1076 - Change Fortify particles to brute color
    
    * #1077 - Reduce level up particles, change DefensiveStance to shield
    
    * #1077 - Remove melee particles
    
    * Fix visuals kindos is dumb
    
    Signed-off-by: Braulio <bchamerry@gmail.com>
    
    ---------
    
    Signed-off-by: Braulio <bchamerry@gmail.com>
    Co-authored-by: Braulio <bchamerry@gmail.com>
    Co-authored-by: Braulio <40250304+braulio-dev@users.noreply.github.com>

--- champions/src/main/java/me/mykindos/betterpvp/champions/champions/skills/skills/brute/passives/Colossus.java
+++ champions/src/main/java/me/mykindos/betterpvp/champions/champions/skills/skills/brute/passives/Colossus.java
@@ -12,6 +12,9 @@ import me.mykindos.betterpvp.core.combat.events.VelocityType;
 import me.mykindos.betterpvp.core.components.champions.Role;
 import me.mykindos.betterpvp.core.components.champions.SkillType;
 import me.mykindos.betterpvp.core.listener.BPvPListener;
+import org.bukkit.Material;
+import org.bukkit.Particle;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 
@@ -53,13 +56,24 @@ public class Colossus extends Skill implements PassiveSkill, UtilitySkill {
 
     @EventHandler
     public void onCustomVelocity(CustomEntityVelocityEvent event) {
-        if(!(event.getEntity() instanceof Player player)) return;
-        if(event.getVelocityType() != VelocityType.KNOCKBACK && event.getVelocityType() != VelocityType.KNOCKBACK_CUSTOM) return;
+        if (!(event.getEntity() instanceof Player player)) return;
+        if (event.getVelocityType() != VelocityType.KNOCKBACK && event.getVelocityType() != VelocityType.KNOCKBACK_CUSTOM) return;
 
         int level = getLevel(player);
-        if(level > 0) {
+        if (level > 0) {
             event.setVector(event.getVector().multiply(1 - ((reductionPerLevel) * level)));
+            player.getWorld().playSound(player.getLocation(), Sound.BLOCK_BELL_USE, 1f, 2f);
+            spawnParticles(player);
         }
+    }
+
+    private void spawnParticles(Player player) {
+        Particle.BLOCK.builder()
+                .location(player.getLocation().clone().add(0, player.getHeight()/2, 0))
+                .data(Material.CHISELED_STONE_BRICKS.createBlockData())
+                .receivers(32)
+                .count(6)
+                .spawn();
     }
 
     @Override
