7f4e412d7d55663d4aa913dc24645d8903114f85
8SIXSect <eightysixthsector@outlook.com>
2024-12-19 08:25:20 -0600

    Ok, changed configuration

--- progression/src/main/java/me/mykindos/betterpvp/progression/profession/woodcutting/specialitems/RebirthOfMagnoliaWeapon.java
+++ progression/src/main/java/me/mykindos/betterpvp/progression/profession/woodcutting/specialitems/RebirthOfMagnoliaWeapon.java
@@ -4,7 +4,10 @@ import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import lombok.CustomLog;
 import me.mykindos.betterpvp.core.combat.weapon.Weapon;
+import me.mykindos.betterpvp.core.config.ExtendedYamlConfiguration;
+import me.mykindos.betterpvp.core.items.ItemHandler;
 import me.mykindos.betterpvp.core.listener.BPvPListener;
+import me.mykindos.betterpvp.core.utilities.UtilItem;
 import me.mykindos.betterpvp.core.utilities.UtilMessage;
 import me.mykindos.betterpvp.core.utilities.model.WeighedList;
 import me.mykindos.betterpvp.progression.Progression;
@@ -18,6 +21,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockFertilizeEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -27,15 +31,18 @@ import java.util.List;
 @BPvPListener
 @Singleton
 public class RebirthOfMagnoliaWeapon extends Weapon implements Listener {
+    private final ItemHandler itemHandler;
+
     private final Material[] ALLOWED_BONEMEALABLE_BLOCKS = { Material.GRASS_BLOCK };
 
+    private WeighedList<ItemStack> lootTypes;
 
-    private WeighedList<Material> lootTypes;
 
 
     @Inject
-    public RebirthOfMagnoliaWeapon(Progression progression) {
+    public RebirthOfMagnoliaWeapon(Progression progression, ItemHandler itemHandler) {
         super(progression, "rebirth_of_magnolia");
+        this.itemHandler = itemHandler;
     }
 
     @EventHandler
@@ -66,7 +73,7 @@ public class RebirthOfMagnoliaWeapon extends Weapon implements Listener {
                 .filter(this::isFlower)
 
                 // Replace each flower with a random one
-                .forEach(blockState -> blockState.setType(lootTypes.random()));
+                .forEach(blockState -> blockState.setType(lootTypes.random().getType()));
 
         // I don't like tall grass so let's replace with a random flower
         blockStates.stream()
@@ -75,7 +82,7 @@ public class RebirthOfMagnoliaWeapon extends Weapon implements Listener {
                 // Tall grass is like a 2-block block, so we only want the bottom one
                 // You need to get the block up 1 because event.getBlock() returns the block that was bonemealed
                 .filter(blockState -> blockState.getY() == event.getBlock().getRelative(0, 1, 0).getY())
-                .forEach(blockState -> blockState.setType(lootTypes.random()));
+                .forEach(blockState -> blockState.setType(lootTypes.random().getType()));
 
         // Remove any leftover tall grass
         blockStates.removeIf(blockState -> blockState.getType().equals(Material.TALL_GRASS));
@@ -105,30 +112,7 @@ public class RebirthOfMagnoliaWeapon extends Weapon implements Listener {
 
     @Override
     public void loadWeaponConfig() {
-        lootTypes = new WeighedList<>();
-
-        ConfigurationSection lootSection = getConfigObject("loot", null, ConfigurationSection.class);
-
-        // Probably should create a section here
-        if (lootSection == null) {
-            log.error("Failed to load flower loot types").submit();
-            return;
-        }
-
-        for (String lootItemKey : lootSection.getKeys(false)) {
-            ConfigurationSection lootItemSection = lootSection.getConfigurationSection(lootItemKey);
-            if (lootItemSection == null) continue;
-
-            String itemMaterialAsString = lootItemSection.getString("material");
-            if (itemMaterialAsString == null) continue;
-
-            Material material = Material.getMaterial(itemMaterialAsString.toUpperCase());
-            if (material == null) continue;
-
-            int frequency = lootItemSection.getInt("frequency");
-
-            lootTypes.add(frequency, 1, material);
-        }
+        lootTypes = UtilItem.getDropTable(itemHandler, JavaPlugin.getPlugin(Progression.class), getConfigName(), "rebirth_of_magnolia.loot");
 
         log.info("Loaded " + lootTypes.size() + " flower loot types").submit();
     }
