f121ee6ed24d3cd44f6792c487b99b857ee9c337
StudentAlleg <studentalleg-pilot@yahoo.com>
2024-12-21 20:57:06 -0800

    #1370 - Add prismarine and saplings to shops
    Signed-off-by: StudentAlleg <studentalleg-pilot@yahoo.com>

---
+++ shops/src/main/resources/shops-migrations/V15_5__Add_prismarine.sql
@@ -0,0 +1,7 @@
+
+INSERT IGNORE INTO shopitems
+(Shopkeeper, Material, ItemName, ModelData, MenuSlot, MenuPage, Amount, BuyPrice, SellPrice)
+VALUES
+('Resources', 'PRISMARINE_SHARD', 'Prismarine Shard', 0, 10, 2, 1, 10, 1),
+('Resources', 'PRISMARINE_CRYSTALS', 'Prismarine Crystals', 0, 11, 2, 1, 10, 1)
+;
