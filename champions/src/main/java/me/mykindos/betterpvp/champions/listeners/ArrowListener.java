147c13f61334b7952299d9f44bdbc9e8ec9d7e83
Mykindos <5389944+Mykindos@users.noreply.github.com>
2024-12-23 11:35:33 +1100

    Number tweaks & allow arctic to extend duration of ice prison in the main world only

--- champions/src/main/java/me/mykindos/betterpvp/champions/listeners/ArrowListener.java
+++ champions/src/main/java/me/mykindos/betterpvp/champions/listeners/ArrowListener.java
@@ -32,7 +32,7 @@ public class ArrowListener implements Listener {
     private boolean critArrowsEnabled;
 
     @Inject
-    @Config(path = "combat.arrow-base-damage", defaultValue = "5.0")
+    @Config(path = "combat.arrow-base-damage", defaultValue = "6.0")
     private double baseArrowDamage;
 
     private final HashMap<Arrow, Float> arrows = new HashMap<>();
