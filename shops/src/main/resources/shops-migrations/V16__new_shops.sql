b388ed69806dd3606bc097a69c60632a634eef32
burghar <amirkargari07@gmail.com>
2024-12-23 17:33:24 +0400

    Update and rename V16__new_page_1_shops.sql to V16__new_shops.sql

---
+++ shops/src/main/resources/shops-migrations/V16__new_shops.sql
@@ -0,0 +1,206 @@
+-- Page 2 of Lumberjack
+INSERT IGNORE INTO shopitems (Shopkeeper, Material, ItemName, ModelData, MenuSlot, MenuPage, Amount, BuyPrice, SellPrice) VALUES ('Lumberjack', 'MOSS_BLOCK', 'Moss Block', 0, 10, 2, 1, 20, 5);
+INSERT IGNORE INTO shopitems (Shopkeeper, Material, ItemName, ModelData, MenuSlot, MenuPage, Amount, BuyPrice, SellPrice) VALUES ('Lumberjack', 'FLOWERING_AZALEA', 'Flowering Azalea', 0, 11, 2, 1, 500, 10);
+INSERT IGNORE INTO shopitems (Shopkeeper, Material, ItemName, ModelData, MenuSlot, MenuPage, Amount, BuyPrice, SellPrice) VALUES ('Lumberjack', 'AZALEA', 'Azalea', 0, 12, 2, 1, 500, 10);
+INSERT IGNORE INTO shopitems (Shopkeeper, Material, ItemName, ModelData, MenuSlot, MenuPage, Amount, BuyPrice, SellPrice) VALUES ('Lumberjack', 'BIG_DRIPLEAF', 'Big Dripleaf', 0, 13, 2, 1, 500, 10);
+INSERT IGNORE INTO shopitems (Shopkeeper, Material, ItemName, ModelData, MenuSlot, MenuPage, Amount, BuyPrice, SellPrice) VALUES ('Lumberjack', 'SMALL_DRIPLEAF', 'Small Dripleaf', 0, 14, 2, 1, 500, 10);
+INSERT IGNORE INTO shopitems (Shopkeeper, Material, ItemName, ModelData, MenuSlot, MenuPage, Amount, BuyPrice, SellPrice) VALUES ('Lumberjack', 'WARPED_NYLIUM', 'Warped Nylium', 0, 15, 2, 1, 40, 10);
+INSERT IGNORE INTO shopitems (Shopkeeper, Material, ItemName, ModelData, MenuSlot, MenuPage, Amount, BuyPrice, SellPrice) VALUES ('Lumberjack', 'CRIMSON_NYLIUM', 'Crimson Nylium', 0, 16, 2, 1, 40, 10);
+INSERT IGNORE INTO shopitems (Shopkeeper, Material, ItemName, ModelData, MenuSlot, MenuPage, Amount, BuyPrice, SellPrice) VALUES ('Lumberjack', 'PINK_PETALS', 'Pink Petals', 0, 19, 2, 1, 150, 5);
+INSERT IGNORE INTO shopitems (Shopkeeper, Material, ItemName, ModelData, MenuSlot, MenuPage, Amount, BuyPrice, SellPrice) VALUES ('Lumberjack', 'SPORE_BLOSSOM', 'Spore Blossom', 0, 20, 2, 1, 2000, 50);
+INSERT IGNORE INTO shopitems (Shopkeeper, Material, ItemName, ModelData, MenuSlot, MenuPage, Amount, BuyPrice, SellPrice) VALUES ('Lumberjack', 'VINE', 'Vines', 0, 21, 2, 1, 50, 5);
+INSERT IGNORE INTO shopitems (Shopkeeper, Material, ItemName, ModelData, MenuSlot, MenuPage, Amount, BuyPrice, SellPrice) VALUES ('Lumberjack', 'LILY_PAD', 'Lily Pad', 0, 22, 2, 1, 1500, 75);
+INSERT IGNORE INTO shopitems (Shopkeeper, Material, ItemName, ModelData, MenuSlot, MenuPage, Amount, BuyPrice, SellPrice) VALUES ('Lumberjack', 'DECORATED_POT', 'Decorated Pot', 0, 23, 2, 1, 2000, 100);
+INSERT IGNORE INTO shopitems (Shopkeeper, Material, ItemName, ModelData, MenuSlot, MenuPage, Amount, BuyPrice, SellPrice) VALUES ('Lumberjack', 'TWISTING_VINES', 'Twisting Vines', 0, 24, 2, 1, 250, 10);
+INSERT IGNORE INTO shopitems (Shopkeeper, Material, ItemName, ModelData, MenuSlot, MenuPage, Amount, BuyPrice, SellPrice) VALUES ('Lumberjack', 'WEEPING_VINES', 'Weeping Vines', 0, 25, 2, 1, 250, 10);
+INSERT IGNORE INTO shopitems (Shopkeeper, Material, ItemName, ModelData, MenuSlot, MenuPage, Amount, BuyPrice, SellPrice) VALUES ('Lumberjack', 'HORN_CORAL', 'Horn Coral', 0, 28, 2, 1, 750, 200);
+INSERT IGNORE INTO shopitems (Shopkeeper, Material, ItemName, ModelData, MenuSlot, MenuPage, Amount, BuyPrice, SellPrice) VALUES ('Lumberjack', 'BRAIN_CORAL', 'Brain Coral', 0, 29, 2, 1, 750, 200);
+INSERT IGNORE INTO shopitems (Shopkeeper, Material, ItemName, ModelData, MenuSlot, MenuPage, Amount, BuyPrice, SellPrice) VALUES ('Lumberjack', 'FIRE_CORAL', 'Fire Coral', 0, 30, 2, 1, 750, 200);
+INSERT IGNORE INTO shopitems (Shopkeeper, Material, ItemName, ModelData, MenuSlot, MenuPage, Amount, BuyPrice, SellPrice) VALUES ('Lumberjack', 'BUBBLE_CORAL', 'Bubble Coral', 0, 31, 2, 1, 750, 200);
+INSERT IGNORE INTO shopitems (Shopkeeper, Material, ItemName, ModelData, MenuSlot, MenuPage, Amount, BuyPrice, SellPrice) VALUES ('Lumberjack', 'TUBE_CORAL', 'Tube Coral', 0, 32, 2, 1, 750, 200);
+INSERT IGNORE INTO shopitems (Shopkeeper, Material, ItemName, ModelData, MenuSlot, MenuPage, Amount, BuyPrice, SellPrice) VALUES ('Lumberjack', 'WARPED_ROOTS', 'Warped Roots', 0, 33, 2, 1, 100, 5);
+INSERT IGNORE INTO shopitems (Shopkeeper, Material, ItemName, ModelData, MenuSlot, MenuPage, Amount, BuyPrice, SellPrice) VALUES ('Lumberjack', 'CRIMSON_ROOTS', 'Crimson Roots', 0, 34, 2, 1, 100, 5);
+INSERT IGNORE INTO shopitems (Shopkeeper, Material, ItemName, ModelData, MenuSlot, MenuPage, Amount, BuyPrice, SellPrice) VALUES ('Lumberjack', 'PEONY', 'Peony', 0, 37, 2, 1, 750, 50);
+INSERT IGNORE INTO shopitems (Shopkeeper, Material, ItemName, ModelData, MenuSlot, MenuPage, Amount, BuyPrice, SellPrice) VALUES ('Lumberjack', 'ROSE_BUSH', 'Rose Bush', 0, 38, 2, 1, 750, 50);
+INSERT IGNORE INTO shopitems (Shopkeeper, Material, ItemName, ModelData, MenuSlot, MenuPage, Amount, BuyPrice, SellPrice) VALUES ('Lumberjack', 'LILAC', 'Lilac', 0, 39, 2, 1, 750, 50);
+INSERT IGNORE INTO shopitems (Shopkeeper, Material, ItemName, ModelData, MenuSlot, MenuPage, Amount, BuyPrice, SellPrice) VALUES ('Lumberjack', 'TORCHFLOWER', 'Torchflower', 0, 40, 2, 1, 1000, 250);
+INSERT IGNORE INTO shopitems (Shopkeeper, Material, ItemName, ModelData, MenuSlot, MenuPage, Amount, BuyPrice, SellPrice) VALUES ('Lumberjack', 'SEAGRASS', 'Seagrass', 0, 41, 2, 1, 50, 5);
+INSERT IGNORE INTO shopitems (Shopkeeper, Material, ItemName, ModelData, MenuSlot, MenuPage, Amount, BuyPrice, SellPrice) VALUES ('Lumberjack', 'SEA_PICKLE', 'Sea Pickle', 0, 42, 2, 1, 2000, 100);
+INSERT IGNORE INTO shopitems (Shopkeeper, Material, ItemName, ModelData, MenuSlot, MenuPage, Amount, BuyPrice, SellPrice) VALUES ('Lumberjack', 'KELP', 'Kelp', 0, 43, 2, 1, 50, 5);
+
+-- Updated Page 1 of Building
+UPDATE shopitems
+    SET MenuSlot = 0, BuyPrice = 40, SellPrice = 10
+    WHERE Shopkeeper = 'Building' AND Material = 'STONE' AND MenuPage = 1;
+
+UPDATE shopitems
+    SET MenuSlot = 1, BuyPrice = 20, SellPrice = 5
+    WHERE Shopkeeper = 'Building' AND Material = 'COBBLESTONE' AND MenuPage = 1;
+
+UPDATE shopitems
+    SET MenuSlot = 2, BuyPrice = 40, SellPrice = 10
+    WHERE Shopkeeper = 'Building' AND Material = 'MOSSY_COBBLESTONE' AND MenuPage = 1;
+
+UPDATE shopitems
+    SET MenuSlot = 3, BuyPrice = 5, SellPrice = 2
+    WHERE Shopkeeper = 'Building' AND Material = 'DIRT' AND MenuPage = 1;
+
+UPDATE shopitems
+    SET MenuSlot = 4, BuyPrice = 20, SellPrice = 5
+    WHERE Shopkeeper = 'Building' AND Material = 'GRASS_BLOCK' AND MenuPage = 1;
+
+UPDATE shopitems
+    SET MenuSlot = 5, BuyPrice = 20, SellPrice = 5
+    WHERE Shopkeeper = 'Building' AND Material = 'PODZOL' AND MenuPage = 1;
+
+UPDATE shopitems
+    SET MenuSlot = 7, BuyPrice = 450, SellPrice = 50
+    WHERE Shopkeeper = 'Building' AND Material = 'WHITE_WOOL' AND MenuPage = 1;
+
+UPDATE shopitems
+    SET MenuSlot = 8, BuyPrice = 40, SellPrice = 10
+    WHERE Shopkeeper = 'Building' AND Material = 'WHITE_CONCRETE' AND MenuPage = 1;
+
+UPDATE shopitems
+    SET MenuSlot = 9, BuyPrice = 40, SellPrice = 10
+    WHERE Shopkeeper = 'Building' AND Material = 'STONE_BRICKS' AND MenuPage = 1;
+
+UPDATE shopitems
+    SET MenuSlot = 10, BuyPrice = 40, SellPrice = 10
+    WHERE Shopkeeper = 'Building' AND Material = 'POLISHED_BLACKSTONE_BRICKS' AND MenuPage = 1;
+
+UPDATE shopitems
+    SET MenuSlot = 11, BuyPrice = 40, SellPrice = 10
+    WHERE Shopkeeper = 'Building' AND Material = 'BLACKSTONE' AND MenuPage = 1;
+
+UPDATE shopitems
+    SET MenuSlot = 12, BuyPrice = 40, SellPrice = 10
+    WHERE Shopkeeper = 'Building' AND Material = 'MUD' AND MenuPage = 1;
+
+UPDATE shopitems
+    SET MenuSlot = 13, BuyPrice = 30, SellPrice = 15
+    WHERE Shopkeeper = 'Building' AND Material = 'CALCITE' AND MenuPage = 1;
+
+UPDATE shopitems
+    SET MenuSlot = 14, BuyPrice = 40, SellPrice = 10
+    WHERE Shopkeeper = 'Building' AND Material = 'NETHER_BRICKS' AND MenuPage = 1;
+
+UPDATE shopitems
+    SET MenuSlot = 16, BuyPrice = 2000, SellPrice = 1000
+    WHERE Shopkeeper = 'Building' AND Material = 'SPONGE' AND MenuPage = 1;
+
+UPDATE shopitems
+    SET MenuSlot = 17, BuyPrice = 2000, SellPrice = 500
+    WHERE Shopkeeper = 'Building' AND Material = 'LAPIS_BLOCK' AND MenuPage = 1;
+
+UPDATE shopitems
+    SET MenuSlot = 18, BuyPrice = 40, SellPrice = 10
+    WHERE Shopkeeper = 'Building' AND Material = 'DEESPLATE' AND MenuPage = 1;
+
+UPDATE shopitems
+    SET MenuSlot = 19, BuyPrice = 40, SellPrice = 10
+    WHERE Shopkeeper = 'Building' AND Material = 'COBBLED_DEEPSLATE' AND MenuPage = 1;
+
+UPDATE shopitems
+    SET MenuSlot = 20, BuyPrice = 40, SellPrice = 10
+    WHERE Shopkeeper = 'Building' AND Material = 'BASALT' AND MenuPage = 1;
+
+UPDATE shopitems
+    SET MenuSlot = 21, BuyPrice = 40, SellPrice = 10
+    WHERE Shopkeeper = 'Building' AND Material = 'MUD_BRICKS' AND MenuPage = 1;
+
+UPDATE shopitems
+    SET MenuSlot = 22, BuyPrice = 40, SellPrice = 10
+    WHERE Shopkeeper = 'Building' AND Material = 'END_STONE_BRICKS' AND MenuPage = 1;
+
+UPDATE shopitems
+    SET MenuSlot = 23, BuyPrice = 40, SellPrice = 10
+    WHERE Shopkeeper = 'Building' AND Material = 'PURPUR_BLOCK' AND MenuPage = 1;
+
+UPDATE shopitems
+    SET MenuSlot = 26, BuyPrice = 10, SellPrice = 2
+    WHERE Shopkeeper = 'Building' AND Material = 'SNOW_BLOCK' AND MenuPage = 1;
+
+UPDATE shopitems
+    SET MenuSlot = 27, BuyPrice = 50, SellPrice = 25
+    WHERE Shopkeeper = 'Building' AND Material = 'JUNGLE_LOG' AND MenuPage = 1;
+
+UPDATE shopitems
+    SET MenuSlot = 28, BuyPrice = 50, SellPrice = 25
+    WHERE Shopkeeper = 'Building' AND Material = 'DARK_OAK_LOG' AND MenuPage = 1;
+
+UPDATE shopitems
+    SET MenuSlot = 29, BuyPrice = 50, SellPrice = 25
+    WHERE Shopkeeper = 'Building' AND Material = 'BIRCH_LOG' AND MenuPage = 1;
+
+UPDATE shopitems
+    SET MenuSlot = 30, BuyPrice = 50, SellPrice = 25
+    WHERE Shopkeeper = 'Building' AND Material = 'SPRUCE_LOG' AND MenuPage = 1;
+
+UPDATE shopitems
+    SET MenuSlot = 31, BuyPrice = 50, SellPrice = 25
+    WHERE Shopkeeper = 'Building' AND Material = 'BAMBOO_BLOCK' AND MenuPage = 1;
+
+UPDATE shopitems
+    SET MenuSlot = 34, BuyPrice = 40, SellPrice = 10
+    WHERE Shopkeeper = 'Building' AND Material = 'SMOOTH_QUARTZ' AND MenuPage = 1;
+
+UPDATE shopitems
+    SET MenuSlot = 35, BuyPrice = 700, SellPrice = 350
+    WHERE Shopkeeper = 'Building' AND Material = 'GLOWSTONE' AND MenuPage = 1;
+
+UPDATE shopitems
+    SET MenuSlot = 36, BuyPrice = 50, SellPrice = 25
+    WHERE Shopkeeper = 'Building' AND Material = 'OAK_LOG' AND MenuPage = 1;
+
+UPDATE shopitems
+    SET MenuSlot = 37, BuyPrice = 50, SellPrice = 25
+    WHERE Shopkeeper = 'Building' AND Material = 'ACACIA_LOG' AND MenuPage = 1;
+
+UPDATE shopitems
+    SET MenuSlot = 38, BuyPrice = 30, SellPrice = 15
+    WHERE Shopkeeper = 'Building' AND Material = 'WARPED_STEM' AND MenuPage = 1;
+
+UPDATE shopitems
+    SET MenuSlot = 39, BuyPrice = 30, SellPrice = 15
+    WHERE Shopkeeper = 'Building' AND Material = 'CRIMSON_STEM' AND MenuPage = 1;
+
+UPDATE shopitems
+    SET MenuSlot = 40, BuyPrice = 50, SellPrice = 25
+    WHERE Shopkeeper = 'Building' AND Material = 'CHERRY_LOG' AND MenuPage = 1;
+
+UPDATE shopitems
+    SET MenuSlot = 42, BuyPrice = 40, SellPrice = 10
+    WHERE Shopkeeper = 'Building' AND Material = 'PRISMARINE_BRICKS' AND MenuPage = 1;
+
+UPDATE shopitems
+    SET MenuSlot = 43, BuyPrice = 20, SellPrice = 5
+    WHERE Shopkeeper = 'Building' AND Material = 'PRISMARINE' AND MenuPage = 1;
+
+UPDATE shopitems
+    SET MenuSlot = 44, BuyPrice = 700, SellPrice = 350
+    WHERE Shopkeeper = 'Building' AND Material = 'SEA_LANTERN' AND MenuPage = 1;
+
+UPDATE shopitems
+    SET MenuSlot = 45, BuyPrice = 100, SellPrice = 20
+    WHERE Shopkeeper = 'Building' AND Material = 'GRAVEL' AND MenuPage = 1;
+
+UPDATE shopitems
+    SET MenuSlot = 46, BuyPrice = 20, SellPrice = 10
+    WHERE Shopkeeper = 'Building' AND Material = 'SOUL_SAND' AND MenuPage = 1;
+
+UPDATE shopitems
+    SET MenuSlot = 47, BuyPrice = 50, SellPrice = 10
+    WHERE Shopkeeper = 'Building' AND Material = 'CLAY' AND MenuPage = 1;
+
+UPDATE shopitems
+    SET MenuSlot = 48, BuyPrice = 40, SellPrice = 10
+    WHERE Shopkeeper = 'Building' AND Material = 'SMOOTH_SANDSTONE' AND MenuPage = 1;
+
+UPDATE shopitems
+    SET MenuSlot = 49, BuyPrice = 10, SellPrice = 2
+    WHERE Shopkeeper = 'Building' AND Material = 'SAND' AND MenuPage = 1;
+
+UPDATE shopitems
+    SET MenuSlot = 50, BuyPrice = 100, SellPrice = 2
+    WHERE Shopkeeper = 'Building' AND Material = 'GLASS' AND MenuPage = 1;
