d6538ea471ea7ea93ba4e372d05df7f649dc41fb
Mykindos <5389944+Mykindos@users.noreply.github.com>
2024-12-23 22:59:50 +1100

    Increase armour durability & buff unbreaking runes dramatically

---
+++ champions/src/main/resources/champions-migrations/V9__Further_tweak_durabilities.sql
@@ -0,0 +1,95 @@
+UPDATE itemdurability
+SET Durability = 650 
+WHERE item = (SELECT id FROM items WHERE Namespace = 'champions' AND Keyname = 'assassin_helmet');
+
+UPDATE itemdurability
+SET Durability = 650 
+WHERE item = (SELECT id FROM items WHERE Namespace = 'champions' AND Keyname = 'assassin_vest');
+
+UPDATE itemdurability
+SET Durability = 650 
+WHERE item = (SELECT id FROM items WHERE Namespace = 'champions' AND Keyname = 'assassin_leggings');
+
+UPDATE itemdurability
+SET Durability = 650 
+WHERE item = (SELECT id FROM items WHERE Namespace = 'champions' AND Keyname = 'assassin_boots');
+
+UPDATE itemdurability
+SET Durability = 650 
+WHERE item = (SELECT id FROM items WHERE Namespace = 'champions' AND Keyname = 'knight_helmet');
+
+UPDATE itemdurability
+SET Durability = 650 
+WHERE item = (SELECT id FROM items WHERE Namespace = 'champions' AND Keyname = 'knight_vest');
+
+UPDATE itemdurability
+SET Durability = 650 
+WHERE item = (SELECT id FROM items WHERE Namespace = 'champions' AND Keyname = 'knight_leggings');
+
+UPDATE itemdurability
+SET Durability = 650 
+WHERE item = (SELECT id FROM items WHERE Namespace = 'champions' AND Keyname = 'knight_boots');
+
+UPDATE itemdurability
+SET Durability = 650 
+WHERE item = (SELECT id FROM items WHERE Namespace = 'champions' AND Keyname = 'mage_helmet');
+
+UPDATE itemdurability
+SET Durability = 650 
+WHERE item = (SELECT id FROM items WHERE Namespace = 'champions' AND Keyname = 'mage_vest');
+
+UPDATE itemdurability
+SET Durability = 650 
+WHERE item = (SELECT id FROM items WHERE Namespace = 'champions' AND Keyname = 'mage_leggings');
+
+UPDATE itemdurability
+SET Durability = 650 
+WHERE item = (SELECT id FROM items WHERE Namespace = 'champions' AND Keyname = 'mage_boots');
+
+UPDATE itemdurability
+SET Durability = 650 
+WHERE item = (SELECT id FROM items WHERE Namespace = 'champions' AND Keyname = 'ranger_helmet');
+
+UPDATE itemdurability
+SET Durability = 650 
+WHERE item = (SELECT id FROM items WHERE Namespace = 'champions' AND Keyname = 'ranger_vest');
+
+UPDATE itemdurability
+SET Durability = 650 
+WHERE item = (SELECT id FROM items WHERE Namespace = 'champions' AND Keyname = 'ranger_leggings');
+
+UPDATE itemdurability
+SET Durability = 650 
+WHERE item = (SELECT id FROM items WHERE Namespace = 'champions' AND Keyname = 'ranger_boots');
+
+UPDATE itemdurability
+SET Durability = 650 
+WHERE item = (SELECT id FROM items WHERE Namespace = 'champions' AND Keyname = 'brute_helmet');
+
+UPDATE itemdurability
+SET Durability = 650 
+WHERE item = (SELECT id FROM items WHERE Namespace = 'champions' AND Keyname = 'brute_vest');
+
+UPDATE itemdurability
+SET Durability = 650 
+WHERE item = (SELECT id FROM items WHERE Namespace = 'champions' AND Keyname = 'brute_leggings');
+
+UPDATE itemdurability
+SET Durability = 650 
+WHERE item = (SELECT id FROM items WHERE Namespace = 'champions' AND Keyname = 'brute_boots');
+
+UPDATE itemdurability
+SET Durability = 650 
+WHERE item = (SELECT id FROM items WHERE Namespace = 'champions' AND Keyname = 'warlock_helmet');
+
+UPDATE itemdurability
+SET Durability = 650 
+WHERE item = (SELECT id FROM items WHERE Namespace = 'champions' AND Keyname = 'warlock_vest');
+
+UPDATE itemdurability
+SET Durability = 650 
+WHERE item = (SELECT id FROM items WHERE Namespace = 'champions' AND Keyname = 'warlock_leggings');
+
+UPDATE itemdurability
+SET Durability = 650 
+WHERE item = (SELECT id FROM items WHERE Namespace = 'champions' AND Keyname = 'warlock_boots');
