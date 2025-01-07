a1a82431cfb015143a2c7d04e0a1301ee7d88a33
Mykindos <5389944+Mykindos@users.noreply.github.com>
2025-01-07 19:27:50 +1100

    Fix some weirdness starting dungeons

--- clans/src/main/java/me/mykindos/betterpvp/clans/clans/listeners/ClanPartyListener.java
+++ clans/src/main/java/me/mykindos/betterpvp/clans/clans/listeners/ClanPartyListener.java
@@ -2,13 +2,19 @@ package me.mykindos.betterpvp.clans.clans.listeners;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import me.mykindos.betterpvp.clans.clans.Clan;
 import me.mykindos.betterpvp.clans.clans.ClanManager;
 import me.mykindos.betterpvp.core.listener.BPvPListener;
 import me.mykindos.betterpvp.core.parties.PartyMember;
 import me.mykindos.betterpvp.core.parties.PartyMemberFilter;
 import me.mykindos.betterpvp.core.parties.events.PartyCreateEvent;
+import me.mykindos.betterpvp.core.utilities.UtilMessage;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+
+import java.util.Optional;
 
 @BPvPListener
 @Singleton
@@ -24,24 +30,46 @@ public class ClanPartyListener implements Listener {
 
     @EventHandler
     public void onPartyCreate(PartyCreateEvent event) {
+
+        Player player = Bukkit.getPlayer(event.getParty().getPartyLeader());
+        if (player == null) return;
+
+        Optional<Clan> locationClanOptional = clanManager.getClanByLocation(player.getLocation());
+        if (locationClanOptional.isEmpty()) {
+            UtilMessage.simpleMessage(player, "Error 1 - You must be at shops to form a party.");
+            return;
+        }
+
+        Clan locationClan = locationClanOptional.get();
+        if (!locationClan.isSafe()) {
+            UtilMessage.simpleMessage(player, "Error 2 - You must be at shops to form a party.");
+            return;
+        }
+
         clanManager.getClanByPlayer(event.getParty().getPartyLeader()).ifPresent(clan -> {
             if (event.getFilter() == PartyMemberFilter.CLAN) {
                 clan.getMembersAsPlayers().forEach(member -> {
-                    if (clanManager.isInSafeZone(member)) {
-                        event.getParty().getMembers().add(new PartyMember(member.getUniqueId()));
-                    }
+                    clanManager.getClanByLocation(member.getLocation()).ifPresent(memberLocClan -> {
+                        if (memberLocClan.equals(locationClan)) {
+                            event.getParty().getMembers().add(new PartyMember(member.getUniqueId()));
+                        }
+                    });
                 });
             } else if (event.getFilter() == PartyMemberFilter.CLAN_ALLIES) {
                 clan.getMembersAsPlayers().forEach(member -> {
-                    if (clanManager.isInSafeZone(member)) {
-                        event.getParty().getMembers().add(new PartyMember(member.getUniqueId()));
-                    }
+                    clanManager.getClanByLocation(member.getLocation()).ifPresent(memberLocClan -> {
+                        if (memberLocClan.equals(locationClan)) {
+                            event.getParty().getMembers().add(new PartyMember(member.getUniqueId()));
+                        }
+                    });
                 });
                 clan.getAlliances().forEach(ally -> {
                     ally.getClan().getMembersAsPlayers().forEach(member -> {
-                        if(clanManager.isInSafeZone(member)) {
-                            event.getParty().getMembers().add(new PartyMember(member.getUniqueId()));
-                        }
+                        clanManager.getClanByLocation(member.getLocation()).ifPresent(memberLocClan -> {
+                            if (memberLocClan.equals(locationClan)) {
+                                event.getParty().getMembers().add(new PartyMember(member.getUniqueId()));
+                            }
+                        });
                     });
                 });
 
