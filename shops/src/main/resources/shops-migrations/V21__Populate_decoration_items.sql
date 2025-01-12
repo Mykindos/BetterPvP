8fdc36c012b9e181c400e898ba4cfbe1ba4a397b
Braulio <bchamerry@gmail.com>
2025-01-12 15:32:18 -0800

    Add more blocks to the shops
    Signed-off-by: Braulio <bchamerry@gmail.com>

---
+++ shops/src/main/resources/shops-migrations/V21__Populate_decoration_items.sql
@@ -0,0 +1,11 @@
+INSERT IGNORE INTO shopitems
+(Shopkeeper, Material, ItemName, ModelData, MenuSlot, MenuPage, Amount, BuyPrice, SellPrice)
+VALUES ('Resources', 'COPPER_BLOCK', 'Copper Block', 0, 16, 1, 1, 900, 225),
+       ('Building', 'DARK_PRISMARINE', 'Dark Prismarine', 0, 41, 1, 1, 60, 15),
+       ('Building', 'LODESTONE', 'Lodestone', 0, 25, 1, 1, 1400, 350),
+       ('Building', 'CHAIN', 'Chain', 0, 25, 2, 1, 1100, 225),
+       ('Building', 'MOSS_BLOCK', 'Moss Block', 0, 6, 1, 1, 10, 4),
+       ('Building', 'NETHERRACK', 'Moss Block', 0, 6, 1, 1, 10, 4),
+       ('Building', 'CRIMSON_NYLIUM', 'Crimson Nylium', 0, 41, 1, 1, 20, 5),
+       ('Building', 'WARPED_NYLIUM', 'Warped Nylium', 0, 32, 1, 1, 20, 5),
+       ('Building', 'FLOWER_POT', 'Flower Pot', 0, 24, 2, 1, 40, 10);
