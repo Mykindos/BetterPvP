7baca25bc9e327c08a7181dfdd15b0d83ebff8bf
Braulio <bchamerry@gmail.com>
2025-01-12 13:04:18 -0800

    Increase assassin health by 5 and remove constant speed amplifier
    Signed-off-by: Braulio <bchamerry@gmail.com>

---
+++ core/src/main/resources/core-migrations/global/V14__Change_assassin_health.sql
@@ -0,0 +1,12 @@
+UPDATE armour
+SET Reduction = 7
+WHERE Item = 'LEATHER_BOOTS';
+UPDATE armour
+SET Reduction = 18
+WHERE Item = 'LEATHER_CHESTPLATE';
+UPDATE armour
+SET Reduction = 9
+WHERE Item = 'LEATHER_HELMET';
+UPDATE armour
+SET Reduction = 13
+WHERE Item = 'LEATHER_LEGGINGS';
