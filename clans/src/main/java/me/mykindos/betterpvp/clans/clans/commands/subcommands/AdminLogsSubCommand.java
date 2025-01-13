bf089aad2a960cc175e76aeec95a73ec94803994
StudentAlleg <36683605+StudentAlleg@users.noreply.github.com>
2025-01-12 18:37:30 -0800

    Fix some item logs, clan formatters, allow admins to view logs by clan id (#1470)
    * #1441 - add space in clan invite log
    
    * #1440 - Fix promote formatter using demoted
    
    * Fix PlayerDeath formatter using client instead of victim, add location button to despawn log
    
    * #1439 - Add a way for admins to see clan logs of clans they are not in, and view clan logs by clan id

---
+++ clans/src/main/java/me/mykindos/betterpvp/clans/clans/commands/subcommands/AdminLogsSubCommand.java
@@ -0,0 +1,82 @@
+package me.mykindos.betterpvp.clans.clans.commands.subcommands;
+
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import me.mykindos.betterpvp.clans.Clans;
+import me.mykindos.betterpvp.clans.clans.Clan;
+import me.mykindos.betterpvp.clans.clans.ClanManager;
+import me.mykindos.betterpvp.clans.clans.commands.ClanCommand;
+import me.mykindos.betterpvp.clans.clans.commands.ClanSubCommand;
+import me.mykindos.betterpvp.core.client.Client;
+import me.mykindos.betterpvp.core.client.repository.ClientManager;
+import me.mykindos.betterpvp.core.command.SubCommand;
+import me.mykindos.betterpvp.core.logging.LogContext;
+import me.mykindos.betterpvp.core.logging.menu.CachedLogMenu;
+import me.mykindos.betterpvp.core.logging.repository.LogRepository;
+import me.mykindos.betterpvp.core.utilities.UtilMessage;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Singleton
+@SubCommand(ClanCommand.class)
+public class AdminLogsSubCommand extends ClanSubCommand {
+
+    private final LogRepository logRepository;
+
+    @Inject
+    public AdminLogsSubCommand(ClanManager clanManager, ClientManager clientManager, LogRepository logRepository) {
+        super(clanManager, clientManager);
+        this.logRepository = logRepository;
+    }
+
+    @Override
+    public String getName() {
+        return "adminlogs";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Get logs associated with the specified clan";
+    }
+
+    @Override
+    public String getUsage() {
+        return super.getUsage() + " <clanname|clanid>";
+    }
+
+    @Override
+    public void execute(Player player, Client client, String... args) {
+        if (args.length < 1) {
+            UtilMessage.message(player, "Logs", getUsage());
+        }
+        Optional<Clan> clanOptional = clanManager.getClanByName(args[0]);
+
+        if (clanOptional.isPresent()) {
+            Clan clan = clanOptional.get();
+            new CachedLogMenu(clan.getName(), LogContext.CLAN, clan.getId().toString(), null, CachedLogMenu.CLANS, JavaPlugin.getPlugin(Clans.class), logRepository, null).show(player);
+            return;
+        }
+        UUID clanId;
+        try {
+             clanId = UUID.fromString(args[0]);
+        } catch (IllegalArgumentException ignored) {
+            UtilMessage.message(player, "Logs", "<yellow>%s</yellow> is not a valid ID", args[0]);
+            return;
+        }
+        new CachedLogMenu(args[0], LogContext.CLAN, clanId.toString(), null, CachedLogMenu.CLANS, JavaPlugin.getPlugin(Clans.class), logRepository, null).show(player);
+
+    }
+
+    @Override
+    public boolean canExecuteWithoutClan() {
+        return true;
+    }
+
+    @Override
+    public String getArgumentType(int argCount) {
+        return argCount == 1 ? ClanArgumentType.CLAN.name() : ArgumentType.NONE.name();
+    }
+}
