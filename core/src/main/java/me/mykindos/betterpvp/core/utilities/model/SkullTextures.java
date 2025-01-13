ce4c8bbfcbcdfd54105a412018b8d7e83b3e49b6
Braulio <bchamerry@gmail.com>
2025-01-12 20:39:22 -0800

    Prettify the build editor menu
    Signed-off-by: Braulio <bchamerry@gmail.com>

---
+++ core/src/main/java/me/mykindos/betterpvp/core/utilities/model/SkullTextures.java
@@ -0,0 +1,33 @@
+package me.mykindos.betterpvp.core.utilities.model;
+
+import lombok.experimental.UtilityClass;
+import me.mykindos.betterpvp.core.inventory.item.builder.SkullBuilder.HeadTexture;
+
+@UtilityClass
+public class SkullTextures {
+
+    public static final HeadTexture SOLID_GREEN = new HeadTexture(
+            "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvYTNlOWY0ZGJhZGRlMGY3MjdjNTgwM2Q3NWQ4YmIzNzhmYjlmY2I0YjYwZDMzYmVjMTkwOTJhM2EyZTdiMDdhOSJ9fX0="
+    );
+
+    public static final HeadTexture SOLID_RED = new HeadTexture(
+            "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvYzY1ZjNiYWUwZDIwM2JhMTZmZTFkYzNkMTMwN2E4NmE2MzhiZTkyNDQ3MWYyM2U4MmFiZDlkNzhmOGEzZmNhIn19fQ=="
+    );
+
+    public static final HeadTexture SOLID_GRAY = new HeadTexture(
+            "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvYzMyOGRjZGUxNzNiZWZmOWYzZjQxYjkyMzIxM2ZjMWJiNzY3ODk2N2NjYjJlZGU3YTdjZjQwYjE4MzZiMWE3MyJ9fX0="
+    );
+
+    public static final HeadTexture EXCLAMATION_MARK = new HeadTexture(
+            "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvYjZlNTIyZDkxODI1MjE0OWU2ZWRlMmVkZjNmZTBmMmMyYzU4ZmVlNmFjMTFjYjg4YzYxNzIwNzIxOGFlNDU5NSJ9fX0="
+    );
+
+    public static final HeadTexture QUESTION_MARK = new HeadTexture(
+            "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvMjcwNWZkOTRhMGM0MzE5MjdmYjRlNjM5YjBmY2ZiNDk3MTdlNDEyMjg1YTAyYjQzOWUwMTEyZGEyMmIyZTJlYyJ9fX0="
+    );
+
+    public static final HeadTexture INFORMATION = new HeadTexture(
+            "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvZDAxYWZlOTczYzU0ODJmZGM3MWU2YWExMDY5ODgzM2M3OWM0MzdmMjEzMDhlYTlhMWEwOTU3NDZlYzI3NGEwZiJ9fX0="
+    );
+
+}
