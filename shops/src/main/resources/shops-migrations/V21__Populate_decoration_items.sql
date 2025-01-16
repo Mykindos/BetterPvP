abf662ca883be2f19defe7c36f097c1043ae0334
Braulio <bchamerry@gmail.com>
2025-01-16 08:25:37 -0800

    Remove duplicated Moss Block
    Signed-off-by: Braulio <bchamerry@gmail.com>

--- shops/src/main/resources/shops-migrations/V21__Populate_decoration_items.sql
+++ shops/src/main/resources/shops-migrations/V21__Populate_decoration_items.sql
@@ -3,6 +3,5 @@ INSERT IGNORE INTO shopitems
 VALUES ('Resources', 'COPPER_BLOCK', 'Copper Block', 0, 16, 1, 1, 900, 225),
        ('Building', 'LODESTONE', 'Lodestone', 0, 25, 1, 1, 1400, 350),
        ('Building', 'CHAIN', 'Chain', 0, 25, 2, 1, 1100, 225),
-       ('Building', 'MOSS_BLOCK', 'Moss Block', 0, 6, 1, 1, 10, 4),
        ('Building', 'NETHERRACK', 'Moss Block', 0, 6, 1, 1, 10, 4),
        ('Building', 'FLOWER_POT', 'Flower Pot', 0, 24, 2, 1, 40, 10);
