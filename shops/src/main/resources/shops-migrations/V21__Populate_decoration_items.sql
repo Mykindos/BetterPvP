36268b14fe031ddc3d9398f2440cab76c3aa2720
Braulio <bchamerry@gmail.com>
2025-01-23 10:01:21 -0800

    Move Chain & Flower Pot to Lumberjack NPC
    Signed-off-by: Braulio <bchamerry@gmail.com>

--- shops/src/main/resources/shops-migrations/V21__Populate_decoration_items.sql
+++ shops/src/main/resources/shops-migrations/V21__Populate_decoration_items.sql
@@ -2,6 +2,6 @@ INSERT IGNORE INTO shopitems
 (Shopkeeper, Material, ItemName, ModelData, MenuSlot, MenuPage, Amount, BuyPrice, SellPrice)
 VALUES ('Resources', 'COPPER_BLOCK', 'Copper Block', 0, 16, 1, 1, 900, 225),
        ('Building', 'LODESTONE', 'Lodestone', 0, 25, 1, 1, 1400, 350),
-       ('Building', 'CHAIN', 'Chain', 0, 25, 2, 1, 1100, 225),
+       ('Lumberjack', 'CHAIN', 'Chain', 0, 3, 2, 1, 1100, 225),
        ('Building', 'NETHERRACK', 'Moss Block', 0, 6, 1, 1, 10, 4),
-       ('Building', 'FLOWER_POT', 'Flower Pot', 0, 24, 2, 1, 40, 10);
+       ('Lumberjack', 'FLOWER_POT', 'Flower Pot', 0, 5, 2, 1, 40, 10);
