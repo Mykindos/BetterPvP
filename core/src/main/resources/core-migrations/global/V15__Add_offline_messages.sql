c031e4d9f6e670b5c8e5e06b857365e463f49e28
StudentAlleg <36683605+StudentAlleg@users.noreply.github.com>
2025-01-23 06:50:43 -0800

    Add OfflineMessages (#1474)
    * #1341 - Add OfflineMessages, messages that are sent to players, that on the next login, they will receive.
    #1277 - Add offline message for when your combat log sheep is clicked
    
    * #1474 - Sonarqube fixes, add documentation
    
    * #1474 - Change to completeable futures, change OfflineMessages to Offline

---
+++ core/src/main/resources/core-migrations/global/V15__Add_offline_messages.sql
@@ -0,0 +1,29 @@
+INSERT IGNORE INTO property_map VALUES ("SHOW_TAG", "long");
+
+create table if not exists offline_messages
+(
+    id        int auto_increment                  primary key,
+    Client    varchar(36)                           not null,
+    Time      bigint                                not null,
+    Action    varchar(36)                           not null,
+    Message   text                                  not null
+);
+
+ALTER TABLE offline_messages ADD INDEX (Client);
+
+DROP PROCEDURE IF EXISTS GetOfflineMessagesByTime;
+CREATE PROCEDURE GetOfflineMessagesByTime(IN client_param VARCHAR(36), IN time_param bigint)
+BEGIN
+    SELECT
+        offline_messages.Time,
+        offline_messages.Action,
+        offline_messages.Message
+    FROM
+        offline_messages
+    WHERE
+        offline_messages.Client = client_param
+    AND
+        offline_messages.Time > time_param
+    ORDER BY
+        offline_messages.Time DESC;
+END;
