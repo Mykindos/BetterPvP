2dc36d5e6068590e086effb5350341c29ba9a800
StudentAlleg <studentalleg-pilot@yahoo.com>
2025-02-15 18:09:04 -0800

    #1420 - Begin framework for BrigadierCommands

---
+++ core/src/main/java/me/mykindos/betterpvp/core/command/brigadier/arguments/UUIDItemArgumentType.java
@@ -0,0 +1,71 @@
+package me.mykindos.betterpvp.core.command.brigadier.arguments;
+
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import com.mojang.brigadier.LiteralMessage;
+import com.mojang.brigadier.arguments.ArgumentType;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.brigadier.exceptions.DynamicCommandExceptionType;
+import com.mojang.brigadier.suggestion.Suggestions;
+import com.mojang.brigadier.suggestion.SuggestionsBuilder;
+import io.papermc.paper.command.brigadier.argument.ArgumentTypes;
+import io.papermc.paper.command.brigadier.argument.CustomArgumentType;
+import me.mykindos.betterpvp.core.items.uuiditem.UUIDItem;
+import me.mykindos.betterpvp.core.items.uuiditem.UUIDManager;
+
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+
+@Singleton
+public class UUIDItemArgumentType extends BPvPArgumentType<UUIDItem, UUID> implements CustomArgumentType.Converted<UUIDItem, UUID> {
+    private final UUIDManager uuidManager;
+    @Inject
+    protected UUIDItemArgumentType(UUIDManager uuidManager) {
+        super("UUIDItem");
+        this.uuidManager = uuidManager;
+    }
+
+    private CommandSyntaxException invalidUUID(UUID id) {
+        return new DynamicCommandExceptionType((uuid) -> new LiteralMessage("Unknown UUIDItem with UUID: " + uuid))
+                .create(id);
+    }
+
+    /**
+     * Converts the value from the native type to the custom argument type.
+     *
+     * @param nativeType native argument provided value
+     * @return converted value
+     * @throws CommandSyntaxException if an exception occurs while parsing
+     */
+    @Override
+    public UUIDItem convert(UUID nativeType) throws CommandSyntaxException {
+        return uuidManager.getObject(nativeType).orElseThrow(() -> invalidUUID(nativeType));
+    }
+
+    /**
+     * Gets the native type that this argument uses,
+     * the type that is sent to the client.
+     *
+     * @return native argument type
+     */
+    @Override
+    public ArgumentType<UUID> getNativeType() {
+        return ArgumentTypes.uuid();
+    }
+
+    /**
+     * Provides a list of suggestions to show to the client.
+     *
+     * @param context command context
+     * @param builder suggestion builder
+     * @return suggestions
+     */
+    @Override
+    public <S> CompletableFuture<Suggestions> listSuggestions(CommandContext<S> context, SuggestionsBuilder builder) {
+        uuidManager.getObjects().keySet().stream()
+                .filter(uuid -> uuid.toLowerCase().contains(builder.getRemainingLowerCase()))
+                .forEach(builder::suggest);
+        return builder.buildFuture();
+    }
+}
