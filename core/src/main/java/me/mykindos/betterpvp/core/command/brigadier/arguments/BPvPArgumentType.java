1dc495ee4ec989eb4a01ef76d71581885c806cb3
StudentAlleg <studentalleg-pilot@yahoo.com>
2025-02-15 18:09:04 -0800

    #1420 - Begin framework for BrigadierCommands

---
+++ core/src/main/java/me/mykindos/betterpvp/core/command/brigadier/arguments/BPvPArgumentType.java
@@ -0,0 +1,14 @@
+package me.mykindos.betterpvp.core.command.brigadier.arguments;
+
+import io.papermc.paper.command.brigadier.argument.CustomArgumentType;
+import lombok.Getter;
+
+public abstract class BPvPArgumentType<T, N> implements CustomArgumentType<T, N> {
+
+    @Getter
+    private final String name;
+
+    protected BPvPArgumentType(String name) {
+        this.name = name;
+    }
+}
