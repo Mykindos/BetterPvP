3a3a334d1291f4daccf814cd4e2faa8278fb31d4
StudentAlleg <studentalleg-pilot@yahoo.com>
2025-02-17 00:44:11 -0800

    #1420 - Explore multiple argument types

--- core/src/main/java/me/mykindos/betterpvp/core/command/brigadier/arguments/BPvPArgumentType.java
+++ core/src/main/java/me/mykindos/betterpvp/core/command/brigadier/arguments/BPvPArgumentType.java
@@ -4,7 +4,6 @@ import io.papermc.paper.command.brigadier.argument.CustomArgumentType;
 import lombok.Getter;
 
 public abstract class BPvPArgumentType<T, N> implements CustomArgumentType<T, N> {
-
     @Getter
     private final String name;
 
