d3ba5c00343486e49c6afe2408993bde4e7a09fd
StudentAlleg <studentalleg-pilot@yahoo.com>
2025-02-19 21:40:41 -0800

    #1420 - Add multiple ClanArgument types, these offer different ranges of suggestions based on executor (which seems to be sender). Change how statics are passed to allow javadoc

--- clans/src/main/java/me/mykindos/betterpvp/clans/commands/arguments/types/TrustedClanArgument.java
+++ clans/src/main/java/me/mykindos/betterpvp/clans/commands/arguments/types/TrustedClanArgument.java
@@ -1,4 +1,4 @@
-package me.mykindos.betterpvp.clans.commands.arguments;
+package me.mykindos.betterpvp.clans.commands.arguments.types;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
@@ -8,17 +8,23 @@ import com.mojang.brigadier.suggestion.SuggestionsBuilder;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
 import me.mykindos.betterpvp.clans.clans.Clan;
 import me.mykindos.betterpvp.clans.clans.ClanManager;
+import me.mykindos.betterpvp.core.components.clans.data.ClanAlliance;
 
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 /**
- * Prompts the sender with a list of enemy Clans to the executor, guarantees a valid Clan return, but not a valid enemy
+ * Prompts the sender with a list of allied Clans to the executor, guarantees a valid Clan return, but not a valid ally
  */
 @Singleton
-public class ExecutorEnemyClanArgument extends ClanArgument {
+public class TrustedClanArgument extends ClanArgument {
     @Inject
-    protected ExecutorEnemyClanArgument(ClanManager clanManager) {
+    protected TrustedClanArgument(ClanManager clanManager) {
         super(clanManager);
+    }
+
+    @Override
+    public String getName() {
+        return "Trusted Clan";
     }
 
     @Override
@@ -26,13 +32,16 @@ public class ExecutorEnemyClanArgument extends ClanArgument {
         //TODO executor isnt updated when typing out command, might be better to rename this and specifically target sender
         if (!(context.getSource() instanceof CommandSourceStack sourceStack)) return super.listSuggestions(context, builder);
         Optional<Clan> executorClanOptional = clanManager.getClanByPlayer(sourceStack.getExecutor().getUniqueId());
-        if (executorClanOptional.isEmpty()) return builder.buildFuture();
+        if (executorClanOptional.isEmpty()) return super.listSuggestions(context, builder);;
         Clan executorClan = executorClanOptional.get();
 
         clanManager.getObjects().values().stream()
-                .filter(executorClan::isEnemy)
+                .filter(clan -> {
+                    Optional<ClanAlliance> clanAllianceOptional = clan.getAlliance(executorClan);
+                    return (clanAllianceOptional.isPresent() && clanAllianceOptional.get().isTrusted());
+                })
                 .map(Clan::getName)
-                .filter(name -> name.contains(builder.getRemainingLowerCase()))
+                .filter(name -> name.toLowerCase().contains(builder.getRemainingLowerCase()))
                 .forEach(builder::suggest);
         return builder.buildFuture();
     }
