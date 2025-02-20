bd0cde72de0f828fe92cf38ff254fa9607cfcd8f
StudentAlleg <studentalleg-pilot@yahoo.com>
2025-02-19 17:48:26 -0800

    #1420 - Change how argument types are retrieved, added ExecutorEnemyClanArgument which only lists enemies of the executor

--- clans/src/main/java/me/mykindos/betterpvp/clans/commands/arguments/BPvPClansArgumentTypes.java
+++ clans/src/main/java/me/mykindos/betterpvp/clans/commands/arguments/BPvPClansArgumentTypes.java
@@ -2,13 +2,31 @@ package me.mykindos.betterpvp.clans.commands.arguments;
 
 import com.google.inject.Inject;
 import me.mykindos.betterpvp.clans.Clans;
-import me.mykindos.betterpvp.core.command.brigadier.arguments.BPvPArgumentType;
+import me.mykindos.betterpvp.clans.clans.Clan;
 import me.mykindos.betterpvp.core.command.brigadier.arguments.BPvPArgumentTypes;
 
 public class BPvPClansArgumentTypes {
-    public static BPvPArgumentType<?, ?> CLAN;
+    private static ClanArgument CLAN;
+    private static ExecutorEnemyClanArgument EXECUTORENEMYCLAN;
     @Inject
     public BPvPClansArgumentTypes(Clans plugin) {
-        BPvPClansArgumentTypes.CLAN = BPvPArgumentTypes.createArgumentType(plugin, ClanArgument.class);
+        BPvPClansArgumentTypes.CLAN = (ClanArgument) BPvPArgumentTypes.createArgumentType(plugin, ClanArgument.class);
+        BPvPClansArgumentTypes.EXECUTORENEMYCLAN = (ExecutorEnemyClanArgument) BPvPArgumentTypes.createArgumentType(plugin, ExecutorEnemyClanArgument.class);
+    }
+
+    /**
+     * Prompts the sender with any {@link Clan}. Guarantees a valid return {@link Clan}.
+     * @return the {@link ClanArgument}
+     */
+    public static ClanArgument clan() {
+        return CLAN;
+    }
+
+    /**
+     * Prompts the sender with the executor's {@link Clan}'s enemies. Guarantees a valid return {@link Clan}, but not if it is an enemy of the executor.
+     * @return the {@link ExecutorEnemyClanArgument}
+     */
+    public static ExecutorEnemyClanArgument executorEnemyClan() {
+        return EXECUTORENEMYCLAN;
     }
 }
