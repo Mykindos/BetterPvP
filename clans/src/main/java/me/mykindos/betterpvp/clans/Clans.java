24d3a05d19e5d0be674fba59502e2eac9b0f638a
StudentAlleg <studentalleg-pilot@yahoo.com>
2025-02-19 22:01:31 -0800

    #1420 - Do statics in a much better way, remove some debug

--- clans/src/main/java/me/mykindos/betterpvp/clans/Clans.java
+++ clans/src/main/java/me/mykindos/betterpvp/clans/Clans.java
@@ -9,7 +9,6 @@ import me.mykindos.betterpvp.clans.clans.ClanManager;
 import me.mykindos.betterpvp.clans.commands.BrigadierClansCommandLoader;
 import me.mykindos.betterpvp.clans.clans.explosion.ExplosiveResistanceBootstrap;
 import me.mykindos.betterpvp.clans.commands.ClansCommandLoader;
-import me.mykindos.betterpvp.clans.commands.arguments.BPvPClansArgumentTypes;
 import me.mykindos.betterpvp.clans.display.ClansSidebarListener;
 import me.mykindos.betterpvp.clans.injector.ClansInjectorModule;
 import me.mykindos.betterpvp.clans.leaderboards.ClansLeaderboardLoader;
@@ -101,9 +100,6 @@ public class Clans extends BPvPPlugin {
 
             var leaderboardLoader = injector.getInstance(ClansLeaderboardLoader.class);
             leaderboardLoader.registerLeaderboards(PACKAGE);
-
-            var argumentTypes = injector.getInstance(BPvPClansArgumentTypes.class);
-            injector.injectMembers(argumentTypes);
 
             updateEventExecutor.loadPlugin(this);
 
