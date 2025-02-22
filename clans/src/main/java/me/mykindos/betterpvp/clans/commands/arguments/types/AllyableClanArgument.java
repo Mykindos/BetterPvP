bbdddfd50e3a0cc8e4ee8bacff4f4b227652eab1
StudentAlleg <studentalleg-pilot@yahoo.com>
2025-02-22 00:23:26 -0800

    $1420 - Change how ClanArgument is specified, add brigadier trust.

--- clans/src/main/java/me/mykindos/betterpvp/clans/commands/arguments/types/AllyableClanArgument.java
+++ clans/src/main/java/me/mykindos/betterpvp/clans/commands/arguments/types/AllyableClanArgument.java
@@ -2,17 +2,10 @@ package me.mykindos.betterpvp.clans.commands.arguments.types;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
-import com.mojang.brigadier.context.CommandContext;
-import com.mojang.brigadier.suggestion.Suggestions;
-import com.mojang.brigadier.suggestion.SuggestionsBuilder;
-import io.papermc.paper.command.brigadier.CommandSourceStack;
 import me.mykindos.betterpvp.clans.clans.Clan;
 import me.mykindos.betterpvp.clans.clans.ClanManager;
-import org.jetbrains.annotations.NotNull;
 
-import java.util.Objects;
-import java.util.Optional;
-import java.util.concurrent.CompletableFuture;
+import java.util.function.Predicate;
 
 /**
  * Prompts the sender with a list of allyable Clans to the executor, guarantees a valid Clan return, but not a valid allyable clan
@@ -29,20 +22,7 @@ public class AllyableClanArgument extends ClanArgument {
         return "Allyable Clan";
     }
 
-    @Override
-    public <S> @NotNull CompletableFuture<Suggestions> listSuggestions(CommandContext<S> context, SuggestionsBuilder builder) {
-        //TODO executor isnt updated when typing out command, might be better to rename this and specifically target sender
-        if (!(context.getSource() instanceof final CommandSourceStack sourceStack))
-            return super.listSuggestions(context, builder);
-        final Optional<Clan> executorClanOptional = clanManager.getClanByPlayer(Objects.requireNonNull(sourceStack.getExecutor()).getUniqueId());
-        if (executorClanOptional.isEmpty()) return super.listSuggestions(context, builder);
-        final Clan executorClan = executorClanOptional.get();
-
-        clanManager.getObjects().values().stream()
-                .filter(clan -> clanManager.canAlly(executorClan, clan))
-                .map(Clan::getName)
-                .filter(name -> name.toLowerCase().contains(builder.getRemainingLowerCase()))
-                .forEach(builder::suggest);
-        return builder.buildFuture();
+    protected Predicate<Clan> executorClanPredicate(Clan executorClan) {
+        return clan -> clanManager.canAlly(executorClan, clan);
     }
 }
