bdc33fffc2fc1d81c38bace3facd1cbd52a7c0c2
StudentAlleg <studentalleg-pilot@yahoo.com>
2025-02-22 00:23:26 -0800

    $1420 - Change how ClanArgument is specified, add brigadier trust.

--- clans/src/main/java/me/mykindos/betterpvp/clans/commands/arguments/types/AllyOrEnemyClanArgument.java
+++ clans/src/main/java/me/mykindos/betterpvp/clans/commands/arguments/types/AllyOrEnemyClanArgument.java
@@ -2,15 +2,11 @@ package me.mykindos.betterpvp.clans.commands.arguments.types;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
-import com.mojang.brigadier.context.CommandContext;
-import com.mojang.brigadier.suggestion.Suggestions;
-import com.mojang.brigadier.suggestion.SuggestionsBuilder;
-import io.papermc.paper.command.brigadier.CommandSourceStack;
 import me.mykindos.betterpvp.clans.clans.Clan;
 import me.mykindos.betterpvp.clans.clans.ClanManager;
 
-import java.util.Optional;
-import java.util.concurrent.CompletableFuture;
+import java.util.function.Predicate;
+
 /**
  * Prompts the sender with a list of allied and enemy Clans to the executor, guarantees a valid Clan return, but not a valid ally
  */
@@ -26,20 +22,8 @@ public class AllyOrEnemyClanArgument extends ClanArgument {
         return "Ally or Enemy Clan";
     }
 
-    @Override
-    public <S> CompletableFuture<Suggestions> listSuggestions(CommandContext<S> context, SuggestionsBuilder builder) {
-        //TODO executor isnt updated when typing out command, might be better to rename this and specifically target sender
-        if (!(context.getSource() instanceof final CommandSourceStack sourceStack)) return super.listSuggestions(context, builder);
-        final Optional<Clan> executorClanOptional = clanManager.getClanByPlayer(sourceStack.getExecutor().getUniqueId());
-        if (executorClanOptional.isEmpty()) return super.listSuggestions(context, builder);
-        final Clan executorClan = executorClanOptional.get();
-
-        clanManager.getObjects().values().stream()
-                .filter(clan -> clan.isAllied(executorClan) || clan.isEnemy(executorClan))
-                .map(Clan::getName)
-                .filter(name -> name.toLowerCase().contains(builder.getRemainingLowerCase()))
-                .forEach(builder::suggest);
-        return builder.buildFuture();
+    protected Predicate<Clan> executorClanPredicate(Clan executorClan) {
+        return clan -> clan.isAllied(executorClan) || clan.isEnemy(executorClan);
     }
 
 }
