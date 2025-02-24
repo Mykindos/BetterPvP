a2d42b30a393f6a489a08503dfe25bd1052499f0
Mykindos <5389944+Mykindos@users.noreply.github.com>
2025-02-25 02:29:09 +1100

    Move logging to global database

--- core/src/main/resources/core-migrations/global/logging/V20250225_1__Add_logging_to_global.sql
+++ core/src/main/resources/core-migrations/global/logging/V20250225_1__Add_logging_to_global.sql
@@ -1,32 +1,37 @@
 create table if not exists logs
 (
     id        varchar(36)                           primary key,
+    Server    varchar(50)                           not null,
     Level     varchar(255)                          not null,
     Action    varchar(255)                          null,
     Message   text                                  not null,
     Time      bigint                                not null
 );
 
-create table logs_context
+ALTER TABLE logs ADD INDEX (Level);
+ALTER TABLE logs ADD INDEX (Server, Action);
+
+create table if not exists logs_context
 (
     id      int auto_increment primary key,
     LogId   varchar(36)  not null,
     Context varchar(255) not null,
-    Value   varchar(255) not null
+    Value   varchar(255) not null,
+    constraint fk_logs_context_logid foreign key (LogId) references logs (id) on delete cascade
 );
 
-ALTER TABLE logs_context ADD INDEX (LogId);
 ALTER TABLE logs_context ADD INDEX (Context, Value);
 
 create table if not exists uuiditems
 (
     UUID        varchar(36)     PRIMARY KEY,
+    Server      varchar(50)     NOT NULL,
     Namespace   varchar(255)    NOT NULL,
     Keyname     varchar(255)    NOT NULL
 );
 
 DROP PROCEDURE IF EXISTS GetLogMessagesByContextAndValue;
-CREATE PROCEDURE GetLogMessagesByContextAndValue(IN context_param VARCHAR(255), IN value_param VARCHAR(255))
+CREATE PROCEDURE GetLogMessagesByContextAndValue(IN context_param VARCHAR(255), IN value_param VARCHAR(255), IN server_param VARCHAR(50))
 BEGIN
     SELECT
         l.Message,
@@ -40,7 +45,7 @@ BEGIN
     WHERE
         lc.LogId IN (
             SELECT LogId FROM logs_context WHERE Context = context_param AND Value = value_param
-        )
+        ) AND l.Server = server_param
     GROUP BY
         lc.LogId, l.time
     ORDER BY
@@ -48,7 +53,7 @@ BEGIN
 END;
 
 DROP PROCEDURE IF EXISTS GetLogMessagesByContextAndAction;
-CREATE PROCEDURE GetLogMessagesByContextAndAction(IN context_param VARCHAR(255), IN value_param VARCHAR(255), IN action_param VARCHAR(255))
+CREATE PROCEDURE GetLogMessagesByContextAndAction(IN context_param VARCHAR(255), IN value_param VARCHAR(255), IN action_param VARCHAR(255), IN server_param VARCHAR(50))
 BEGIN
     SELECT
         l.Message,
@@ -62,7 +67,7 @@ BEGIN
     WHERE
         lc.LogId IN (
             SELECT LogId FROM logs_context WHERE Context = context_param AND Value = value_param
-        ) AND l.Action LIKE CONCAT(action_param, '%')
+        ) AND l.Server = server_param AND l.Action LIKE CONCAT(action_param, '%')
     GROUP BY
         lc.LogId, l.time
     ORDER BY
