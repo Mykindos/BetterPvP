fe8f5ba8a5a4b584d1b454ead2202a694afc1eec
StudentAlleg <studentalleg-pilot@yahoo.com>
2025-02-27 01:21:32 -0800

    #1420 - Add static method that only shows suggestions when players have permission to use that argument, mirrored statics there for consistency. Update documentation, temporary fix for CommandListener preventing non-opped players from using commands.

---
+++ core/src/main/java/me/mykindos/betterpvp/core/command/brigadier/arguments/types/OnlinePlayerNameArgument.java
@@ -0,0 +1,117 @@
+package me.mykindos.betterpvp.core.command.brigadier.arguments.types;
+
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import com.mojang.brigadier.LiteralMessage;
+import com.mojang.brigadier.arguments.ArgumentType;
+import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.brigadier.exceptions.DynamicCommandExceptionType;
+import com.mojang.brigadier.suggestion.Suggestions;
+import com.mojang.brigadier.suggestion.SuggestionsBuilder;
+import io.papermc.paper.command.brigadier.CommandSourceStack;
+import io.papermc.paper.command.brigadier.argument.CustomArgumentType;
+import me.mykindos.betterpvp.core.command.brigadier.arguments.BPvPArgumentType;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Objects;
+import java.util.concurrent.CompletableFuture;
+
+@Singleton
+public class OnlinePlayerNameArgument extends BPvPArgumentType<Player, String> implements CustomArgumentType.Converted<Player, String> {
+    public static final DynamicCommandExceptionType UNKNOWN_PLAYER = new DynamicCommandExceptionType(
+            (playerName) -> new LiteralMessage(playerName + " does not match an online player")
+    );
+
+
+    //TODO refine can see check to admin vanish
+    @Inject
+    public OnlinePlayerNameArgument() {
+        super("Online Player");
+    }
+
+    /**
+     * Gets the native type that this argument uses,
+     * the type that is sent to the client.
+     *
+     * @return native argument type
+     */
+    @Override
+    public @NotNull ArgumentType<String> getNativeType() {
+        return StringArgumentType.word();
+    }
+
+    /**
+     * Converts the value from the native type to the custom argument type.
+     *
+     * @param nativeType native argument provided value
+     * @return converted value
+     * @throws CommandSyntaxException if an exception occurs while parsing
+     * @see #convert(Object, Object)
+     */
+    @Override
+    public @NotNull Player convert(@NotNull String nativeType) throws CommandSyntaxException {
+        final Player player = Bukkit.getPlayerExact(nativeType);
+        if (player == null) {
+            throw UNKNOWN_PLAYER.create(nativeType);
+        }
+
+        playerChecker(null, player);
+
+        return player;
+    }
+
+    @Override
+    public <S> @NotNull Player convert(@NotNull String nativeType, @NotNull S source) throws CommandSyntaxException {
+        final Player player = Bukkit.getPlayerExact(nativeType);
+        if (player == null) {
+            throw UNKNOWN_PLAYER.create(nativeType);
+        }
+
+        if (!(source instanceof CommandSourceStack sourceStack)) return player;
+
+        final @Nullable Player executor = Bukkit.getPlayer(Objects.requireNonNull(sourceStack.getExecutor()).getUniqueId());
+
+        playerChecker(executor, player);
+
+        return player;
+    }
+
+    @Override
+    public <S> @NotNull CompletableFuture<Suggestions> listSuggestions(CommandContext<S> context, @NotNull SuggestionsBuilder builder) {
+        if (!(context.getSource() instanceof final CommandSourceStack sourceStack))
+            return super.listSuggestions(context, builder);
+
+        final @Nullable Player executor = Bukkit.getPlayer(Objects.requireNonNull(sourceStack.getExecutor()).getUniqueId());
+
+        Bukkit.getOnlinePlayers().stream()
+                .filter(player -> {
+                    try {
+                        playerChecker(executor, player);
+                    } catch (CommandSyntaxException ignored) {
+                        return false;
+                    }
+
+                    if (!(sourceStack.getSender() instanceof Player sender)) return true;
+                    return sender.canSee(player);
+                })
+                .map(Player::getName)
+                .filter(name -> name.toLowerCase().contains(builder.getRemainingLowerCase()))
+                .forEach(builder::suggest);
+        return builder.buildFuture();
+    }
+
+    /**
+     * Check to see if the given {@link Player}
+     * should throw a {@link CommandSyntaxException} if invalid
+     * @param target the {@link Player} being checked
+     * @throws CommandSyntaxException if target is invalid
+     */
+    protected void playerChecker(@Nullable Player executor, @NotNull Player target) throws CommandSyntaxException {
+        //intentionally empty, extended classes should override
+    }
+}
