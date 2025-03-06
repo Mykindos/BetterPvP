6a100487eca435bbb89f56fdaa99ad304385abbc
StudentAlleg <studentalleg-pilot@yahoo.com>
2025-03-05 16:01:26 -0800

    #1420 - Change ClanMember to return a ClanMember. Refactor to do checking entirely in argument. Add disband, enemy, setcore, and leave. Add admin arguments for promote, kick, and demote.

--- core/src/main/java/me/mykindos/betterpvp/core/command/brigadier/arguments/ArgumentException.java
+++ core/src/main/java/me/mykindos/betterpvp/core/command/brigadier/arguments/ArgumentException.java
@@ -2,6 +2,7 @@ package me.mykindos.betterpvp.core.command.brigadier.arguments;
 
 import com.mojang.brigadier.LiteralMessage;
 import com.mojang.brigadier.exceptions.Dynamic3CommandExceptionType;
+import com.mojang.brigadier.exceptions.DynamicCommandExceptionType;
 
 public class ArgumentException {
     public static final Dynamic3CommandExceptionType TARGET_ALREADY_INVITED_BY_ORIGIN_TYPE = new Dynamic3CommandExceptionType(
@@ -9,5 +10,9 @@ public class ArgumentException {
     );
     public static final Dynamic3CommandExceptionType TARGET_NOT_INVITED_BY_ORIGIN_TYPE = new Dynamic3CommandExceptionType(
             (originName, targetName, type) -> new LiteralMessage(targetName + " does not have a " + type + " invite with " + originName)
+    );
+
+    public static final DynamicCommandExceptionType TARGET_MUST_BE_PLAYER = new DynamicCommandExceptionType(
+            (targetName) -> new LiteralMessage(targetName + " is not a player")
     );
 }
