0eac74f06f7586e9bc9f5e8f448d525e72c54576
StudentAlleg <studentalleg-pilot@yahoo.com>
2025-03-05 20:52:33 -0800

    #1420 - Add claim, change clan command to legacyclan.

--- clans/src/main/java/me/mykindos/betterpvp/clans/clans/listeners/ClansCommandListener.java
+++ clans/src/main/java/me/mykindos/betterpvp/clans/clans/listeners/ClansCommandListener.java
@@ -4,6 +4,7 @@ import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import me.mykindos.betterpvp.clans.Clans;
 import me.mykindos.betterpvp.clans.clans.ClanManager;
+import me.mykindos.betterpvp.clans.clans.events.ClanCreateEvent;
 import me.mykindos.betterpvp.clans.clans.events.ClanKickMemberEvent;
 import me.mykindos.betterpvp.clans.clans.events.MemberJoinClanEvent;
 import me.mykindos.betterpvp.clans.clans.events.MemberLeaveClanEvent;
@@ -42,6 +43,14 @@ public class ClansCommandListener extends ClanListener {
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void onMemberKick(ClanKickMemberEvent event) {
         final Player target = Bukkit.getPlayer(event.getClanMember().getUuid());
+        if (target != null) {
+            UtilServer.runTaskLater(JavaPlugin.getPlugin(Clans.class), target::updateCommands, 1L);
+        }
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void onClanCreate(ClanCreateEvent event) {
+        final Player target = event.getPlayer();
         if (target != null) {
             UtilServer.runTaskLater(JavaPlugin.getPlugin(Clans.class), target::updateCommands, 1L);
         }
