34d874e17f36493acd523c33fa196ee62fddb2d8
StudentAlleg <studentalleg-pilot@yahoo.com>
2025-03-14 20:51:07 -0700

    #1420 - Experimental brigadier reload, bank subcommands, core, setadmin, recovery, setenergy, setexp, setsafe

--- core/src/main/java/me/mykindos/betterpvp/core/command/brigadier/arguments/BPvPArgumentTypes.java
+++ core/src/main/java/me/mykindos/betterpvp/core/command/brigadier/arguments/BPvPArgumentTypes.java
@@ -6,6 +6,7 @@ import lombok.CustomLog;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import me.mykindos.betterpvp.core.Core;
+import me.mykindos.betterpvp.core.command.brigadier.arguments.types.BooleanArgumentType;
 import me.mykindos.betterpvp.core.command.brigadier.arguments.types.CustomEffectArgumentType;
 import me.mykindos.betterpvp.core.command.brigadier.arguments.types.PlayerNameArgumentType;
 import me.mykindos.betterpvp.core.command.brigadier.arguments.types.UUIDItemArgumentType;
@@ -30,6 +31,7 @@ public class BPvPArgumentTypes {
     private static final UUIDItemArgumentType UUIDItem = (UUIDItemArgumentType) createArgumentType(JavaPlugin.getPlugin(Core.class), UUIDItemArgumentType.class);
     private static final PlayerNameArgumentType PlayerName = (PlayerNameArgumentType) createArgumentType(JavaPlugin.getPlugin(Core.class), PlayerNameArgumentType.class);
     private static final CustomEffectArgumentType EFFECT_TYPE = (CustomEffectArgumentType) createArgumentType(JavaPlugin.getPlugin(Core.class), CustomEffectArgumentType.class);
+    private static final BooleanArgumentType BOOLEAN_TYPE = (BooleanArgumentType) createArgumentType(JavaPlugin.getPlugin(Core.class), BooleanArgumentType.class);
 
     public static BPvPArgumentType<?, ?> createArgumentType(BPvPPlugin plugin, Class<? extends BPvPArgumentType<?, ?>> clazz) {
 
@@ -42,6 +44,7 @@ public class BPvPArgumentTypes {
 
     /**
      * Prompts the sender with a list of valid {@link UUID}'s. Guarantees the return value is a valid {@link UUIDItem}
+     * <p>Casting class {@link UUIDItem}</p>
      * @return the {@link UUIDItemArgumentType}
      */
     public static UUIDItemArgumentType uuidItem() {
@@ -50,6 +53,7 @@ public class BPvPArgumentTypes {
 
     /**
      * Ensures that the return value is a valid Minecraft Player name.
+     * <p>Casting class {@link String}</p>
      * @return the {@link PlayerNameArgumentType}
      */
     public static PlayerNameArgumentType playerName() {
@@ -58,9 +62,20 @@ public class BPvPArgumentTypes {
 
     /**
      * Suggest matching {@link EffectType}s, ensures return value is a valid {@link EffectType}
+     * <p>Casting class {@link EffectType}</p>
      * @return the {@link CustomEffectArgumentType}
      */
     public static CustomEffectArgumentType customEffect() {
         return EFFECT_TYPE;
+    }
+
+    /**
+     * Suggests {@code true} or {@code false}.
+     * <p>Casting class {@link Boolean}</p>
+     * @return the {@link BooleanArgumentType}
+     * @see Boolean#getBoolean(String) 
+     */
+    public static BooleanArgumentType booleanType() {
+        return BOOLEAN_TYPE;
     }
 }
