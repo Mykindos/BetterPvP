13c4178909a2d264b2c09576c8cf218ae8e38de6
StudentAlleg <studentalleg-pilot@yahoo.com>
2025-03-14 20:51:07 -0700

    #1420 - Experimental brigadier reload, bank subcommands, core, setadmin, recovery, setenergy, setexp, setsafe

---
+++ clans/src/main/java/me/mykindos/betterpvp/clans/clans/commands/subcommands/brigadier/admin/BrigadierSetExpCommand.java
@@ -0,0 +1,101 @@
+package me.mykindos.betterpvp.clans.clans.commands.subcommands.brigadier.admin;
+
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import com.mojang.brigadier.Command;
+import com.mojang.brigadier.arguments.DoubleArgumentType;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import io.papermc.paper.command.brigadier.CommandSourceStack;
+import io.papermc.paper.command.brigadier.Commands;
+import me.mykindos.betterpvp.clans.clans.Clan;
+import me.mykindos.betterpvp.clans.clans.ClanManager;
+import me.mykindos.betterpvp.clans.clans.commands.BrigadierClansCommand;
+import me.mykindos.betterpvp.clans.commands.arguments.BPvPClansArgumentTypes;
+import me.mykindos.betterpvp.clans.commands.commands.ClanBrigadierCommand;
+import me.mykindos.betterpvp.core.client.Rank;
+import me.mykindos.betterpvp.core.client.repository.ClientManager;
+import me.mykindos.betterpvp.core.command.brigadier.BrigadierSubCommand;
+import me.mykindos.betterpvp.core.command.brigadier.IBrigadierCommand;
+import me.mykindos.betterpvp.core.utilities.UtilMessage;
+import net.kyori.adventure.text.Component;
+import org.bukkit.command.CommandSender;
+
+
+@Singleton
+@BrigadierSubCommand(BrigadierClansCommand.class)
+public class BrigadierSetExpCommand extends ClanBrigadierCommand {
+
+    @Inject
+    protected BrigadierSetExpCommand(ClientManager clientManager, ClanManager clanManager) {
+        super(clientManager, clanManager);
+    }
+
+    /**
+     * Used in retrieving path for config options
+     *
+     * @return the name of this command
+     */
+    @Override
+    public String getName() {
+        return "setexp";
+    }
+
+    /**
+     * Gets the description of this command, used in registration
+     *
+     * @return the description
+     */
+    @Override
+    public String getDescription() {
+        return "Force set a clan's experience";
+    }
+
+    /**
+     * Define the command, using normal rank based permissions
+     * Requires sender to have required rank and executor to be a player
+     *
+     * @return the builder to be used in Build
+     */
+    @Override
+    public LiteralArgumentBuilder<CommandSourceStack> define() {
+        return Commands.literal(getName())
+                .then(IBrigadierCommand.argument("Clan", BPvPClansArgumentTypes.clan())
+                        .then(IBrigadierCommand.argument("experience", DoubleArgumentType.doubleArg(0))
+                                .executes(context -> {
+                                    final CommandSender sender = context.getSource().getSender();
+                                    final Clan target = context.getArgument("Clan", Clan.class);
+                                    final double experience = context.getArgument("experience", Double.class);
+
+                                    double prevExperience = target.getExperience();
+                                    long prevLevel = target.getLevel();
+                                    target.setExperience(experience);
+                                    double newExperience = target.getExperience();
+                                    long newLevel = target.getLevel();
+
+                                    UtilMessage.message(sender,
+                                            "Clans",
+                                            "Set clan <alt>%s</alt>'s experience from <alt2>%,.1f (level %,d)</alt2> to <alt2>%,.1f (level %,d)</alt2>.",
+                                            target.getName(),
+                                            prevExperience,
+                                            prevLevel,
+                                            newExperience,
+                                            newLevel);
+
+
+                                    final Component alert = UtilMessage.deserialize("%s set clan <alt>%s</alt>'s experience from <alt2>%,.1f (level %,d)</alt2> to <alt2>%,.1f (level %,d)</alt2>.",
+                                            sender.getName(),
+                                            target.getName(),
+                                            prevExperience,
+                                            prevLevel,
+                                            newExperience,
+                                            newLevel);
+                                    clientManager.sendMessageToRank("Clans", alert, Rank.HELPER);
+
+                                    return Command.SINGLE_SUCCESS;
+                                })
+                        )
+
+                );
+    }
+
+}
---
+++ clans/src/main/java/me/mykindos/betterpvp/clans/clans/commands/subcommands/brigadier/general/bank/BrigadierBankDepositSubCommand.java
@@ -0,0 +1,90 @@
+package me.mykindos.betterpvp.clans.clans.commands.subcommands.brigadier.general.bank;
+
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import com.mojang.brigadier.Command;
+import com.mojang.brigadier.arguments.IntegerArgumentType;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import io.papermc.paper.command.brigadier.CommandSourceStack;
+import lombok.CustomLog;
+import me.mykindos.betterpvp.clans.clans.Clan;
+import me.mykindos.betterpvp.clans.clans.ClanManager;
+import me.mykindos.betterpvp.clans.clans.ClanProperty;
+import me.mykindos.betterpvp.clans.clans.commands.subcommands.brigadier.BrigadierClanSubCommand;
+import me.mykindos.betterpvp.core.client.Client;
+import me.mykindos.betterpvp.core.client.gamer.Gamer;
+import me.mykindos.betterpvp.core.client.gamer.properties.GamerProperty;
+import me.mykindos.betterpvp.core.client.repository.ClientManager;
+import me.mykindos.betterpvp.core.command.brigadier.BrigadierSubCommand;
+import me.mykindos.betterpvp.core.command.brigadier.IBrigadierCommand;
+import me.mykindos.betterpvp.core.components.clans.data.ClanMember;
+import org.bukkit.entity.Player;
+
+@Singleton
+@CustomLog
+@BrigadierSubCommand(BrigadierBankSubCommand.class)
+public class BrigadierBankDepositSubCommand extends BrigadierClanSubCommand {
+
+    @Inject
+    protected BrigadierBankDepositSubCommand(ClientManager clientManager, ClanManager clanManager) {
+        super(clientManager, clanManager);
+    }
+
+    @Override
+    protected ClanMember.MemberRank requiredMemberRank() {
+        return ClanMember.MemberRank.ADMIN;
+    }
+
+    /**
+     * Used in retrieving path for config options
+     *
+     * @return the name of this command
+     */
+    @Override
+    public String getName() {
+        return "deposit";
+    }
+
+    /**
+     * Gets the description of this command, used in registration
+     *
+     * @return the description
+     */
+    @Override
+    public String getDescription() {
+        return "Deposit funds into the clan bank";
+    }
+
+    /**
+     * Define the command, using normal rank based permissions
+     * Requires sender to have required rank and executor to be a player
+     *
+     * @return the builder to be used in Build
+     */
+    @Override
+    public LiteralArgumentBuilder<CommandSourceStack> define() {
+        return IBrigadierCommand.literal(getName())
+                .then(IBrigadierCommand.argument("amount", IntegerArgumentType.integer(1))
+                        .executes(context -> {
+                            final Player executor = getPlayerFromExecutor(context);
+                            final Client executorClient = getClientFromExecutor(context);
+                            final Clan executorClan = getClanByExecutor(context);
+
+                            final Gamer gamer = executorClient.getGamer();
+
+                            int amountToDeposit = context.getArgument("amount", Integer.class);
+                            if (gamer.getBalance() < amountToDeposit) {
+                                amountToDeposit = gamer.getBalance();
+                            }
+
+                            executorClan.saveProperty(ClanProperty.BALANCE, executorClan.getBalance() + amountToDeposit);
+                            gamer.saveProperty(GamerProperty.BALANCE, gamer.getBalance() - amountToDeposit);
+
+                            executorClan.messageClan("<yellow>" + executor.getName() + " <gray>deposited <green>$" + amountToDeposit + " <gray>into the clan bank.", null, true);
+                            log.info("{} deposited ${} into clan {}", executor.getName(), amountToDeposit, executorClan.getId().toString()).submit();
+
+                            return Command.SINGLE_SUCCESS;
+                        })
+                );
+    }
+}
