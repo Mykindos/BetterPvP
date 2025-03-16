a69de1b44152ec15a6d5ded9c761217a4d53bee2
StudentAlleg <studentalleg-pilot@yahoo.com>
2025-03-16 00:58:43 -0700

    #1420 - Addoutskirts, help, help infrastructure, prevent naming clans subcommands

--- clans/src/main/java/me/mykindos/betterpvp/clans/clans/commands/subcommands/brigadier/general/BrigadierCreateSubCommand.java
+++ clans/src/main/java/me/mykindos/betterpvp/clans/clans/commands/subcommands/brigadier/general/BrigadierCreateSubCommand.java
@@ -5,6 +5,7 @@ import com.google.inject.Singleton;
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.context.CommandContext;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
 import io.papermc.paper.command.brigadier.Commands;
 import lombok.CustomLog;
@@ -20,6 +21,8 @@ import me.mykindos.betterpvp.core.client.repository.ClientManager;
 import me.mykindos.betterpvp.core.command.brigadier.BrigadierSubCommand;
 import me.mykindos.betterpvp.core.utilities.UtilMessage;
 import me.mykindos.betterpvp.core.utilities.UtilServer;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.entity.Player;
 
 import java.util.UUID;
@@ -68,7 +71,7 @@ public class BrigadierCreateSubCommand extends ClanBrigadierCommand {
                         //dont let administrating senders use this argument, they use the other argument, which does not check for filter or correct clan name
                         .requires((source) -> !this.executorHasAClan(source) && !this.senderIsAdministrating(source))
                 )
-                .then(Commands.argument("Admin Clan Name", StringArgumentType.greedyString())
+                .then(Commands.argument("Admin Clan Name", StringArgumentType.string())
                         .executes(context -> {
                             String name = context.getArgument("Admin Clan Name", String.class);
                             if (!(context.getSource().getSender() instanceof final Player player)) return Command.SINGLE_SUCCESS;
@@ -85,6 +88,18 @@ public class BrigadierCreateSubCommand extends ClanBrigadierCommand {
     @Override
     public boolean requirement(CommandSourceStack source) {
         return super.requirement(source) && !executorHasAClan(source);
+    }
+
+    @Override
+    public Component getRequirementComponent(CommandContext<CommandSourceStack> context) {
+        Component component = super.getRequirementComponent(context);
+        boolean inClan = executorHasAClan(context.getSource());
+        component = component.appendNewline();
+        component = component.append(Component.text("Need a Clan: ", NamedTextColor.WHITE))
+                .append(Component.text(false, NamedTextColor.RED).append(Component.text(" | ", NamedTextColor.GRAY))
+                        .append((Component.text("You: ", NamedTextColor.WHITE))
+                                .append(Component.text(inClan, inClan ? NamedTextColor.GREEN : NamedTextColor.RED))));
+        return component;
     }
 
 
