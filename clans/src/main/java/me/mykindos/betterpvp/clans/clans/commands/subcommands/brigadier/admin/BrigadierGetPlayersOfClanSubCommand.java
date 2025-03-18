c52b0fdc9ffa9bb7518acd28d40932887973d058
StudentAlleg <studentalleg-pilot@yahoo.com>
2025-03-17 17:00:12 -0700

    #1420 - Add skills, kill logs, clans of player, players of clan, and base commands.yml files. Add brigadier infrastructure to champions.

---
+++ clans/src/main/java/me/mykindos/betterpvp/clans/clans/commands/subcommands/brigadier/admin/BrigadierGetPlayersOfClanSubCommand.java
@@ -0,0 +1,57 @@
+package me.mykindos.betterpvp.clans.clans.commands.subcommands.brigadier.admin;
+
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import com.mojang.brigadier.Command;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import io.papermc.paper.command.brigadier.CommandSourceStack;
+import lombok.CustomLog;
+import me.mykindos.betterpvp.clans.clans.Clan;
+import me.mykindos.betterpvp.clans.clans.ClanManager;
+import me.mykindos.betterpvp.clans.clans.commands.BrigadierClansCommand;
+import me.mykindos.betterpvp.clans.commands.arguments.BPvPClansArgumentTypes;
+import me.mykindos.betterpvp.clans.commands.commands.ClanBrigadierCommand;
+import me.mykindos.betterpvp.clans.logging.menu.PlayersOfClanMenu;
+import me.mykindos.betterpvp.core.client.repository.ClientManager;
+import me.mykindos.betterpvp.core.command.brigadier.BrigadierSubCommand;
+import me.mykindos.betterpvp.core.command.brigadier.IBrigadierCommand;
+import org.bukkit.entity.Player;
+
+@Singleton
+@CustomLog
+@BrigadierSubCommand(BrigadierClansCommand.class)
+public class BrigadierGetPlayersOfClanSubCommand extends ClanBrigadierCommand {
+    @Inject
+    protected BrigadierGetPlayersOfClanSubCommand(ClientManager clientManager, ClanManager clanManager) {
+        super(clientManager, clanManager);
+    }
+
+    @Override
+    public String getName() {
+        return "clanplayers";
+    }
+
+    /**
+     * Define the command, using normal rank based permissions
+     * Requires sender to have required rank and executor to be a player
+     *
+     * @return the builder to be used in Build
+     */
+    @Override
+    public LiteralArgumentBuilder<CommandSourceStack> define() {
+        return IBrigadierCommand.literal(getName())
+                .then(IBrigadierCommand.argument("Clan", BPvPClansArgumentTypes.clan())
+                        .executes(context -> {
+                            final Clan target = context.getArgument("Clan", Clan.class);
+                            final Player player = getPlayerFromExecutor(context);
+                            new PlayersOfClanMenu(target.getName(), target.getId(), clanManager, clientManager, null).show(player);
+                            return Command.SINGLE_SUCCESS;
+                        })
+                );
+    }
+
+    @Override
+    public String getDescription() {
+        return "Get players associated with a clan";
+    }
+}
