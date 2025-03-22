c9fe9d965bff21749ce8db0a69d3dcb804dff474
StudentAlleg <studentalleg-pilot@yahoo.com>
2025-03-22 15:35:26 -0700

    #1420 - SonarQube issues

--- core/src/main/java/me/mykindos/betterpvp/core/command/listener/CommandListener.java
+++ core/src/main/java/me/mykindos/betterpvp/core/command/listener/CommandListener.java
@@ -34,8 +34,6 @@ public class CommandListener implements Listener {
     private final CommandManager commandManager;
     private final BrigadierCommandManager brigadierCommandManager;
 
-    //TODO allow brig commands
-    //TODO block non-plugin commands (other plugins, keep ours)
     @Inject
     public CommandListener(ClientManager clientManager, CommandManager commandManager, BrigadierCommandManager brigadierCommandManager) {
         this.clientManager = clientManager;
@@ -93,13 +91,10 @@ public class CommandListener implements Listener {
         String finalCommandName = commandName;
         Optional<ICommand> commandOptional = commandManager.getCommand(finalCommandName, finalArgs);
         if (commandOptional.isEmpty() && !client.hasRank(Rank.ADMIN) && !event.getPlayer().isOp()) {
-            log.info("no legacy command with name {}", finalCommandName).submit();
-            //event.setCancelled(true);
             return;
         }
 
         if (commandOptional.isPresent()) {
-            log.info("Yes legacy command with name {}", finalCommandName).submit();
             ICommand command = commandOptional.get();
 
             if (!command.isEnabled()) {
@@ -151,37 +146,6 @@ public class CommandListener implements Listener {
             return !permission.startsWith("bpvp");
         });
     }
-
-    /*@EventHandler
-    public void onCommandListSent(PlayerCommandSendEvent event) {
-        Client client = clientManager.search().online(event.getPlayer());
-
-        event.getCommands().removeIf(command -> {
-
-            String[] args = command.split(":");
-            if(args.length == 2) {
-                log.info("namespaced command {}", command).submit();
-                return args[0].equalsIgnoreCase(args[1]);
-            }
-
-            return false;
-        });
-
-        if (event.getPlayer().isOp() || client.hasRank(Rank.ADMIN)) return;
-
-
-        event.getCommands().removeIf(command -> {
-
-            Optional<ICommand> commandOptional = commandManager.getCommand(command, new String[]{});
-            if (commandOptional.isPresent()) {
-                ICommand command1 = commandOptional.get();
-                log.info("Command show {}", command).submit();
-                return !client.hasRank(command1.getRequiredRank()) && !event.getPlayer().isOp();
-            }
-
-            return false;
-        });
-    }*/
 
     private void promptInsufficientPrivileges(ICommand command, Player player) {
         if (command.informInsufficientRank()) {
