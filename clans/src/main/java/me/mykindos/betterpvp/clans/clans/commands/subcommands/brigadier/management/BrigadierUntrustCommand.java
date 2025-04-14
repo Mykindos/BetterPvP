7af5ba6be5b8a4e9a80fd2af83a6dbd47cdc368e
StudentAlleg <studentalleg-pilot@yahoo.com>
2025-04-13 20:50:19 -0700

    #1420 - Fix /c subcommands showing when the player is unable to execute them

--- clans/src/main/java/me/mykindos/betterpvp/clans/clans/commands/subcommands/brigadier/management/BrigadierUntrustCommand.java
+++ clans/src/main/java/me/mykindos/betterpvp/clans/clans/commands/subcommands/brigadier/management/BrigadierUntrustCommand.java
@@ -3,6 +3,8 @@ package me.mykindos.betterpvp.clans.clans.commands.subcommands.brigadier.managem
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import com.mojang.brigadier.Command;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import io.papermc.paper.command.brigadier.CommandSourceStack;
 import io.papermc.paper.command.brigadier.Commands;
 import me.mykindos.betterpvp.clans.clans.Clan;
 import me.mykindos.betterpvp.clans.clans.ClanManager;
@@ -13,7 +15,6 @@ import me.mykindos.betterpvp.clans.commands.arguments.BPvPClansArgumentTypes;
 import me.mykindos.betterpvp.core.client.repository.ClientManager;
 import me.mykindos.betterpvp.core.command.brigadier.BrigadierSubCommand;
 import me.mykindos.betterpvp.core.command.brigadier.IBrigadierCommand;
-import me.mykindos.betterpvp.core.command.brigadier.impl.BPvPLiteralArgumentBuilder;
 import me.mykindos.betterpvp.core.components.clans.data.ClanMember;
 import me.mykindos.betterpvp.core.utilities.UtilServer;
 import org.bukkit.entity.Player;
@@ -58,7 +59,7 @@ public class BrigadierUntrustCommand extends BrigadierClanSubCommand {
      * @return the builder to be used in Build
      */
     @Override
-    public BPvPLiteralArgumentBuilder define() {
+    public LiteralArgumentBuilder<CommandSourceStack> define() {
         return IBrigadierCommand.literal(getName())
                 .then(Commands.argument("Trusted Clan", BPvPClansArgumentTypes.trustedClan())
                         .executes(context -> {
