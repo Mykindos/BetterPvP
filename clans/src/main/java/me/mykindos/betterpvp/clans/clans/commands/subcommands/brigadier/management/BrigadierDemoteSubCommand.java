3cf32f9fecbca027e12522b98b89347b53c5b635
StudentAlleg <studentalleg-pilot@yahoo.com>
2025-04-13 20:40:34 -0700

    Temp - LiteralTesting

--- clans/src/main/java/me/mykindos/betterpvp/clans/clans/commands/subcommands/brigadier/management/BrigadierDemoteSubCommand.java
+++ clans/src/main/java/me/mykindos/betterpvp/clans/clans/commands/subcommands/brigadier/management/BrigadierDemoteSubCommand.java
@@ -3,10 +3,7 @@ package me.mykindos.betterpvp.clans.clans.commands.subcommands.brigadier.managem
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import com.mojang.brigadier.Command;
-import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
-import io.papermc.paper.command.brigadier.CommandSourceStack;
-import io.papermc.paper.command.brigadier.Commands;
 import me.mykindos.betterpvp.clans.clans.Clan;
 import me.mykindos.betterpvp.clans.clans.ClanManager;
 import me.mykindos.betterpvp.clans.clans.commands.BrigadierClansCommand;
@@ -17,6 +14,7 @@ import me.mykindos.betterpvp.clans.commands.arguments.exceptions.ClanArgumentExc
 import me.mykindos.betterpvp.core.client.repository.ClientManager;
 import me.mykindos.betterpvp.core.command.brigadier.BrigadierSubCommand;
 import me.mykindos.betterpvp.core.command.brigadier.IBrigadierCommand;
+import me.mykindos.betterpvp.core.command.brigadier.impl.BPvPLiteralArgumentBuilder;
 import me.mykindos.betterpvp.core.components.clans.data.ClanMember;
 import me.mykindos.betterpvp.core.utilities.UtilServer;
 import me.mykindos.betterpvp.core.utilities.model.SoundEffect;
@@ -64,8 +62,8 @@ public class BrigadierDemoteSubCommand extends BrigadierClanSubCommand {
      * @return the builder to be used in Build
      */
     @Override
-    public LiteralArgumentBuilder<CommandSourceStack> define() {
-        return Commands.literal(getName())
+    public BPvPLiteralArgumentBuilder define() {
+        return IBrigadierCommand.literal(getName())
                 .then(IBrigadierCommand.argument("Demotable Clan Member",
                                 BPvPClansArgumentTypes.demotableClanMember(),
                                 sourceStack -> this.executorHasAClan(sourceStack) && !senderIsAdministrating(sourceStack))
