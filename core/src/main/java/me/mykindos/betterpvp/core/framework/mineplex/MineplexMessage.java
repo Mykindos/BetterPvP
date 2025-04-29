b9c93a9dee7962216c9d6f45e8b9f8296055e197
Mykindos <5389944+Mykindos@users.noreply.github.com>
2025-04-30 02:29:57 +1000

    Add Mineplex Message listener

---
+++ core/src/main/java/me/mykindos/betterpvp/core/framework/mineplex/MineplexMessage.java
@@ -0,0 +1,32 @@
+package me.mykindos.betterpvp.core.framework.mineplex;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.HashMap;
+
+@Data
+@Builder
+public class MineplexMessage {
+
+    private String message;
+    private HashMap<String, String> metadata;
+
+    public static class MineplexMessageBuilder {
+
+        public MineplexMessageBuilder metadata(HashMap<String, String> metadata) {
+            this.metadata = metadata;
+            return this;
+        }
+
+        public MineplexMessageBuilder metadata(String key, String value) {
+            if (this.metadata == null) {
+                this.metadata = new HashMap<>();
+            }
+            this.metadata.put(key, value);
+            return this;
+        }
+
+
+    }
+}
