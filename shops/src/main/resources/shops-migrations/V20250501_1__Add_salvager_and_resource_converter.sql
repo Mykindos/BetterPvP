afc13d0db86f4d104b1bb4601e874837be40614d
Mykindos <5389944+Mykindos@users.noreply.github.com>
2025-05-02 02:57:01 +1000

    Fixes

---
+++ shops/src/main/resources/shops-migrations/V20250501_1__Add_salvager_and_resource_converter.sql
@@ -0,0 +1,8 @@
+INSERT IGNORE INTO shopitems
+(Shopkeeper, Material, ItemName, ModelData, MenuSlot, MenuPage, Amount, BuyPrice, SellPrice)
+VALUES ('Resources', 'STONECUTTER', '<gray>Salvager', 0, 41, 1, 1, 25000, 0);
+
+INSERT IGNORE INTO shopitems
+(Shopkeeper, Material, ItemName, ModelData, MenuSlot, MenuPage, Amount, BuyPrice, SellPrice)
+VALUES ('Resources', 'GRINDSTONE', '<gray>Resource Converter', 0, 42, 1, 1, 25000, 0);
+
