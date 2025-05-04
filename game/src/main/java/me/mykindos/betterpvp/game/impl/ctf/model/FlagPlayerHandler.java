86106a8f9853bc24f2de1ae554e64416ce1c195d
StudentAlleg <studentalleg-pilot@yahoo.com>
2025-05-04 14:57:26 -0700

    Fix resistance removing flag slowness

--- game/src/main/java/me/mykindos/betterpvp/game/impl/ctf/model/FlagPlayerHandler.java
+++ game/src/main/java/me/mykindos/betterpvp/game/impl/ctf/model/FlagPlayerHandler.java
@@ -48,9 +48,7 @@ public class FlagPlayerHandler implements HatProvider, ItemProvider, Lifecycled 
     }
 
     public void tick(Player holder) {
-        if (!effectManager.hasEffect(holder, EffectTypes.SLOWNESS, "Flag")) {
-            effectManager.addEffect(holder, EffectTypes.SLOWNESS, "Flag", 1, Long.MAX_VALUE);
-        }
+        effectManager.addEffect(holder, holder,EffectTypes.SLOWNESS, "Flag", 1, 600 * 1000);
     }
     
     public void drop(Player holder) {
