3ce6152dabe60f5d0a6cf4660c1443e70516da38
StudentAlleg <studentalleg-pilot@yahoo.com>
2025-05-08 18:58:49 -0700

    Achievement WIP loaders

--- core/src/main/java/me/mykindos/betterpvp/core/client/achievements/test/DeathAchievement.java
+++ core/src/main/java/me/mykindos/betterpvp/core/client/achievements/test/DeathAchievement.java
@@ -1,18 +1,27 @@
 package me.mykindos.betterpvp.core.client.achievements.test;
 
+import com.google.inject.Singleton;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
+import lombok.CustomLog;
 import me.mykindos.betterpvp.core.client.achievements.SingleSimpleAchievement;
 import me.mykindos.betterpvp.core.client.achievements.repository.AchievementCompletion;
 import me.mykindos.betterpvp.core.client.gamer.Gamer;
 import me.mykindos.betterpvp.core.client.gamer.properties.GamerProperty;
 import me.mykindos.betterpvp.core.client.gamer.properties.GamerPropertyUpdateEvent;
+import me.mykindos.betterpvp.core.inventory.item.ItemProvider;
+import me.mykindos.betterpvp.core.properties.PropertyContainer;
 import me.mykindos.betterpvp.core.utilities.UtilMessage;
 import me.mykindos.betterpvp.core.utilities.UtilTime;
+import me.mykindos.betterpvp.core.utilities.model.description.Description;
+import me.mykindos.betterpvp.core.utilities.model.item.ItemView;
+import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 
-public abstract class DeathAchievement extends SingleSimpleAchievement<Gamer, GamerPropertyUpdateEvent, Integer> {
+@CustomLog
+@Singleton
+public class DeathAchievement extends SingleSimpleAchievement<Gamer, GamerPropertyUpdateEvent, Integer> {
     public DeathAchievement(String key, int goal) {
         super(new NamespacedKey("core", key), goal, GamerProperty.DEATHS);
     }
@@ -37,5 +46,23 @@ public abstract class DeathAchievement extends SingleSimpleAchievement<Gamer, Ga
         }
     }
 
-
+    /**
+     * Gets the description of this achievement for the specified container
+     * For use in UI's
+     *
+     * @param container the {@link PropertyContainer}
+     * @return
+     */
+    @Override
+    public Description getDescription(Gamer container) {
+        final int current = getProperty(container);
+        ItemProvider itemProvider = ItemView.builder()
+                .material(Material.BOOK)
+                .displayName(UtilMessage.deserialize("Death %s", goal))
+                .lore(UtilMessage.deserialize("Progress: <green>%s</green>/<yellow>%s</yellow> (<green>%s</green>%%)", current, goal, getPercentComplete(container) * 100))
+                .build();
+        return Description.builder()
+                .icon(itemProvider)
+                .build();
+    }
 }
