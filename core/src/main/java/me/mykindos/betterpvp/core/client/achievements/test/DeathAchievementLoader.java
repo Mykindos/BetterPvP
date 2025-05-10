ff4483cfaae1a4383fa40c457ced5bedf29304d4
StudentAlleg <studentalleg-pilot@yahoo.com>
2025-05-09 21:53:50 -0700

    Achievements - Messy implementation of loaded from config achievements

--- core/src/main/java/me/mykindos/betterpvp/core/client/achievements/test/DeathAchievementLoader.java
+++ core/src/main/java/me/mykindos/betterpvp/core/client/achievements/test/DeathAchievementLoader.java
@@ -2,15 +2,19 @@ package me.mykindos.betterpvp.core.client.achievements.test;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import java.util.Objects;
 import lombok.CustomLog;
+import me.mykindos.betterpvp.core.Core;
 import me.mykindos.betterpvp.core.client.achievements.types.SingleSimpleAchievementConfigLoader;
+import me.mykindos.betterpvp.core.config.ExtendedYamlConfiguration;
 import org.bukkit.NamespacedKey;
 
 @Singleton
 @CustomLog
 public class DeathAchievementLoader extends SingleSimpleAchievementConfigLoader<DeathAchievement> {
     @Inject
-    public DeathAchievementLoader() {
+    public DeathAchievementLoader(Core core) {
+        super(core);
     }
 
     @Override
@@ -18,4 +22,12 @@ public class DeathAchievementLoader extends SingleSimpleAchievementConfigLoader<
         return new NamespacedKey("core", "deaths");
     }
 
+    @Override
+    protected DeathAchievement loadAchievement(String basePath, ExtendedYamlConfiguration config,  String namespacedKey) {
+        NamespacedKey key = NamespacedKey.fromString(namespacedKey);
+        int goal = config.getOrSaveInt(basePath + namespacedKey + ".goal", 5);
+        DeathAchievement achievement = new DeathAchievement(Objects.requireNonNull(key).getKey(), goal);
+        achievement.loadConfig(basePath, config);
+        return achievement;
+    }
 }
