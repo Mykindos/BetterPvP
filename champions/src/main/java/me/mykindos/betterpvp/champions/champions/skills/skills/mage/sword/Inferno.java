eeff2e8fe1afff9abaff821063a5fafa31198975
Mykindos <5389944+Mykindos@users.noreply.github.com>
2025-05-16 08:20:34 +1000

    Old inferno & Vulnerability 1 on immolate

--- champions/src/main/java/me/mykindos/betterpvp/champions/champions/skills/skills/mage/sword/Inferno.java
+++ champions/src/main/java/me/mykindos/betterpvp/champions/champions/skills/skills/mage/sword/Inferno.java
@@ -1,17 +1,12 @@
 package me.mykindos.betterpvp.champions.champions.skills.skills.mage.sword;
 
 
-import com.destroystokyo.paper.ParticleBuilder;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
-import lombok.AllArgsConstructor;
-import lombok.Data;
 import me.mykindos.betterpvp.champions.Champions;
 import me.mykindos.betterpvp.champions.champions.ChampionsManager;
-import me.mykindos.betterpvp.champions.champions.skills.data.ChargeData;
 import me.mykindos.betterpvp.champions.champions.skills.data.SkillActions;
 import me.mykindos.betterpvp.champions.champions.skills.types.ChannelSkill;
-import me.mykindos.betterpvp.champions.champions.skills.types.CooldownSkill;
 import me.mykindos.betterpvp.champions.champions.skills.types.DamageSkill;
 import me.mykindos.betterpvp.champions.champions.skills.types.EnergyChannelSkill;
 import me.mykindos.betterpvp.champions.champions.skills.types.FireSkill;
@@ -26,13 +21,10 @@ import me.mykindos.betterpvp.core.components.champions.SkillType;
 import me.mykindos.betterpvp.core.framework.updater.UpdateEvent;
 import me.mykindos.betterpvp.core.listener.BPvPListener;
 import me.mykindos.betterpvp.core.utilities.UtilDamage;
-import me.mykindos.betterpvp.core.utilities.UtilEntity;
 import me.mykindos.betterpvp.core.utilities.UtilMath;
-import me.mykindos.betterpvp.core.utilities.UtilMessage;
-import me.mykindos.betterpvp.core.utilities.model.display.DisplayComponent;
-import org.bukkit.Location;
+import me.mykindos.betterpvp.core.utilities.UtilTime;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
-import org.bukkit.Particle;
 import org.bukkit.Sound;
 import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.Item;
@@ -43,31 +35,24 @@ import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.util.Vector;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
+import java.util.UUID;
 import java.util.WeakHashMap;
 
 @Singleton
 @BPvPListener
-public class Inferno extends ChannelSkill implements InteractSkill, CooldownSkill, EnergyChannelSkill, ThrowableListener, FireSkill, OffensiveSkill, DamageSkill {
-    private final WeakHashMap<Player, ChargeData> charging = new WeakHashMap<>();
-    private List<Item> blazePowders = new ArrayList<>();
-    private final HashMap<Player, Shotgun> shotguns = new HashMap<>();
+public class Inferno extends ChannelSkill implements InteractSkill, EnergyChannelSkill, ThrowableListener, FireSkill, OffensiveSkill, DamageSkill {
+    private final WeakHashMap<LivingEntity, Long> tempImmune = new WeakHashMap<>();
 
-    private final DisplayComponent actionBarComponent = ChargeData.getActionBar(this,
-            charging,
-            gamer -> true);
-    private double baseCharge;
-    private double chargeIncreasePerLevel;
     private double baseFireDuration;
+
     private double fireDurationIncreasePerLevel;
+
     private double baseDamage;
+
     private double damageIncreasePerLevel;
-    private int baseNumFlames;
-    private int numFlamesIncreasePerLevel;
+
+    private double immuneTime;
 
     @Inject
     public Inferno(Champions champions, ChampionsManager championsManager) {
@@ -85,12 +70,9 @@ public class Inferno extends ChannelSkill implements InteractSkill, CooldownSkil
         return new String[]{
                 "Hold right click with a Sword to channel",
                 "",
-                "Charges up to " + getValueString(this::getNumFlames, level) + " flames",
+                "You spray fire at high speed igniting",
+                "anything it hits for " + getValueString(this::getFireDuration, level) + " seconds",
                 "",
-                "Release to shoot a scorching blast of fire",
-                "that ignites anything it hits for " + getValueString(this::getFireDuration, level) + " seconds",
-                "",
-                "Cooldown: " + getValueString(this::getCooldown, level),
                 "Energy: " + getValueString(this::getEnergyPerSecond, level),
         };
     }
@@ -104,43 +86,19 @@ public class Inferno extends ChannelSkill implements InteractSkill, CooldownSkil
         return (float) (energy - ((level - 1) * energyDecreasePerLevel));
     }
 
-    public int getNumFlames(int level) {
-        return baseNumFlames + ((level - 1) * numFlamesIncreasePerLevel);
-    }
-
     public double getDamage(int level) {
         return baseDamage + ((level - 1) * damageIncreasePerLevel);
     }
 
-    private double getChargePerSecond(int level) {
-        return baseCharge + (chargeIncreasePerLevel * (level - 1));
-    }
     @Override
     public float getEnergy(int level) {
         return energy;
     }
 
-    @Override
-    public double getCooldown(int level) {
-        return cooldown - (level - 1) * cooldownDecreasePerLevel;
-    }
 
     @Override
     public Action[] getActions() {
         return SkillActions.RIGHT_CLICK;
-    }
-
-    public void trackPlayer(Player player, Gamer gamer) {
-        gamer.getActionBar().add(900, actionBarComponent);
-    }
-
-    public void invalidatePlayer(Player player, Gamer gamer) {
-        gamer.getActionBar().remove(actionBarComponent);
-    }
-
-    @Override
-    public boolean shouldDisplayActionBar(Gamer gamer) {
-        return !charging.containsKey(gamer.getPlayer()) && isHolding(gamer.getPlayer());
     }
 
     @Override
@@ -155,131 +113,69 @@ public class Inferno extends ChannelSkill implements InteractSkill, CooldownSkil
 
     @Override
     public void activate(Player player, int level) {
-        final ChargeData chargeData = new ChargeData((float) getChargePerSecond(level) / 100);
-        charging.put(player, chargeData);
+        active.add(player.getUniqueId());
+    }
+
+    @UpdateEvent(delay = 125)
+    public void updateImmunes() {
+        tempImmune.entrySet().removeIf(entry -> UtilTime.elapsed(entry.getValue(), (long) (immuneTime * 1000L)));
     }
 
     @Override
     public void onThrowableHit(ThrowableItem throwableItem, LivingEntity thrower, LivingEntity hit) {
+
         if (hit instanceof ArmorStand) {
             return;
         }
-
-        Item fireItem = throwableItem.getItem();
-        if (fireItem != null) {
-            fireItem.remove();
-        }
-
-        if (thrower instanceof Player damager) {
+        if (hit instanceof Player damager) {
             int level = getLevel(damager);
-            UtilEntity.setFire(hit, damager, (long) (1000L * getFireDuration(level)));
-
-            CustomDamageEvent cde = new CustomDamageEvent(hit, damager, null, DamageCause.CUSTOM, getDamage(level), false, "Inferno");
-            cde.setDamageDelay(0);
-            UtilDamage.doCustomDamage(cde);
+            if (hit.getFireTicks() <= 0) {
+                hit.setFireTicks((int) (getFireDuration(level) * 20));
+            }
+            if (!throwableItem.getImmunes().contains(hit)) {
+                if (tempImmune.containsKey(hit)) return;
+                CustomDamageEvent cde = new CustomDamageEvent(hit, damager, null, DamageCause.FIRE, getDamage(level), false, "Inferno");
+                cde.setDamageDelay(0);
+                UtilDamage.doCustomDamage(cde);
+                throwableItem.getImmunes().add(hit);
+                tempImmune.put(hit, System.currentTimeMillis());
+            }
         }
+
     }
 
     @UpdateEvent
-    public void updateCharge() {
-        Iterator<Player> iterator = charging.keySet().iterator();
+    public void doInferno() {
+        final Iterator<UUID> iterator = active.iterator();
         while (iterator.hasNext()) {
-            Player player = iterator.next();
-            ChargeData charge = charging.get(player);
-            if (player == null || !player.isOnline()) {
+            Player cur = Bukkit.getPlayer(iterator.next());
+            if (cur == null) {
                 iterator.remove();
                 continue;
             }
 
-            Gamer gamer = championsManager.getClientManager().search().online(player).getGamer();
-            int level = getLevel(player);
+            Gamer gamer = championsManager.getClientManager().search().online(cur).getGamer();
+            if (!isHolding(cur) || !gamer.isHoldingRightClick()) {
+                iterator.remove();
+                continue;
+            }
+
+            int level = getLevel(cur);
             if (level <= 0) {
                 iterator.remove();
-                continue;
             }
-
-            if (isHolding(player) && gamer.isHoldingRightClick() && championsManager.getEnergy().use(player, getName(), getEnergyPerSecond(level) / 20, true)) {
-                charge.tick();
-                charge.tickSound(player);
-                continue;
+            if (!championsManager.getEnergy().use(cur, getName(), getEnergy(level) / 20, true)) {
+                iterator.remove();
+            } else {
+                Item fire = cur.getWorld().dropItem(cur.getEyeLocation(), new ItemStack(Material.BLAZE_POWDER));
+                ThrowableItem throwableItem = new ThrowableItem(this, fire, cur, getName(), 5000L);
+                throwableItem.setCanHitFriendlies(false);
+                throwableItem.setRemoveInWater(true);
+                championsManager.getThrowables().addThrowable(throwableItem);
+                fire.teleport(cur.getEyeLocation());
+                fire.setVelocity(cur.getLocation().getDirection().add(new Vector(UtilMath.randDouble(-0.2, 0.2), UtilMath.randDouble(-0.2, 0.3), UtilMath.randDouble(-0.2, 0.2))));
+                cur.getWorld().playSound(cur.getLocation(), Sound.ENTITY_GHAST_SHOOT, 0.1F, 1.0F);
             }
-
-            iterator.remove();
-            shotgun(player, charge, level);
-        }
-    }
-
-    private void shotgun(Player player, ChargeData chargeData, int level) {
-        UtilMessage.simpleMessage(player, getClassType().getName(), "You used <green>%s %d<gray>.", getName(), level);
-
-        float chargePercent = Math.min(chargeData.getCharge(), 1.0f);
-        int numFlames = 1 + (int) (chargePercent * (getNumFlames(level) - 1));
-
-        Shotgun shotgunInstance = new Shotgun(player, numFlames, 0, 1, System.currentTimeMillis() + 50); // Assuming 1 tick = 50 ms
-        shotguns.put(player, shotgunInstance);
-
-        championsManager.getCooldowns().removeCooldown(player, getName(), true);
-        championsManager.getCooldowns().use(player,
-                getName(),
-                getCooldown(level),
-                true,
-                true,
-                isCancellable(),
-                this::shouldDisplayActionBar);
-    }
-
-
-    @UpdateEvent
-    public void onUpdate() {
-        long currentTick = System.currentTimeMillis();
-        Iterator<Map.Entry<Player, Shotgun>> shotgunIterator = shotguns.entrySet().iterator();
-
-        while (shotgunIterator.hasNext()) {
-            Map.Entry<Player, Shotgun> entry = shotgunIterator.next();
-            Shotgun shotgun = entry.getValue();
-
-            if (currentTick >= shotgun.getNextShotTick() && shotgun.getFlamesShot() < shotgun.getTotalFlames() && isHolding(shotgun.getPlayer())) {
-                Item fire = shotgun.getPlayer().getWorld().dropItem(shotgun.getPlayer().getEyeLocation(), new ItemStack(Material.BLAZE_POWDER));
-                championsManager.getThrowables().addThrowable(this, fire, shotgun.getPlayer(), getName(), 2000L, true);
-                blazePowders.add(fire);
-
-                fire.teleport(shotgun.getPlayer().getEyeLocation());
-                Vector randomVector = new Vector(UtilMath.randDouble(-0.01, 0.01), UtilMath.randDouble(-0.01, 0.01), UtilMath.randDouble(-0.01, 0.1));
-                Vector increasedVelocity = shotgun.getPlayer().getLocation().getDirection().add(randomVector).multiply(3);
-                fire.setVelocity(increasedVelocity);
-                shotgun.getPlayer().getWorld().playSound(shotgun.getPlayer().getLocation(), Sound.ENTITY_GHAST_SHOOT, 0.1F, 1.0F);
-
-                shotgun.setFlamesShot(shotgun.getFlamesShot() + 1);
-                shotgun.setNextShotTick(currentTick + shotgun.getDelayBetweenShots());
-            }
-
-            if (shotgun.getFlamesShot() >= shotgun.getTotalFlames() || !isHolding(shotgun.getPlayer())) {
-                shotgunIterator.remove();
-            }
-        }
-
-        Iterator<Item> blazePowderIterator = blazePowders.iterator();
-        while (blazePowderIterator.hasNext()) {
-            Item blazePowder = blazePowderIterator.next();
-
-            if (!blazePowder.isValid()) {
-                blazePowderIterator.remove();
-                continue;
-            }
-
-            Location location = blazePowder.getLocation();
-
-            if (location.getBlock().getType() == Material.WATER) {
-                blazePowder.remove();
-                blazePowderIterator.remove();
-                continue;
-            }
-
-            new ParticleBuilder(Particle.FLAME)
-                    .extra(0)
-                    .location(location)
-                    .receivers(60)
-                    .spawn();
         }
     }
 
@@ -288,25 +184,11 @@ public class Inferno extends ChannelSkill implements InteractSkill, CooldownSkil
     public void loadSkillConfig() {
         baseFireDuration = getConfig("baseFireDuration", 2.5, Double.class);
         fireDurationIncreasePerLevel = getConfig("fireDurationIncreasePerLevel", 0.0, Double.class);
-
         baseDamage = getConfig("baseDamage", 1.0, Double.class);
         damageIncreasePerLevel = getConfig("damageIncreasePerLevel", 0.0, Double.class);
+        immuneTime = getConfig("immuneTime", 0.45, Double.class);
 
-        baseCharge = getConfig("baseCharge", 100.0, Double.class);
-        chargeIncreasePerLevel = getConfig("chargeIncreasePerLevel", 0.0, Double.class);
-
-        baseNumFlames = getConfig("baseNumFlames", 4, Integer.class);
-        numFlamesIncreasePerLevel = getConfig("numFlamesIncreasePerLevel", 2, Integer.class);
     }
 
-    @Data
-    @AllArgsConstructor
-    private static class Shotgun {
-        private final Player player;
-        private final int totalFlames;
-        private int flamesShot;
-        private final long delayBetweenShots;
-        private long nextShotTick;
-    }
 }
 
