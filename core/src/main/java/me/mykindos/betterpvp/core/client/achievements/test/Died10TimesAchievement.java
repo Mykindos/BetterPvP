5355829f8d39d64eea0efb38464d8e0d43294726
StudentAlleg <studentalleg-pilot@yahoo.com>
2025-05-14 17:49:27 -0700

    Achievements - Basic UI

--- core/src/main/java/me/mykindos/betterpvp/core/client/achievements/test/Died10TimesAchievement.java
+++ core/src/main/java/me/mykindos/betterpvp/core/client/achievements/test/Died10TimesAchievement.java
@@ -2,10 +2,7 @@ package me.mykindos.betterpvp.core.client.achievements.test;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
-import me.mykindos.betterpvp.core.client.gamer.Gamer;
 import me.mykindos.betterpvp.core.listener.BPvPListener;
-import me.mykindos.betterpvp.core.properties.PropertyContainer;
-import me.mykindos.betterpvp.core.utilities.model.description.Description;
 
 @Singleton
 @BPvPListener
@@ -13,17 +10,5 @@ public class Died10TimesAchievement extends DeathAchievement {
     @Inject
     public Died10TimesAchievement() {
         super("death_10", 10);
-    }
-
-    /**
-     * Gets the description of this achievement for the specified container
-     * For use in UI's
-     *
-     * @param container the {@link PropertyContainer}
-     * @return
-     */
-    @Override
-    public Description getDescription(Gamer container) {
-        return null;
     }
 }
