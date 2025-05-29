a5ab73f854a669032b5344ba7b5085bc29a8fabd
Mykindos <5389944+Mykindos@users.noreply.github.com>
2025-05-30 03:10:03 +1000

    Progression rework beginning

--- progression/src/main/java/me/mykindos/betterpvp/progression/profession/skill/ProfessionAttribute.java
+++ progression/src/main/java/me/mykindos/betterpvp/progression/profession/skill/ProfessionAttribute.java
@@ -16,7 +16,6 @@ public enum ProfessionAttribute {
     BAIT_NON_CONSUMPTION_CHANCE("Chance to not consume Baits", "%"),
     BAIT_MULTIPLIER("Bait multiplier", "%"),
     FISHING_TREASURE_CHANCE("Treasure chance", "%"),
-    FISHING_DOUBLE_TREASURE_CHANCE("Double treasure chance", "%"),
     FISHING_DURABILITY_REDUCTION("Reduced durability loss", "%"),
     FISH_INCREASED_STACK_SIZE("Maximum stack size (fish)", "");
 
