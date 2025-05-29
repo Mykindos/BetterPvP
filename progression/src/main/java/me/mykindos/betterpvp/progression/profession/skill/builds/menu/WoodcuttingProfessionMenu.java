63fcba9287a82c151284f82d6b82f096a04e164c
Mykindos <5389944+Mykindos@users.noreply.github.com>
2025-05-30 03:10:03 +1000

    Progression rework beginning
    # Conflicts:
    #	progression/src/main/java/me/mykindos/betterpvp/progression/commands/WipeSubCommand.java
    #	progression/src/main/java/me/mykindos/betterpvp/progression/profession/skill/ProfessionAttribute.java
    #	progression/src/main/java/me/mykindos/betterpvp/progression/profession/skill/ProfessionNodeManager.java
    #	progression/src/main/java/me/mykindos/betterpvp/progression/profession/skill/ProfessionSkillNode.java
    #	progression/src/main/java/me/mykindos/betterpvp/progression/profession/skill/builds/menu/ProfessionMenu.java
    #	progression/src/main/java/me/mykindos/betterpvp/progression/profession/skill/builds/menu/WoodcuttingProfessionMenu.java
    #	progression/src/main/java/me/mykindos/betterpvp/progression/profession/skill/builds/menu/buttons/ProfessionInfoButton.java
    #	progression/src/main/java/me/mykindos/betterpvp/progression/profession/skill/builds/menu/buttons/ProfessionSkillConnectionButton.java
    #	progression/src/main/java/me/mykindos/betterpvp/progression/profession/skill/builds/menu/buttons/ProgressionSkillButton.java
    #	progression/src/main/java/me/mykindos/betterpvp/progression/profession/skill/builds/menu/buttons/ScrollDownItem.java
    #	progression/src/main/java/me/mykindos/betterpvp/progression/profession/skill/builds/menu/buttons/ScrollUpItem.java
    #	progression/src/main/java/me/mykindos/betterpvp/progression/profession/skill/fishing/BaseFishing.java
    #	progression/src/main/java/me/mykindos/betterpvp/progression/profession/skill/fishing/ExpertBaiter.java
    #	progression/src/main/java/me/mykindos/betterpvp/progression/profession/skill/fishing/NoMoreMobs.java
    #	progression/src/main/java/me/mykindos/betterpvp/progression/profession/skill/woodcutting/AutoPlanter.java
    #	progression/src/main/resources/configs/professions/fishing/fishing.yml
    #	progression/src/main/resources/configs/professions/fishing/skill_tree.yml
    #	progression/src/main/resources/configs/professions/woodcutting/woodcutting.yml

--- progression/src/main/java/me/mykindos/betterpvp/progression/profession/skill/builds/menu/WoodcuttingProfessionMenu.java
+++ progression/src/main/java/me/mykindos/betterpvp/progression/profession/skill/builds/menu/WoodcuttingProfessionMenu.java
@@ -1,13 +1,47 @@
 package me.mykindos.betterpvp.progression.profession.skill.builds.menu;
 
 import me.mykindos.betterpvp.progression.profession.skill.ProfessionNodeManager;
+import me.mykindos.betterpvp.progression.profession.skill.builds.menu.tree.ConnectionType;
 import me.mykindos.betterpvp.progression.profession.skill.builds.menu.tree.SkillNodeType;
 import me.mykindos.betterpvp.progression.profession.woodcutting.WoodcuttingHandler;
 import me.mykindos.betterpvp.progression.profile.ProfessionProfile;
 
+import java.util.List;
+
 public class WoodcuttingProfessionMenu extends ProfessionMenu {
     public WoodcuttingProfessionMenu(WoodcuttingHandler woodcuttingHandler, ProfessionProfile professionProfile, ProfessionNodeManager progressionSkillManager) {
         super("Woodcutting", woodcuttingHandler, professionProfile, progressionSkillManager);
+
+        setContent(List.of(
+                // Row 1
+                AIR, AIR, getSkillItem("Bark Bounty"), AIR, getSkillItem("Tree Tactician"),
+                AIR, getSkillItem("Forest Flourisher"), AIR, AIR,
+
+                // Row 2
+                AIR, AIR, getConnectionItem(ConnectionType.STRAIGHT_VERTICAL, "Auto Planter"), AIR, AIR, AIR, AIR, AIR, AIR,
+
+                // Row 3
+                AIR, AIR, getSkillItem("Auto Planter"), AIR, getSkillItem("Tree Feller"),
+                AIR, getSkillItem("Tree Compactor"), AIR, AIR,
+
+                // Row 4
+                AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR,
+
+                // Row 5
+                AIR, AIR, AIR, AIR, getSkillItem("Enchanted Lumberfall"), AIR, AIR, AIR, AIR,
+
+                // Row 6
+                AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR,
+
+                // Row 7
+                AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR,
+
+                // Row 8
+                AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR,
+
+                // Row 9
+                AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR
+        ));
 
         // Update the content to display the skills
         updateContent();
