b0f5c35d604068b0038b47bb3ef9a9f5c8640131
Mykindos <5389944+Mykindos@users.noreply.github.com>
2025-05-30 03:10:03 +1000

    Progression rework beginning

--- progression/src/main/java/me/mykindos/betterpvp/progression/profession/skill/ProfessionAttribute.java
+++ progression/src/main/java/me/mykindos/betterpvp/progression/profession/skill/ProfessionAttribute.java
@@ -16,18 +16,8 @@ public enum ProfessionAttribute {
     BAIT_NON_CONSUMPTION_CHANCE("Chance to not consume Baits", "%"),
     BAIT_MULTIPLIER("Bait multiplier", "%"),
     FISHING_TREASURE_CHANCE("Treasure chance", "%"),
-    FISHING_DOUBLE_TREASURE_CHANCE("Double treasure chance", "%"),
     FISHING_DURABILITY_REDUCTION("Reduced durability loss", "%"),
-    FISH_INCREASED_STACK_SIZE("Maximum stack size (fish)", ""),
-
-    // Woodcutting
-    CHOP_SPEED("Chop speed", "%"),
-    BARK_CHANCE("Chance to get bark", "%"),
-    SAPLING_GROWTH_SPEED("Sapling growth speed", "%"),
-    WOODCUTTING_TREASURE_CHANCE("Treasure chance", "%"),
-    LOG_INCREASED_STACK_SIZE("Maximum stack size (log)", ""),
-    TREEFELLER_COOLDOWN_REDUCTION("Reduced Treefeller cooldown", "%"),
-    WOODCUTTING_DURABILITY_REDUCTION("Reduced durability loss", "%");
+    FISH_INCREASED_STACK_SIZE("Maximum stack size (fish)", "");
 
     private final String name;
     private final String operation;
--- progression/src/main/java/me/mykindos/betterpvp/progression/profession/skill/builds/menu/buttons/ScrollUpItem.java
+++ progression/src/main/java/me/mykindos/betterpvp/progression/profession/skill/builds/menu/buttons/ScrollUpItem.java
@@ -2,12 +2,9 @@ package me.mykindos.betterpvp.progression.profession.skill.builds.menu.buttons;
 
 import me.mykindos.betterpvp.core.inventory.gui.ScrollGui;
 import me.mykindos.betterpvp.core.inventory.item.ItemProvider;
+import me.mykindos.betterpvp.core.inventory.item.builder.ItemBuilder;
 import me.mykindos.betterpvp.core.inventory.item.impl.controlitem.ScrollItem;
-import me.mykindos.betterpvp.core.utilities.model.item.ClickActions;
-import me.mykindos.betterpvp.core.utilities.model.item.ItemView;
-import net.kyori.adventure.text.Component;
 import org.bukkit.Material;
-import org.bukkit.inventory.ItemStack;
 
 public class ScrollUpItem extends ScrollItem {
 
@@ -17,14 +14,12 @@ public class ScrollUpItem extends ScrollItem {
 
     @Override
     public ItemProvider getItemProvider(ScrollGui<?> gui) {
-        final ItemView.ItemViewBuilder builder = ItemView.of(new ItemStack(Material.BARRIER)).toBuilder();
-        builder.customModelData(1);
-        builder.displayName(Component.text("Scroll up"));
-        builder.action(ClickActions.LEFT_SHIFT, Component.text("Scroll up 5 levels"));
-        if (!gui.canScroll(1))
-            builder.lore(Component.text("You've reached the top"));
+        ItemBuilder builder = new ItemBuilder(Material.BARRIER).setCustomModelData(1);
+        builder.setDisplayName("Scroll up");
+        if (!gui.canScroll(-1))
+            builder.addLoreLines("You've reached the top");
 
-        return builder.build();
+        return builder;
     }
 
 }
