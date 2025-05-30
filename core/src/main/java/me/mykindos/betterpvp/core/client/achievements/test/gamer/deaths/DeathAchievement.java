e27698053f1641b7eab067c543a05eb9ff81e822
StudentAlleg <studentalleg-pilot@yahoo.com>
2025-05-29 22:46:38 -0700

    WIP Categories

--- core/src/main/java/me/mykindos/betterpvp/core/client/achievements/test/gamer/deaths/DeathAchievement.java
+++ core/src/main/java/me/mykindos/betterpvp/core/client/achievements/test/gamer/deaths/DeathAchievement.java
@@ -1,13 +1,12 @@
-package me.mykindos.betterpvp.core.client.achievements.test;
+package me.mykindos.betterpvp.core.client.achievements.test.gamer.deaths;
 
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 import lombok.CustomLog;
+import me.mykindos.betterpvp.core.client.achievements.category.AchievementCategories;
 import me.mykindos.betterpvp.core.client.achievements.repository.AchievementCompletion;
-import me.mykindos.betterpvp.core.client.achievements.types.AchievementCategory;
-import me.mykindos.betterpvp.core.client.achievements.types.AchievementTypes;
 import me.mykindos.betterpvp.core.client.achievements.types.ConfigLoadedAchievement;
 import me.mykindos.betterpvp.core.client.achievements.types.SingleSimpleAchievement;
 import me.mykindos.betterpvp.core.client.achievements.types.containertypes.IGamerAchievement;
@@ -25,7 +24,11 @@ import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 
 @CustomLog
+//Config loaded achievement, this class will be skipped by reflaction
 @ConfigLoadedAchievement
+/**
+ * Super class, is either extended or loaded by a loader {@link DeathAchievementLoader}
+ */
 public class DeathAchievement extends SingleSimpleAchievement<Gamer, GamerPropertyUpdateEvent, Integer> implements IGamerAchievement {
 
     public DeathAchievement(String key, int goal) {
@@ -33,7 +36,7 @@ public class DeathAchievement extends SingleSimpleAchievement<Gamer, GamerProper
     }
 
     public DeathAchievement(NamespacedKey key, int goal) {
-        super(key, AchievementCategory.COMBAT, AchievementTypes.DEATH_TYPE, goal, GamerProperty.DEATHS);
+        super(key, AchievementCategories.DEATH_TYPE, goal, GamerProperty.DEATHS);
     }
 
     @Override
@@ -46,6 +49,7 @@ public class DeathAchievement extends SingleSimpleAchievement<Gamer, GamerProper
             if (achievementCompletionOptional.isEmpty()) {
                 //todo make more descriptionalble'
 
+                //todo pull completion logic up
                 UtilMessage.message(container.getPlayer(), "Achievement", "You completed this achievement!");
                 complete(container);
             } else {
