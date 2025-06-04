7abcb2a59d90516f9d520025a91953d8439620f5
StudentAlleg <studentalleg-pilot@yahoo.com>
2025-06-03 21:49:18 -0700

    WIP champions/clans framework + set core

---
+++ champions/src/main/java/me/mykindos/betterpvp/champions/achievements/ChampionsAchievementCategories.java
@@ -0,0 +1,7 @@
+package me.mykindos.betterpvp.champions.achievements;
+
+import org.bukkit.NamespacedKey;
+
+public class ChampionsAchievementCategories {
+    public static NamespacedKey CHAMPIONS = new NamespacedKey("champions", "champions_category");
+}
