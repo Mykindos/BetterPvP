4fab5bfe92a00097a0759a6fe8cc758ab1890027
StudentAlleg <studentalleg-pilot@yahoo.com>
2025-06-07 13:34:33 -0700

    Basic Stats implementation

--- core/src/main/java/me/mykindos/betterpvp/core/client/achievements/impl/gamer/deaths/DeathCategory.java
+++ core/src/main/java/me/mykindos/betterpvp/core/client/achievements/impl/gamer/deaths/DeathCategory.java
@@ -1,13 +1,10 @@
 package me.mykindos.betterpvp.core.client.achievements.impl.gamer.deaths;
 
 import com.google.inject.Singleton;
-import java.util.Set;
 import me.mykindos.betterpvp.core.client.achievements.category.AchievementCategories;
 import me.mykindos.betterpvp.core.client.achievements.category.AchievementCategory;
 import me.mykindos.betterpvp.core.client.achievements.category.SubCategory;
 import me.mykindos.betterpvp.core.client.achievements.category.types.CombatCategory;
-import me.mykindos.betterpvp.core.client.gamer.Gamer;
-import me.mykindos.betterpvp.core.properties.PropertyContainer;
 import me.mykindos.betterpvp.core.utilities.UtilMessage;
 import me.mykindos.betterpvp.core.utilities.model.item.ItemView;
 import org.bukkit.Material;
@@ -20,16 +17,6 @@ import org.bukkit.Material;
 public class DeathCategory extends AchievementCategory {
     public DeathCategory() {
         super(AchievementCategories.DEATH_TYPE);
-    }
-
-    /**
-     * Get the set of Classes of containers that use this category
-     *
-     * @return
-     */
-    @Override
-    public Set<Class<? extends PropertyContainer>> allowedTypes() {
-        return Set.of(Gamer.class);
     }
 
     @Override
