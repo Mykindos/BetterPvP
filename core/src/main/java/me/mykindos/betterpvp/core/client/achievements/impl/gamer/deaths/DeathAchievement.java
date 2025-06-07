72c10427760fa24ab66c450563651e027f33e586
StudentAlleg <studentalleg-pilot@yahoo.com>
2025-06-07 13:34:33 -0700

    Basic Stats implementation

--- core/src/main/java/me/mykindos/betterpvp/core/client/achievements/impl/gamer/deaths/DeathAchievement.java
+++ core/src/main/java/me/mykindos/betterpvp/core/client/achievements/impl/gamer/deaths/DeathAchievement.java
@@ -1,15 +1,15 @@
 package me.mykindos.betterpvp.core.client.achievements.impl.gamer.deaths;
 
+import java.util.ArrayList;
 import java.util.List;
 import lombok.CustomLog;
+import me.mykindos.betterpvp.core.client.achievements.AchievementType;
 import me.mykindos.betterpvp.core.client.achievements.category.AchievementCategories;
 import me.mykindos.betterpvp.core.client.achievements.types.SingleSimpleAchievement;
-import me.mykindos.betterpvp.core.client.achievements.types.containertypes.IGamerAchievement;
 import me.mykindos.betterpvp.core.client.achievements.types.loaded.ConfigLoadedAchievement;
-import me.mykindos.betterpvp.core.client.gamer.Gamer;
-import me.mykindos.betterpvp.core.client.gamer.properties.GamerProperty;
-import me.mykindos.betterpvp.core.client.gamer.properties.GamerPropertyUpdateEvent;
 import me.mykindos.betterpvp.core.client.rewards.RewardBox;
+import me.mykindos.betterpvp.core.client.stats.ClientStat;
+import me.mykindos.betterpvp.core.client.stats.StatContainer;
 import me.mykindos.betterpvp.core.inventory.item.ItemProvider;
 import me.mykindos.betterpvp.core.properties.PropertyContainer;
 import me.mykindos.betterpvp.core.utilities.UtilMessage;
@@ -26,19 +26,19 @@ import org.bukkit.inventory.ItemStack;
 /**
  * Super class, is either extended or loaded by a loader {@link DeathAchievementLoader}
  */
-public class DeathAchievement extends SingleSimpleAchievement<Gamer, GamerPropertyUpdateEvent, Integer> implements IGamerAchievement {
+public class DeathAchievement extends SingleSimpleAchievement {
 
     public DeathAchievement(String key, int goal) {
         this(new NamespacedKey("core", key), goal);
     }
 
     public DeathAchievement(NamespacedKey key, int goal) {
-        super(key, AchievementCategories.DEATH_TYPE, goal, GamerProperty.DEATHS);
+        super(key, AchievementCategories.DEATH_TYPE, AchievementType.PERIOD, (double) goal, ClientStat.DEATHS);
     }
 
     @Override
     public String getName() {
-        return "Death " + goal;
+        return "Death " + getGoal().intValue();
     }
 
     /**
@@ -49,11 +49,11 @@ public class DeathAchievement extends SingleSimpleAchievement<Gamer, GamerProper
      * @return
      */
     @Override
-    public Description getDescription(Gamer container) {
-        List<Component> lore = new java.util.ArrayList<>(List.of(
-            UtilMessage.deserialize("<gray>Die <yellow>%s</yellow> times", goal)
+    public Description getDescription(StatContainer container, String period) {
+        List<Component> lore = new ArrayList<>(List.of(
+            UtilMessage.deserialize("<gray>Die <yellow>%s</yellow> times", getGoal().intValue())
         ));
-        lore.addAll(this.getProgressComponent(container));
+        lore.addAll(this.getProgressComponent(container, period));
         lore.addAll(this.getCompletionComponent(container));
         ItemProvider itemProvider = ItemView.builder()
                 .material(Material.BOOK)
@@ -66,7 +66,7 @@ public class DeathAchievement extends SingleSimpleAchievement<Gamer, GamerProper
     }
 
     @Override
-    public void processRewards(Gamer container) {
+    public void processRewards(StatContainer container) {
         super.processRewards(container);
         RewardBox rewardBox = clientSQLLayer.getRewardBox(container.getUniqueId());
         rewardBox.getContents().add(ItemStack.of(Material.SKELETON_SKULL));
