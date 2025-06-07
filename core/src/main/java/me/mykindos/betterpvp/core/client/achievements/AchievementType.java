f5669e063af90458ccaa094de739428b0a6487eb
StudentAlleg <studentalleg-pilot@yahoo.com>
2025-06-07 13:34:33 -0700

    Basic Stats implementation

---
+++ core/src/main/java/me/mykindos/betterpvp/core/client/achievements/AchievementType.java
@@ -0,0 +1,11 @@
+package me.mykindos.betterpvp.core.client.achievements;
+public enum AchievementType {
+    /**
+     * Achievement watches gross total of a stat
+     */
+    GLOBAL,
+    /**
+     * Achievement watches on the current period of a stat collection
+     */
+    PERIOD
+}
