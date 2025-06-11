35bc4484dd0f4eecbd803c7b91413b5898526cc0
StudentAlleg <studentalleg-pilot@yahoo.com>
2025-06-10 18:12:29 -0700

    Stats/Achievements - CompositeStats + backend upgrades

---
+++ core/src/main/java/me/mykindos/betterpvp/core/client/stats/IStat.java
@@ -0,0 +1,26 @@
+package me.mykindos.betterpvp.core.client.stats;
+
+public interface IStat {
+    /**
+     * Get the stat represented by this object from the statContainer
+     * @param statContainer
+     * @param period
+     * @return
+     */
+    Double getStat(StatContainer statContainer, String period);
+
+    String getStatName();
+
+    /**
+     * Whether this stat is directly savable to the database
+     * @return {@code true} if it is, {@code false} otherwise
+     */
+    boolean isSavable();
+
+    /**
+     * Whether this stat contains this statName
+     * @param statName
+     * @return
+     */
+    boolean containsStat(String statName);
+}
