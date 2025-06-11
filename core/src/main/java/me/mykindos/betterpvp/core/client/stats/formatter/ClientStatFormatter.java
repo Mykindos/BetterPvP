81ee8b40d3ce11bb4c6a7f81b57c2815131a98cd
StudentAlleg <studentalleg-pilot@yahoo.com>
2025-06-10 18:12:29 -0700

    Stats/Achievements - CompositeStats + backend upgrades

--- core/src/main/java/me/mykindos/betterpvp/core/client/stats/formatter/ClientStatFormatter.java
+++ core/src/main/java/me/mykindos/betterpvp/core/client/stats/formatter/ClientStatFormatter.java
@@ -1,28 +1,30 @@
 package me.mykindos.betterpvp.core.client.stats.formatter;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import me.mykindos.betterpvp.core.client.stats.IClientStat;
+import me.mykindos.betterpvp.core.client.stats.StatContainer;
 import me.mykindos.betterpvp.core.utilities.UtilMessage;
 import me.mykindos.betterpvp.core.utilities.model.description.Description;
 import me.mykindos.betterpvp.core.utilities.model.item.ItemView;
 import net.kyori.adventure.text.Component;
 import org.bukkit.Material;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
 public abstract class ClientStatFormatter extends StatFormatter {
-    private final IClientStat clientStat;
 
     public ClientStatFormatter(IClientStat iClientStat) {
-        super(iClientStat.name());
-        this.clientStat = iClientStat;
+        super(iClientStat);
     }
 
     @Override
-    public Description getDescription(String statName, Double stat) {
+    public Description getDescription(String statName, StatContainer container, String period) {
+        IClientStat clientStat = (IClientStat) Objects.requireNonNull(getStat());
         final List<Component> lore = new ArrayList<>(Arrays.stream(clientStat.getDescription()).map(UtilMessage::deserialize).toList());
         lore.add(Component.empty());
-        lore.add(UtilMessage.deserialize("<white>Value</white>: <green>%s</green>", stat));
+        lore.add(UtilMessage.deserialize("<white>Value</white>: <green>%s</green>", clientStat.getStat(container, period)));
         final ItemView itemView = ItemView.builder()
                 .displayName(Component.text(clientStat.getName()))
                 .lore(lore)
