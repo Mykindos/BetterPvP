4ccde8531a89a5b9d7c2d26afa3bd2e76fa3330f
StudentAlleg <studentalleg-pilot@yahoo.com>
2025-06-11 15:43:09 -0700

    Change global period to static

--- core/src/main/java/me/mykindos/betterpvp/core/client/stats/commands/StatsCommand.java
+++ core/src/main/java/me/mykindos/betterpvp/core/client/stats/commands/StatsCommand.java
@@ -1,6 +1,7 @@
 package me.mykindos.betterpvp.core.client.stats.commands;
 
 import me.mykindos.betterpvp.core.client.Client;
+import me.mykindos.betterpvp.core.client.stats.StatContainer;
 import me.mykindos.betterpvp.core.client.stats.display.StatMenu;
 import me.mykindos.betterpvp.core.command.Command;
 import me.mykindos.betterpvp.core.utilities.model.IStringName;
@@ -19,7 +20,7 @@ public class StatsCommand extends Command implements IStringName {
 
     @Override
     public void execute(Player player, Client client, String... args) {
-        final String period = args.length > 0 ? args[0] : "";
+        final String period = args.length > 0 ? args[0] : StatContainer.GLOBAL_PERIOD;
         new StatMenu(client, null, period, null).show(player);
     }
 
