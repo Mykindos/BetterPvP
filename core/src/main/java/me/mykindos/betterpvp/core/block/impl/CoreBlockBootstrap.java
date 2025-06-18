fb17c8821300faa1d4f72caca03228e6cf6ee17c
braulio-dev <bchamerry@gmail.com>
2025-06-18 10:58:45 -0700

    Add NexoItemAdapter

--- core/src/main/java/me/mykindos/betterpvp/core/block/impl/CoreBlockBootstrap.java
+++ core/src/main/java/me/mykindos/betterpvp/core/block/impl/CoreBlockBootstrap.java
@@ -2,22 +2,31 @@ package me.mykindos.betterpvp.core.block.impl;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import me.mykindos.betterpvp.core.Core;
 import me.mykindos.betterpvp.core.block.SmartBlockRegistry;
 import me.mykindos.betterpvp.core.block.impl.workbench.Workbench;
+import me.mykindos.betterpvp.core.block.impl.workbench.WorkbenchItem;
+import me.mykindos.betterpvp.core.item.ItemRegistry;
+import org.bukkit.NamespacedKey;
 
 @Singleton
 public class CoreBlockBootstrap {
 
+    private final Core core;
     private final SmartBlockRegistry smartBlockRegistry;
+    private final ItemRegistry itemRegistry;
 
     @Inject
-    private CoreBlockBootstrap(SmartBlockRegistry smartBlockRegistry) {
+    private CoreBlockBootstrap(Core core, SmartBlockRegistry smartBlockRegistry, ItemRegistry itemRegistry) {
+        this.core = core;
         this.smartBlockRegistry = smartBlockRegistry;
+        this.itemRegistry = itemRegistry;
     }
 
     @Inject
-    private void registerBlocks(Workbench workbench) {
+    private void registerTools(Workbench workbench, WorkbenchItem workbenchItem) {
         smartBlockRegistry.registerBlock(workbench);
+        itemRegistry.registerItem(new NamespacedKey(core, "workbench"), workbenchItem);
     }
 
 }
