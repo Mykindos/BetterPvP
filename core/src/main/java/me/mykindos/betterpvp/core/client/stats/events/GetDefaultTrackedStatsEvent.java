626e0ecd4bd089f7dd2bb5333fb76078b52ffaea
StudentAlleg <studentalleg-pilot@yahoo.com>
2025-06-19 15:06:18 -0700

    WIP Champions Stats

---
+++ core/src/main/java/me/mykindos/betterpvp/core/client/stats/events/GetDefaultTrackedStatsEvent.java
@@ -0,0 +1,24 @@
+package me.mykindos.betterpvp.core.client.stats.events;
+
+import me.mykindos.betterpvp.core.client.stats.impl.IStat;
+import me.mykindos.betterpvp.core.framework.events.CustomEvent;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class GetDefaultTrackedStatsEvent extends CustomEvent {
+    private final List<IStat> defaultTrackedStats = new ArrayList<>();
+
+    public void addStat(IStat iStat) {
+        defaultTrackedStats.add(iStat);
+    }
+
+    public void addStats(Collection<IStat> iStats) {
+        defaultTrackedStats.addAll(iStats);
+    }
+
+    public void addStats(IStat... iStats) {
+        defaultTrackedStats.addAll(List.of(iStats));
+    }
+}
