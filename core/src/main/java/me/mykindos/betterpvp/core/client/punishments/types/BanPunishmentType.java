1646a9b54d4025b82b23958b68e0b5be28d0bdab
Mykindos <5389944+Mykindos@users.noreply.github.com>
2025-06-19 10:54:53 +1000

    Kick on main thread

--- core/src/main/java/me/mykindos/betterpvp/core/client/punishments/types/BanPunishmentType.java
+++ core/src/main/java/me/mykindos/betterpvp/core/client/punishments/types/BanPunishmentType.java
@@ -1,10 +1,13 @@
 package me.mykindos.betterpvp.core.client.punishments.types;
 
+import me.mykindos.betterpvp.core.Core;
 import me.mykindos.betterpvp.core.client.punishments.Punishment;
+import me.mykindos.betterpvp.core.utilities.UtilServer;
 import net.kyori.adventure.text.Component;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerKickEvent;
+import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.UUID;
 
@@ -23,8 +26,11 @@ public class BanPunishmentType implements IPunishmentType {
     public void onReceive(UUID client, Punishment punishment) {
         Player player = Bukkit.getPlayer(client);
         if (player != null) {
-            Component message = Component.text("You have been banned from the server").append(Component.newline()).append(Component.text("Reason: " + punishment.getReason()));
-            player.kick(message, PlayerKickEvent.Cause.BANNED);
+            UtilServer.runTask(JavaPlugin.getPlugin(Core.class), () -> {
+                Component message = Component.text("You have been banned from the server").append(Component.newline()).append(Component.text("Reason: " + punishment.getReason()));
+                player.kick(message, PlayerKickEvent.Cause.BANNED);
+            });
+
         }
     }
 }
