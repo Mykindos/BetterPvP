462c488600dfbacaddee1cc70983beba113f8fb9
StudentAlleg <studentalleg-pilot@yahoo.com>
2025-06-19 22:19:19 -0700

    WIP formatters and default stats

--- core/src/main/java/me/mykindos/betterpvp/core/client/stats/formatter/ChampionsSkillStatFormatter.java
+++ core/src/main/java/me/mykindos/betterpvp/core/client/stats/formatter/ChampionsSkillStatFormatter.java
@@ -4,15 +4,12 @@ import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import me.mykindos.betterpvp.core.client.stats.StatContainer;
 import me.mykindos.betterpvp.core.client.stats.impl.ChampionsSkillStat;
-import me.mykindos.betterpvp.core.client.stats.impl.MinecraftStat;
 import me.mykindos.betterpvp.core.utilities.UtilMessage;
 import me.mykindos.betterpvp.core.utilities.model.description.Description;
 import me.mykindos.betterpvp.core.utilities.model.item.ItemView;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.Material;
-import org.bukkit.Statistic;
-import org.bukkit.entity.EntityType;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
@@ -32,12 +29,12 @@ public class ChampionsSkillStatFormatter extends StatFormatter {
 
     @Override
     public @Nullable String getCategory() {
-        return "Minecraft Stats";
+        return "Champions Skill Stats";
     }
 
     @Override
     public String getStatType() {
-        return this.getStat() == null ? MinecraftStat.prefix : super.getStatType();
+        return this.getStat() == null ? ChampionsSkillStat.PREFIX : super.getStatType();
     }
 
 
@@ -46,58 +43,25 @@ public class ChampionsSkillStatFormatter extends StatFormatter {
         final ChampionsSkillStat championsSkillStat = getStat() == null ? ChampionsSkillStat.fromString(statName) : (ChampionsSkillStat) getStat();
 
         final List<Component> lore = new ArrayList<>();
-        lore.add(UtilMessage.deserialize("An %s Minecraft Statistic", minecraftStat.getStatistic().getType().toString()));
-        lore.add(Component.empty());
-        lore.add(UtilMessage.deserialize("<white>Value</white>: <green>%s</green>", minecraftStat.getStat(statContainer, period)));
+        lore.add(UtilMessage.deserialize("<white>Value</white>: <green>%s</green>", championsSkillStat.getStat(statContainer, period)));
 
-        Component name = Component.empty().append(Component.text(minecraftStat.getStatistic().toString(), NamedTextColor.WHITE));
-        Material material = Material.GRASS_BLOCK;
+        Component name = Component.empty().append(Component.text(championsSkillStat.getAction().name(), NamedTextColor.GREEN));
 
-        if (minecraftStat.getMaterial() != null) {
-            name = name.appendSpace().append(Component.text(minecraftStat.getMaterial().toString(), NamedTextColor.GOLD));
-            material = minecraftStat.getMaterial();
-        } else if (minecraftStat.getEntityType() != null) {
-            name = name.appendSpace().append(Component.text(minecraftStat.getEntityType().toString(), NamedTextColor.GOLD));
-            material = Material.CREEPER_SPAWN_EGG;
+        if (championsSkillStat.getSkillName() != null) {
+            name = name.appendSpace().append(Component.text(championsSkillStat.getSkillName(), NamedTextColor.GOLD));
+        }
+        if (championsSkillStat.getLevel() != -1) {
+            name = name.appendSpace().append(Component.text(championsSkillStat.getLevel(), NamedTextColor.GOLD));
         }
 
         final ItemView itemView = ItemView.builder()
                 .displayName(name)
                 .lore(lore)
+                .material(championsSkillStat.getAction() == ChampionsSkillStat.Action.EQUIP ? Material.SADDLE : Material.BLAZE_ROD)
                 .frameLore(true)
-                .material(material)
                 .build();
         return Description.builder()
                 .icon(itemView)
                 .build();
-    }
-
-
-    /**
-     * Get the {@link Material} associated with this stat if this {@link Statistic} is of type
-     * {@link Statistic.Type#ITEM} or {@link Statistic.Type#BLOCK}
-     * @param statName the statName to parse
-     * @return the {@link Material} if the {@link Statistic#getType()} {@code =} {@link Statistic.Type#ITEM} or {@link Statistic.Type#BLOCK},
-     * {@code null} otherwise
-     */
-    @Nullable
-    protected Material getMaterial(String statName) {
-        //format is as follows, MINECRAFT_STATISTIC__MATERIAL
-        final String materialName = statName.substring(getStatType().length() + 2);
-        return Material.getMaterial(materialName);
-    }
-
-    /**
-     * Get the {@link EntityType} associated with this stat if this {@link Statistic} is of type
-     * {@link Statistic.Type#ENTITY}
-     * @param statName the statName to parse
-     * @return the {@link EntityType} if the {@link Statistic#getType()} {@code =} {@link Statistic.Type#ENTITY},
-     * {@code null} otherwise
-     */
-    @Nullable
-    protected EntityType getEntityType(String statName) {
-        //format is as follows, MINECRAFT_STATISTIC__ENTITY
-        final String entityName = statName.substring(getStatType().length() + 2);
-        return EntityType.fromName(entityName);
     }
 }
