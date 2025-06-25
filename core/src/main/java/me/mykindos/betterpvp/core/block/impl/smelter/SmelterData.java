e098535bff00e939af89ef8ef027796f8f62ddda
braulio-dev <bchamerry@gmail.com>
2025-06-25 16:04:37 -0700

    Add chunk-based smart block data management and WorldEdit integration

--- core/src/main/java/me/mykindos/betterpvp/core/block/impl/smelter/SmelterData.java
+++ core/src/main/java/me/mykindos/betterpvp/core/block/impl/smelter/SmelterData.java
@@ -3,12 +3,17 @@ package me.mykindos.betterpvp.core.block.impl.smelter;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
-import me.mykindos.betterpvp.core.block.data.storage.StorageBlockData;
+import me.mykindos.betterpvp.core.block.SmartBlockInstance;
+import me.mykindos.betterpvp.core.block.data.BlockRemovalCause;
+import me.mykindos.betterpvp.core.block.data.RemovalHandler;
+import me.mykindos.betterpvp.core.block.data.UnloadHandler;
+import me.mykindos.betterpvp.core.block.data.impl.StorageBlockData;
+import org.jetbrains.annotations.NotNull;
 
 @RequiredArgsConstructor
 @Getter
 @Setter
-public class SmelterData {
+public class SmelterData implements RemovalHandler, UnloadHandler {
 
     private final long maxBurnTime;
     private StorageBlockData contentItems = new StorageBlockData(10);
@@ -16,4 +21,20 @@ public class SmelterData {
     private long burnTime = 0L; // Millis
     private float temperature = 0.0f; // Celsius
 
+    @Override
+    public void onRemoval(@NotNull SmartBlockInstance instance, @NotNull BlockRemovalCause cause) {
+        // Drop items from both nested inventories by delegating to their removal handlers
+        contentItems.onRemoval(instance, cause);
+        fuelItems.onRemoval(instance, cause);
+    }
+
+    @Override
+    public void onUnload(@NotNull SmartBlockInstance instance) {
+        if (contentItems instanceof UnloadHandler contentUnloadHandler) {
+            contentUnloadHandler.onUnload(instance);
+        }
+        if (fuelItems instanceof UnloadHandler fuelUnloadHandler) {
+            fuelUnloadHandler.onUnload(instance);
+        }
+    }
 }
