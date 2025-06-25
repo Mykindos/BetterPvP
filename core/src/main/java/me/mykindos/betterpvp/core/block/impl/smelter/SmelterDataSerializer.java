9d514be9a1f82dc17fa9f524ef5e236698786bc2
braulio-dev <bchamerry@gmail.com>
2025-06-25 12:31:32 -0700

    Add database block data storage support

--- core/src/main/java/me/mykindos/betterpvp/core/block/impl/smelter/SmelterDataSerializer.java
+++ core/src/main/java/me/mykindos/betterpvp/core/block/impl/smelter/SmelterDataSerializer.java
@@ -5,22 +5,18 @@ import me.mykindos.betterpvp.core.block.data.storage.StorageBlockData;
 import me.mykindos.betterpvp.core.block.data.storage.StorageBlockDataSerializer;
 import me.mykindos.betterpvp.core.item.ItemFactory;
 import org.bukkit.NamespacedKey;
-import org.bukkit.persistence.PersistentDataContainer;
-import org.bukkit.persistence.PersistentDataType;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Objects;
+import java.io.*;
 
 public class SmelterDataSerializer implements SmartBlockDataSerializer<SmelterData> {
 
     private final StorageBlockDataSerializer<StorageBlockData> contentSerializer;
     private final StorageBlockDataSerializer<StorageBlockData> fuelSerializer;
-    private final Smelter smelter;
 
-    public SmelterDataSerializer(ItemFactory itemFactory, Smelter smelter) {
+    public SmelterDataSerializer(ItemFactory itemFactory) {
         this.contentSerializer = new StorageBlockDataSerializer<>("contents", StorageBlockData.class, itemFactory, StorageBlockData::new);
         this.fuelSerializer = new StorageBlockDataSerializer<>("fuel", StorageBlockData.class, itemFactory, StorageBlockData::new);
-        this.smelter = smelter;
     }
 
     @Override
@@ -34,33 +30,60 @@ public class SmelterDataSerializer implements SmartBlockDataSerializer<SmelterDa
     }
 
     @Override
-    public void serialize(@NotNull SmelterData data, @NotNull PersistentDataContainer container) {
-        contentSerializer.serialize(data.getContentItems(), container);
-        fuelSerializer.serialize(data.getFuelItems(), container);
-        container.set(new NamespacedKey("betterpvp", "burn_time"), PersistentDataType.LONG, data.getBurnTime());
-        container.set(new NamespacedKey("betterpvp", "temperature"), PersistentDataType.FLOAT, data.getTemperature());
+    public byte[] serializeToBytes(@NotNull SmelterData data) throws IOException {
+        try (ByteArrayOutputStream baos = new ByteArrayOutputStream();
+             DataOutputStream dos = new DataOutputStream(baos)) {
+
+            // Serialize content items
+            byte[] contentBytes = contentSerializer.serializeToBytes(data.getContentItems());
+            dos.writeInt(contentBytes.length);
+            dos.write(contentBytes);
+
+            // Serialize fuel items
+            byte[] fuelBytes = fuelSerializer.serializeToBytes(data.getFuelItems());
+            dos.writeInt(fuelBytes.length);
+            dos.write(fuelBytes);
+
+            // Serialize primitive fields
+            dos.writeLong(data.getMaxBurnTime());
+            dos.writeLong(data.getBurnTime());
+            dos.writeFloat(data.getTemperature());
+
+            dos.flush();
+            return baos.toByteArray();
+        }
     }
 
     @Override
-    public @NotNull SmelterData deserialize(@NotNull PersistentDataContainer container) {
-        StorageBlockData contentItems = contentSerializer.deserialize(container);
-        StorageBlockData fuelItems = fuelSerializer.deserialize(container);
-        long burnTime = Objects.requireNonNull(container.get(new NamespacedKey("betterpvp", "burn_time"), PersistentDataType.LONG));
-        float temperature = Objects.requireNonNull(container.get(new NamespacedKey("betterpvp", "temperature"), PersistentDataType.FLOAT));
+    public @NotNull SmelterData deserializeFromBytes(byte[] bytes) throws IOException {
+        try (ByteArrayInputStream bais = new ByteArrayInputStream(bytes);
+             DataInputStream dis = new DataInputStream(bais)) {
 
-        final SmelterData defaultData = smelter.createDefaultData();
-        defaultData.setContentItems(contentItems);
-        defaultData.setFuelItems(fuelItems);
-        defaultData.setBurnTime(burnTime);
-        defaultData.setTemperature(temperature);
-        return defaultData;
-    }
+            // Deserialize content items
+            int contentLength = dis.readInt();
+            byte[] contentBytes = new byte[contentLength];
+            dis.readFully(contentBytes);
+            StorageBlockData contentItems = contentSerializer.deserializeFromBytes(contentBytes);
 
-    @Override
-    public boolean hasData(@NotNull PersistentDataContainer container) {
-        return container.has(new NamespacedKey("betterpvp", "burn_time"), PersistentDataType.LONG) &&
-               container.has(new NamespacedKey("betterpvp", "temperature"), PersistentDataType.FLOAT) &&
-               contentSerializer.hasData(container) &&
-               fuelSerializer.hasData(container);
+            // Deserialize fuel items
+            int fuelLength = dis.readInt();
+            byte[] fuelBytes = new byte[fuelLength];
+            dis.readFully(fuelBytes);
+            StorageBlockData fuelItems = fuelSerializer.deserializeFromBytes(fuelBytes);
+
+            // Deserialize primitive fields
+            long maxBurnTime = dis.readLong();
+            long burnTime = dis.readLong();
+            float temperature = dis.readFloat();
+
+            // Create and populate SmelterData
+            SmelterData smelterData = new SmelterData(maxBurnTime);
+            smelterData.setContentItems(contentItems);
+            smelterData.setFuelItems(fuelItems);
+            smelterData.setBurnTime(burnTime);
+            smelterData.setTemperature(temperature);
+
+            return smelterData;
+        }
     }
 }
