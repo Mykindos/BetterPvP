2c8179555d74608841d8d964751a602f7b28adb9
StudentAlleg <studentalleg-pilot@yahoo.com>
2025-06-26 13:12:01 -0700

    Refactor to use manager/loader architecture

--- core/src/main/java/me/mykindos/betterpvp/core/Core.java
+++ core/src/main/java/me/mykindos/betterpvp/core/Core.java
@@ -10,6 +10,7 @@ import me.mykindos.betterpvp.core.client.achievements.category.loader.CoreAchiev
 import me.mykindos.betterpvp.core.client.achievements.loader.CoreAchievementLoader;
 import me.mykindos.betterpvp.core.client.punishments.rules.RuleManager;
 import me.mykindos.betterpvp.core.client.repository.ClientManager;
+import me.mykindos.betterpvp.core.client.stats.formatter.manager.StatFormatterLoader;
 import me.mykindos.betterpvp.core.combat.stats.impl.GlobalCombatStatsRepository;
 import me.mykindos.betterpvp.core.combat.weapon.WeaponManager;
 import me.mykindos.betterpvp.core.command.loader.CoreCommandLoader;
@@ -141,6 +142,9 @@ public class Core extends BPvPPlugin {
 
         var coreAchievementLoader = injector.getInstance(CoreAchievementLoader.class);
         coreAchievementLoader.loadAchievements(PACKAGE);
+
+        var coreStatFormatterLoader = injector.getInstance(StatFormatterLoader.class);
+        coreStatFormatterLoader.loadAll();
 
         updateEventExecutor.loadPlugin(this);
         updateEventExecutor.initialize();
--- core/src/main/java/me/mykindos/betterpvp/core/client/stats/commands/RawStatsCommand.java
+++ core/src/main/java/me/mykindos/betterpvp/core/client/stats/commands/RawStatsCommand.java
@@ -1,8 +1,10 @@
 package me.mykindos.betterpvp.core.client.stats.commands;
 
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
 import me.mykindos.betterpvp.core.client.Client;
 import me.mykindos.betterpvp.core.client.stats.StatContainer;
-import me.mykindos.betterpvp.core.client.stats.formatter.manager.StatFormatters;
+import me.mykindos.betterpvp.core.client.stats.formatter.manager.StatFormatterManager;
 import me.mykindos.betterpvp.core.command.Command;
 import me.mykindos.betterpvp.core.inventory.item.Item;
 import me.mykindos.betterpvp.core.menu.impl.ViewCollectionMenu;
@@ -12,7 +14,15 @@ import org.bukkit.entity.Player;
 
 import java.util.List;
 
+@Singleton
 public class RawStatsCommand extends Command implements IStringName {
+    private final StatFormatterManager statFormatterManager;
+
+    @Inject
+    public RawStatsCommand(StatFormatterManager statFormatterManager) {
+        this.statFormatterManager = statFormatterManager;
+    }
+
     @Override
     public String getName() {
         return "rawstats";
@@ -32,7 +42,7 @@ public class RawStatsCommand extends Command implements IStringName {
         final List<Item> statItems = container.getStats().getStatsOfPeriod(period).entrySet().stream()
                 .map(entry -> {
                     final String statName = entry.getKey();
-                    return StatFormatters.getStatFormatter(statName).getValue().getDescription(statName, container, period);
+                    return statFormatterManager.getStatFormatter(statName).getValue().getDescription(statName, container, period);
                 })
                 .map(Description::toSimpleItem)
                 .map(Item.class::cast)
