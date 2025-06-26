06df9234aa388239d30755c46a7487f2188d9862
StudentAlleg <studentalleg-pilot@yahoo.com>
2025-06-26 13:12:01 -0700

    Refactor to use manager/loader architecture

--- core/src/main/java/me/mykindos/betterpvp/core/Core.java
+++ core/src/main/java/me/mykindos/betterpvp/core/Core.java
@@ -14,6 +14,7 @@ import me.mykindos.betterpvp.core.block.data.manager.SmartBlockDataManager;
 import me.mykindos.betterpvp.core.block.impl.CoreBlockBootstrap;
 import me.mykindos.betterpvp.core.client.punishments.rules.RuleManager;
 import me.mykindos.betterpvp.core.client.repository.ClientManager;
+import me.mykindos.betterpvp.core.client.stats.formatter.manager.StatFormatterLoader;
 import me.mykindos.betterpvp.core.combat.stats.impl.GlobalCombatStatsRepository;
 import me.mykindos.betterpvp.core.command.loader.CoreCommandLoader;
 import me.mykindos.betterpvp.core.config.Config;
@@ -155,6 +156,9 @@ public class Core extends BPvPPlugin {
 
         var coreAchievementLoader = injector.getInstance(CoreAchievementLoader.class);
         coreAchievementLoader.loadAchievements(PACKAGE);
+
+        var coreStatFormatterLoader = injector.getInstance(StatFormatterLoader.class);
+        coreStatFormatterLoader.loadAll();
 
         updateEventExecutor.loadPlugin(this);
         updateEventExecutor.initialize();
