29ea0138de80a23701ac51f679792555eeac9187
StudentAlleg <studentalleg-pilot@yahoo.com>
2025-06-29 01:40:13 -0700

    Initial Clans Stats

--- core/src/main/java/me/mykindos/betterpvp/core/client/stats/impl/ClientStat.java
+++ core/src/main/java/me/mykindos/betterpvp/core/client/stats/impl/ClientStat.java
@@ -47,14 +47,41 @@ public enum ClientStat implements IClientStat {
     HEAL_LEECH("Leech Heal", "Healing from procing Leach"),
     HEAL_WREATH("Wreath Heal", "Healing from procing Wreath"),
 
-    //todo bloodsphere heal
     HEAL_SCYTHE("Scythe Heal", "Healing from procing the Scythe"),
 
     CLONE_ATTACK("Clone Attacks", "Number of times your Clone successfully attacks"),
 
     //clans
-    SET_CORE("Set Core", "Number of times you set your clan core"),
-    TELEPORT_CORE("Teleport to Core", "Number of times you teleported to your clan core"),
+    CLANS_SET_CORE("Set Core", "Number of times you set your clan core"),
+    CLANS_TELEPORT_CORE("Teleport to Core", "Number of times you teleported to your clan core"),
+
+    CLANS_CLANS_EXPERIENCE("Clan Experience", "Clan experience you earned"),
+
+    CLANS_ENERGY_DROPPED("Energy Dropped", "Energy dropped as a result of your actions", "I.e. killing a boss, mining a block"),
+
+    CLANS_ENERGY_COLLECTED("Energy Collected", "Amount of Energy you picked up for your Clan"),
+
+    CLANS_CLAIM_TERRITORY("Claim Territory", "Amount of territory you claimed for your clan"),
+    CLANS_UNCLAIM_TERRITORY("Unclaim Territory", "Amount of territory you unclaimed for your clan"),
+    CLANS_UNCLAIM_OTHER_TERRITORY("Unclaim Enemy Territory", "Amount of territory you unclaimed from another Clan"),
+
+    CLANS_ATTACK_PILLAGE("Pillage (Attacker)", "Number of times you pillaged another Clan"),
+    CLANS_DEFEND_PILLAGE("Pillage (Defender)", "Number of times your Clan was pillaged"),
+
+    CLANS_DESTROY_CORE("Destroy Core", "Number of times you destroyed an opposing Clan's core"),
+    CLANS_CORE_DESTROYED("Core Destroyed", "Number of times your Clan was destroyed"),
+    CLANS_CORE_DAMAGE("Core Damage", "Amount of damage you dealt to the core"),
+
+
+    CLANS_CANNON_SHOT("Cannon Shots", "Number of times you fired a cannon"),
+    CLANS_CANNON_BLOCK_DAMAGE("Cannon Block Damage", "Amount of block damage you have dealt with cannons"),
+
+    CLANS_DOMINANCE_GAINED("Dominance Gained", "Dominance you gained for your Clan"),
+    CLANS_DOMINANCE_LOST("Dominance Lost", "Dominance you lost for your Clan"),
+
+    CLANS_CLAN_LEAVE("Clan Leave", "Number of times you left a Clan"),
+    CLANS_CLAN_JOIN("Join Clan", "Number of times you joined a Clan"),
+    CLANS_CLAN_CREATE("Create Clan", "Number of Clans you created"),
 
     //events
     //todo undead city keys collected/chests opened
