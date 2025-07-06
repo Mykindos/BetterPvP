63978d101c596a7bff02837a8be396714c9d0e1e
StudentAlleg <studentalleg-pilot@yahoo.com>
2025-07-05 17:57:05 -0700

    Achievement as a Stat

---
+++ champions/src/main/java/me/mykindos/betterpvp/champions/achievements/impl/CastFireball.java
@@ -0,0 +1,62 @@
+package me.mykindos.betterpvp.champions.achievements.impl;
+
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import me.mykindos.betterpvp.champions.achievements.ChampionsAchievementCategories;
+import me.mykindos.betterpvp.core.client.achievements.AchievementType;
+import me.mykindos.betterpvp.core.client.achievements.types.SingleSimpleAchievement;
+import me.mykindos.betterpvp.core.client.stats.StatContainer;
+import me.mykindos.betterpvp.core.client.stats.impl.champions.ChampionsSkillStat;
+import me.mykindos.betterpvp.core.inventory.item.ItemProvider;
+import me.mykindos.betterpvp.core.listener.BPvPListener;
+import me.mykindos.betterpvp.core.properties.PropertyContainer;
+import me.mykindos.betterpvp.core.utilities.UtilMessage;
+import me.mykindos.betterpvp.core.utilities.model.description.Description;
+import me.mykindos.betterpvp.core.utilities.model.item.ItemView;
+import net.kyori.adventure.text.Component;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Singleton
+@BPvPListener
+public class CastFireball extends SingleSimpleAchievement {
+    @Inject
+    public CastFireball() {
+        super("I Cast Fireball", new NamespacedKey("champions", "cast_fireball_100"), ChampionsAchievementCategories.CHAMPIONS, AchievementType.GLOBAL, 100d,
+                ChampionsSkillStat.builder()
+                        .action(ChampionsSkillStat.Action.USE)
+                        .skillName("Fire Blast")
+                        .build()
+        );
+    }
+
+    /**
+     * Gets the description of this achievement for the specified container
+     * For use in UI's
+     *
+     * @param container the {@link PropertyContainer}
+     * @param period
+     * @return
+     */
+    @Override
+    public Description getDescription(StatContainer container, @Nullable String period) {
+        List<Component> lore = new ArrayList<>(
+                List.of(
+                        UtilMessage.deserialize("<gold>Use <white>Fire Blast <yellow>100 <gray>times")
+                ));
+        lore.addAll(this.getProgressComponent(container, period));
+        lore.addAll(this.getCompletionComponent(container));
+        ItemProvider itemProvider = ItemView.builder()
+                .material(Material.FIRE_CHARGE)
+                .displayName(UtilMessage.deserialize("<white>%s", getName()))
+                .lore(lore)
+                .build();
+        return Description.builder()
+                .icon(itemProvider)
+                .build();
+    }
+}
