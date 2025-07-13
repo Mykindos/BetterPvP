73d70509cec6b47c282461b0a78edea1a8c1b074
StudentAlleg <studentalleg-pilot@yahoo.com>
2025-07-12 21:02:23 -0700

    Flag Captures, points captured, and begin move all achievements to core. Fix incorrect logic for contains stat for most buildable stats

--- game/src/main/java/me/mykindos/betterpvp/game/achievements/category/GameAchievementCategories.java
+++
@@ -1,7 +0,0 @@
-package me.mykindos.betterpvp.game.achievements.category;
-
-import org.bukkit.NamespacedKey;
-
-public class GameAchievementCategories {
-    public static NamespacedKey GAME = new NamespacedKey("game", "game_category");
-}
