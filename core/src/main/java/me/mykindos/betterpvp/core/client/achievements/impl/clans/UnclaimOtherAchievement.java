9d58680875ae9fee3e8e1a5a9bf89dd64793d9d6
StudentAlleg <studentalleg-pilot@yahoo.com>
2025-07-15 15:52:19 -0700

    Add Champions, Clans, and event achievements. Moved most into core.

---
+++ core/src/main/java/me/mykindos/betterpvp/core/client/achievements/impl/clans/UnclaimOtherAchievement.java
@@ -0,0 +1,42 @@
+package me.mykindos.betterpvp.core.client.achievements.impl.clans;
+
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import lombok.CustomLog;
+import me.mykindos.betterpvp.core.client.achievements.AchievementType;
+import me.mykindos.betterpvp.core.client.achievements.category.AchievementCategories;
+import me.mykindos.betterpvp.core.client.achievements.types.SingleSimpleAchievement;
+import me.mykindos.betterpvp.core.client.stats.StatContainer;
+import me.mykindos.betterpvp.core.client.stats.impl.ClientStat;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+
+import java.util.List;
+
+@CustomLog
+@Singleton
+public class UnclaimOtherAchievement extends SingleSimpleAchievement {
+    @Inject
+    public UnclaimOtherAchievement() {
+        super("Don't Get Greedy",
+                new NamespacedKey("clans", "unclaim_other"),
+                AchievementCategories.CLANS,
+                AchievementType.GLOBAL,
+                1d,
+                ClientStat.CLANS_UNCLAIM_OTHER_TERRITORY
+        );
+    }
+
+    @Override
+    public Material getMaterial(StatContainer container, String period) {
+        return Material.TNT;
+    }
+
+    @Override
+    public List<String> getStringDescription(StatContainer container, String period) {
+        return List.of( "Unclaim territory from another",
+                        "Clan that does not have",
+                        "enough members to hold",
+                        "its territory");
+    }
+}
