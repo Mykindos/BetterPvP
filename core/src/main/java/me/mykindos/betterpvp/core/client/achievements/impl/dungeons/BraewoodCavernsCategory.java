32c373e377dcee21c1d57f420f38e0f76c7b9b77
StudentAlleg <studentalleg-pilot@yahoo.com>
2025-07-16 14:52:28 -0700

    Braewoods caverns achievement, sort achievements

--- core/src/main/java/me/mykindos/betterpvp/core/client/achievements/impl/dungeons/BraewoodCavernsCategory.java
+++ core/src/main/java/me/mykindos/betterpvp/core/client/achievements/impl/dungeons/BraewoodCavernsCategory.java
@@ -1,10 +1,10 @@
-package me.mykindos.betterpvp.core.client.achievements.impl.events.undeadchests;
+package me.mykindos.betterpvp.core.client.achievements.impl.dungeons;
 
 import com.google.inject.Singleton;
 import me.mykindos.betterpvp.core.client.achievements.category.AchievementCategories;
 import me.mykindos.betterpvp.core.client.achievements.category.AchievementCategory;
 import me.mykindos.betterpvp.core.client.achievements.category.SubCategory;
-import me.mykindos.betterpvp.core.client.achievements.category.types.EventCategory;
+import me.mykindos.betterpvp.core.client.achievements.category.types.DungeonsCategory;
 import me.mykindos.betterpvp.core.utilities.UtilMessage;
 import me.mykindos.betterpvp.core.utilities.model.item.ItemView;
 import org.bukkit.Material;
@@ -13,18 +13,18 @@ import org.bukkit.Material;
 /**
  * The category, which defines the types and display view
  */
-@SubCategory(EventCategory.class)
-public class UndeadChestsCategory extends AchievementCategory {
-    public UndeadChestsCategory() {
-        super(AchievementCategories.GAME_FLAG_CAPTURES);
+@SubCategory(DungeonsCategory.class)
+public class BraewoodCavernsCategory extends AchievementCategory {
+    public BraewoodCavernsCategory() {
+        super(AchievementCategories.DUNGEONS_BRAEWOOD_CAVERNS_PERIOD);
     }
 
 
     @Override
     public ItemView getItemView() {
         return ItemView.builder()
-                .material(Material.ENDER_CHEST)
-                .displayName(UtilMessage.deserialize("<white>Open Undead Chests"))
+                .material(Material.OAK_SAPLING)
+                .displayName(UtilMessage.deserialize("<white>Defeat Braewood Caverns"))
                 .build();
     }
 }
---
+++ core/src/main/java/me/mykindos/betterpvp/core/client/achievements/impl/dungeons/BraewoodCavernsPeriodAchievement.java
@@ -0,0 +1,67 @@
+package me.mykindos.betterpvp.core.client.achievements.impl.dungeons;
+
+import lombok.CustomLog;
+import me.mykindos.betterpvp.core.client.achievements.AchievementType;
+import me.mykindos.betterpvp.core.client.achievements.category.AchievementCategories;
+import me.mykindos.betterpvp.core.client.achievements.impl.general.deaths.DeathAchievementLoader;
+import me.mykindos.betterpvp.core.client.achievements.types.SingleSimpleAchievement;
+import me.mykindos.betterpvp.core.client.stats.StatContainer;
+import me.mykindos.betterpvp.core.client.stats.impl.ClientStat;
+import me.mykindos.betterpvp.core.utilities.model.NoReflection;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+
+import java.util.List;
+
+@CustomLog
+//Config loaded achievement, this class will be skipped by reflection
+@NoReflection
+/**
+ * Super class, is either extended or loaded by a loader {@link DeathAchievementLoader}
+ */
+public class BraewoodCavernsPeriodAchievement extends SingleSimpleAchievement {
+
+    public BraewoodCavernsPeriodAchievement(String key, int goal) {
+        this(new NamespacedKey("events", key), goal);
+    }
+
+    public BraewoodCavernsPeriodAchievement(NamespacedKey key, int goal) {
+        super("Beat the Braewoods Cavern", key,
+                AchievementCategories.EVENT_UNDEAD_CHESTS,
+                AchievementType.PERIOD,
+                (double) goal,
+                ClientStat.EVENT_UNDEAD_CITY_OPEN_CHEST
+        );
+    }
+
+    @Override
+    public String getName() {
+        return "Beat the Braewoods Cavern " + getGoal().intValue();
+    }
+
+    /**
+     * gets the material for the itemprovider
+     *
+     * @param container
+     * @param period
+     * @return
+     */
+    @Override
+    public Material getMaterial(StatContainer container, String period) {
+        return Material.OAK_SAPLING;
+    }
+
+    /**
+     * A helper method, to easily add a description to the lore
+     * without duplicating adding the progress and completion component
+     * Used in getLore
+     *
+     * @param container
+     * @param period
+     * @return
+     */
+    @Override
+    public List<String> getStringDescription(StatContainer container, String period) {
+        return List.of("<gray>Beat the Braewoods Cavern <yellow>" + getGoal().intValue() + "</yellow> times");
+    }
+}
