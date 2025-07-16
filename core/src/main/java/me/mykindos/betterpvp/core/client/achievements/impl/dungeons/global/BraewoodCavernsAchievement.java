c164f95955095d45bc5028d0683d5def0e7d2279
StudentAlleg <studentalleg-pilot@yahoo.com>
2025-07-16 14:52:28 -0700

    Braewoods caverns achievement, sort achievements

---
+++ core/src/main/java/me/mykindos/betterpvp/core/client/achievements/impl/dungeons/global/BraewoodCavernsAchievement.java
@@ -0,0 +1,42 @@
+package me.mykindos.betterpvp.core.client.achievements.impl.dungeons.global;
+
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import lombok.CustomLog;
+import me.mykindos.betterpvp.core.client.achievements.AchievementType;
+import me.mykindos.betterpvp.core.client.achievements.category.AchievementCategories;
+import me.mykindos.betterpvp.core.client.achievements.types.SingleSimpleAchievement;
+import me.mykindos.betterpvp.core.client.stats.StatContainer;
+import me.mykindos.betterpvp.core.client.stats.impl.dungeons.DungeonStat;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+
+import java.util.List;
+
+@CustomLog
+@Singleton
+public class BraewoodCavernsAchievement extends SingleSimpleAchievement {
+    @Inject
+    public BraewoodCavernsAchievement() {
+        super("Beat the Braewood Caverns",
+                new NamespacedKey("dungeons", "braewood_caverns_global_1"),
+                AchievementCategories.DUNGEONS,
+                AchievementType.GLOBAL,
+                1d,
+                DungeonStat.builder()
+                        .action(DungeonStat.Action.WIN)
+                        .dungeonName("Braewood Caverns")
+                        .build()
+        );
+    }
+
+    @Override
+    public Material getMaterial(StatContainer container, String period) {
+        return Material.OAK_WOOD;
+    }
+
+    @Override
+    public List<String> getStringDescription(StatContainer container, String period) {
+        return List.of("Defeat the boss of the Braewood Caverns");
+    }
+}
