55ebe97f8e591863305c7301b4de6c018a4dbba0
StudentAlleg <studentalleg-pilot@yahoo.com>
2025-07-18 16:59:02 -0700

    Store data about periods, add period filter button to achievements

---
+++ core/src/main/java/me/mykindos/betterpvp/core/client/stats/period/StatPeriod.java
@@ -0,0 +1,34 @@
+package me.mykindos.betterpvp.core.client.stats.period;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import org.jetbrains.annotations.NotNull;
+
+import java.sql.Date;
+
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class StatPeriod implements Comparable<StatPeriod> {
+    @Getter
+    @EqualsAndHashCode.Include
+    @NotNull
+    private final String period;
+    @NotNull
+    private final Date date;
+
+    public StatPeriod(@NotNull String period, @NotNull Date date) {
+        this.period = period;
+        this.date = date;
+    }
+
+    @Override
+    public int compareTo(@NotNull StatPeriod o) {
+        return -date.compareTo(o.date);
+    }
+
+    @Override
+    public String toString() {
+        return "StatPeriod{" +
+                "period='" + period + '\'' +
+                '}';
+    }
+}
