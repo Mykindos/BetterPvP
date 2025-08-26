658a19b032a7aa60f7f4f27797cc9b8e62b06aad
braulio-dev <bchamerry@gmail.com>
2025-08-26 09:41:04 -0700

    Add ranger recipes

--- champions/src/main/java/me/mykindos/betterpvp/champions/item/RangerLeggings.java
+++ champions/src/main/java/me/mykindos/betterpvp/champions/item/RangerLeggings.java
@@ -3,8 +3,13 @@ package me.mykindos.betterpvp.champions.item;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import me.mykindos.betterpvp.champions.Champions;
+import me.mykindos.betterpvp.core.item.BaseItem;
+import me.mykindos.betterpvp.core.item.ItemFactory;
 import me.mykindos.betterpvp.core.item.ItemRarity;
 import me.mykindos.betterpvp.core.item.model.ArmorItem;
+import me.mykindos.betterpvp.core.recipe.RecipeIngredient;
+import me.mykindos.betterpvp.core.recipe.crafting.CraftingRecipeRegistry;
+import me.mykindos.betterpvp.core.recipe.crafting.ShapedCraftingRecipe;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 
@@ -13,6 +18,18 @@ public class RangerLeggings extends ArmorItem {
     @Inject
     private RangerLeggings(Champions champions) {
         super(champions, "Ranger Leggings", ItemStack.of(Material.CHAINMAIL_LEGGINGS), ItemRarity.COMMON);
-        // TODO: Add shaped recipe
+    }
+
+    @Inject
+    private void registerRecipe(CraftingRecipeRegistry registry, ItemFactory itemFactory) {
+        final BaseItem emerald = itemFactory.getFallbackItem(Material.EMERALD);
+        String[] pattern = new String[] {
+                "EEE",
+                "E E",
+                "E E",
+        };
+        final ShapedCraftingRecipe.Builder builder = new ShapedCraftingRecipe.Builder(this, pattern, itemFactory);
+        builder.setIngredient('E', new RecipeIngredient(emerald, 1));
+        registry.registerRecipe(builder.build());
     }
 }
