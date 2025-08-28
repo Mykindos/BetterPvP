c7a36b4ae8bee4ef8220743f16247bbb9b43be9c
braulio-dev <bchamerry@gmail.com>
2025-08-28 11:48:35 -0700

    Quick crafting and single-blueprint migration

---
+++ core/src/main/java/me/mykindos/betterpvp/core/menu/button/BackTabButton.java
@@ -0,0 +1,41 @@
+package me.mykindos.betterpvp.core.menu.button;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import me.mykindos.betterpvp.core.inventory.item.ItemProvider;
+import me.mykindos.betterpvp.core.inventory.item.impl.AbstractItem;
+import me.mykindos.betterpvp.core.utilities.model.item.ItemView;
+import net.kyori.adventure.key.Key;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.TextColor;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.ClickType;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Button that shows a description and wiki entries.
+ */
+@Getter
+@Builder
+@RequiredArgsConstructor
+public class BackTabButton extends AbstractItem {
+
+    private final Runnable onBack;
+
+    @Override
+    public ItemProvider getItemProvider() {
+        return ItemView.builder()
+                .material(Material.PAPER)
+                .itemModel(Key.key("betterpvp", "menu/tab/back"))
+                .displayName(Component.text("Back", TextColor.color(245, 71, 59)))
+                .build();
+    }
+
+    @Override
+    public void handleClick(@NotNull ClickType clickType, @NotNull Player player, @NotNull InventoryClickEvent event) {
+        onBack.run();
+    }
+}
