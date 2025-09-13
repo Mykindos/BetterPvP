d43ea8c162a5252d8c804121a8e449111aa38766
8SixSect <eightysixthsector@outlook.com>
2025-09-13 14:55:38 -0500

    Added a component that displays excessive force's duration and also fixed it's cooldown calculation

--- champions/src/main/java/me/mykindos/betterpvp/champions/champions/skills/skills/assassin/sword/ExcessiveForce.java
+++ champions/src/main/java/me/mykindos/betterpvp/champions/champions/skills/skills/assassin/sword/ExcessiveForce.java
@@ -10,18 +10,27 @@ import me.mykindos.betterpvp.champions.champions.skills.data.SkillActions;
 import me.mykindos.betterpvp.champions.champions.skills.types.CooldownSkill;
 import me.mykindos.betterpvp.champions.champions.skills.types.InteractSkill;
 import me.mykindos.betterpvp.champions.champions.skills.types.OffensiveSkill;
+import me.mykindos.betterpvp.core.client.gamer.Gamer;
 import me.mykindos.betterpvp.core.combat.events.CustomDamageEvent;
 import me.mykindos.betterpvp.core.components.champions.Role;
 import me.mykindos.betterpvp.core.components.champions.SkillType;
 import me.mykindos.betterpvp.core.framework.updater.UpdateEvent;
 import me.mykindos.betterpvp.core.listener.BPvPListener;
+import me.mykindos.betterpvp.core.utilities.UtilFormat;
 import me.mykindos.betterpvp.core.utilities.UtilMessage;
+import me.mykindos.betterpvp.core.utilities.model.display.DisplayComponent;
+import me.mykindos.betterpvp.core.utilities.model.display.PermanentComponent;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Iterator;
 import java.util.Map;
@@ -31,10 +40,45 @@ import java.util.WeakHashMap;
 @BPvPListener
 public class ExcessiveForce extends Skill implements InteractSkill, CooldownSkill, Listener, OffensiveSkill {
 
+    /**
+     * Players using this ability mapped to their expiration time.
+     * <p>
+     * When the player uses this ability, we calculate the time, in milliseconds, in the future when the ability should
+     * expire then store that value here.
+     * <p>
+     * See {@link #activate(Player, int)} for where that calculation is done.
+     */
     private final WeakHashMap<Player, Long> active = new WeakHashMap<>();
 
-    private double baseDuration;
+    /**
+     * An action bar component to show the remaining duration of excessive force.
+     * <p>
+     * Calculates the remaining time left based on the current time and the expiration time stored in {@link #active}.
+     */
+    private final DisplayComponent durationActionBar = new PermanentComponent(
+            gamer -> {
+                final @Nullable Player player = gamer.getPlayer();
+                if (player == null) return null;
 
+                int level = getLevel(gamer.getPlayer());
+                if (level <= 0) return null;
+
+                if (!active.containsKey(gamer.getPlayer())) return null;
+                long timeLeftInMillis = active.get(player) - System.currentTimeMillis();
+
+                // If true, ability has expired and `active` will be updated in the #onUpdate method within a couple ticks
+                if (timeLeftInMillis <= 0) return null;
+                double timeLeftInSeconds = timeLeftInMillis / 1000.0D;
+                final String timeLeftWithOneDecimalPlace = UtilFormat.formatNumber(timeLeftInSeconds, 1);
+
+                // ex: Excessive Force: 2.6s
+                return Component.text(getName() + ":" + " ").color(NamedTextColor.WHITE).decorate(TextDecoration.BOLD)
+                        .append(Component.text(timeLeftWithOneDecimalPlace + "s").color(NamedTextColor.GREEN).decoration(TextDecoration.BOLD, false));
+            }
+    );
+
+
+    private double baseDuration;
     private double durationIncreasePerLevel;
 
     @Inject
@@ -62,18 +106,39 @@ public class ExcessiveForce extends Skill implements InteractSkill, CooldownSkil
         };
     }
 
+    /**
+     * The duration of the excessive force ability for the given level.
+     */
     public double getDuration(int level) {
         return baseDuration + (level - 1) * durationIncreasePerLevel;
     }
 
-
+    // entrypoint
     @Override
     public void activate(Player player, int level) {
-        active.put(player, System.currentTimeMillis() + (long) (getDuration(level) * 1000L));
+
+        // current time + duration in milliseconds = time in future when ability expires
+        final long expirationTimeInMillis = System.currentTimeMillis() + (long) (getDuration(level) * 1000L);
+        active.put(player, expirationTimeInMillis);
+
         player.getWorld().playSound(player.getLocation(), Sound.ENTITY_ILLUSIONER_PREPARE_MIRROR, 1f, 1.7f);
     }
 
+    @Override
+    public void trackPlayer(Player player, Gamer gamer) {
+        gamer.getActionBar().add(900, durationActionBar);
+    }
 
+    @Override
+    public void invalidatePlayer(Player player, Gamer gamer) {
+        gamer.getActionBar().remove(durationActionBar);
+    }
+
+    /**
+     * This listener's purpose is to override the default no-knockback behavior of assassin's melee attacks.
+     * <p>
+     * Note: This might also affect other skills that set no-knockback, but currently there are none.
+     */
     @EventHandler(priority = EventPriority.HIGH)
     public void setKnockback(CustomDamageEvent event) {
         if (event.getDamager() instanceof Player damager) {
@@ -83,23 +148,68 @@ public class ExcessiveForce extends Skill implements InteractSkill, CooldownSkil
         }
     }
 
+    /**
+     * Monitors active excessive force users and removes them when they expire.
+     * Also removes players who no longer have the assassin role.
+     */
     @UpdateEvent(delay = 100)
     public void onUpdate() {
         Iterator<Map.Entry<Player, Long>> it = active.entrySet().iterator();
         while (it.hasNext()) {
             Map.Entry<Player, Long> next = it.next();
             Player player = next.getKey();
-            if (next.getValue() - System.currentTimeMillis() <= 0) {
+
+            if (!player.isOnline()) {
                 it.remove();
-                UtilMessage.message(player, getClassType().getName(), UtilMessage.deserialize("<green>%s %d</green> has ended.", getName(), getLevel(player)));
                 continue;
             }
 
+            // No longer is assassin
             if (!championsManager.getRoles().hasRole(next.getKey(), Role.ASSASSIN)) {
                 it.remove();
+                continue;
             }
 
+            // No longer has the skill
+            if (getLevel(player) <= 0) {
+                it.remove();
+                continue;
+            }
+
+            // Remove ability if it expires
+            if (next.getValue() - System.currentTimeMillis() <= 0) {
+                it.remove();
+                doWhenExcessiveForceExpires(player);
+            }
         }
+    }
+
+    /**
+     * Cleans up cooldowns and notifies the player when their excessive force duration ends naturally.
+     * <p>
+     * Non-natural expirations (like losing assassin role or logging off) are handled in {@link #onUpdate()}.
+     */
+    private void doWhenExcessiveForceExpires(@NotNull Player player) {
+        int level = getLevel(player);
+        if (level <= 0) return;  // #onUpdate already checks this, but just in case
+
+        championsManager.getCooldowns().removeCooldown(player, getName(), true);
+        championsManager.getCooldowns().use(player,
+                getName(),
+                getCooldown(level),
+                showCooldownFinished(),
+                true,
+                isCancellable(),
+                this::shouldDisplayActionBar);
+
+        // Notify
+        Component messageToSend = UtilMessage.deserialize("<green>%s %d</green> has ended.", getName(), level);
+        UtilMessage.message(player, getClassType().getName(), messageToSend);
+    }
+
+    @Override
+    public boolean shouldDisplayActionBar(Gamer gamer) {
+        return !active.containsKey(gamer.getPlayer());  // only show if the player is actively using the skill
     }
 
     @Override
@@ -119,7 +229,10 @@ public class ExcessiveForce extends Skill implements InteractSkill, CooldownSkil
 
     @Override
     public double getCooldown(int level) {
-        return cooldown - ((level - 1) * cooldownDecreasePerLevel);
+        final double calculatedCooldown = cooldown - ((level - 1) * cooldownDecreasePerLevel);
+        final double calculatedDuration = getDuration(level);
+
+        return calculatedCooldown - calculatedDuration;
     }
 
     @Override
@@ -127,6 +240,4 @@ public class ExcessiveForce extends Skill implements InteractSkill, CooldownSkil
         baseDuration = getConfig("baseDuration", 3.0, Double.class);
         durationIncreasePerLevel = getConfig("durationPerLevel", 0.5, Double.class);
     }
-
-
 }
