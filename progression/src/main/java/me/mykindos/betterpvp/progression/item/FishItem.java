8a0bdce0a02efc73ef6efb60eb3c29b747df44f7
braulio-dev <bchamerry@gmail.com>
2025-09-14 18:22:22 -0700

    Fix fish

--- progression/src/main/java/me/mykindos/betterpvp/progression/item/FishItem.java
+++ progression/src/main/java/me/mykindos/betterpvp/progression/item/FishItem.java
@@ -1,5 +1,6 @@
 package me.mykindos.betterpvp.progression.item;
 
+import lombok.EqualsAndHashCode;
 import me.mykindos.betterpvp.core.item.BaseItem;
 import me.mykindos.betterpvp.core.item.ItemGroup;
 import me.mykindos.betterpvp.core.item.ItemRarity;
@@ -8,6 +9,7 @@ import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
+@EqualsAndHashCode(callSuper = true)
 public class FishItem extends BaseItem implements NexoItem {
 
     private final String nexoId;
