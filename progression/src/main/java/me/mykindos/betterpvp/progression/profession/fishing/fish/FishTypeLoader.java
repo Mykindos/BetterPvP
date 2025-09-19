a4c2be15461ccd17fa74cc0f37f6207867e8f34f
braulio-dev <bchamerry@gmail.com>
2025-09-19 12:30:54 -0700

    Fix fish and progression drop tables

--- progression/src/main/java/me/mykindos/betterpvp/progression/profession/fishing/fish/FishTypeLoader.java
+++ progression/src/main/java/me/mykindos/betterpvp/progression/profession/fishing/fish/FishTypeLoader.java
@@ -1,11 +1,18 @@
 package me.mykindos.betterpvp.progression.profession.fishing.fish;
 
 import com.google.common.base.Preconditions;
+import me.mykindos.betterpvp.core.item.ItemFactory;
 import me.mykindos.betterpvp.progression.profession.fishing.model.FishingConfigLoader;
 import org.bukkit.configuration.ConfigurationSection;
 import org.jetbrains.annotations.NotNull;
 
 public class FishTypeLoader implements FishingConfigLoader<FishType> {
+
+    private final ItemFactory itemFactory;
+
+    public FishTypeLoader(ItemFactory itemFactory) {
+        this.itemFactory = itemFactory;
+    }
 
     @Override
     public String getTypeKey() {
@@ -14,9 +21,9 @@ public class FishTypeLoader implements FishingConfigLoader<FishType> {
 
     @Override
     public @NotNull FishType read(ConfigurationSection section) {
-        final String name = section.getString("name");
-        Preconditions.checkNotNull(name, "Fish name cannot be null");
-        return new SimpleFishType(name);
+        final String key = section.getName();
+        Preconditions.checkNotNull(key, "Fish name cannot be null");
+        return new SimpleFishType(key, itemFactory);
     }
 
 }
