d932a99077a42eb2c917bd38f62dfc36e940c952
braulio-dev <bchamerry@gmail.com>
2025-09-27 15:16:52 -0700

    Fix AH listing removal message

--- clans/src/main/java/me/mykindos/betterpvp/clans/auctionhouse/ClansAuctionDeliveryService.java
+++ clans/src/main/java/me/mykindos/betterpvp/clans/auctionhouse/ClansAuctionDeliveryService.java
@@ -5,15 +5,20 @@ import me.mykindos.betterpvp.clans.clans.Clan;
 import me.mykindos.betterpvp.clans.clans.ClanManager;
 import me.mykindos.betterpvp.clans.clans.ClanProperty;
 import me.mykindos.betterpvp.clans.clans.core.mailbox.ClanMailbox;
+import me.mykindos.betterpvp.core.item.ItemFactory;
+import me.mykindos.betterpvp.core.item.ItemInstance;
 import me.mykindos.betterpvp.core.utilities.UtilFormat;
 import me.mykindos.betterpvp.core.utilities.UtilMessage;
 import me.mykindos.betterpvp.core.utilities.UtilSound;
 import me.mykindos.betterpvp.shops.auctionhouse.Auction;
 import me.mykindos.betterpvp.shops.auctionhouse.IAuctionDeliveryService;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
 import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
 import org.bukkit.Bukkit;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -22,9 +27,11 @@ import java.util.UUID;
 public class ClansAuctionDeliveryService implements IAuctionDeliveryService {
 
     private final ClanManager clanManager;
+    private final ItemFactory itemFactory;
 
-    public ClansAuctionDeliveryService(ClanManager clanManager) {
+    public ClansAuctionDeliveryService(ClanManager clanManager, ItemFactory itemFactory) {
         this.clanManager = clanManager;
+        this.itemFactory = itemFactory;
     }
 
     @Override
@@ -52,10 +59,15 @@ public class ClansAuctionDeliveryService implements IAuctionDeliveryService {
 
             Player player = Bukkit.getPlayer(target);
             if (player != null) {
+                final ItemStack itemStack = auction.getItemStack();
+                final ItemInstance instance = itemFactory.fromItemStack(itemStack).orElseThrow();
+                final Component name = instance.getBaseItem().getItemNameRenderer().createName(instance);
                 if (auction.isCancelled()) {
-                    UtilMessage.simpleMessage(player, "Auction House", "Your listing for " + PlainTextComponentSerializer.plainText().serialize(auction.getItemStack().displayName()) + " has been cancelled.");
+                    final TextComponent text = Component.text("Your listing for ").append(name).append(Component.text(" has been cancelled."));
+                    UtilMessage.simpleMessage(player, "Auction House", text);
                 } else if (auction.hasExpired()) {
-                    UtilMessage.simpleMessage(player, "Auction House", "Your listing for " + PlainTextComponentSerializer.plainText().serialize(auction.getItemStack().displayName()) + " has expired.");
+                    final TextComponent text = Component.text("Your listing for ").append(name).append(Component.text(" has expired."));
+                    UtilMessage.simpleMessage(player, "Auction House", text);
                 } else {
                     UtilMessage.simpleMessage(player, "Auction House", "Your purchase has been delivered to your Clan mailbox.");
                     UtilSound.playSound(player, Sound.BLOCK_NOTE_BLOCK_BELL, 2f, 1f, false);
