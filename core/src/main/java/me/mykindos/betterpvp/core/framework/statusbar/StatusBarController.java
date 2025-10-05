bc6378699b07c1216ac49a2be9f1daaedebf05ce
braulio-dev <bchamerry@gmail.com>
2025-10-05 10:53:01 -0700

    Add implicit StatusBar so it doesnt get overwritten by other components

--- core/src/main/java/me/mykindos/betterpvp/core/framework/statusbar/StatusBarController.java
+++ core/src/main/java/me/mykindos/betterpvp/core/framework/statusbar/StatusBarController.java
@@ -3,24 +3,25 @@ package me.mykindos.betterpvp.core.framework.statusbar;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import me.mykindos.betterpvp.core.client.gamer.Gamer;
+import me.mykindos.betterpvp.core.combat.health.EntityHealthService;
+import me.mykindos.betterpvp.core.energy.EnergyHandler;
 import me.mykindos.betterpvp.core.framework.adapter.PluginAdapter;
 
 @Singleton
 @PluginAdapter("Core")
 public class StatusBarController {
 
-    private final StatusBar statusBar;
+    private final EntityHealthService healthService;
+    private final EnergyHandler energyHandler;
 
     @Inject
-    private StatusBarController(StatusBar statusBar) {
-        this.statusBar = statusBar;
+    private StatusBarController(EntityHealthService healthService, EnergyHandler energyHandler) {
+        this.healthService = healthService;
+        this.energyHandler = energyHandler;
     }
 
     public void setup(Gamer gamer) {
-        gamer.getActionBar().add(Integer.MAX_VALUE, statusBar);
+        gamer.setActionBar(new StatusBar(healthService, energyHandler));
     }
 
-    public void remove(Gamer gamer) {
-        gamer.getActionBar().remove(statusBar);
-    }
 }
