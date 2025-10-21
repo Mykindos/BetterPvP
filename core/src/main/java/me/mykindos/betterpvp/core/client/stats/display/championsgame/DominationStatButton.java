3005b5a6c96f9fc65982e8350211e3e97655be4b
StudentAlleg <studentalleg-pilot@yahoo.com>
2025-10-21 14:50:24 -0700

    Use dungeon wrapper, fix capture time, fix native stats not getting composite stats correctly. Do better abstraction and reduce code duplication for domination stat display. Add way to get full of a stat (including instances of it being wrapped)

--- core/src/main/java/me/mykindos/betterpvp/core/client/stats/display/championsgame/DominationStatButton.java
+++ core/src/main/java/me/mykindos/betterpvp/core/client/stats/display/championsgame/DominationStatButton.java
@@ -4,9 +4,9 @@ import me.mykindos.betterpvp.core.client.stats.StatContainer;
 import me.mykindos.betterpvp.core.client.stats.display.IAbstractStatMenu;
 import me.mykindos.betterpvp.core.client.stats.display.StatFormatterUtility;
 import me.mykindos.betterpvp.core.client.stats.display.championsgame.domination.DominationStatMenu;
+import me.mykindos.betterpvp.core.client.stats.display.start.ChampionsStatButton;
 import me.mykindos.betterpvp.core.client.stats.impl.game.GameTeamMapNativeStat;
 import me.mykindos.betterpvp.core.inventory.item.ItemProvider;
-import me.mykindos.betterpvp.core.inventory.item.impl.controlitem.ControlItem;
 import me.mykindos.betterpvp.core.utilities.UtilTime;
 import me.mykindos.betterpvp.core.utilities.model.item.ClickActions;
 import me.mykindos.betterpvp.core.utilities.model.item.ItemView;
@@ -19,9 +19,10 @@ import org.jetbrains.annotations.NotNull;
 
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
 import java.util.List;
 
-public class DominationStatButton extends ControlItem<IAbstractStatMenu> {
+public class DominationStatButton extends ChampionsStatButton {
     /**
      * Gets the {@link ItemProvider}.
      * This method gets called every time a {@link Window} is notified ({@link #notifyWindows()}).
@@ -30,13 +31,11 @@ public class DominationStatButton extends ControlItem<IAbstractStatMenu> {
      */
     @Override
     public ItemProvider getItemProvider(IAbstractStatMenu gui) {
-        final StatContainer statContainer = gui.getClient().getStatContainer();
-        final String period = gui.getPeriodKey();
 
         return ItemView.builder()
                 .material(Material.BEACON)
                 .displayName(Component.text("Domination Stats"))
-                .lore(getStatsDescription(statContainer, period, "", ""))
+                .lore(getDominationStatsDescription("", ""))
                 .frameLore(true)
                 .action(ClickActions.ALL, Component.text("Show Detailed Stats"))
                 .build();
@@ -44,39 +43,16 @@ public class DominationStatButton extends ControlItem<IAbstractStatMenu> {
 
     /**
      *
-     * @param statContainer
-     * @param periodKey
      * @param teamName {@code ""} if empty
      * @param mapName {@code ""} if empty
      * @return
      */
-    protected static List<Component> getStatsDescription(final StatContainer statContainer, final String periodKey, final String teamName, final String mapName) {
-        final String gameName = "Domination";
+    protected List<Component> getDominationStatsDescription(final String teamName, final String mapName) {
+        final IAbstractStatMenu gui = getGui();
+        final StatContainer statContainer = gui.getClient().getStatContainer();
+        final String periodKey = gui.getPeriodKey();
 
-        final GameTeamMapNativeStat winStat = GameTeamMapNativeStat.builder()
-                .gameName(gameName)
-                .action(GameTeamMapNativeStat.Action.WIN)
-                .teamName(teamName)
-                .mapName(mapName)
-                .build();
-        final GameTeamMapNativeStat lossStat = GameTeamMapNativeStat.builder()
-                .gameName(gameName)
-                .action(GameTeamMapNativeStat.Action.LOSS)
-                .teamName(teamName)
-                .mapName(mapName)
-                .build();
-        final GameTeamMapNativeStat matchesPlayedStat = GameTeamMapNativeStat.builder()
-                .gameName(gameName)
-                .action(GameTeamMapNativeStat.Action.MATCHES_PLAYED)
-                .teamName(teamName)
-                .mapName(mapName)
-                .build();
-        final GameTeamMapNativeStat timePlayedStat = GameTeamMapNativeStat.builder()
-                .gameName(gameName)
-                .action(GameTeamMapNativeStat.Action.GAME_TIME_PLAYED)
-                .teamName(teamName)
-                .mapName(mapName)
-                .build();
+        final String gameName = "Domination";
 
         final GameTeamMapNativeStat killPointsStat = GameTeamMapNativeStat.builder()
                 .action(GameTeamMapNativeStat.Action.POINTS_KILLS)
@@ -109,11 +85,6 @@ public class DominationStatButton extends ControlItem<IAbstractStatMenu> {
                 .mapName(mapName)
                 .build();
 
-        final int wins = winStat.getStat(statContainer, periodKey).intValue();
-        final int losses = lossStat.getStat(statContainer, periodKey).intValue();
-        final int matchesPlayed = matchesPlayedStat.getStat(statContainer, periodKey).intValue();
-        final Duration timePlayed = Duration.of(timePlayedStat.getStat(statContainer, periodKey).longValue(), ChronoUnit.MILLIS);
-
         final int killPoints = killPointsStat.getStat(statContainer, periodKey).intValue();
         final int gemPoints = gemPointsStat.getStat(statContainer, periodKey).intValue();
         final int gemPickup = gemPickupStat.getStat(statContainer, periodKey).intValue();
@@ -121,11 +92,11 @@ public class DominationStatButton extends ControlItem<IAbstractStatMenu> {
         final Duration captureTime = Duration.of(captureTimeStat.getStat(statContainer, periodKey).longValue(), ChronoUnit.MILLIS);
         final Duration contestTime = Duration.of(contestTimeStat.getStat(statContainer, periodKey).longValue(), ChronoUnit.MILLIS);
 
-        return List.of(
-                StatFormatterUtility.formatStat("Wins", wins),
-                StatFormatterUtility.formatStat("Losses", losses),
-                StatFormatterUtility.formatStat("Matches Played", matchesPlayed),
-                StatFormatterUtility.formatStat("Time Played", UtilTime.humanReadableFormat(timePlayed)),
+        final List<Component> dominationStats = new ArrayList<>(
+                getChampionsStatsDescription(gameName, teamName, mapName)
+
+        );
+        dominationStats.addAll(List.of(
                 Component.empty(),
                 StatFormatterUtility.formatStat("Kill Score", killPoints),
                 StatFormatterUtility.formatStat("Gem Score", gemPoints),
@@ -133,7 +104,10 @@ public class DominationStatButton extends ControlItem<IAbstractStatMenu> {
                 StatFormatterUtility.formatStat("Points Captured", pointsCaptured),
                 StatFormatterUtility.formatStat("Time Capturing", UtilTime.humanReadableFormat(captureTime)),
                 StatFormatterUtility.formatStat("Time Contesting", UtilTime.humanReadableFormat(contestTime))
+            )
         );
+
+        return dominationStats;
     }
 
     /**
