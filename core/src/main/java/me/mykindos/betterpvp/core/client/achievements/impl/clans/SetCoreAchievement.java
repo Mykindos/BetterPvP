37266bc03081f2416b3ce102148225010daddb0c
StudentAlleg <studentalleg-pilot@yahoo.com>
2025-10-31 19:26:53 -0700

    #1686 - Update achievements to use new wrapper/generics

--- core/src/main/java/me/mykindos/betterpvp/core/client/achievements/impl/clans/SetCoreAchievement.java
+++ core/src/main/java/me/mykindos/betterpvp/core/client/achievements/impl/clans/SetCoreAchievement.java
@@ -8,19 +8,34 @@ import me.mykindos.betterpvp.core.client.achievements.category.AchievementCatego
 import me.mykindos.betterpvp.core.client.achievements.types.SingleSimpleAchievement;
 import me.mykindos.betterpvp.core.client.stats.StatContainer;
 import me.mykindos.betterpvp.core.client.stats.impl.ClientStat;
+import me.mykindos.betterpvp.core.client.stats.impl.clans.ClanWrapperStat;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
+
+import java.util.List;
 
 @CustomLog
 @Singleton
 public class SetCoreAchievement extends SingleSimpleAchievement {
     @Inject
     public SetCoreAchievement() {
-        super("Set Core", new NamespacedKey("clans", "set_core"), AchievementCategories.CLANS, AchievementType.GLOBAL, 1d, ClientStat.CLANS_SET_CORE);
+        super("Set Core", new NamespacedKey("clans", "set_core"),
+                AchievementCategories.CLANS,
+                AchievementType.GLOBAL,
+                1d,
+                ClanWrapperStat.builder()
+                        .wrappedStat(ClientStat.CLANS_SET_CORE)
+                        .build()
+        );
     }
 
     @Override
     public Material getMaterial(StatContainer container, String period) {
         return Material.RESPAWN_ANCHOR;
+    }
+
+    @Override
+    public List<String> getStringDescription(StatContainer container, String period) {
+        return List.of("Set your clan core");
     }
 }
