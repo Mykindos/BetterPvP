3c860015e56caa48e063f09634a6bea49eae4877
Mykindos <5389944+Mykindos@users.noreply.github.com>
2025-11-08 13:25:47 +1100

    Also add migration for game to champions, for easier jooq setup

---
+++ champions/src/main/resources/champions-migrations/postgres/V20251108_1__Create_game_tables.sql
@@ -0,0 +1,14 @@
+
+-- Store hotbar layouts
+CREATE TABLE IF NOT EXISTS champions_hotbar_layouts
+(
+    client  BIGINT REFERENCES clients (id) ON DELETE CASCADE,
+    role    varchar(20) not null,
+    id      int          not null,
+    slot    int          not null,
+    item    varchar(50) not null,
+    PRIMARY KEY (client, role, id, slot)
+);
+
+INSERT INTO property_map (property, type) VALUES ('CHAMPIONS_WINS', 'int')
+ON CONFLICT (property, type) DO NOTHING;
