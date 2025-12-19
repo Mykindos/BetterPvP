7ec69cf8b3a90296ec4d2e3863909c0242d344bb
StudentAlleg <studentalleg-pilot@yahoo.com>
2025-12-18 18:08:21 -0800

    #1686 - Remove unused GetDefaultTrackedStatsEvent.java, update damage stat to use new DamageCause

--- core/src/main/java/me/mykindos/betterpvp/core/client/stats/listeners/StatListener.java
+++ core/src/main/java/me/mykindos/betterpvp/core/client/stats/listeners/StatListener.java
@@ -6,21 +6,11 @@ import lombok.CustomLog;
 import me.mykindos.betterpvp.core.client.events.ClientQuitEvent;
 import me.mykindos.betterpvp.core.client.repository.ClientManager;
 import me.mykindos.betterpvp.core.client.stats.StatContainer;
-import me.mykindos.betterpvp.core.client.stats.events.GetDefaultTrackedStatsEvent;
-import me.mykindos.betterpvp.core.client.stats.impl.ClientStat;
-import me.mykindos.betterpvp.core.client.stats.impl.IStat;
-import me.mykindos.betterpvp.core.client.stats.impl.core.DamageStat;
-import me.mykindos.betterpvp.core.client.stats.impl.core.MinecraftStat;
-import me.mykindos.betterpvp.core.client.stats.impl.utilitiy.Relation;
-import me.mykindos.betterpvp.core.client.stats.impl.utilitiy.Type;
 import me.mykindos.betterpvp.core.listener.BPvPListener;
-import org.bukkit.Statistic;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.Set;
 
 @BPvPListener
@@ -32,29 +22,6 @@ public class StatListener implements Listener {
     @Inject
     public StatListener(ClientManager clientManager) {
         this.clientManager = clientManager;
-    }
-
-    @EventHandler
-    public void standardStats(GetDefaultTrackedStatsEvent event) {
-        event.addStats(ClientStat.values());
-    }
-
-    @EventHandler
-    public void statisticsStats(GetDefaultTrackedStatsEvent event) {
-        List<IStat> stats = Arrays.stream(Statistic.values()).map(statistic ->
-            MinecraftStat.builder()
-                    .statistic(statistic)
-                .build()
-        ).map(IStat.class::cast)
-                .toList();
-
-        event.addStats(stats);
-    }
-
-    @EventHandler
-    public void damageStats(GetDefaultTrackedStatsEvent event) {
-        event.addStat(DamageStat.builder().relation(Relation.DEALT).type(Type.AMOUNT).build());
-        event.addStat(DamageStat.builder().relation(Relation.RECEIVED).type(Type.AMOUNT).build());
     }
 
     @EventHandler(priority = EventPriority.HIGHEST)
