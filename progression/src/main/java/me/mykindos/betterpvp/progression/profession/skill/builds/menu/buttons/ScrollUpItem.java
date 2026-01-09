ca5396fa3ea19fe98063dcc6807aaa790b62737b
braulio-dev <bchamerry@gmail.com>
2026-01-09 09:23:21 -0800

    Fix resourcepack

--- progression/src/main/java/me/mykindos/betterpvp/progression/profession/skill/builds/menu/buttons/ScrollUpItem.java
+++ progression/src/main/java/me/mykindos/betterpvp/progression/profession/skill/builds/menu/buttons/ScrollUpItem.java
@@ -3,6 +3,7 @@ package me.mykindos.betterpvp.progression.profession.skill.builds.menu.buttons;
 import me.mykindos.betterpvp.core.inventory.gui.ScrollGui;
 import me.mykindos.betterpvp.core.inventory.item.ItemProvider;
 import me.mykindos.betterpvp.core.inventory.item.impl.controlitem.ScrollItem;
+import me.mykindos.betterpvp.core.utilities.Resources;
 import me.mykindos.betterpvp.core.utilities.model.item.ClickActions;
 import me.mykindos.betterpvp.core.utilities.model.item.ItemView;
 import net.kyori.adventure.text.Component;
@@ -20,6 +21,7 @@ public class ScrollUpItem extends ScrollItem {
         final ItemView.ItemViewBuilder builder = ItemView.of(new ItemStack(Material.BARRIER)).toBuilder();
         builder.customModelData(1);
         builder.displayName(Component.text("Scroll up"));
+        builder.itemModel(Resources.ItemModel.INVISIBLE);
         builder.action(ClickActions.LEFT_SHIFT, Component.text("Scroll up 5 levels"));
         if (!gui.canScroll(1))
             builder.lore(Component.text("You've reached the top"));
