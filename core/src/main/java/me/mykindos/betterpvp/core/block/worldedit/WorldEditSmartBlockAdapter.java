4f22e0e44e68f50026e3b020d8f34c6451ec2a25
Mykindos <5389944+Mykindos@users.noreply.github.com>
2026-04-05 04:41:28 +1000

    Comment out `SmartBlockExtent` integration in `WorldEditSmartBlockAdapter` during EditSession initialization

--- core/src/main/java/me/mykindos/betterpvp/core/block/worldedit/WorldEditSmartBlockAdapter.java
+++ core/src/main/java/me/mykindos/betterpvp/core/block/worldedit/WorldEditSmartBlockAdapter.java
@@ -2,12 +2,10 @@ package me.mykindos.betterpvp.core.block.worldedit;
 
 import com.google.inject.Inject;
 import com.sk89q.worldedit.WorldEdit;
-import com.sk89q.worldedit.bukkit.BukkitAdapter;
 import com.sk89q.worldedit.event.extent.EditSessionEvent;
 import com.sk89q.worldedit.util.eventbus.Subscribe;
 import me.mykindos.betterpvp.core.block.SmartBlockFactory;
 import me.mykindos.betterpvp.core.framework.adapter.PluginAdapter;
-import org.bukkit.World;
 
 @PluginAdapter("WorldEdit")
 public class WorldEditSmartBlockAdapter {
@@ -23,8 +21,8 @@ public class WorldEditSmartBlockAdapter {
     // This is a hook to disable smart blocks changes in WorldEdit.
     @Subscribe
     public void onEditSession(EditSessionEvent event) {
-        World world = BukkitAdapter.adapt(event.getWorld());
-        event.setExtent(new SmartBlockExtent(event.getExtent(), world, factory));
+        // World world = BukkitAdapter.adapt(event.getWorld());
+        //event.setExtent(new SmartBlockExtent(event.getExtent(), world, factory));
     }
 
 }
