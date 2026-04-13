1a4fd2e9aec07b47fc4c585453c3ab884e8aac20
Mykindos <5389944+Mykindos@users.noreply.github.com>
2026-04-13 15:17:32 +1000

    Delete boss bar from async

--- core/src/main/java/me/mykindos/betterpvp/core/client/gamer/repository/GamerListener.java
+++ core/src/main/java/me/mykindos/betterpvp/core/client/gamer/repository/GamerListener.java
@@ -73,8 +73,6 @@ public class GamerListener implements Listener {
                 gamer.getPlayerList().show(gamer);
                 gamer.getExperienceBar().show(gamer);
                 gamer.getExperienceLevel().show(gamer);
-                gamer.getBossBarQueue().show(gamer);
-                gamer.getBossBarOverlay().show(gamer);
             });
         }catch(Exception ex) {
             log.error("Error with gamer async onUpdateAsync", ex).submit();
