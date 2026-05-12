07efee6d7fb29f5624b542ef0880380ab2bc7c54
8SIXSect <europeangamer431142@outlook.com>
2026-05-12 17:56:08 -0500

    Created Mangrove Propagule description

---
+++ progression/src/main/java/me/mykindos/betterpvp/progression/profession/woodcutting/item/MangrovePropagule.java
@@ -0,0 +1,31 @@
+package me.mykindos.betterpvp.progression.profession.woodcutting.item;
+
+
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import me.mykindos.betterpvp.core.item.FallbackItem;
+import me.mykindos.betterpvp.core.item.ItemKey;
+import me.mykindos.betterpvp.core.item.ItemRarity;
+import me.mykindos.betterpvp.core.item.component.impl.DescriptionComponent;
+import me.mykindos.betterpvp.core.item.model.VanillaItem;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.Material;
+
+@Singleton
+@ItemKey("progression:mangrove_propagule")
+@FallbackItem(value = Material.MANGROVE_PROPAGULE, keepRecipes = true)
+public class MangrovePropagule extends VanillaItem {
+
+    @Inject
+    public MangrovePropagule() {
+        super("Mangrove Propagule", Material.MANGROVE_PROPAGULE, ItemRarity.UNCOMMON);
+
+        addBaseComponent(new DescriptionComponent(1,
+                Component.text("Mangrove Trees grant more" + " ")
+                        .append(Component.text("Woodcutting Experience").color(NamedTextColor.AQUA))
+                        .append(Component.text(" " + "than any other tree."))
+        ));
+
+    }
+}
