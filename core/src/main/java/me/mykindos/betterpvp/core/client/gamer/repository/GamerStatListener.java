4033b7197cc7cf49dd884270b1ba6594f5cba6ce
Mykindos <5389944+Mykindos@users.noreply.github.com>
2026-06-01 21:11:23 +1000

    Remove unused event handlers and comments in `GamerStatListener` for cleanup.

--- core/src/main/java/me/mykindos/betterpvp/core/client/gamer/repository/GamerStatListener.java
+++ core/src/main/java/me/mykindos/betterpvp/core/client/gamer/repository/GamerStatListener.java
@@ -8,9 +8,7 @@ import me.mykindos.betterpvp.core.client.repository.ClientManager;
 import me.mykindos.betterpvp.core.combat.damagelog.DamageLogManager;
 import me.mykindos.betterpvp.core.listener.BPvPListener;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerQuitEvent;
 
 @BPvPListener
 @Singleton
@@ -30,38 +28,5 @@ public class GamerStatListener implements Listener {
     public void onSettingsUpdated(GamerPropertyUpdateEvent event) {
         clientManager.saveGamerProperty(event.getContainer(), event.getProperty(), event.getNewValue());
     }
-    //todo add migrations/one off stats for legacy period, all of these stats are currently done via Statistic (Minecraft)
-/*
-    @EventHandler (priority = EventPriority.MONITOR)
-    public void onBlockPlace(BlockPlaceEvent event) {
-        if (event.isCancelled()) return;
-
-        Player player = event.getPlayer();
-        final Client client = clientManager.search().online(player);
-        final Gamer gamer = client.getGamer();
-
-        int blocksPlaced = (int) (gamer.getProperty(GamerProperty.BLOCKS_PLACED).orElse(0)) + 1;
-        gamer.saveProperty(GamerProperty.BLOCKS_PLACED, blocksPlaced);
-    }
-
-    @EventHandler (priority = EventPriority.MONITOR)
-    public void onBlockBroken(BlockBreakEvent event) {
-        if (event.isCancelled()) return;
-
-        Player player = event.getPlayer();
-        final Client client = clientManager.search().online(player);
-        final Gamer gamer = client.getGamer();
-
-        int blocksBroken = (gamer.getIntProperty(GamerProperty.BLOCKS_BROKEN)) + 1;
-        gamer.saveProperty(GamerProperty.BLOCKS_BROKEN, blocksBroken);
-    }
- */
-
-    @EventHandler (priority = EventPriority.HIGHEST)
-    public void onQuit(PlayerQuitEvent event) {
-            clientManager.getSqlLayer().processPropertyUpdates(event.getPlayer().getUniqueId(), true);
-
-    }
-
 
 }
