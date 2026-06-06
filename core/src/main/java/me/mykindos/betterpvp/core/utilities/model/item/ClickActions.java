7158458ccda7a5060665968570e5a05ff8018c06
braulio-dev <bchamerry@gmail.com>
2026-06-06 09:52:27 -0700

    Improve item lore & input pack

--- core/src/main/java/me/mykindos/betterpvp/core/utilities/model/item/ClickActions.java
+++ core/src/main/java/me/mykindos/betterpvp/core/utilities/model/item/ClickActions.java
@@ -1,8 +1,6 @@
 package me.mykindos.betterpvp.core.utilities.model.item;
 
 import lombok.Getter;
-import me.mykindos.betterpvp.core.utilities.Resources;
-import net.kyori.adventure.key.Key;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.event.inventory.ClickType;
@@ -10,48 +8,49 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
+import static me.mykindos.betterpvp.core.utilities.Resources.Font.INPUT;
+import static me.mykindos.betterpvp.core.utilities.Resources.Font.SPACE;
+import static net.kyori.adventure.text.Component.*;
+import static net.kyori.adventure.text.format.NamedTextColor.WHITE;
+
 public enum ClickActions implements ClickAction {
 
-    ALL(Component.empty()
-            .append(Component.translatable("space.-4").font(Resources.Font.SPACE))
-            .append(Component.text("\uE073", NamedTextColor.WHITE).font(Resources.Font.GLYPH_16)),
+    ALL(empty()
+            .append(text("\uE04C", WHITE).font(INPUT)),
             ClickType.values()),
 
-    LEFT(Component.empty()
-            .append(Component.translatable("space.-4").font(Resources.Font.SPACE))
-            .append(Component.text("\uE074", NamedTextColor.WHITE).font(Resources.Font.GLYPH_16)),
+    LEFT(empty()
+            .append(text("\uE04D", WHITE).font(INPUT)),
             ClickType.LEFT),
 
-    RIGHT(Component.empty()
-            .append(Component.translatable("space.-4").font(Resources.Font.SPACE))
-            .append(Component.text("\uE075", NamedTextColor.WHITE).font(Resources.Font.GLYPH_16)),
+    RIGHT(empty()
+            .append(text("\uE04E", WHITE).font(INPUT)),
             ClickType.RIGHT),
 
-    SHIFT(Component.empty()
-            .append(Component.translatable("space.-3").font(Resources.Font.SPACE))
-            .append(Component.text("\uE084", NamedTextColor.WHITE).font(Resources.Font.GLYPH_16)),
+    SHIFT(empty()
+            .append(text("\uE0FF", WHITE).font(INPUT))
+            .append(translatable("space.-1").font(SPACE))
+            .append(text("\uE100", WHITE).font(INPUT)),
             ClickType.SHIFT_LEFT, ClickType.SHIFT_RIGHT),
 
-    LEFT_SHIFT(Component.empty()
-            .append(Component.translatable("space.-3").font(Resources.Font.SPACE))
-            .append(Component.text("\uE084", NamedTextColor.WHITE).font(Resources.Font.GLYPH_16))
-            .appendSpace()
-            .append(Component.translatable("space.-2").font(Resources.Font.SPACE))
-            .append(Component.text("+", NamedTextColor.GRAY))
-            .appendSpace()
-            .append(Component.translatable("space.-4").font(Resources.Font.SPACE))
-            .append(Component.text("\uE074", NamedTextColor.WHITE).font(Resources.Font.GLYPH_16)),
+    LEFT_SHIFT(empty()
+            .append(text("\uE0FF", WHITE).font(INPUT))
+            .append(translatable("space.-1").font(SPACE))
+            .append(text("\uE100", WHITE).font(INPUT))
+            .append(translatable("space.2").font(SPACE))
+            .append(text("+", NamedTextColor.GRAY))
+            .append(translatable("space.2").font(SPACE))
+            .append(text("\uE04D", WHITE).font(INPUT)),
             ClickType.SHIFT_LEFT),
 
-    RIGHT_SHIFT(Component.empty()
-            .append(Component.translatable("space.-3").font(Resources.Font.SPACE))
-            .append(Component.text("\uE084", NamedTextColor.WHITE).font(Resources.Font.GLYPH_16))
-            .appendSpace()
-            .append(Component.translatable("space.-2").font(Resources.Font.SPACE))
-            .append(Component.text("+", NamedTextColor.GRAY))
-            .appendSpace()
-            .append(Component.translatable("space.-4").font(Resources.Font.SPACE))
-            .append(Component.text("\uE075", NamedTextColor.WHITE).font(Resources.Font.GLYPH_16)),
+    RIGHT_SHIFT(empty()
+            .append(text("\uE0FF", WHITE).font(INPUT))
+            .append(translatable("space.-1").font(SPACE))
+            .append(text("\uE100", WHITE).font(INPUT))
+            .append(translatable("space.2").font(SPACE))
+            .append(text("+", NamedTextColor.GRAY))
+            .append(translatable("space.2").font(SPACE))
+            .append(text("\uE04E", WHITE).font(INPUT)),
             ClickType.SHIFT_RIGHT);
 
     @Getter
